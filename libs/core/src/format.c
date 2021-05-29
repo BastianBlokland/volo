ebb9cb356080e42d440d58c44eae65e355cfd591
Bastian Blokland <mail@bastian.tech>
2021-05-29 12:49:23 +0300

    core: Optimize int formatting
    Use a temporary buffer instead of recursion

--- libs/core/src/format.c
+++ libs/core/src/format.c
@@ -1,16 +1,20 @@
 #include "core_diag.h"
 #include "core_format.h"
 
-void format_write_u64(DynString* str, const u64 val, const FormatIntOptions* opts) {
+void format_write_u64(DynString* str, u64 val, const FormatIntOptions* opts) {
   diag_assert(opts->base > 1 && opts->base <= 16);
 
+  Mem buffer = mem_stack(64);
+  u8* ptr    = mem_end(buffer);
+
   const char* chars = "0123456789ABCDEF";
-  if (val < opts->base) {
-    dynstring_append_char(str, chars[val]);
-    return;
-  }
-  format_write_u64(str, val / opts->base, opts);
-  dynstring_append_char(str, chars[val % opts->base]);
+  do {
+    *--ptr = chars[val % opts->base];
+    val /= opts->base;
+  } while (val);
+
+  const u8 numDigits = mem_end(buffer) - ptr;
+  dynstring_append(str, mem_create(ptr, numDigits));
 }
 
 void format_write_i64(DynString* str, const i64 val, const FormatIntOptions* opts) {
