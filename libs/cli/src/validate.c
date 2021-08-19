db6e09eaa62570ba1c4042880e6e684ff2e725b1
Bastian Blokland <bastianblokland@gmail.com>
2021-08-19 20:49:22 +0300

    core: Format read f64 (#13)
    * core: Add util for reading f64's
    
    * cli: Add support for reading f64's

--- libs/cli/src/validate.c
+++ libs/cli/src/validate.c
@@ -13,3 +13,8 @@ bool cli_validate_u64(const String input) {
   const String rem  = format_read_u64(input, null, base);
   return string_is_empty(rem);
 }
+
+bool cli_validate_f64(const String input) {
+  const String rem = format_read_f64(input, null);
+  return string_is_empty(rem);
+}
--- libs/cli/test/test_validate.c
+++ libs/cli/test/test_validate.c
@@ -18,4 +18,11 @@ spec(validate) {
     check(!cli_validate_u64(string_lit("Hello")));
     check(!cli_validate_u64(string_lit("-42")));
   }
+
+  it("supports validating f64's") {
+    check(cli_validate_f64(string_lit("42.1337e-2")));
+
+    check(!cli_validate_f64(string_lit("Hello")));
+    check(!cli_validate_f64(string_lit("42.1337f-2")));
+  }
 }
