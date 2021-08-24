b9d9b7f3e4b81fb528f0f22df90fff654dc25081
Bastian Blokland <mail@bastian.tech>
2021-08-24 23:36:13 +0300

    json: Add document container

--- libs/json/include/json_doc.h
+++ libs/json/include/json_doc.h
@@ -1,1 +1,235 @@
 #pragma once
+#include "core_string.h"
+
+/**
+ * Definition for a Json Document.
+ * Aims for compatiblity with rfc7159 json (https://datatracker.ietf.org/doc/html/rfc7159).
+ */
+typedef struct sJsonDoc JsonDoc;
+
+/**
+ * Type of a json value.
+ */
+typedef enum {
+  JsonType_Array,
+  JsonType_Object,
+  JsonType_String,
+  JsonType_Number,
+  JsonType_Bool,
+  JsonType_Null,
+} JsonType;
+
+/**
+ * Parent kind of a json value.
+ */
+typedef enum {
+  JsonParent_None,
+  JsonParent_Array,
+  JsonParent_Object,
+} JsonParent;
+
+/**
+ * Handle to a Json value.
+ */
+typedef u32 JsonVal;
+
+/**
+ * Iterator for iterating object fields.
+ */
+typedef struct {
+  String  name;  // 'string_empty' when no field was found.
+  JsonVal value; // 'sentinel_u32' when no field was found.
+} JsonFieldItr;
+
+// clang-format off
+
+/**
+ * Iterate over all elements in an array value.
+ *
+ * Pre-condition: _ARRAY_ is a value of type JsonType_Array in the given document.
+ */
+#define json_for_elems(_DOC_, _ARRAY_, _VAR_, ...)                                                 \
+  {                                                                                                \
+    for (JsonVal _VAR_ = json_elem_begin(_DOC_, _ARRAY_); !sentinel_check(_VAR_);                  \
+                 _VAR_ = json_elem_next(_DOC_, _VAR_)) {                                           \
+      __VA_ARGS__                                                                                  \
+    }                                                                                              \
+  }
+
+/**
+ * Iterate over all fields in an object value.
+ *
+ * Pre-condition: _OBJECT_ is a value of type JsonType_Object in the given document.
+ */
+#define json_for_fields(_DOC_, _OBJECT_, _VAR_, ...)                                               \
+  {                                                                                                \
+    for (JsonFieldItr _VAR_ = json_field_begin(_DOC_, _OBJECT_); !sentinel_check(_VAR_.value);     \
+                      _VAR_ = json_field_next(_DOC_, _VAR_.value)) {                               \
+      __VA_ARGS__                                                                                  \
+    }                                                                                              \
+  }
+
+// clang-format on
+
+/**
+ * Create a new Json document.
+ * Note: 'valueCapacity' is only the initial capacity, more space is automatically allocated when
+ * required. Capacity of 0 is legal and will allocate memory when the first value is added.
+ * Should be destroyed using 'json_destroy()'.
+ */
+JsonDoc* json_create(Allocator*, usize valueCapacity);
+
+/**
+ * Destroy a Json document.
+ */
+void json_destroy(JsonDoc*);
+
+/**
+ * Add a new array to the document.
+ */
+JsonVal json_add_array(JsonDoc*);
+
+/**
+ * Add a new object to the document.
+ */
+JsonVal json_add_object(JsonDoc*);
+
+/**
+ * Add a new string to the document.
+ */
+JsonVal json_add_string(JsonDoc*, String);
+
+/**
+ * Add a new number to the document.
+ */
+JsonVal json_add_number(JsonDoc*, f64);
+
+/**
+ * Add a new bool to the document.
+ */
+JsonVal json_add_bool(JsonDoc*, bool);
+
+/**
+ * Add a new null to the document.
+ */
+JsonVal json_add_null(JsonDoc*);
+
+/**
+ * Add a new element to an array.
+ *
+ * Pre-condition: array is a value of type JsonType_Array in the given document.
+ * Pre-condition: elem is valid in the given document.
+ * Pre-condition: elem doesn't have a parent yet.
+ * Pre-condition: Adding elem to array does not create cycles.
+ */
+void json_add_elem(JsonDoc*, JsonVal array, JsonVal elem);
+
+/**
+ * Add new field to an object.
+ * Returns 'false' if the object already contains a field with the given name.
+ * Note: When 'false' is returned the state of the object is not modified.
+ *
+ * Pre-condition: object is a value of type JsonType_Object in the given document.
+ * Pre-condition: !string_is_empty(name).
+ * Pre-condition: val is valid in the given document.
+ * Pre-condition: val doesn't have a parent yet.
+ * Pre-condition: Adding val to object does not create cycles.
+ */
+bool json_add_field(JsonDoc*, JsonVal object, String name, JsonVal val);
+
+/**
+ * Retrieve the type of a value.
+ *
+ * Pre-condition: JsonVal is valid in the given document.
+ */
+JsonType json_type(const JsonDoc*, JsonVal);
+
+/**
+ * Retrieve the parent kind of a value.
+ *
+ * Pre-condition: JsonVal is valid in the given document.
+ */
+JsonParent json_parent(const JsonDoc*, JsonVal);
+
+/**
+ * Lookup an element by its index.
+ * Returns 'sentinel_u32' when there is no element at the given index.
+ *
+ * Pre-condition: array is a value of type JsonType_Array in the given document.
+ */
+JsonVal json_elem(const JsonDoc*, JsonVal array, u32 idx);
+
+/**
+ * Retrieve the amount of elements in an array.
+ *
+ * Pre-condition: array is a value of type JsonType_Array in the given document.
+ */
+u32 json_elem_count(const JsonDoc*, JsonVal array);
+
+/**
+ * Retrieve the first element in an array.
+ * Returns 'sentinel_u32' when the array has no elements.
+ *
+ * Pre-condition: array is a value of type JsonType_Array in the given document.
+ */
+JsonVal json_elem_begin(const JsonDoc*, JsonVal array);
+
+/**
+ * Retrieve the next element in an array.
+ * Returns 'sentinel_u32' when there are no more elements.
+ *
+ * Pre-condition: elem is a value with a parent kind of JsonParent_Array.
+ */
+JsonVal json_elem_next(const JsonDoc*, JsonVal elem);
+
+/**
+ * Lookup a object field by its name.
+ * Returns 'sentinel_u32' when no field was found with the given name.
+ *
+ * Pre-condition: object is a value of type JsonType_Object in the given document.
+ */
+JsonVal json_field(const JsonDoc*, JsonVal object, String name);
+
+/**
+ * Retrieve the amount of fields in an object.
+ *
+ * Pre-condition: object is a value of type JsonType_Object in the given document.
+ */
+u32 json_field_count(const JsonDoc*, JsonVal object);
+
+/**
+ * Retrieve the first field in an object.
+ * Returns an iterator with a value of 'sentinel_u32' when the object has no fields.
+ *
+ * Pre-condition: object is a value of type JsonType_Object in the given document.
+ */
+JsonFieldItr json_field_begin(const JsonDoc*, JsonVal object);
+
+/**
+ * Retrieve the next field in an object.
+ * Returns an iterator with a value of 'sentinel_u32' when there are no more fields
+ *
+ * Pre-condition: fieldVal is a value with a parent kind of JsonParent_Object.
+ */
+JsonFieldItr json_field_next(const JsonDoc*, JsonVal fieldVal);
+
+/**
+ * Retrieve the value of a string.
+ *
+ * Pre-condition: JsonVal is a value of type JsonType_String in the given document.
+ */
+String json_string(const JsonDoc*, JsonVal);
+
+/**
+ * Retrieve the value of a number.
+ *
+ * Pre-condition: JsonVal is a value of type JsonType_Number in the given document.
+ */
+f64 json_number(const JsonDoc*, JsonVal);
+
+/**
+ * Retrieve the value of a bool.
+ *
+ * Pre-condition: JsonVal is a value of type JsonType_Bool in the given document.
+ */
+bool json_bool(const JsonDoc*, JsonVal);
--- libs/json/src/doc.c
+++ libs/json/src/doc.c
@@ -1,1 +1,276 @@
+#include "core_alloc.h"
+#include "core_diag.h"
+#include "core_dynarray.h"
+
 #include "json_doc.h"
+
+typedef struct {
+  JsonVal elemHead;
+  u32     elemCount;
+} JsonArrayData;
+
+typedef struct {
+  JsonVal fieldHead;
+  u32     fieldCount;
+} JsonObjectData;
+
+typedef struct {
+  u32     typeAndParent;
+  JsonVal next;
+  union {
+    JsonArrayData  val_array;
+    JsonObjectData val_object;
+    String         val_string;
+    f64            val_number;
+    bool           val_bool;
+  };
+} JsonValData;
+
+struct sJsonDoc {
+  DynArray   values; // JsonValData[]
+  Allocator* alloc;
+};
+
+static JsonValData* json_val_data(const JsonDoc* doc, const JsonVal val) {
+  diag_assert_msg(val < doc->values.size, "Out of bounds JsonVal");
+  return dynarray_at_t(&doc->values, val, JsonValData);
+}
+
+static JsonVal json_add_data(JsonDoc* doc, JsonValData data) {
+  const JsonVal val                           = (JsonVal)doc->values.size;
+  *dynarray_push_t(&doc->values, JsonValData) = data;
+  return val;
+}
+
+JsonDoc* json_create(Allocator* alloc, usize valueCapacity) {
+  JsonDoc* doc = alloc_alloc_t(alloc, JsonDoc);
+  *doc         = (JsonDoc){
+      .values = dynarray_create_t(alloc, JsonValData, valueCapacity),
+      .alloc  = alloc,
+  };
+  return doc;
+}
+
+void json_destroy(JsonDoc* doc) {
+  dynarray_for_t(&doc->values, JsonValData, data, {
+    switch (data->typeAndParent & 0xFFFF) {
+    case JsonType_String:
+      if (data->val_string.ptr) {
+        string_free(doc->alloc, data->val_string);
+      }
+      break;
+    default:
+      break;
+    }
+  });
+
+  dynarray_destroy(&doc->values);
+
+  alloc_free_t(doc->alloc, doc);
+}
+
+JsonVal json_add_array(JsonDoc* doc) {
+  return json_add_data(
+      doc,
+      (JsonValData){
+          .typeAndParent = JsonType_Array,
+          .next          = sentinel_u32,
+          .val_array     = {.elemHead = sentinel_u32, .elemCount = 0},
+      });
+}
+
+JsonVal json_add_object(JsonDoc* doc) {
+  return json_add_data(
+      doc,
+      (JsonValData){
+          .typeAndParent = JsonType_Object,
+          .next          = sentinel_u32,
+          .val_object    = {.fieldHead = sentinel_u32, .fieldCount = 0},
+      });
+}
+
+JsonVal json_add_string(JsonDoc* doc, const String string) {
+  return json_add_data(
+      doc,
+      (JsonValData){
+          .typeAndParent = JsonType_String,
+          .next          = sentinel_u32,
+          .val_string    = string_is_empty(string) ? string_empty : string_dup(doc->alloc, string),
+      });
+}
+
+JsonVal json_add_number(JsonDoc* doc, const f64 number) {
+  return json_add_data(
+      doc,
+      (JsonValData){
+          .typeAndParent = JsonType_Number,
+          .next          = sentinel_u32,
+          .val_number    = number,
+      });
+}
+
+JsonVal json_add_bool(JsonDoc* doc, const bool boolean) {
+  return json_add_data(
+      doc,
+      (JsonValData){
+          .typeAndParent = JsonType_Bool,
+          .next          = sentinel_u32,
+          .val_bool      = boolean,
+      });
+}
+
+JsonVal json_add_null(JsonDoc* doc) {
+  return json_add_data(
+      doc,
+      (JsonValData){
+          .typeAndParent = JsonType_Null,
+          .next          = sentinel_u32,
+      });
+}
+
+void json_add_elem(JsonDoc* doc, const JsonVal array, const JsonVal elem) {
+  diag_assert_msg(json_parent(doc, elem) == JsonParent_None, "Given value is already parented");
+  diag_assert_msg(json_type(doc, array) == JsonType_Array, "Invalid array value");
+  diag_assert_msg(array != elem, "Arrays cannot contain cycles"); // TODO: Detect indirect cycles.
+
+  JsonValData* arrayData = json_val_data(doc, array);
+  JsonValData* elemData  = json_val_data(doc, elem);
+
+  // Add the element to the end of the array linked-list.
+  JsonVal* link = &arrayData->val_array.elemHead;
+  while (!sentinel_check(*link)) {
+    link = &json_val_data(doc, *link)->next;
+  }
+  *link = elem;
+
+  elemData->typeAndParent |= (u32)JsonParent_Array << 16;
+  ++arrayData->val_array.elemCount;
+}
+
+bool json_add_field(JsonDoc* doc, const JsonVal object, const String name, const JsonVal val) {
+  diag_assert_msg(json_parent(doc, val) == JsonParent_None, "Given value is already parented");
+  diag_assert_msg(json_type(doc, object) == JsonType_Object, "Invalid object value");
+  diag_assert_msg(!string_is_empty(name), "Field name cannot be empty");
+  diag_assert_msg(val != object, "Objects cannot contain cycles"); // TODO: Detect indirect cycles.
+
+  // Add the name as a value to the document.
+  // Note: Adding first to avoid invalidating pointers during the linked-list walk.
+  const JsonVal newNameVal = json_add_string(doc, name);
+
+  JsonValData* objectData = json_val_data(doc, object);
+
+  // Walk the linked-list of fields to check for duplicate names and to find the last link.
+  JsonVal* link = &objectData->val_object.fieldHead;
+  while (!sentinel_check(*link)) {
+    const JsonValData* nameValData = json_val_data(doc, *link);
+    if (string_eq(nameValData->val_string, name)) {
+      // Existing field found with the same name.
+      return false;
+    }
+    link = &json_val_data(doc, nameValData->next)->next;
+  }
+
+  *link = newNameVal;
+  ++objectData->val_object.fieldCount;
+  json_val_data(doc, newNameVal)->next = val;
+  json_val_data(doc, val)->typeAndParent |= (u32)JsonParent_Object << 16;
+  return true;
+}
+
+JsonType json_type(const JsonDoc* doc, const JsonVal val) {
+  return (JsonType)(json_val_data(doc, val)->typeAndParent & 0xFFFF);
+}
+
+JsonParent json_parent(const JsonDoc* doc, const JsonVal val) {
+  return (JsonParent)(json_val_data(doc, val)->typeAndParent >> 16);
+}
+
+JsonVal json_elem(const JsonDoc* doc, const JsonVal array, const u32 idx) {
+  diag_assert_msg(json_type(doc, array) == JsonType_Array, "Invalid array value");
+
+  JsonVal link = json_val_data(doc, array)->val_array.elemHead;
+  for (u32 i = 0; !sentinel_check(link); ++i) {
+    if (i == idx) {
+      return link;
+    }
+    link = json_val_data(doc, link)->next;
+  }
+
+  return sentinel_u32;
+}
+
+u32 json_elem_count(const JsonDoc* doc, const JsonVal array) {
+  diag_assert_msg(json_type(doc, array) == JsonType_Array, "Invalid array value");
+  return json_val_data(doc, array)->val_array.elemCount;
+}
+
+JsonVal json_elem_begin(const JsonDoc* doc, const JsonVal array) {
+  diag_assert_msg(json_type(doc, array) == JsonType_Array, "Invalid array value");
+  return json_val_data(doc, array)->val_array.elemHead;
+}
+
+JsonVal json_elem_next(const JsonDoc* doc, const JsonVal elem) {
+  diag_assert_msg(json_parent(doc, elem) == JsonParent_Array, "Invalid array elem");
+  return json_val_data(doc, elem)->next;
+}
+
+JsonVal json_field(const JsonDoc* doc, const JsonVal object, const String name) {
+  diag_assert_msg(json_type(doc, object) == JsonType_Object, "Invalid object value");
+
+  JsonValData* objectData = json_val_data(doc, object);
+
+  // Search the linked-list for a entry that matches the given name.
+  JsonVal link = objectData->val_object.fieldHead;
+  while (!sentinel_check(link)) {
+    const JsonValData* nameValData = json_val_data(doc, link);
+    if (string_eq(nameValData->val_string, name)) {
+      return nameValData->next;
+    }
+    link = json_val_data(doc, nameValData->next)->next;
+  }
+
+  // Not found.
+  return sentinel_u32;
+}
+
+u32 json_field_count(const JsonDoc* doc, const JsonVal object) {
+  diag_assert_msg(json_type(doc, object) == JsonType_Object, "Invalid object value");
+  return json_val_data(doc, object)->val_object.fieldCount;
+}
+
+JsonFieldItr json_field_begin(const JsonDoc* doc, const JsonVal object) {
+  diag_assert_msg(json_type(doc, object) == JsonType_Object, "Invalid object value");
+
+  JsonValData* objectData = json_val_data(doc, object);
+  if (sentinel_check(objectData->val_object.fieldHead)) {
+    return (JsonFieldItr){.name = string_empty, .value = sentinel_u32};
+  }
+  JsonValData* nameData = json_val_data(doc, objectData->val_object.fieldHead);
+  return (JsonFieldItr){.name = nameData->val_string, .value = nameData->next};
+}
+
+JsonFieldItr json_field_next(const JsonDoc* doc, JsonVal fieldVal) {
+  diag_assert_msg(json_parent(doc, fieldVal) == JsonParent_Object, "Invalid field value");
+
+  JsonValData* itrValData = json_val_data(doc, fieldVal);
+  if (sentinel_check(itrValData->next)) {
+    return (JsonFieldItr){.name = string_empty, .value = sentinel_u32};
+  }
+  JsonValData* nameData = json_val_data(doc, itrValData->next);
+  return (JsonFieldItr){.name = nameData->val_string, .value = nameData->next};
+}
+
+String json_string(const JsonDoc* doc, const JsonVal val) {
+  diag_assert_msg(json_type(doc, val) == JsonType_String, "Given JsonVal is not a string");
+  return json_val_data(doc, val)->val_string;
+}
+
+f64 json_number(const JsonDoc* doc, const JsonVal val) {
+  diag_assert_msg(json_type(doc, val) == JsonType_Number, "Given JsonVal is not a number");
+  return json_val_data(doc, val)->val_number;
+}
+
+bool json_bool(const JsonDoc* doc, const JsonVal val) {
+  diag_assert_msg(json_type(doc, val) == JsonType_Bool, "Given JsonVal is not a boolean");
+  return json_val_data(doc, val)->val_bool;
+}
