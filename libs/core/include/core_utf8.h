7802748404ab9623a915cdc89d05431250e0b689
Bastian Blokland <mail@bastian.tech>
2021-08-25 22:12:34 +0300

    core: Add utf8 encoding routine

--- libs/core/include/core_utf8.h
+++ libs/core/include/core_utf8.h
@@ -1,5 +1,11 @@
 #pragma once
-#include "core_string.h"
+#include "core_dynstring.h"
+
+/**
+ * A single unicode codepoint.
+ * https://en.wikipedia.org/wiki/Unicode#Architecture_and_terminology
+ */
+typedef u32 Utf8Codepoint;
 
 /**
  * Test if the given byte is a utf8 continuation byte.
@@ -11,3 +17,13 @@ bool utf8_contchar(u8);
  * Pre-condition: Input string is valid utf8.
  */
 usize utf8_cp_count(String);
+
+/**
+ * Returns how many utf8 bytes are required to represent the given codepoint.
+ */
+usize utf8_cp_bytes(Utf8Codepoint);
+
+/**
+ * Write a utf8 string (1 - 4 bytes) for a Unicode codepoint.
+ */
+void utf8_cp_write(DynString*, Utf8Codepoint);
--- libs/core/test/test_utf8.c
+++ libs/core/test/test_utf8.c
@@ -1,3 +1,4 @@
+#include "core_array.h"
 #include "core_utf8.h"
 
 #include "check_spec.h"
@@ -13,5 +14,35 @@ spec(utf8) {
     check_eq_int(utf8_cp_count(string_lit("")), 0);
     check_eq_int(utf8_cp_count(string_lit("Hello")), 5);
     check_eq_int(utf8_cp_count(testStr), 184);
+  }
+
+  it("can compute the required utf8 bytes") {
+    check_eq_int(utf8_cp_bytes(0x26), 1);
+    check_eq_int(utf8_cp_bytes(0x39B), 2);
+    check_eq_int(utf8_cp_bytes(0xE3F), 3);
+    check_eq_int(utf8_cp_bytes(0x1D459), 4);
+  }
+
+  it("can encode codepoints as utf8") {
+    struct {
+      Utf8Codepoint cp;
+      String        expected;
+    } const data[] = {
+        {0x0, string_lit("\0")},
+        {0x61, string_lit("a")},
+        {0x26, string_lit("&")},
+        {0x39B, string_lit("Λ")},
+        {0xE3F, string_lit("฿")},
+        {0xFFFD, string_lit("�")},
+        {0x283C, string_lit("⠼")},
+    };
+
+    DynString string = dynstring_create_over(mem_stack(128));
+    for (usize i = 0; i != array_elems(data); ++i) {
+      dynstring_clear(&string);
+      utf8_cp_write(&string, data[i].cp);
+      check_eq_string(dynstring_view(&string), data[i].expected);
+    }
+    dynstring_destroy(&string);
   }
 }
