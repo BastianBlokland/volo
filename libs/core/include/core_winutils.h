3142abcb101c278d5a41aa92e313ca7c9df4fd0c
Bastian Blokland <mail@bastian.tech>
2021-09-01 15:23:08 +0300

    core: Rename utf16 to 'wide'
    Reason is that its not necesarilly utf16 but whatever wide
    encoding Windows uses on your system.

--- libs/core/include/core_winutils.h
+++ libs/core/include/core_winutils.h
@@ -5,7 +5,7 @@
 
 /**
  * Returns the size (in bytes) required to store the input string (assumed to be utf8) as a
- * null-terminated utf16 string (as is commonly used in win32 apis).
+ * null-terminated wide string (as is commonly used in win32 apis).
  * Note: Returns 'sentinel_usize' if the input is invalid utf8.
  *
  * Pre-condition: !string_is_empty(input)
@@ -13,9 +13,9 @@
 usize winutils_to_widestr_size(String input);
 
 /**
- * Convert a string (assumed to be utf8) to a null-terminated utf16 string (as is commonly used in
+ * Convert a string (assumed to be utf8) to a null-terminated wide string (as is commonly used in
  * win32 apis).
- * Returns the amount of utf16 characters written to the output buffer.
+ * Returns the amount of wide characters written to the output buffer.
  * Note: Returns 'sentinel_usize' if the input is invalid utf8 or the output buffer is too small.
  *
  * Pre-condition: !string_is_empty(input)
@@ -23,7 +23,7 @@ usize winutils_to_widestr_size(String input);
 usize winutils_to_widestr(Mem output, String input);
 
 /**
- * Returns the size (in bytes) required to store the input utf16 string (as is commonly used in
+ * Returns the size (in bytes) required to store the input wide string (as is commonly used in
  * win32 apis) as utf8.
  * Note: Returns 'sentinel_usize' if the input cannot be represented as utf8.
  *
@@ -32,7 +32,7 @@ usize winutils_to_widestr(Mem output, String input);
 usize winutils_from_widestr_size(void* input, usize inputCharCount);
 
 /**
- * Convert a utf16 string (as is commonly used in win32 apis) to a string (encoded as utf8).
+ * Convert a wide string (as is commonly used in win32 apis) to a string (encoded as utf8).
  * Returns the amount of utf8 bytes written to the output string.
  * Note: Returns 'sentinel_usize' if the input cannot be represented as utf8 or the output string is
  * too small.
