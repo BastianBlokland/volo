002785e923b1b2cee83c99f8717791df178ced05
Bastian Blokland <mail@bastian.tech>
2021-09-17 23:26:30 +0300

    check: Add application wrapper

--- libs/core/test/main.c
+++ libs/core/test/main.c
@@ -1,17 +1,19 @@
-#include "core_file.h"
 #include "core_init.h"
-
-#include "cli.h"
-
-#include "log.h"
 
 #include "jobs_init.h"
 
-#include "check_runner.h"
+#include "log.h"
 
-static int run_tests(const bool outputPassingTests) {
+#include "check_app.h"
+
+int main(const int argc, const char** argv) {
+  core_init();
+  jobs_init();
+  log_init();
+
+  log_add_sink(g_logger, log_sink_json_default(g_alloc_heap, LogMask_All));
+
   CheckDef* check = check_create(g_alloc_heap);
-
   register_spec(check, alloc_bump);
   register_spec(check, alloc_page);
   register_spec(check, alloc_scratch);
@@ -40,51 +42,9 @@ static int run_tests(const bool outputPassingTests) {
   register_spec(check, utf8);
   register_spec(check, winutils);
 
-  const CheckRunFlags flags =
-      outputPassingTests ? CheckRunFlags_OutputPassingTests : CheckRunFlags_None;
-  const CheckResultType result = check_run(check, flags);
+  const int exitCode = check_app(check, argc, argv);
 
   check_destroy(check);
-  return result;
-}
-
-int main(const int argc, const char** argv) {
-  core_init();
-  jobs_init();
-  log_init();
-
-  int exitCode = 0;
-
-  CliApp* app = cli_app_create(g_alloc_heap, string_lit("Test harness for the volo core library."));
-
-  const CliId outputPassingTestsFlag =
-      cli_register_flag(app, 'o', string_lit("output-passing"), CliOptionFlags_None);
-  cli_register_desc(app, outputPassingTestsFlag, string_lit("Display passing tests."));
-
-  const CliId helpFlag = cli_register_flag(app, 'h', string_lit("help"), CliOptionFlags_None);
-  cli_register_desc(app, helpFlag, string_lit("Display this help page."));
-  cli_register_exclusion(app, helpFlag, outputPassingTestsFlag);
-
-  CliInvocation* invoc = cli_parse(app, argc - 1, argv + 1);
-  if (cli_parse_result(invoc) == CliParseResult_Fail) {
-    cli_failure_write_file(invoc, g_file_stderr);
-    exitCode = 2;
-    goto exit;
-  }
-
-  if (cli_parse_provided(invoc, helpFlag)) {
-    cli_help_write_file(app, g_file_stdout);
-    goto exit;
-  }
-
-  log_add_sink(g_logger, log_sink_json_default(g_alloc_heap, LogMask_All));
-
-  const bool outputPassingTests = cli_parse_provided(invoc, outputPassingTestsFlag);
-  exitCode                      = run_tests(outputPassingTests);
-
-exit:
-  cli_parse_destroy(invoc);
-  cli_app_destroy(app);
 
   log_teardown();
   jobs_teardown();
--- libs/log/test/main.c
+++ libs/log/test/main.c
@@ -1,66 +1,26 @@
-#include "core_file.h"
 #include "core_init.h"
-
-#include "cli.h"
-
-#include "log.h"
 
 #include "jobs_init.h"
 
-#include "check_runner.h"
+#include "log.h"
 
-static int run_tests(const bool outputPassingTests) {
-  CheckDef* check = check_create(g_alloc_heap);
-
-  register_spec(check, logger);
-  register_spec(check, sink_json);
-  register_spec(check, sink_pretty);
-
-  const CheckRunFlags flags =
-      outputPassingTests ? CheckRunFlags_OutputPassingTests : CheckRunFlags_None;
-  const CheckResultType result = check_run(check, flags);
-
-  check_destroy(check);
-  return result;
-}
+#include "check_app.h"
 
 int main(const int argc, const char** argv) {
   core_init();
   jobs_init();
   log_init();
 
-  int exitCode = 0;
-
-  CliApp* app = cli_app_create(g_alloc_heap, string_lit("Test harness for the volo log library."));
-
-  const CliId outputPassingTestsFlag =
-      cli_register_flag(app, 'o', string_lit("output-passing"), CliOptionFlags_None);
-  cli_register_desc(app, outputPassingTestsFlag, string_lit("Display passing tests."));
-
-  const CliId helpFlag = cli_register_flag(app, 'h', string_lit("help"), CliOptionFlags_None);
-  cli_register_desc(app, helpFlag, string_lit("Display this help page."));
-  cli_register_exclusion(app, helpFlag, outputPassingTestsFlag);
-
-  CliInvocation* invoc = cli_parse(app, argc - 1, argv + 1);
-  if (cli_parse_result(invoc) == CliParseResult_Fail) {
-    cli_failure_write_file(invoc, g_file_stderr);
-    exitCode = 2;
-    goto exit;
-  }
-
-  if (cli_parse_provided(invoc, helpFlag)) {
-    cli_help_write_file(app, g_file_stdout);
-    goto exit;
-  }
-
   log_add_sink(g_logger, log_sink_json_default(g_alloc_heap, LogMask_All));
 
-  const bool outputPassingTests = cli_parse_provided(invoc, outputPassingTestsFlag);
-  exitCode                      = run_tests(outputPassingTests);
+  CheckDef* check = check_create(g_alloc_heap);
+  register_spec(check, logger);
+  register_spec(check, sink_json);
+  register_spec(check, sink_pretty);
 
-exit:
-  cli_parse_destroy(invoc);
-  cli_app_destroy(app);
+  const int exitCode = check_app(check, argc, argv);
+
+  check_destroy(check);
 
   log_teardown();
   jobs_teardown();
