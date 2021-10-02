e96aa7a2860893b7d152bf9e04347a5b6fb36e71
Bastian Blokland <mail@bastian.tech>
2021-10-02 12:59:01 +0300

    jobs: Pad job data to the cachline size

--- libs/jobs/src/job_internal.h
+++ libs/jobs/src/job_internal.h
@@ -1,28 +1,32 @@
 #pragma once
+#include "core_annotation.h"
 #include "jobs_graph.h"
+
+// Padded to 64 bytes to avoid false-sharing of cachelines.
+#define job_size 64
 
 typedef u64 JobId;
 
 /**
  * Per task runtime data.
- * TODO: Consider padding this to 64 bits to avoid false-sharing of cachelines. At the moment we
- * induce full memory barriers when updating these atomically so 'false-sharing' is not that big of
- * a deal yet, but as soon we improve that the sharing of cachelines will likely start to hurt
- * performance.
  */
 typedef struct {
-  i64 dependencies; // Remaining dependencies (parent tasks).
+  ALIGNAS(job_size) i64 dependencies; // Remaining dependencies (parent tasks).
 } JobTaskData;
+
+ASSERT(sizeof(JobTaskData) == job_size, "Invalid JobTaskData size");
 
 /**
  * Per job runtime data.
  */
 typedef struct {
-  JobId           id;
+  ALIGNAS(job_size) JobId id;
   const JobGraph* graph;
   i64             dependencies; // Remaining dependencies (leaf tasks).
   JobTaskData     taskData[];   // Allocated after this struct.
 } Job;
+
+ASSERT(sizeof(Job) == job_size, "Invalid Job size");
 
 Job* job_create(Allocator*, const JobId id, const JobGraph*);
 void job_destroy(Allocator*, Job*);
