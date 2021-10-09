25f41e00ee58212fdf224fadc679c4094de54e66
Bastian Blokland <mail@bastian.tech>
2021-10-09 11:32:54 +0300

    ecs: Add runner test

--- libs/ecs/test/test_runner.c
+++ libs/ecs/test/test_runner.c
@@ -6,29 +6,86 @@
 #include "ecs_runner.h"
 #include "ecs_world.h"
 
-// TODO: We should avoid global state like this, as it prevents running the test multiple times in
-// the same process.
-static i64 test_sys1_counter, test_sys2_counter;
+ecs_comp_define(RunnerCompA) { u32 f1; };
+ecs_comp_define(RunnerCompB) { u32 f1; };
+ecs_comp_define(RunnerCompC) { u32 f1; };
 
-ecs_system_define(TestSys1) {
-  diag_assert(g_ecsRunningSystem);
-  diag_assert(g_ecsRunningSystemId == ecs_system_id(TestSys1));
-  diag_assert(ecs_world_busy(world));
+ecs_view_define(ReadA) { ecs_access_read(RunnerCompA); }
 
-  thread_atomic_add_i64(&test_sys1_counter, 1);
+ecs_view_define(ReadAWriteBC) {
+  ecs_access_read(RunnerCompA);
+  ecs_access_write(RunnerCompB);
+  ecs_access_write(RunnerCompC);
 }
 
-ecs_system_define(TestSys2) {
+ecs_view_define(ReadBWriteA) {
+  ecs_access_read(RunnerCompB);
+  ecs_access_write(RunnerCompA);
+}
+
+ecs_view_define(ReadCWriteA) {
+  ecs_access_read(RunnerCompC);
+  ecs_access_write(RunnerCompA);
+}
+
+ecs_system_define(RunnerSys1) {
   diag_assert(g_ecsRunningSystem);
-  diag_assert(g_ecsRunningSystemId == ecs_system_id(TestSys2));
+  diag_assert(g_ecsRunningSystemId == ecs_system_id(RunnerSys1));
   diag_assert(ecs_world_busy(world));
 
-  thread_atomic_add_i64(&test_sys2_counter, 1);
+  EcsView* view = ecs_world_view_t(world, ReadAWriteBC);
+  for (EcsIterator* itr = ecs_view_itr_stack(view); ecs_view_itr_walk(itr);) {
+    const RunnerCompA* compA = ecs_view_read_t(itr, RunnerCompA);
+    RunnerCompB*       compB = ecs_view_write_t(itr, RunnerCompB);
+    RunnerCompC*       compC = ecs_view_write_t(itr, RunnerCompC);
+
+    compB->f1 = compA->f1 * 2;
+    compC->f1 = compB->f1 / 4;
+  }
+}
+
+ecs_system_define(RunnerSys2) {
+  diag_assert(g_ecsRunningSystem);
+  diag_assert(g_ecsRunningSystemId == ecs_system_id(RunnerSys2));
+  diag_assert(ecs_world_busy(world));
+
+  EcsView* view = ecs_world_view_t(world, ReadBWriteA);
+  for (EcsIterator* itr = ecs_view_itr_stack(view); ecs_view_itr_walk(itr);) {
+    const RunnerCompB* compB = ecs_view_read_t(itr, RunnerCompB);
+    RunnerCompA*       compA = ecs_view_write_t(itr, RunnerCompA);
+
+    compA->f1 += compB->f1 * 4;
+  }
+}
+
+ecs_system_define(RunnerSys3) {
+  diag_assert(g_ecsRunningSystem);
+  diag_assert(g_ecsRunningSystemId == ecs_system_id(RunnerSys3));
+  diag_assert(ecs_world_busy(world));
+
+  EcsView* view = ecs_world_view_t(world, ReadCWriteA);
+  for (EcsIterator* itr = ecs_view_itr_stack(view); ecs_view_itr_walk(itr);) {
+    const RunnerCompC* compC = ecs_view_read_t(itr, RunnerCompC);
+    RunnerCompA*       compA = ecs_view_write_t(itr, RunnerCompA);
+
+    compA->f1 += compC->f1 * compC->f1;
+  }
 }
 
 ecs_module_init(runner_test_module) {
-  ecs_register_system(TestSys1);
-  ecs_register_system(TestSys2);
+  ecs_register_view(ReadA);
+  ecs_register_view(ReadAWriteBC);
+
+  ecs_register_comp(RunnerCompA);
+  ecs_register_comp(RunnerCompB);
+  ecs_register_comp(RunnerCompC);
+
+  ecs_register_view(ReadBWriteA);
+  ecs_register_view(ReadCWriteA);
+
+  ecs_register_system(RunnerSys1, ecs_view_id(ReadAWriteBC));
+  ecs_register_system(RunnerSys2, ecs_view_id(ReadBWriteA));
+  ecs_register_system(RunnerSys3, ecs_view_id(ReadCWriteA));
 }
 
 spec(runner) {
@@ -45,18 +102,28 @@ spec(runner) {
     runner = ecs_runner_create(g_alloc_heap, world);
   }
 
-  it("executes every system once") {
+  it("executes every system once in registration order") {
     check(!g_ecsRunningSystem);
     diag_assert(g_ecsRunningSystemId == sentinel_u16);
     check(!ecs_world_busy(world));
 
-    ecs_run_sync(runner);
-    check_eq_int(thread_atomic_load_i64(&test_sys1_counter), 1);
-    check_eq_int(thread_atomic_load_i64(&test_sys2_counter), 1);
+    const EcsEntityId entity = ecs_world_entity_create(world);
+    ecs_world_comp_add_t(world, entity, RunnerCompA, .f1 = 42);
+    ecs_world_comp_add_t(world, entity, RunnerCompB);
+    ecs_world_comp_add_t(world, entity, RunnerCompC);
+    ecs_world_flush(world);
 
     ecs_run_sync(runner);
-    check_eq_int(thread_atomic_load_i64(&test_sys1_counter), 2);
-    check_eq_int(thread_atomic_load_i64(&test_sys2_counter), 2);
+
+    EcsIterator* itr = ecs_view_itr_stack(ecs_world_view_t(world, ReadA));
+    ecs_view_itr_jump(itr, entity);
+    check_eq_int(ecs_view_read_t(itr, RunnerCompA)->f1, 819);
+
+    ecs_run_sync(runner);
+
+    ecs_view_itr_reset(itr);
+    ecs_view_itr_jump(itr, entity);
+    check_eq_int(ecs_view_read_t(itr, RunnerCompA)->f1, 174652);
   }
 
   teardown() {
