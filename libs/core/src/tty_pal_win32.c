59aeebabdd969259cf277f5a150bf3193241d188
Bastian Blokland <mail@bastian.tech>
2021-10-10 17:10:05 +0300

    core: Win32 override output console cp to utf8

--- libs/core/src/tty_pal_win32.c
+++ libs/core/src/tty_pal_win32.c
@@ -14,7 +14,8 @@ struct ConsoleModeOverride {
 static struct ConsoleModeOverride g_consoleModeInputOverride;
 static struct ConsoleModeOverride g_consoleModeOutputOverride;
 static struct ConsoleModeOverride g_consoleModeErrorOverride;
-static UINT                       g_consoleCodePageOriginal;
+static UINT                       g_consoleInputCodePageOriginal;
+static UINT                       g_consoleOutputCodePageOriginal;
 
 static void tty_pal_override_input_mode(File* file, struct ConsoleModeOverride* override) {
   if (GetConsoleMode(file->handle, &override->original)) {
@@ -47,15 +48,20 @@ void tty_pal_init() {
   tty_pal_override_output_mode(g_file_stderr, &g_consoleModeErrorOverride);
 
   // Setup the console to use the utf8 code-page.
-  g_consoleCodePageOriginal = GetConsoleCP();
+  g_consoleInputCodePageOriginal  = GetConsoleCP();
+  g_consoleOutputCodePageOriginal = GetConsoleOutputCP();
+
   SetConsoleCP(CP_UTF8);
+  SetConsoleOutputCP(CP_UTF8);
 }
 
 void tty_pal_teardown() {
   tty_pal_restore_mode(g_file_stdin, &g_consoleModeInputOverride);
   tty_pal_restore_mode(g_file_stdout, &g_consoleModeOutputOverride);
   tty_pal_restore_mode(g_file_stderr, &g_consoleModeErrorOverride);
-  SetConsoleCP(g_consoleCodePageOriginal);
+
+  SetConsoleCP(g_consoleInputCodePageOriginal);
+  SetConsoleOutputCP(g_consoleOutputCodePageOriginal);
 }
 
 bool tty_pal_isatty(File* file) { return GetFileType(file->handle) == FILE_TYPE_CHAR; }
