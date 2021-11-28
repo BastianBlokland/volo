553a14b5f4a196a5879cdd0d6838dba90bf91ec5
Bastian Blokland <mail@bastian.tech>
2021-11-28 23:11:22 +0200

    geo: Add axis aligned box type

---
+++ libs/geo/test/test_box.c
@@ -0,0 +1,43 @@
+#include "check_spec.h"
+#include "core_math.h"
+
+#include "utils_internal.h"
+
+spec(box) {
+
+  it("is formed by a minimum and maximum point") {
+    const GeoVector min = {-1, -1, -1};
+    const GeoVector max = {+1, +1, +1};
+    const GeoBox    b   = {min, max};
+    check_eq_vector(geo_box_center(&b), geo_vector(0));
+    check_eq_vector(geo_box_size(&b), geo_vector(2, 2, 2));
+  }
+
+  it("can construct an infinitely small box") {
+    const GeoBox    b    = geo_box_inverted();
+    const GeoVector size = geo_box_size(&b);
+    check(size.x < -9999999);
+    check(size.y < -9999999);
+    check(size.z < -9999999);
+  }
+
+  it("creates a zero-sized box around a point when encapsulating it in inverted box") {
+    const GeoVector p = {.1337f, -42, 123};
+    GeoBox          b = geo_box_inverted();
+    b                 = geo_box_encapsulate(&b, p);
+    check_eq_vector(geo_box_center(&b), p);
+    check_eq_vector(geo_box_size(&b), geo_vector(0));
+  }
+
+  it("expands to fit the given points when encapsulating points") {
+    const GeoVector p1 = {.1337f, 0, -1};
+    const GeoVector p2 = {.1337f, 0, +2};
+    const GeoVector p3 = {.1337f, 0, +1};
+    GeoBox          b  = geo_box_inverted();
+    b                  = geo_box_encapsulate(&b, p1);
+    b                  = geo_box_encapsulate(&b, p2);
+    b                  = geo_box_encapsulate(&b, p3);
+
+    check_eq_vector(geo_box_size(&b), geo_vector(0, 0, 3));
+  }
+}
