da0ddfc612605b9208e08f4cef85391682597cd6
Bastian Blokland <mail@bastian.tech>
2021-12-06 20:23:55 +0200

    rend: Add buffer abstraction

---
+++ libs/rend/src/rvk/buffer_internal.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "core_string.h"
+
+#include "device_internal.h"
+#include "mem_internal.h"
+
+typedef enum {
+  RvkBufferKind_DeviceIndex,
+  RvkBufferKind_DeviceStorage,
+  RvkBufferKind_HostUniform,
+  RvkBufferKind_HostTransfer,
+
+  RvkBufferKind_Count,
+} RvkBufferKind;
+
+typedef struct {
+  RvkDevice*    dev;
+  RvkMem        mem;
+  RvkBufferKind kind;
+  VkBuffer      vkBuffer;
+} RvkBuffer;
+
+RvkBuffer rvk_buffer_create(RvkDevice*, u64 size, RvkBufferKind);
+void      rvk_buffer_destroy(RvkBuffer*);
+void      rvk_buffer_upload(RvkBuffer*, Mem data, u64 offset);
+String    rvk_buffer_kind_str(RvkBufferKind);
