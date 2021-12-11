b93dcd43f238426c02c14acae95c1b67fd0214c3
Bastian Blokland <mail@bastian.tech>
2021-12-11 14:31:16 +0200

    scene: Rename graphix -> gfx

--- libs/scene/src/register.c
+++ libs/scene/src/register.c
@@ -3,6 +3,6 @@
 
 void scene_register(EcsDef* def) {
   ecs_register_module(def, scene_camera_module);
-  ecs_register_module(def, scene_graphic_module);
+  ecs_register_module(def, scene_gfx_module);
   ecs_register_module(def, scene_transform_module);
 }
