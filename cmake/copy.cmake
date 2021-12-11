53099d5f9c3362272c7152f94d861633c29c6ed7
Bastian Blokland <mail@bastian.tech>
2021-12-11 20:16:34 +0200

    assets: Support external assets

--- cmake/copy.cmake
+++ cmake/copy.cmake
@@ -15,3 +15,27 @@ function(configure_copies target)
 
   add_custom_target(${target} DEPENDS ${artifacts})
 endfunction(configure_copies)
+
+#
+# Configure a target that consists of remote files to be copied.
+# Note: for license info see: https://www.bastian.tech/assets/license.txt
+#
+function(configure_copies_external target)
+  message(STATUS "> Configuring external copies: ${target}")
+
+  set(remoteUrl "https://www.bastian.tech/assets")
+  set(cacheDir "${CMAKE_SOURCE_DIR}/.cache/files")
+
+  foreach(filePath IN LISTS ARGN)
+    set(cacheFilePath ${cacheDir}/${filePath})
+    if(NOT EXISTS ${cacheFilePath})
+      message(STATUS ">> Downloading: ${remoteUrl}/${filePath}")
+      file(DOWNLOAD "${remoteUrl}/${filePath}" ${cacheFilePath})
+    endif()
+
+    configure_file(${cacheFilePath} ${CMAKE_CURRENT_BINARY_DIR}/${filePath} COPYONLY)
+    list(APPEND artifacts ${CMAKE_CURRENT_BINARY_DIR}/${filePath})
+  endforeach()
+
+  add_custom_target(${target} DEPENDS ${artifacts})
+endfunction(configure_copies_external)
