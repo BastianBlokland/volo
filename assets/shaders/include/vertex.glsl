d3a5a8f1d4ce681ad00eac4073409e7351a9211a
Bastian Blokland <mail@bastian.tech>
2021-12-26 00:40:04 +0200

    rend: Refactor mesh binding

---
+++ assets/shaders/include/vertex.glsl
@@ -0,0 +1,15 @@
+#ifndef INCLUDE_VERTEX
+#define INCLUDE_VERTEX
+
+#include "binding.glsl"
+#include "types.glsl"
+
+struct VertexPacked {
+  f16_vec4 position; // x, y, z position
+  f16_vec4 texcoord; // x, y texcoord1
+};
+
+#define vert_position(_VERT_) f32_vec4((_VERT_).position).xyz
+#define vert_texcoord(_VERT_) f32_vec4((_VERT_).texcoord).xy
+
+#endif // INCLUDE_VERTEX
