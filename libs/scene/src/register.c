cbca90d95ea50095a9bcdbf32492a57daf95db9f
Bastian Blokland <mail@bastian.tech>
2021-12-28 17:16:45 +0200

    time: Add time module

--- libs/scene/src/register.c
+++ libs/scene/src/register.c
@@ -2,6 +2,7 @@
 #include "scene_register.h"
 
 void scene_register(EcsDef* def) {
+  ecs_register_module(def, scene_time_module);
   ecs_register_module(def, scene_camera_module);
   ecs_register_module(def, scene_transform_module);
 }
