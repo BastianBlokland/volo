a551e0c73610a54928df1a6886036efcbd1a2933
Bastian Blokland <mail@bastian.tech>
2022-01-11 19:49:30 +0200

    scene: Add camera zoom input

--- libs/scene/src/camera.c
+++ libs/scene/src/camera.c
@@ -9,13 +9,16 @@
 #include "scene_time.h"
 #include "scene_transform.h"
 
-static const f32       g_cameraDefaultFov         = 60.0f * math_deg_to_rad;
-static const f32       g_cameraDefaultNearPlane   = 0.1f;
-static const GeoVector g_cameraDefaultPosition    = {0, 1.5f, -3.0f};
-static const f32       g_cameraDefaultAngle       = 10 * math_deg_to_rad;
-static const f32       g_cameraDefaultMoveSpeed   = 10.0f;
+static const f32       g_cameraNearPlane          = 0.1f;
+static const GeoVector g_cameraPosition           = {0, 1.5f, -3.0f};
+static const f32       g_cameraAngle              = 10 * math_deg_to_rad;
+static const f32       g_cameraFovDefault         = 60.0f * math_deg_to_rad;
+static const f32       g_cameraFovMin             = 25.0f * math_deg_to_rad;
+static const f32       g_cameraFovMax             = 120.0f * math_deg_to_rad;
+static const f32       g_cameraMoveSpeed          = 10.0f;
+static const f32       g_cameraMoveSpeedBoostMult = 4.0f;
 static const f32       g_cameraRotateSensitivity  = 0.0025f;
-static const f32       g_cameraBoostMoveSpeedMult = 4.0f;
+static const f32       g_cameraZoomSensitivity    = 0.1f;
 
 ecs_comp_define_public(SceneCameraComp);
 ecs_comp_define_public(SceneCameraMovementComp);
@@ -38,21 +41,17 @@ ecs_system_define(SceneCameraCreateSys) {
     const EcsEntityId entity = ecs_view_entity(itr);
 
     ecs_world_add_t(
-        world,
-        entity,
-        SceneCameraComp,
-        .fov   = g_cameraDefaultFov,
-        .zNear = g_cameraDefaultNearPlane);
+        world, entity, SceneCameraComp, .fov = g_cameraFovDefault, .zNear = g_cameraNearPlane);
 
-    ecs_world_add_t(world, entity, SceneCameraMovementComp, .moveSpeed = g_cameraDefaultMoveSpeed);
+    ecs_world_add_t(world, entity, SceneCameraMovementComp, .moveSpeed = g_cameraMoveSpeed);
 
     if (!ecs_world_has_t(world, entity, SceneTransformComp)) {
       ecs_world_add_t(
           world,
           entity,
           SceneTransformComp,
-          .position = g_cameraDefaultPosition,
-          .rotation = geo_quat_angle_axis(geo_right, g_cameraDefaultAngle));
+          .position = g_cameraPosition,
+          .rotation = geo_quat_angle_axis(geo_right, g_cameraAngle));
     }
   }
 }
@@ -78,16 +77,86 @@ ecs_system_define(SceneCameraCreateSkySys) {
       .graphic = asset_lookup(world, assets, string_lit("graphics/sky.gra")));
 }
 
-ecs_view_define(CameraMovementView) {
-  ecs_access_with(SceneCameraComp);
+static void camera_move_update(
+    SceneTransformComp*            trans,
+    const SceneCameraMovementComp* move,
+    const GapWindowComp*           win,
+    const f32                      deltaSeconds) {
+  const bool boosted   = gap_window_key_down(win, GapKey_Shift);
+  const f32  moveSpeed = move->moveSpeed * (boosted ? g_cameraMoveSpeedBoostMult : 1.0f);
+  const f32  posDelta  = deltaSeconds * moveSpeed;
+
+  const GeoVector forward = geo_quat_rotate(trans->rotation, geo_forward);
+  const GeoVector right   = geo_quat_rotate(trans->rotation, geo_right);
+
+  if (gap_window_key_down(win, GapKey_W) || gap_window_key_down(win, GapKey_ArrowUp)) {
+    trans->position = geo_vector_add(trans->position, geo_vector_mul(forward, posDelta));
+  }
+  if (gap_window_key_down(win, GapKey_S) || gap_window_key_down(win, GapKey_ArrowDown)) {
+    trans->position = geo_vector_sub(trans->position, geo_vector_mul(forward, posDelta));
+  }
+  if (gap_window_key_down(win, GapKey_D) || gap_window_key_down(win, GapKey_ArrowRight)) {
+    trans->position = geo_vector_add(trans->position, geo_vector_mul(right, posDelta));
+  }
+  if (gap_window_key_down(win, GapKey_A) || gap_window_key_down(win, GapKey_ArrowLeft)) {
+    trans->position = geo_vector_sub(trans->position, geo_vector_mul(right, posDelta));
+  }
+}
+
+static void camera_rotate_update(
+    SceneTransformComp* trans, const SceneCameraMovementComp* move, const GapWindowComp* win) {
+
+  const GeoVector right = geo_quat_rotate(trans->rotation, geo_right);
+
+  const bool lookEnable = gap_window_key_down(win, GapKey_MouseRight) ||
+                          gap_window_key_down(win, GapKey_Control) || move->locked;
+  if (lookEnable) {
+    const f32 deltaX = gap_window_param(win, GapParam_CursorDelta).x * g_cameraRotateSensitivity;
+    const f32 deltaY = gap_window_param(win, GapParam_CursorDelta).y * g_cameraRotateSensitivity;
+
+    trans->rotation = geo_quat_mul(geo_quat_angle_axis(right, deltaY), trans->rotation);
+    trans->rotation = geo_quat_mul(geo_quat_angle_axis(geo_up, deltaX), trans->rotation);
+    trans->rotation = geo_quat_norm(trans->rotation);
+  }
+}
+
+static void camera_zoom_update(SceneCameraComp* cam, GapWindowComp* win) {
+  const f32 zoomDelta = gap_window_param(win, GapParam_ScrollDelta).y * g_cameraZoomSensitivity;
+  cam->fov            = math_clamp_f32(cam->fov + zoomDelta, g_cameraFovMin, g_cameraFovMax);
+}
+
+static void camera_lock_update(SceneCameraMovementComp* move, GapWindowComp* win) {
+  if (gap_window_key_pressed(win, GapKey_Tab)) {
+    if (move->locked) {
+      gap_window_flags_unset(win, GapWindowFlags_CursorLock | GapWindowFlags_CursorHide);
+    } else {
+      gap_window_flags_set(win, GapWindowFlags_CursorLock | GapWindowFlags_CursorHide);
+    }
+    log_i("Update camera lock", log_param("locked", fmt_bool(move->locked)));
+    move->locked ^= true;
+  }
+}
+
+static void camera_fullscreen_update(SceneCameraInternalComp* internal, GapWindowComp* win) {
+  if (gap_window_key_pressed(win, GapKey_F)) {
+    if (gap_window_mode(win) == GapWindowMode_Fullscreen) {
+      gap_window_resize(win, internal->lastWindowedSize, GapWindowMode_Windowed);
+    } else {
+      internal->lastWindowedSize = gap_window_param(win, GapParam_WindowSize);
+      gap_window_resize(win, gap_vector(0, 0), GapWindowMode_Fullscreen);
+    }
+  }
+}
+
+ecs_view_define(CameraUpdateView) {
+  ecs_access_write(SceneCameraComp);
+  ecs_access_write(SceneCameraMovementComp);
   ecs_access_write(SceneTransformComp);
   ecs_access_write(GapWindowComp);
-
-  ecs_access_write(SceneCameraMovementComp);
   ecs_access_maybe_write(SceneCameraInternalComp);
 }
 
-ecs_system_define(SceneCameraMovementSys) {
+ecs_system_define(SceneCameraUpdateSys) {
   EcsView*     view      = ecs_world_view_t(world, GlobalTimeView);
   EcsIterator* globalItr = ecs_view_maybe_at(view, ecs_world_global(world));
   if (!globalItr) {
@@ -96,66 +165,22 @@ ecs_system_define(SceneCameraMovementSys) {
   const SceneTimeComp* time         = ecs_view_read_t(globalItr, SceneTimeComp);
   const f32            deltaSeconds = time->delta / (f32)time_second;
 
-  EcsView* cameraView = ecs_world_view_t(world, CameraMovementView);
+  EcsView* cameraView = ecs_world_view_t(world, CameraUpdateView);
   for (EcsIterator* itr = ecs_view_itr(cameraView); ecs_view_walk(itr);) {
-    GapWindowComp*           win   = ecs_view_write_t(itr, GapWindowComp);
-    SceneTransformComp*      trans = ecs_view_write_t(itr, SceneTransformComp);
-    SceneCameraMovementComp* move  = ecs_view_write_t(itr, SceneCameraMovementComp);
-
+    SceneCameraComp*         cam      = ecs_view_write_t(itr, SceneCameraComp);
+    GapWindowComp*           win      = ecs_view_write_t(itr, GapWindowComp);
+    SceneTransformComp*      trans    = ecs_view_write_t(itr, SceneTransformComp);
+    SceneCameraMovementComp* move     = ecs_view_write_t(itr, SceneCameraMovementComp);
     SceneCameraInternalComp* internal = ecs_view_write_t(itr, SceneCameraInternalComp);
     if (!internal) {
       internal = ecs_world_add_t(world, ecs_view_entity(itr), SceneCameraInternalComp);
     }
 
-    const bool boosted = gap_window_key_down(win, GapKey_Shift);
-    const f32  posDelta =
-        deltaSeconds * move->moveSpeed * (boosted ? g_cameraBoostMoveSpeedMult : 1.0f);
-
-    const GeoVector forward = geo_quat_rotate(trans->rotation, geo_forward);
-    const GeoVector right   = geo_quat_rotate(trans->rotation, geo_right);
-
-    if (gap_window_key_down(win, GapKey_W) || gap_window_key_down(win, GapKey_ArrowUp)) {
-      trans->position = geo_vector_add(trans->position, geo_vector_mul(forward, posDelta));
-    }
-    if (gap_window_key_down(win, GapKey_S) || gap_window_key_down(win, GapKey_ArrowDown)) {
-      trans->position = geo_vector_sub(trans->position, geo_vector_mul(forward, posDelta));
-    }
-    if (gap_window_key_down(win, GapKey_D) || gap_window_key_down(win, GapKey_ArrowRight)) {
-      trans->position = geo_vector_add(trans->position, geo_vector_mul(right, posDelta));
-    }
-    if (gap_window_key_down(win, GapKey_A) || gap_window_key_down(win, GapKey_ArrowLeft)) {
-      trans->position = geo_vector_sub(trans->position, geo_vector_mul(right, posDelta));
-    }
-
-    if (gap_window_key_pressed(win, GapKey_Tab)) {
-      if (move->locked) {
-        gap_window_flags_unset(win, GapWindowFlags_CursorLock | GapWindowFlags_CursorHide);
-      } else {
-        gap_window_flags_set(win, GapWindowFlags_CursorLock | GapWindowFlags_CursorHide);
-      }
-      log_i("Update camera lock", log_param("locked", fmt_bool(move->locked)));
-      move->locked ^= true;
-    }
-
-    if (gap_window_key_pressed(win, GapKey_F)) {
-      if (gap_window_mode(win) == GapWindowMode_Fullscreen) {
-        gap_window_resize(win, internal->lastWindowedSize, GapWindowMode_Windowed);
-      } else {
-        internal->lastWindowedSize = gap_window_param(win, GapParam_WindowSize);
-        gap_window_resize(win, gap_vector(0, 0), GapWindowMode_Fullscreen);
-      }
-    }
-
-    const bool lookEnable = gap_window_key_down(win, GapKey_MouseRight) ||
-                            gap_window_key_down(win, GapKey_Control) || move->locked;
-    if (lookEnable) {
-      const f32 deltaX = gap_window_param(win, GapParam_CursorDelta).x * g_cameraRotateSensitivity;
-      const f32 deltaY = gap_window_param(win, GapParam_CursorDelta).y * g_cameraRotateSensitivity;
-
-      trans->rotation = geo_quat_mul(geo_quat_angle_axis(right, deltaY), trans->rotation);
-      trans->rotation = geo_quat_mul(geo_quat_angle_axis(geo_up, deltaX), trans->rotation);
-      trans->rotation = geo_quat_norm(trans->rotation);
-    }
+    camera_move_update(trans, move, win, deltaSeconds);
+    camera_rotate_update(trans, move, win);
+    camera_zoom_update(cam, win);
+    camera_lock_update(move, win);
+    camera_fullscreen_update(internal, win);
   }
 }
 
@@ -169,13 +194,13 @@ ecs_module_init(scene_camera_module) {
   ecs_register_view(GlobalAssetsView);
   ecs_register_view(SkyView);
   ecs_register_view(CameraCreateView);
-  ecs_register_view(CameraMovementView);
+  ecs_register_view(CameraUpdateView);
 
   ecs_register_system(SceneCameraCreateSys, ecs_view_id(CameraCreateView));
   ecs_register_system(SceneCameraCreateSkySys, ecs_view_id(GlobalAssetsView), ecs_view_id(SkyView));
 
   ecs_register_system(
-      SceneCameraMovementSys, ecs_view_id(GlobalTimeView), ecs_view_id(CameraMovementView));
+      SceneCameraUpdateSys, ecs_view_id(GlobalTimeView), ecs_view_id(CameraUpdateView));
 }
 
 GeoMatrix scene_camera_proj(const SceneCameraComp* cam, const f32 aspect) {
