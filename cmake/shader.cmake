7fa03eb255f84a57a1460851a1fb2542b581f70b
Bastian Blokland <mail@bastian.tech>
2022-01-21 21:33:04 +0200

    cmake: Update shader compilation to use dep-files
    Allows cmake to track changes to include files

--- cmake/shader.cmake
+++ cmake/shader.cmake
@@ -12,16 +12,18 @@ function(configure_shaders target)
   message(STATUS ">> Vulkan glslc: ${Vulkan_GLSLC_EXECUTABLE}")
 
   foreach(shaderPath IN LISTS ARGN)
-
     # Create a custom command to compile the shader to spir-v using glslc.
     add_custom_command(
       OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${shaderPath}.spv
       DEPENDS ${shaderPath}
-      COMMAND ${Vulkan_GLSLC_EXECUTABLE}
-      -x glsl --target-env=vulkan1.1 --target-spv=spv1.3 -Werror -O
-      -o ${CMAKE_CURRENT_SOURCE_DIR}/${shaderPath}.spv
-      ${CMAKE_CURRENT_SOURCE_DIR}/${shaderPath})
-
+      DEPFILE ${shaderPath}.d
+      COMMAND
+        ${Vulkan_GLSLC_EXECUTABLE}
+        -MD -MF ${shaderPath}.d
+        -x glsl --target-env=vulkan1.1 --target-spv=spv1.3 -Werror -O
+        -o ${CMAKE_CURRENT_SOURCE_DIR}/${shaderPath}.spv
+        ${CMAKE_CURRENT_SOURCE_DIR}/${shaderPath}
+      )
     list(APPEND artifacts ${CMAKE_CURRENT_SOURCE_DIR}/${shaderPath}.spv)
   endforeach()
 
