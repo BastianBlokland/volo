4e7e362a48748c2abe1bd9a8131988b97b77a04b
Bastian Blokland <mail@bastian.tech>
2022-01-22 16:04:10 +0200

    scene: Text rendering restructuring

--- libs/scene/src/text.c
+++ libs/scene/src/text.c
@@ -11,6 +11,44 @@
 static const String g_textGraphic = string_static("graphics/ui/text.gra");
 static const String g_textFont    = string_static("fonts/mono.ftx");
 
+static void scene_text_build(
+    const SceneTextComp* textComp, SceneRenderableUniqueComp* renderable, const AssetFtxComp* ftx) {
+
+  (void)textComp;
+
+  typedef struct {
+    ALIGNAS(16)
+    f32 glyphsPerDim;
+    f32 invGlyphsPerDim;
+  } ShaderFontData;
+
+  typedef struct {
+    ALIGNAS(16)
+    f32 position[2];
+    f32 size;
+    f32 index;
+  } ShaderCharData;
+
+  // TODO: Compute chars.
+  // TODO: Clamp max chars.
+  const u32 charCount = 1;
+
+  const usize dataSize            = sizeof(ShaderFontData) + sizeof(ShaderCharData) * charCount;
+  Mem         data                = scene_renderable_unique_data(renderable, dataSize);
+  *mem_as_t(data, ShaderFontData) = (ShaderFontData){
+      .glyphsPerDim    = (f32)ftx->glyphsPerDim,
+      .invGlyphsPerDim = 1.0f / (f32)ftx->glyphsPerDim,
+  };
+  ShaderCharData* charData = mem_consume(data, sizeof(ShaderFontData)).ptr;
+  charData[0]              = (ShaderCharData){
+      .position = {100, 100},
+      .size     = 100,
+      .index    = (f32)asset_ftx_lookup(ftx, 65)->glyphIndex,
+  };
+
+  renderable->vertexCountOverride = charCount * 6;
+}
+
 typedef enum {
   SceneGlobalFont_Acquired  = 1 << 0,
   SceneGlobalFont_Unloading = 1 << 1,
@@ -26,25 +64,58 @@ ecs_view_define(GlobalAssetsView) { ecs_access_write(AssetManagerComp); }
 ecs_view_define(GlobalFontView) { ecs_access_write(SceneGlobalFontComp); }
 ecs_view_define(FtxView) { ecs_access_read(AssetFtxComp); }
 
+static AssetManagerComp* scene_asset_manager(EcsWorld* world) {
+  EcsView*     globalView = ecs_world_view_t(world, GlobalAssetsView);
+  EcsIterator* globalItr  = ecs_view_maybe_at(globalView, ecs_world_global(world));
+  return globalItr ? ecs_view_write_t(globalItr, AssetManagerComp) : null;
+}
+
+static SceneGlobalFontComp* scene_global_fonts(EcsWorld* world) {
+  EcsView*     globalView = ecs_world_view_t(world, GlobalFontView);
+  EcsIterator* globalItr  = ecs_view_maybe_at(globalView, ecs_world_global(world));
+  return globalItr ? ecs_view_write_t(globalItr, SceneGlobalFontComp) : null;
+}
+
+static const AssetFtxComp* scene_font_get(EcsWorld* world) {
+  SceneGlobalFontComp* globalFonts = scene_global_fonts(world);
+  if (!globalFonts) {
+    return null; // Global fonts have not been initialized yet.
+  }
+  if (!(globalFonts->flags & (SceneGlobalFont_Acquired | SceneGlobalFont_Unloading))) {
+    log_i("Acquiring global font", log_param("id", fmt_text(g_textFont)));
+    asset_acquire(world, globalFonts->asset);
+    globalFonts->flags |= SceneGlobalFont_Acquired;
+  }
+
+  EcsView* ftxView = ecs_world_view_t(world, FtxView);
+  if (!ecs_view_contains(ftxView, globalFonts->asset)) {
+    return null; // Ftx font is not loaded yet.
+  }
+  return ecs_utils_read_t(world, FtxView, globalFonts->asset, AssetFtxComp);
+}
+
 ecs_view_define(TextInitView) {
   ecs_access_with(SceneTextComp);
   ecs_access_without(SceneRenderableUniqueComp);
 }
 
 ecs_system_define(SceneTextInitSys) {
-  EcsView*     globalView = ecs_world_view_t(world, GlobalAssetsView);
-  EcsIterator* globalItr  = ecs_view_maybe_at(globalView, ecs_world_global(world));
-  if (!globalItr) {
-    return;
+  AssetManagerComp* assets = scene_asset_manager(world);
+  if (!assets) {
+    return; // Asset manager hasn't been initialized yet.
   }
-  AssetManagerComp* assets = ecs_view_write_t(globalItr, AssetManagerComp);
-  if (!ecs_world_has_t(world, ecs_view_entity(globalItr), SceneGlobalFontComp)) {
+
+  if (!ecs_world_has_t(world, ecs_world_global(world), SceneGlobalFontComp)) {
+    // Initialize global fonts lookup.
     const EcsEntityId fontAsset = asset_lookup(world, assets, g_textFont);
-    ecs_world_add_t(world, ecs_view_entity(globalItr), SceneGlobalFontComp, .asset = fontAsset);
+    ecs_world_add_t(world, ecs_world_global(world), SceneGlobalFontComp, .asset = fontAsset);
   }
 
   EcsView* renderView = ecs_world_view_t(world, TextInitView);
   for (EcsIterator* itr = ecs_view_itr(renderView); ecs_view_walk(itr);) {
+    /**
+     * Create a 'SceneRenderableUniqueComp' for every text instance.
+     */
     ecs_world_add_t(
         world,
         ecs_view_entity(itr),
@@ -54,17 +125,19 @@ ecs_system_define(SceneTextInitSys) {
 }
 
 ecs_system_define(SceneTextUnloadChangedFontsSys) {
-  EcsView*     globalView = ecs_world_view_t(world, GlobalFontView);
-  EcsIterator* globalItr  = ecs_view_maybe_at(globalView, ecs_world_global(world));
-  if (!globalItr) {
+  SceneGlobalFontComp* globalFonts = scene_global_fonts(world);
+  if (!globalFonts) {
     return;
   }
-  SceneGlobalFontComp* globalFonts = ecs_view_write_t(globalItr, SceneGlobalFontComp);
-  const bool           isLoaded    = ecs_world_has_t(world, globalFonts->asset, AssetLoadedComp);
-  const bool           hasChanged  = ecs_world_has_t(world, globalFonts->asset, AssetChangedComp);
+  const bool isLoaded   = ecs_world_has_t(world, globalFonts->asset, AssetLoadedComp);
+  const bool hasChanged = ecs_world_has_t(world, globalFonts->asset, AssetChangedComp);
 
   if (globalFonts->flags & SceneGlobalFont_Acquired && isLoaded && hasChanged) {
-    log_i("Unloading global font due to changed asset", log_param("id", fmt_text(g_textFont)));
+    log_i(
+        "Unloading global font",
+        log_param("id", fmt_text(g_textFont)),
+        log_param("reason", fmt_text_lit("Asset changed")));
+
     asset_release(world, globalFonts->asset);
     globalFonts->flags &= ~SceneGlobalFont_Acquired;
     globalFonts->flags |= SceneGlobalFont_Unloading;
@@ -80,61 +153,15 @@ ecs_view_define(TextRenderView) {
 }
 
 ecs_system_define(SceneTextRenderSys) {
-  EcsView*     globalView = ecs_world_view_t(world, GlobalFontView);
-  EcsIterator* globalItr  = ecs_view_maybe_at(globalView, ecs_world_global(world));
-  if (!globalItr) {
+  const AssetFtxComp* ftx = scene_font_get(world);
+  if (!ftx) {
     return;
   }
-  SceneGlobalFontComp* globalFonts = ecs_view_write_t(globalItr, SceneGlobalFontComp);
-  if (!(globalFonts->flags & (SceneGlobalFont_Acquired | SceneGlobalFont_Unloading))) {
-    asset_acquire(world, globalFonts->asset);
-    globalFonts->flags |= SceneGlobalFont_Acquired;
-  }
-  EcsView* ftxView = ecs_world_view_t(world, FtxView);
-  if (!ecs_view_contains(ftxView, globalFonts->asset)) {
-    return; // Ftx font is not loaded.
-  }
-  const AssetFtxComp* ftx = ecs_utils_read_t(world, FtxView, globalFonts->asset, AssetFtxComp);
-  (void)ftx;
-
   EcsView* renderView = ecs_world_view_t(world, TextRenderView);
   for (EcsIterator* itr = ecs_view_itr(renderView); ecs_view_walk(itr);) {
     const SceneTextComp*       textComp   = ecs_view_read_t(itr, SceneTextComp);
     SceneRenderableUniqueComp* renderable = ecs_view_write_t(itr, SceneRenderableUniqueComp);
-
-    (void)textComp;
-
-    typedef struct {
-      ALIGNAS(16)
-      f32 glyphsPerDim;
-      f32 invGlyphsPerDim;
-    } ShaderFontData;
-
-    typedef struct {
-      ALIGNAS(16)
-      f32 position[2];
-      f32 size;
-      f32 index;
-    } ShaderCharData;
-
-    // TODO: Compute chars.
-    // TODO: Clamp max chars.
-    const u32 charCount = 1;
-
-    const usize dataSize            = sizeof(ShaderFontData) + sizeof(ShaderCharData) * charCount;
-    Mem         data                = scene_renderable_unique_data(renderable, dataSize);
-    *mem_as_t(data, ShaderFontData) = (ShaderFontData){
-        .glyphsPerDim    = (f32)ftx->glyphsPerDim,
-        .invGlyphsPerDim = 1.0f / (f32)ftx->glyphsPerDim,
-    };
-    ShaderCharData* charData = mem_consume(data, sizeof(ShaderFontData)).ptr;
-    charData[0]              = (ShaderCharData){
-        .position = {100, 100},
-        .size     = 100,
-        .index    = (f32)asset_ftx_lookup(ftx, 65)->glyphIndex,
-    };
-
-    renderable->vertexCountOverride = charCount * 6;
+    scene_text_build(textComp, renderable, ftx);
   }
 }
 
