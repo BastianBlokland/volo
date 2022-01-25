db20b288577007d3ad49735ab9234857b390e408
Bastian Blokland <mail@bastian.tech>
2022-01-25 21:10:03 +0200

    rend: Refactor draw data storage

--- libs/rend/src/draw.c
+++ libs/rend/src/draw.c
@@ -11,25 +11,48 @@
 ecs_comp_define(RendDrawComp) {
   EcsEntityId graphic;
   u32         vertexCountOverride;
-  DynArray    instances;
+  u32         dataSize; // NOTE: Size per instance.
+  Mem         dataMemory;
+  u32         instances;
 };
 
 static void ecs_destruct_draw(void* data) {
   RendDrawComp* comp = data;
-  dynarray_destroy(&comp->instances);
+  if (mem_valid(comp->dataMemory)) {
+    alloc_free(g_alloc_heap, comp->dataMemory);
+  }
 }
 
 static void ecs_combine_draw(void* dataA, void* dataB) {
-  RendDrawComp* compA = dataA;
-  RendDrawComp* compB = dataB;
+  RendDrawComp* drawA = dataA;
+  RendDrawComp* drawB = dataB;
   diag_assert_msg(
-      compA->instances.stride == compB->instances.stride,
-      "Only draws with the same data-stride can be combined");
+      drawA->dataSize == drawB->dataSize, "Only draws with the same data-stride can be combined");
 
-  mem_cpy(
-      dynarray_push(&compA->instances, compB->instances.size),
-      dynarray_at(&compB->instances, 0, compB->instances.size));
-  dynarray_destroy(&compB->instances);
+  for (u32 i = 0; i != drawB->instances; ++i) {
+    const Mem instanceData = mem_slice(drawB->dataMemory, drawB->dataSize * i, drawB->dataSize);
+    mem_cpy(rend_draw_add_instance(drawA), instanceData);
+  }
+
+  if (mem_valid(drawB->dataMemory)) {
+    alloc_free(g_alloc_heap, drawB->dataMemory);
+  }
+}
+
+static void rend_draw_ensure_data_storage(RendDrawComp* draw, const u32 instances) {
+  const u32 neededSize = instances * draw->dataSize;
+  if (UNLIKELY(draw->dataMemory.size < neededSize)) {
+    const Mem newMem = alloc_alloc(g_alloc_heap, bits_nextpow2_32(neededSize), rend_draw_min_align);
+    if (mem_valid(draw->dataMemory)) {
+      mem_cpy(newMem, draw->dataMemory);
+      alloc_free(g_alloc_heap, draw->dataMemory);
+    }
+    draw->dataMemory = newMem;
+  }
+}
+
+static Mem rend_draw_data(const RendDrawComp* draw, const u32 instance) {
+  return mem_slice(draw->dataMemory, instance * draw->dataSize, draw->dataSize);
 }
 
 ecs_view_define(DrawView) { ecs_access_write(RendDrawComp); }
@@ -37,7 +60,7 @@ ecs_view_define(DrawView) { ecs_access_write(RendDrawComp); }
 ecs_system_define(RendClearDrawsSys) {
   EcsView* drawView = ecs_world_view_t(world, DrawView);
   for (EcsIterator* itr = ecs_view_itr(drawView); ecs_view_walk(itr);) {
-    dynarray_clear(&ecs_view_write_t(itr, RendDrawComp)->instances);
+    ecs_view_write_t(itr, RendDrawComp)->instances = 0;
   }
 }
 
@@ -52,24 +75,23 @@ ecs_module_init(rend_draw_module) {
 }
 
 RendDrawComp* rend_draw_create(EcsWorld* world, const EcsEntityId entity) {
-  return ecs_world_add_t(
-      world, entity, RendDrawComp, .instances = dynarray_create(g_alloc_heap, 1, 16, 0));
+  return ecs_world_add_t(world, entity, RendDrawComp);
 }
 
 EcsEntityId rend_draw_graphic(const RendDrawComp* draw) { return draw->graphic; }
 
 bool rend_draw_gather(RendDrawComp* draw) {
   // TODO: Perform culling etc.
-  return draw->instances.size != 0;
+  return draw->instances != 0;
 }
 
 RvkPassDraw rend_draw_output(const RendDrawComp* draw, RvkGraphic* graphic) {
   return (RvkPassDraw){
       .graphic             = graphic,
       .vertexCountOverride = draw->vertexCountOverride,
-      .instanceCount       = (u32)draw->instances.size,
-      .data                = dynarray_at(&draw->instances, 0, draw->instances.size),
-      .dataStride          = draw->instances.stride,
+      .instanceCount       = draw->instances,
+      .data                = mem_slice(draw->dataMemory, 0, draw->instances * draw->dataSize),
+      .dataStride          = draw->dataSize,
   };
 }
 
@@ -82,9 +104,12 @@ void rend_draw_set_vertex_count(RendDrawComp* comp, const u32 vertexCount) {
 }
 
 void rend_draw_set_data_size(RendDrawComp* draw, const u32 size) {
-  // TODO: size 0 should probably be valid, but is not properly handled at the moment.
-  dynarray_clear(&draw->instances);
-  draw->instances.stride = bits_align(size, rend_draw_min_align);
+  draw->instances = 0;
+  draw->dataSize  = bits_align(size, rend_draw_min_align);
 }
 
-Mem rend_draw_add_instance(RendDrawComp* draw) { return dynarray_push(&draw->instances, 1); }
+Mem rend_draw_add_instance(RendDrawComp* draw) {
+  ++draw->instances;
+  rend_draw_ensure_data_storage(draw, draw->instances);
+  return rend_draw_data(draw, draw->instances - 1);
+}
