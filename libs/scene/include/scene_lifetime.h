2a4b0ae608f13d8f83a48f8b90efa789306f0a82
Bastian Blokland <mail@bastian.tech>
2022-01-26 22:02:34 +0200

    scene: Add duration lifetime component

--- libs/scene/include/scene_lifetime.h
+++ libs/scene/include/scene_lifetime.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "core_time.h"
 #include "ecs_entity.h"
 #include "ecs_module.h"
 
@@ -6,3 +7,8 @@
  * The entity is destroyed as soon as its owner is destroyed.
  */
 ecs_comp_extern_public(SceneLifetimeOwnerComp) { EcsEntityId owner; };
+
+/**
+ * The entity is destroyed as when its duration expires.
+ */
+ecs_comp_extern_public(SceneLifetimeDurationComp) { TimeDuration duration; };
--- libs/scene/src/lifetime.c
+++ libs/scene/src/lifetime.c
@@ -1,10 +1,14 @@
 #include "core_annotation.h"
 #include "ecs_world.h"
 #include "scene_lifetime.h"
+#include "scene_time.h"
 
 ecs_comp_define_public(SceneLifetimeOwnerComp);
+ecs_comp_define_public(SceneLifetimeDurationComp);
 
+ecs_view_define(GlobalView) { ecs_access_read(SceneTimeComp); }
 ecs_view_define(LifetimeOwnerView) { ecs_access_read(SceneLifetimeOwnerComp); }
+ecs_view_define(LifetimeDurationView) { ecs_access_write(SceneLifetimeDurationComp); }
 
 ecs_system_define(SceneLifetimeOwnerSys) {
   EcsView* lifetimeView = ecs_world_view_t(world, LifetimeOwnerView);
@@ -16,10 +20,32 @@ ecs_system_define(SceneLifetimeOwnerSys) {
   }
 }
 
+ecs_system_define(SceneLifetimeDurationSys) {
+  EcsView*     globalView = ecs_world_view_t(world, GlobalView);
+  EcsIterator* globalItr  = ecs_view_maybe_at(globalView, ecs_world_global(world));
+  if (!globalItr) {
+    return;
+  }
+  const SceneTimeComp* time = ecs_view_read_t(globalItr, SceneTimeComp);
+
+  EcsView* lifetimeView = ecs_world_view_t(world, LifetimeDurationView);
+  for (EcsIterator* itr = ecs_view_itr(lifetimeView); ecs_view_walk(itr);) {
+    SceneLifetimeDurationComp* lifetime = ecs_view_write_t(itr, SceneLifetimeDurationComp);
+    if ((lifetime->duration -= time->delta) < 0) {
+      ecs_world_entity_destroy(world, ecs_view_entity(itr));
+    }
+  }
+}
+
 ecs_module_init(scene_lifetime_module) {
   ecs_register_comp(SceneLifetimeOwnerComp);
+  ecs_register_comp(SceneLifetimeDurationComp);
 
+  ecs_register_view(GlobalView);
   ecs_register_view(LifetimeOwnerView);
+  ecs_register_view(LifetimeDurationView);
 
   ecs_register_system(SceneLifetimeOwnerSys, ecs_view_id(LifetimeOwnerView));
+  ecs_register_system(
+      SceneLifetimeDurationSys, ecs_view_id(GlobalView), ecs_view_id(LifetimeDurationView));
 }
