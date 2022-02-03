66b3cb8a007bc78a8582a1f11c50be784f1b9380
Bastian Blokland <mail@bastian.tech>
2022-02-03 22:29:52 +0200

    assets: Add configurable axis to pme

--- libs/asset/src/loader_mesh_pme.c
+++ libs/asset/src/loader_mesh_pme.c
@@ -20,11 +20,21 @@ static DataReg* g_dataReg;
 static DataMeta g_dataPmeDefMeta;
 
 typedef enum {
+  PmeAxis_Up,
+  PmeAxis_Down,
+  PmeAxis_Right,
+  PmeAxis_Left,
+  PmeAxis_Forward,
+  PmeAxis_Backward,
+} PmeAxis;
+
+typedef enum {
   PmeType_Triangle,
 } PmeType;
 
 typedef struct {
   PmeType type;
+  PmeAxis axis;
   f32     scaleX, scaleY, scaleZ;
   f32     offsetX, offsetY, offsetZ;
 } PmeDef;
@@ -42,8 +52,17 @@ static void pme_datareg_init() {
     data_reg_enum_t(g_dataReg, PmeType);
     data_reg_const_t(g_dataReg, PmeType, Triangle);
 
+    data_reg_enum_t(g_dataReg, PmeAxis);
+    data_reg_const_t(g_dataReg, PmeAxis, Up);
+    data_reg_const_t(g_dataReg, PmeAxis, Down);
+    data_reg_const_t(g_dataReg, PmeAxis, Right);
+    data_reg_const_t(g_dataReg, PmeAxis, Left);
+    data_reg_const_t(g_dataReg, PmeAxis, Forward);
+    data_reg_const_t(g_dataReg, PmeAxis, Backward);
+
     data_reg_struct_t(g_dataReg, PmeDef);
     data_reg_field_t(g_dataReg, PmeDef, type, t_PmeType);
+    data_reg_field_t(g_dataReg, PmeDef, axis, t_PmeAxis);
     data_reg_field_t(g_dataReg, PmeDef, scaleX, data_prim_t(f32), .flags = DataFlags_Opt | DataFlags_NotEmpty);
     data_reg_field_t(g_dataReg, PmeDef, scaleY, data_prim_t(f32), .flags = DataFlags_Opt | DataFlags_NotEmpty);
     data_reg_field_t(g_dataReg, PmeDef, scaleZ, data_prim_t(f32), .flags = DataFlags_Opt | DataFlags_NotEmpty);
@@ -57,37 +76,51 @@ static void pme_datareg_init() {
   thread_spinlock_unlock(&g_initLock);
 }
 
-static GeoVector pme_position(const PmeDef* def, const f32 x, const f32 y, const f32 z) {
+static GeoVector pme_position(const PmeDef* def, const GeoVector vec) {
   const f32 scaleX = def->scaleX != 0.0f ? def->scaleX : 1.0f;
   const f32 scaleY = def->scaleY != 0.0f ? def->scaleY : 1.0f;
   const f32 scaleZ = def->scaleZ != 0.0f ? def->scaleZ : 1.0f;
   return geo_vector(
-      def->offsetX + x * scaleX, def->offsetY + y * scaleY, def->offsetZ + z * scaleZ);
+      def->offsetX + vec.x * scaleX, def->offsetY + vec.y * scaleY, def->offsetZ + vec.z * scaleZ);
+}
+
+static GeoVector pme_axis_normal(const PmeDef* def) {
+  switch (def->axis) {
+  case PmeAxis_Up:
+    return geo_down;
+  case PmeAxis_Down:
+    return geo_up;
+  case PmeAxis_Right:
+    return geo_left;
+  case PmeAxis_Left:
+    return geo_right;
+  case PmeAxis_Forward:
+    return geo_backward;
+  case PmeAxis_Backward:
+    return geo_forward;
+  }
+  diag_crash();
 }
 
 static void pme_generate_triangle(const PmeDef* def, AssetMeshBuilder* builder) {
+  const GeoVector fwd = pme_axis_normal(def);
+  const GeoQuat   rot = geo_quat_look(fwd, geo_up);
   asset_mesh_builder_push(
       builder,
       (AssetMeshVertex){
-          .position = pme_position(def, -0.5f, -0.5f, 0.0),
-          .normal   = geo_backward,
-          .tangent  = geo_vector(0, 1, 0, -1),
+          .position = pme_position(def, geo_quat_rotate(rot, geo_vector(-0.5, -0.5))),
           .texcoord = geo_vector(0, 0),
       });
   asset_mesh_builder_push(
       builder,
       (AssetMeshVertex){
-          .position = pme_position(def, 0, 0.5f, 0.0),
-          .normal   = geo_backward,
-          .tangent  = geo_vector(0, 1, 0, -1),
+          .position = pme_position(def, geo_quat_rotate(rot, geo_vector(0, 0.5))),
           .texcoord = geo_vector(0.5, 1),
       });
   asset_mesh_builder_push(
       builder,
       (AssetMeshVertex){
-          .position = pme_position(def, 0.5f, -0.5f, 0.0),
-          .normal   = geo_backward,
-          .tangent  = geo_vector(0, 1, 0, -1),
+          .position = pme_position(def, geo_quat_rotate(rot, geo_vector(0.5, -0.5))),
           .texcoord = geo_vector(1, 0),
       });
 }
