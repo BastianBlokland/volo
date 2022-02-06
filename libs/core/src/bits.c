a10a91bc3b83f4a0aa0b584fa6366bb87a4e4e5a
Bastian Blokland <mail@bastian.tech>
2022-02-06 16:42:18 +0200

    core: Add float quantize routine

--- libs/core/src/bits.c
+++ libs/core/src/bits.c
@@ -253,3 +253,25 @@ u64 bits_f64_as_u64(f64 val) { return *(u64*)(&val); }
 
 f16 bits_f32_to_f16(const f32 val) { return g_bitsF32ToF16Impl(val); }
 f32 bits_f16_to_f32(const f16 val) { return g_bitsF16ToF32Impl(val); }
+
+f32 bits_quantize_f32(const f32 val, const u8 maxMantissaBits) {
+  u32 valBits = bits_f32_as_u32(val);
+
+  /**
+   * Generates +-inf for overflow, preserves NaN, flushes denormals to zero, rounds to nearest.
+   * Based on the MeshOptimizer implementation by Zeux (https://github.com/zeux/meshoptimizer).
+   */
+
+  const i32 mask           = (1 << (f32_mantissa_bits - maxMantissaBits)) - 1;
+  const i32 round          = (1 << (f32_mantissa_bits - maxMantissaBits)) >> 1;
+  const i32 exp            = valBits & 0x7f800000;
+  const u32 roundedValBits = (valBits + round) & ~mask;
+
+  // Round all numbers except inf/nan; this is important to make sure nan doesn't overflow into -0.
+  valBits = exp == 0x7f800000 ? valBits : roundedValBits;
+
+  // Flush denormals to zero.
+  valBits = exp == 0 ? 0 : valBits;
+
+  return bits_u32_as_f32(valBits);
+}
