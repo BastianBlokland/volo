3c87e2c1bc4cd89308a0550e1c79dfe44732176c
Bastian Blokland <mail@bastian.tech>
2022-03-06 19:35:01 +0200

    apps: Enable debug menus for new windows

--- apps/pedestal.c
+++ apps/pedestal.c
@@ -170,7 +170,8 @@ ecs_system_define(AppUpdateSys) {
       app->flags ^= AppFlags_Rotate;
     }
     if (gap_window_key_pressed(window, GapKey_Return)) {
-      gap_window_create(world, GapWindowFlags_Default, g_windowSize);
+      const EcsEntityId newWindow = gap_window_create(world, GapWindowFlags_Default, g_windowSize);
+      debug_menu_create(world, newWindow);
     }
     if (gap_window_key_pressed(window, GapKey_Alpha1)) {
       app->subjectCount = 1;
