751d211c5085bf2397943d85a2560bab5d97eb41
Bastian Blokland <mail@bastian.tech>
2022-03-06 17:16:43 +0200

    scene: Remove old text component

--- libs/scene/src/register.c
+++ libs/scene/src/register.c
@@ -8,7 +8,6 @@ void scene_register(EcsDef* def) {
   ecs_register_module(def, scene_lifetime_module);
   ecs_register_module(def, scene_renderable_module);
   ecs_register_module(def, scene_tag_module);
-  ecs_register_module(def, scene_text_module);
   ecs_register_module(def, scene_time_module);
   ecs_register_module(def, scene_transform_module);
   ecs_register_module(def, scene_velocity_module);
