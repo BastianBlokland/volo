1ea20fdfab0bb70a2e2ba5e95e39b4768bdf87a9
Bastian Blokland <mail@bastian.tech>
2022-03-13 16:53:00 +0200

    debug: Remove grid input system

--- libs/debug/src/grid.c
+++ libs/debug/src/grid.c
@@ -2,14 +2,11 @@
 #include "core_math.h"
 #include "ecs_utils.h"
 #include "ecs_world.h"
-#include "gap_window.h"
 #include "rend_draw.h"
 
 static const u32 g_gridSegments          = 400;
 static const u32 g_gridHighlightInterval = 5;
 static const f32 g_gridCellSizeDefault   = 1.0f;
-static const f32 g_gridCellSizeMin       = 0.1f;
-static const f32 g_gridCellSizeMax       = 8.0f;
 
 typedef struct {
   f32 cellSize;
@@ -20,7 +17,6 @@ typedef struct {
 ecs_comp_define(DebugGridComp) { f32 cellSize; };
 
 ecs_view_define(GlobalAssetsView) { ecs_access_write(AssetManagerComp); }
-ecs_view_define(WindowView) { ecs_access_read(GapWindowComp); }
 
 ecs_view_define(GridView) { ecs_access_write(DebugGridComp); }
 
@@ -42,27 +38,6 @@ ecs_system_define(DebugGridCreateSys) {
   RendDrawComp* draw = rend_draw_create(world, gridEntity, RendDrawFlags_NoInstanceFiltering);
   rend_draw_set_graphic(draw, asset_lookup(world, assets, string_lit("graphics/debug/grid.gra")));
   rend_draw_set_vertex_count(draw, g_gridSegments * 4);
-}
-
-ecs_system_define(DebugGridInputSys) {
-  EcsIterator* windowItr = ecs_view_itr(ecs_world_view_t(world, WindowView));
-  EcsIterator* gridItr   = ecs_view_itr(ecs_world_view_t(world, GridView));
-
-  while (ecs_view_walk(windowItr)) {
-    const GapWindowComp* win = ecs_view_read_t(windowItr, GapWindowComp);
-
-    ecs_view_itr_reset(gridItr);
-    while (ecs_view_walk(gridItr)) {
-      DebugGridComp* grid = ecs_view_write_t(gridItr, DebugGridComp);
-      if (gap_window_key_pressed(win, GapKey_Plus)) {
-        grid->cellSize *= 2.0f;
-      }
-      if (gap_window_key_pressed(win, GapKey_Minus)) {
-        grid->cellSize *= 0.5f;
-      }
-      grid->cellSize = math_clamp_f32(grid->cellSize, g_gridCellSizeMin, g_gridCellSizeMax);
-    }
-  }
 }
 
 ecs_view_define(GridUpdateDataView) {
@@ -89,11 +64,9 @@ ecs_module_init(debug_grid_module) {
   ecs_register_comp(DebugGridComp);
 
   ecs_register_view(GlobalAssetsView);
-  ecs_register_view(WindowView);
   ecs_register_view(GridView);
   ecs_register_view(GridUpdateDataView);
 
   ecs_register_system(DebugGridCreateSys, ecs_view_id(GlobalAssetsView), ecs_view_id(GridView));
-  ecs_register_system(DebugGridInputSys, ecs_view_id(WindowView), ecs_view_id(GridView));
   ecs_register_system(DebugGridUpdateDataSys, ecs_view_id(GridUpdateDataView));
 }
