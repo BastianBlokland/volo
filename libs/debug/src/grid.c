e844d3fe994e1b4bedc2d2785b2389e94d1d87c2
Bastian Blokland <mail@bastian.tech>
2022-03-13 17:10:19 +0200

    debug: Create a grid per window

--- libs/debug/src/grid.c
+++ libs/debug/src/grid.c
@@ -1,55 +1,76 @@
 #include "asset_manager.h"
-#include "core_math.h"
-#include "ecs_utils.h"
 #include "ecs_world.h"
+#include "gap_window.h"
 #include "rend_draw.h"
+#include "scene_lifetime.h"
 
 static const u32 g_gridSegments          = 400;
 static const u32 g_gridHighlightInterval = 5;
 static const f32 g_gridCellSizeDefault   = 1.0f;
 
 typedef struct {
+  ALIGNAS(16)
   f32 cellSize;
   u32 segments;
   u32 highlightInterval;
 } DebugGridData;
 
-ecs_comp_define(DebugGridComp) { f32 cellSize; };
+ASSERT(sizeof(DebugGridData) == 16, "Size needs to match the size defined in glsl");
+
+ecs_comp_define(DebugGridComp) {
+  f32         cellSize;
+  EcsEntityId draw;
+};
 
 ecs_view_define(GlobalAssetsView) { ecs_access_write(AssetManagerComp); }
 
-ecs_view_define(GridView) { ecs_access_write(DebugGridComp); }
+ecs_view_define(GridCreateView) {
+  ecs_access_with(GapWindowComp);
+  ecs_access_without(DebugGridComp);
+}
 
-ecs_system_define(DebugGridCreateSys) {
-  if (ecs_utils_any(world, GridView)) {
-    return;
-  }
+ecs_view_define(GridReadView) { ecs_access_read(DebugGridComp); }
+ecs_view_define(DrawWriteView) { ecs_access_write(RendDrawComp); }
 
+static AssetManagerComp* debug_grid_asset_manager(EcsWorld* world) {
   EcsView*     globalView = ecs_world_view_t(world, GlobalAssetsView);
   EcsIterator* globalItr  = ecs_view_maybe_at(globalView, ecs_world_global(world));
-  if (!globalItr) {
-    return;
-  }
-  AssetManagerComp* assets = ecs_view_write_t(globalItr, AssetManagerComp);
+  return globalItr ? ecs_view_write_t(globalItr, AssetManagerComp) : null;
+}
 
-  const EcsEntityId gridEntity = ecs_world_entity_create(world);
-  ecs_world_add_t(world, gridEntity, DebugGridComp, .cellSize = g_gridCellSizeDefault);
+static void debug_grid_create(EcsWorld* world, const EcsEntityId entity, AssetManagerComp* assets) {
+  const EcsEntityId drawEntity = ecs_world_entity_create(world);
+  ecs_world_add_t(world, drawEntity, SceneLifetimeOwnerComp, .owner = entity);
 
-  RendDrawComp* draw = rend_draw_create(world, gridEntity, RendDrawFlags_NoInstanceFiltering);
+  RendDrawComp* draw = rend_draw_create(world, drawEntity, RendDrawFlags_NoInstanceFiltering);
   rend_draw_set_graphic(draw, asset_lookup(world, assets, string_lit("graphics/debug/grid.gra")));
   rend_draw_set_vertex_count(draw, g_gridSegments * 4);
+
+  ecs_world_add_t(
+      world, entity, DebugGridComp, .cellSize = g_gridCellSizeDefault, .draw = drawEntity);
 }
 
-ecs_view_define(GridUpdateDataView) {
-  ecs_access_read(DebugGridComp);
-  ecs_access_write(RendDrawComp);
+ecs_system_define(DebugGridCreateSys) {
+  AssetManagerComp* assets = debug_grid_asset_manager(world);
+  if (!assets) {
+    return;
+  }
+  EcsView* createView = ecs_world_view_t(world, GridCreateView);
+  for (EcsIterator* itr = ecs_view_itr(createView); ecs_view_walk(itr);) {
+    const EcsEntityId windowEntity = ecs_view_entity(itr);
+    debug_grid_create(world, windowEntity, assets);
+  }
 }
 
-ecs_system_define(DebugGridUpdateDataSys) {
-  EcsView* gridView = ecs_world_view_t(world, GridUpdateDataView);
+ecs_system_define(DebugGridDrawSys) {
+  EcsIterator* drawItr = ecs_view_itr(ecs_world_view_t(world, DrawWriteView));
+
+  EcsView* gridView = ecs_world_view_t(world, GridReadView);
   for (EcsIterator* itr = ecs_view_itr(gridView); ecs_view_walk(itr);) {
     const DebugGridComp* grid = ecs_view_read_t(itr, DebugGridComp);
-    RendDrawComp*        draw = ecs_view_write_t(itr, RendDrawComp);
+
+    ecs_view_jump(drawItr, grid->draw);
+    RendDrawComp* draw = ecs_view_write_t(drawItr, RendDrawComp);
 
     const DebugGridData data = {
         .cellSize          = grid->cellSize,
@@ -64,9 +85,11 @@ ecs_module_init(debug_grid_module) {
   ecs_register_comp(DebugGridComp);
 
   ecs_register_view(GlobalAssetsView);
-  ecs_register_view(GridView);
-  ecs_register_view(GridUpdateDataView);
+  ecs_register_view(GridCreateView);
+  ecs_register_view(GridReadView);
+  ecs_register_view(DrawWriteView);
 
-  ecs_register_system(DebugGridCreateSys, ecs_view_id(GlobalAssetsView), ecs_view_id(GridView));
-  ecs_register_system(DebugGridUpdateDataSys, ecs_view_id(GridUpdateDataView));
+  ecs_register_system(
+      DebugGridCreateSys, ecs_view_id(GlobalAssetsView), ecs_view_id(GridCreateView));
+  ecs_register_system(DebugGridDrawSys, ecs_view_id(GridReadView), ecs_view_id(DrawWriteView));
 }
