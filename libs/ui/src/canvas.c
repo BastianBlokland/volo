6ab7ba2efd7f0815dfe5367836f54aaaf862500e
Bastian Blokland <mail@bastian.tech>
2022-03-18 20:14:17 +0200

    ui: Use a single draw for all canvasses

--- libs/ui/src/canvas.c
+++ libs/ui/src/canvas.c
@@ -1,5 +1,6 @@
 #include "asset_ftx.h"
 #include "core_diag.h"
+#include "ecs_utils.h"
 #include "ecs_world.h"
 #include "gap_register.h"
 #include "gap_window.h"
@@ -12,17 +13,22 @@
 #include "resource_internal.h"
 
 #define ui_canvas_clip_rects_max 10
+#define ui_canvas_canvasses_max 100
 
 typedef struct {
   UiRect rect;
 } UiElement;
 
+ecs_comp_define(UiRendererComp) {
+  EcsEntityId draw;
+  DynArray    overlayGlyphs; // UiGlyphData[]
+};
+
 ecs_comp_define(UiCanvasComp) {
   EcsEntityId  window;
   UiCmdBuffer* cmdBuffer;
   UiId         nextId;
-  DynArray     elements;      // UiElement[]
-  DynArray     overlayGlyphs; // UiGlyphData[]
+  DynArray     elements; // UiElement[]
   UiVector     windowSize;
   UiVector     inputDelta, inputPos;
   UiId         activeId;
@@ -30,11 +36,15 @@ ecs_comp_define(UiCanvasComp) {
   TimeSteady   activeStatusStart;
 };
 
+static void ecs_destruct_renderer(void* data) {
+  UiRendererComp* comp = data;
+  dynarray_destroy(&comp->overlayGlyphs);
+}
+
 static void ecs_destruct_canvas(void* data) {
   UiCanvasComp* comp = data;
   ui_cmdbuffer_destroy(comp->cmdBuffer);
   dynarray_destroy(&comp->elements);
-  dynarray_destroy(&comp->overlayGlyphs);
 }
 
 typedef struct {
@@ -48,11 +58,13 @@ typedef struct {
 ASSERT(sizeof(UiDrawMetaData) == 176, "Size needs to match the size defined in glsl");
 
 typedef struct {
-  RendDrawComp* draw;
-  DynArray*     elementsOutput;      // UiElement[]*
-  DynArray*     overlayGlyphsOutput; // UiGlyphData[]*
-  UiRect        clipRects[ui_canvas_clip_rects_max];
-  u32           clipRectCount;
+  const AssetFtxComp*  font;
+  const GapWindowComp* window;
+  UiRendererComp*      renderer;
+  RendDrawComp*        draw;
+  UiCanvasComp*        canvas;
+  UiRect               clipRects[ui_canvas_clip_rects_max];
+  u32                  clipRectCount;
 } UiRenderState;
 
 static UiDrawMetaData ui_draw_metadata(const UiRenderState* state, const AssetFtxComp* font) {
@@ -64,7 +76,7 @@ static UiDrawMetaData ui_draw_metadata(const UiRenderState* state, const AssetFt
   return meta;
 }
 
-static const UiElement* ui_build_elem(const UiCanvasComp* canvas, const UiId id) {
+static const UiElement* ui_canvas_elem(const UiCanvasComp* canvas, const UiId id) {
   if (id >= canvas->elements.size) {
     return null;
   }
@@ -72,28 +84,28 @@ static const UiElement* ui_build_elem(const UiCanvasComp* canvas, const UiId id)
 }
 
 static u8 ui_canvas_output_clip_rect(void* userCtx, const UiRect rect) {
-  UiRenderState* renderState = userCtx;
-  diag_assert(renderState->clipRectCount < ui_canvas_clip_rects_max);
-  const u8 id                = renderState->clipRectCount++;
-  renderState->clipRects[id] = rect;
+  UiRenderState* state = userCtx;
+  diag_assert(state->clipRectCount < ui_canvas_clip_rects_max);
+  const u8 id          = state->clipRectCount++;
+  state->clipRects[id] = rect;
   return id;
 }
 
 static void ui_canvas_output_glyph(void* userCtx, const UiGlyphData data, const UiLayer layer) {
-  UiRenderState* renderState = userCtx;
+  UiRenderState* state = userCtx;
   switch (layer) {
   case UiLayer_Normal:
-    rend_draw_add_instance(renderState->draw, mem_var(data), SceneTags_None, (GeoBox){0});
+    rend_draw_add_instance(state->draw, mem_var(data), SceneTags_None, (GeoBox){0});
     break;
   case UiLayer_Overlay:
-    *dynarray_push_t(renderState->overlayGlyphsOutput, UiGlyphData) = data;
+    *dynarray_push_t(&state->renderer->overlayGlyphs, UiGlyphData) = data;
     break;
   }
 }
 
 static void ui_canvas_output_rect(void* userCtx, const UiId id, const UiRect rect) {
-  UiRenderState* renderState                                      = userCtx;
-  dynarray_at_t(renderState->elementsOutput, id, UiElement)->rect = rect;
+  UiRenderState* state                                         = userCtx;
+  dynarray_at_t(&state->canvas->elements, id, UiElement)->rect = rect;
 }
 
 static void ui_canvas_set_active(UiCanvasComp* canvas, const UiId id, const UiStatus status) {
@@ -133,57 +145,31 @@ static void ui_canvas_update_active(
       sentinel_check(result.hoveredId) ? UiStatus_Idle : UiStatus_Hovered);
 }
 
-static UiBuildResult ui_canvas_render(
-    UiCanvasComp*        canvas,
-    RendDrawComp*        draw,
-    const GapWindowComp* window,
-    const AssetFtxComp*  font) {
+static UiBuildResult ui_canvas_build(UiRenderState* state) {
 
   // Ensure we have UiElement structures for all elements that are requested to be drawn.
-  dynarray_resize(&canvas->elements, canvas->nextId);
-  mem_set(dynarray_at(&canvas->elements, 0, canvas->nextId), 0);
-
-  // Clear last frame's overlay glyphs.
-  dynarray_clear(&canvas->overlayGlyphs);
-
-  const GapVector winSize     = gap_window_param(window, GapParam_WindowSize);
-  UiRenderState   renderState = {
-      .draw                = draw,
-      .elementsOutput      = &canvas->elements,
-      .overlayGlyphsOutput = &canvas->overlayGlyphs,
-      .clipRects[0]        = {.size = {winSize.width, winSize.height}},
-      .clipRectCount       = 1,
-  };
+  dynarray_resize(&state->canvas->elements, state->canvas->nextId);
+  mem_set(dynarray_at(&state->canvas->elements, 0, state->canvas->nextId), 0);
 
   const UiBuildCtx buildCtx = {
-      .window         = window,
-      .font           = font,
-      .userCtx        = &renderState,
+      .window         = state->window,
+      .font           = state->font,
+      .userCtx        = state,
       .outputClipRect = &ui_canvas_output_clip_rect,
       .outputGlyph    = &ui_canvas_output_glyph,
       .outputRect     = &ui_canvas_output_rect,
   };
-  const UiBuildResult result = ui_build(canvas->cmdBuffer, &buildCtx);
-
-  const UiDrawMetaData meta = ui_draw_metadata(&renderState, font);
-  rend_draw_set_data(draw, mem_var(meta));
-
-  // Add the overlay glyphs, at this stage all the normal glyphs have already been added.
-  dynarray_for_t(&canvas->overlayGlyphs, UiGlyphData, glyph) {
-    rend_draw_add_instance(draw, mem_var(*glyph), SceneTags_None, (GeoBox){0});
-  }
-
-  return result;
+  return ui_build(state->canvas->cmdBuffer, &buildCtx);
 }
 
 ecs_view_define(GlobalResourcesView) { ecs_access_read(UiGlobalResourcesComp); }
 ecs_view_define(FtxView) { ecs_access_read(AssetFtxComp); }
-ecs_view_define(WindowView) { ecs_access_read(GapWindowComp); }
-
-ecs_view_define(CanvasBuildView) {
-  ecs_access_write(UiCanvasComp);
-  ecs_access_write(RendDrawComp);
+ecs_view_define(WindowView) {
+  ecs_access_read(GapWindowComp);
+  ecs_access_maybe_write(UiRendererComp);
 }
+ecs_view_define(CanvasView) { ecs_access_write(UiCanvasComp); }
+ecs_view_define(DrawView) { ecs_access_write(RendDrawComp); }
 
 static const UiGlobalResourcesComp* ui_global_resources(EcsWorld* world) {
   EcsView*     globalView = ecs_world_view_t(world, GlobalResourcesView);
@@ -200,8 +186,7 @@ ecs_system_define(UiCanvasInputSys) {
   EcsView*     windowView = ecs_world_view_t(world, WindowView);
   EcsIterator* windowItr  = ecs_view_itr(windowView);
 
-  EcsView* buildView = ecs_world_view_t(world, CanvasBuildView);
-  for (EcsIterator* itr = ecs_view_itr(buildView); ecs_view_walk(itr);) {
+  for (EcsIterator* itr = ecs_view_itr(ecs_world_view_t(world, CanvasView)); ecs_view_walk(itr);) {
     UiCanvasComp* canvas = ecs_view_write_t(itr, UiCanvasComp);
     if (!ecs_view_maybe_jump(windowItr, canvas->window)) {
       continue;
@@ -221,7 +206,38 @@ ecs_system_define(UiCanvasInputSys) {
   }
 }
 
-ecs_system_define(UiCanvasBuildSys) {
+static void ui_renderer_create(
+    EcsWorld* world, const UiGlobalResourcesComp* globalRes, const EcsEntityId window) {
+
+  const EcsEntityId drawEntity = ecs_world_entity_create(world);
+  ecs_world_add_t(world, drawEntity, SceneLifetimeOwnerComp, .owner = window);
+
+  RendDrawComp* draw = rend_draw_create(world, drawEntity, RendDrawFlags_NoInstanceFiltering);
+  rend_draw_set_camera_filter(draw, window);
+  rend_draw_set_graphic(draw, ui_resource_graphic(globalRes));
+
+  ecs_world_add_t(
+      world,
+      window,
+      UiRendererComp,
+      .draw          = drawEntity,
+      .overlayGlyphs = dynarray_create_t(g_alloc_heap, UiGlyphData, 32));
+}
+
+static u32 ui_canvass_query(
+    EcsWorld* world, const EcsEntityId window, UiCanvasComp* out[ui_canvas_canvasses_max]) {
+  u32 count = 0;
+  for (EcsIterator* itr = ecs_view_itr(ecs_world_view_t(world, CanvasView)); ecs_view_walk(itr);) {
+    UiCanvasComp* canvas = ecs_view_write_t(itr, UiCanvasComp);
+    if (canvas->window == window) {
+      diag_assert(count < ui_canvas_canvasses_max);
+      out[count++] = canvas;
+    }
+  }
+  return count;
+}
+
+ecs_system_define(UiRenderSys) {
   const UiGlobalResourcesComp* globalRes = ui_global_resources(world);
   if (!globalRes) {
     return; // Global resources not initialized yet.
@@ -230,44 +246,70 @@ ecs_system_define(UiCanvasBuildSys) {
   if (!font) {
     return; // Global font not loaded yet.
   }
-  EcsView*     windowView = ecs_world_view_t(world, WindowView);
-  EcsIterator* windowItr  = ecs_view_itr(windowView);
 
-  EcsView* buildView = ecs_world_view_t(world, CanvasBuildView);
-  for (EcsIterator* itr = ecs_view_itr(buildView); ecs_view_walk(itr);) {
-    UiCanvasComp* canvas = ecs_view_write_t(itr, UiCanvasComp);
-    RendDrawComp* draw   = ecs_view_write_t(itr, RendDrawComp);
-    if (!ecs_view_maybe_jump(windowItr, canvas->window)) {
+  for (EcsIterator* itr = ecs_view_itr(ecs_world_view_t(world, WindowView)); ecs_view_walk(itr);) {
+    const EcsEntityId    entity   = ecs_view_entity(itr);
+    const GapWindowComp* window   = ecs_view_read_t(itr, GapWindowComp);
+    UiRendererComp*      renderer = ecs_view_write_t(itr, UiRendererComp);
+    if (!renderer) {
+      ui_renderer_create(world, globalRes, entity);
       continue;
     }
-    const GapWindowComp* window = ecs_view_read_t(windowItr, GapWindowComp);
+    RendDrawComp* draw = ecs_utils_write_t(world, DrawView, renderer->draw, RendDrawComp);
 
-    rend_draw_set_graphic(draw, ui_resource_graphic(globalRes));
+    const GapVector winSize     = gap_window_param(window, GapParam_WindowSize);
+    UiRenderState   renderState = {
+        .font          = font,
+        .window        = window,
+        .renderer      = renderer,
+        .draw          = draw,
+        .clipRects[0]  = {.size = {winSize.width, winSize.height}},
+        .clipRectCount = 1,
+    };
 
-    const UiBuildResult buildResult = ui_canvas_render(canvas, draw, window, font);
-    ui_canvas_update_active(canvas, window, buildResult);
+    // Build all canvasses.
+    UiCanvasComp* canvasses[ui_canvas_canvasses_max];
+    const u32     canvasCount = ui_canvass_query(world, entity, canvasses);
+    for (u32 i = 0; i != canvasCount; ++i) {
+      renderState.canvas         = canvasses[i];
+      const UiBuildResult result = ui_canvas_build(&renderState);
+      ui_canvas_update_active(canvasses[i], window, result);
+    }
+
+    // Add the overlay glyphs, at this stage all the normal glyphs have already been added.
+    dynarray_for_t(&renderer->overlayGlyphs, UiGlyphData, glyph) {
+      rend_draw_add_instance(draw, mem_var(*glyph), SceneTags_None, (GeoBox){0});
+    }
+    dynarray_clear(&renderer->overlayGlyphs);
+
+    // Set the metadata.
+    const UiDrawMetaData meta = ui_draw_metadata(&renderState, font);
+    rend_draw_set_data(draw, mem_var(meta));
   }
 }
 
 ecs_module_init(ui_canvas_module) {
   ecs_register_comp(UiCanvasComp, .destructor = ecs_destruct_canvas);
+  ecs_register_comp(UiRendererComp, .destructor = ecs_destruct_renderer);
 
-  ecs_register_view(CanvasBuildView);
+  ecs_register_view(CanvasView);
+  ecs_register_view(DrawView);
   ecs_register_view(GlobalResourcesView);
   ecs_register_view(FtxView);
   ecs_register_view(WindowView);
 
-  ecs_register_system(UiCanvasInputSys, ecs_view_id(CanvasBuildView), ecs_view_id(WindowView));
+  ecs_register_system(UiCanvasInputSys, ecs_view_id(CanvasView), ecs_view_id(WindowView));
 
   ecs_register_system(
-      UiCanvasBuildSys,
-      ecs_view_id(CanvasBuildView),
+      UiRenderSys,
       ecs_view_id(GlobalResourcesView),
       ecs_view_id(FtxView),
-      ecs_view_id(WindowView));
+      ecs_view_id(WindowView),
+      ecs_view_id(CanvasView),
+      ecs_view_id(DrawView));
 
   ecs_order(UiCanvasInputSys, GapOrder_WindowUpdate + 1);
-  ecs_order(UiCanvasBuildSys, UiOrder_CanvasBuild);
+  ecs_order(UiRenderSys, UiOrder_Render);
 }
 
 EcsEntityId ui_canvas_create(EcsWorld* world, const EcsEntityId window) {
@@ -276,16 +318,11 @@ EcsEntityId ui_canvas_create(EcsWorld* world, const EcsEntityId window) {
       world,
       canvasEntity,
       UiCanvasComp,
-      .window        = window,
-      .cmdBuffer     = ui_cmdbuffer_create(g_alloc_heap),
-      .elements      = dynarray_create_t(g_alloc_heap, UiElement, 128),
-      .overlayGlyphs = dynarray_create_t(g_alloc_heap, UiGlyphData, 32));
+      .window    = window,
+      .cmdBuffer = ui_cmdbuffer_create(g_alloc_heap),
+      .elements  = dynarray_create_t(g_alloc_heap, UiElement, 128));
 
   ecs_world_add_t(world, canvasEntity, SceneLifetimeOwnerComp, .owner = window);
-
-  RendDrawComp* draw = rend_draw_create(world, canvasEntity, RendDrawFlags_NoInstanceFiltering);
-  rend_draw_set_camera_filter(draw, window);
-
   return canvasEntity;
 }
 
@@ -307,7 +344,7 @@ TimeDuration ui_canvas_elem_status_duration(const UiCanvasComp* comp, const UiId
 }
 
 UiRect ui_canvas_elem_rect(const UiCanvasComp* comp, const UiId id) {
-  const UiElement* elem = ui_build_elem(comp, id);
+  const UiElement* elem = ui_canvas_elem(comp, id);
   return elem ? elem->rect : ui_rect(ui_vector(0, 0), ui_vector(0, 0));
 }
 
