6b6f6ebb4c77d9f08eab00ddeed7e0f11e731071
Bastian Blokland <mail@bastian.tech>
2022-04-03 21:57:10 +0300

    gap: Add dependency to the xkbcommon-x11 library

--- cmake/modules/Findxkbcommon-x11.cmake
+++ cmake/modules/Findxkbcommon-x11.cmake
@@ -1,11 +1,11 @@
 # --------------------------------------------------------------------------------------------------
-# CMake utility to find the xkbcommon (https://xkbcommon.org) x keyboard library.
+# CMake utility to find the x11 extension for the xkbcommon (https://xkbcommon.org) library.
 # --------------------------------------------------------------------------------------------------
 
 #
 # Available for (most?) linux distributions that are using the x window-server.
-# For debian based distributions: apt install libxkbcommon-dev
+# For debian based distributions: apt install libxkbcommon-x11-dev
 # More info: https://xkbcommon.org
 #
 
-findpkg(LIB "xkbcommon" HEADER "xkbcommon/xkbcommon.h")
+findpkg(LIB "xkbcommon-x11" HEADER "xkbcommon/xkbcommon-x11.h")
