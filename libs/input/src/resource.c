cad51113efb6278bba049ed8d565e5c604fb3c68
Bastian Blokland <mail@bastian.tech>
2022-04-07 22:56:29 +0300

    input: Refactor input resources

--- libs/input/src/resource.c
+++ libs/input/src/resource.c
@@ -8,17 +8,17 @@
 static const String g_inputGlobalMap = string_static("input/global.imp");
 
 typedef enum {
-  InputGlobalRes_MapAcquired  = 1 << 0,
-  InputGlobalRes_MapUnloading = 1 << 1,
-} InputGlobalResFlags;
+  InputRes_MapAcquired  = 1 << 0,
+  InputRes_MapUnloading = 1 << 1,
+} InputResFlags;
 
-ecs_comp_define(InputGlobalResourcesComp) {
-  InputGlobalResFlags flags;
-  EcsEntityId         map;
+ecs_comp_define(InputResourcesComp) {
+  InputResFlags flags;
+  EcsEntityId   map;
 };
 
 ecs_view_define(GlobalAssetsView) { ecs_access_write(AssetManagerComp); }
-ecs_view_define(GlobalResourcesView) { ecs_access_write(InputGlobalResourcesComp); }
+ecs_view_define(GlobalResourcesView) { ecs_access_write(InputResourcesComp); }
 
 static AssetManagerComp* input_asset_manager(EcsWorld* world) {
   EcsView*     globalView = ecs_world_view_t(world, GlobalAssetsView);
@@ -26,10 +26,10 @@ static AssetManagerComp* input_asset_manager(EcsWorld* world) {
   return globalItr ? ecs_view_write_t(globalItr, AssetManagerComp) : null;
 }
 
-static InputGlobalResourcesComp* input_global_resources(EcsWorld* world) {
+static InputResourcesComp* input_resources(EcsWorld* world) {
   EcsView*     globalView = ecs_world_view_t(world, GlobalResourcesView);
   EcsIterator* globalItr  = ecs_view_maybe_at(globalView, ecs_world_global(world));
-  return globalItr ? ecs_view_write_t(globalItr, InputGlobalResourcesComp) : null;
+  return globalItr ? ecs_view_write_t(globalItr, InputResourcesComp) : null;
 }
 
 ecs_system_define(InputResourceInitSys) {
@@ -38,50 +38,50 @@ ecs_system_define(InputResourceInitSys) {
     return; // Asset manager hasn't been initialized yet.
   }
 
-  InputGlobalResourcesComp* globalResources = input_global_resources(world);
-  if (!globalResources) {
+  InputResourcesComp* resources = input_resources(world);
+  if (!resources) {
     // Initialize global resources lookup.
     ecs_world_add_t(
         world,
         ecs_world_global(world),
-        InputGlobalResourcesComp,
+        InputResourcesComp,
         .map = asset_lookup(world, assets, g_inputGlobalMap));
     return;
   }
 
-  if (!(globalResources->flags & (InputGlobalRes_MapAcquired | InputGlobalRes_MapUnloading))) {
+  if (!(resources->flags & (InputRes_MapAcquired | InputRes_MapUnloading))) {
     log_i("Acquiring global inputmap", log_param("id", fmt_text(g_inputGlobalMap)));
-    asset_acquire(world, globalResources->map);
-    globalResources->flags |= InputGlobalRes_MapAcquired;
+    asset_acquire(world, resources->map);
+    resources->flags |= InputRes_MapAcquired;
   }
 }
 
 ecs_system_define(InputResourceUnloadChangedMapSys) {
-  InputGlobalResourcesComp* globalResources = input_global_resources(world);
-  if (!globalResources) {
+  InputResourcesComp* resources = input_resources(world);
+  if (!resources) {
     return;
   }
-  const bool isLoaded   = ecs_world_has_t(world, globalResources->map, AssetLoadedComp);
-  const bool isFailed   = ecs_world_has_t(world, globalResources->map, AssetFailedComp);
-  const bool hasChanged = ecs_world_has_t(world, globalResources->map, AssetChangedComp);
+  const bool isLoaded   = ecs_world_has_t(world, resources->map, AssetLoadedComp);
+  const bool isFailed   = ecs_world_has_t(world, resources->map, AssetFailedComp);
+  const bool hasChanged = ecs_world_has_t(world, resources->map, AssetChangedComp);
 
-  if (globalResources->flags & InputGlobalRes_MapAcquired && (isLoaded || isFailed) && hasChanged) {
+  if (resources->flags & InputRes_MapAcquired && (isLoaded || isFailed) && hasChanged) {
     log_i(
         "Unloading global inputmap",
         log_param("id", fmt_text(g_inputGlobalMap)),
         log_param("reason", fmt_text_lit("Asset changed")));
 
-    asset_release(world, globalResources->map);
-    globalResources->flags &= ~InputGlobalRes_MapAcquired;
-    globalResources->flags |= InputGlobalRes_MapUnloading;
+    asset_release(world, resources->map);
+    resources->flags &= ~InputRes_MapAcquired;
+    resources->flags |= InputRes_MapUnloading;
   }
-  if (globalResources->flags & InputGlobalRes_MapUnloading && !isLoaded) {
-    globalResources->flags &= ~InputGlobalRes_MapUnloading;
+  if (resources->flags & InputRes_MapUnloading && !isLoaded) {
+    resources->flags &= ~InputRes_MapUnloading;
   }
 }
 
 ecs_module_init(input_resource_module) {
-  ecs_register_comp(InputGlobalResourcesComp);
+  ecs_register_comp(InputResourcesComp);
 
   ecs_register_view(GlobalAssetsView);
   ecs_register_view(GlobalResourcesView);
@@ -91,4 +91,4 @@ ecs_module_init(input_resource_module) {
   ecs_register_system(InputResourceUnloadChangedMapSys, ecs_view_id(GlobalResourcesView));
 }
 
-EcsEntityId input_resource_map(const InputGlobalResourcesComp* comp) { return comp->map; }
+EcsEntityId input_resource_map(const InputResourcesComp* comp) { return comp->map; }
--- libs/input/src/resource_internal.h
+++ libs/input/src/resource_internal.h
@@ -2,6 +2,6 @@
 #include "ecs_entity.h"
 #include "ecs_module.h"
 
-ecs_comp_extern(InputGlobalResourcesComp);
+ecs_comp_extern(InputResourcesComp);
 
-EcsEntityId input_resource_map(const InputGlobalResourcesComp*);
+EcsEntityId input_resource_map(const InputResourcesComp*);
