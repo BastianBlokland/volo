43f6ac827a96d1e1024b2a96d2ac570d2588ed54
Bastian Blokland <mail@bastian.tech>
2022-04-09 20:34:28 +0300

    input: Add layer concept

--- libs/input/include/input_manager.h
+++ libs/input/include/input_manager.h
@@ -3,10 +3,25 @@
 #include "ecs_entity.h"
 #include "ecs_module.h"
 
+/**
+ * Controls the cursor behaviour:
+ * - Normal:  Cursor is visible and can be moved freely.
+ * - Locked:  Cursor is hidden and kept centered, NOTE: Delta position values are still produced.
+ */
 typedef enum {
   InputCursorMode_Normal,
   InputCursorMode_Locked,
 } InputCursorMode;
+
+/**
+ * Controls which inputs are currently allowed:
+ * - Normal:    All triggered mappings are active.
+ * - TextInput: All triggered mappings are disabled.
+ */
+typedef enum {
+  InputLayer_Normal,
+  InputLayer_TextInput,
+} InputLayer;
 
 /**
  * Global input manager component.
@@ -18,6 +33,9 @@ ecs_comp_extern(InputManagerComp);
  * NOTE: Returns 0 when there is no active window.
  */
 EcsEntityId input_active_window(const InputManagerComp*);
+
+InputLayer input_layer(const InputManagerComp*);
+void       input_layer_set(InputManagerComp*, InputLayer);
 
 InputCursorMode input_cursor_mode(const InputManagerComp*);
 void            input_cursor_mode_set(InputManagerComp*, InputCursorMode);
