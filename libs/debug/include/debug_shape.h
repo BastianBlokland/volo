f93326ede71d6202de3b0c358f4884080f015225
Bastian Blokland <mail@bastian.tech>
2022-05-02 22:12:32 +0300

    debug: Add shape drawing skeleton

---
+++ libs/debug/include/debug_shape.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "ecs_entity.h"
+#include "ecs_module.h"
+#include "geo_box.h"
+#include "geo_color.h"
+
+ecs_comp_extern(DebugShapeCanvasComp);
+
+/**
+ * Add a new debug-shape canvas component to the given entity.
+ */
+DebugShapeCanvasComp* debug_shape_canvas_create(EcsWorld*, EcsEntityId entity);
+
+/**
+ * Draw primitives.
+ */
+void debug_draw_shape_box(DebugShapeCanvasComp*, GeoBox, GeoColor);
---
+++ libs/debug/src/shape.c
@@ -0,0 +1,146 @@
+#include "asset_manager.h"
+#include "core_alloc.h"
+#include "core_array.h"
+#include "core_diag.h"
+#include "core_dynarray.h"
+#include "debug_register.h"
+#include "debug_shape.h"
+#include "ecs_world.h"
+#include "rend_draw.h"
+
+typedef enum {
+  DebugShapeType_Box,
+
+  DebugShapeType_Count,
+} DebugShapeType;
+
+typedef struct {
+  GeoBox   box;
+  GeoColor color;
+} DebugShapeBox;
+
+typedef struct {
+  DebugShapeType type;
+  union {
+    DebugShapeBox data_box;
+  };
+} DebugShape;
+
+static const String g_debugGraphics[DebugShapeType_Count] = {
+    [DebugShapeType_Box] = string_static("graphics/debug/shape_box_alpha.gra"),
+};
+
+ecs_comp_define(DebugShapeRendererComp) { EcsEntityId drawEntities[DebugShapeType_Count]; };
+
+ecs_comp_define(DebugShapeCanvasComp) {
+  DynArray shapes; // DebugShape[]
+};
+
+static void ecs_destruct_canvas(void* data) {
+  DebugShapeCanvasComp* comp = data;
+  dynarray_destroy(&comp->shapes);
+}
+
+ecs_view_define(GlobalAssetsView) { ecs_access_write(AssetManagerComp); }
+ecs_view_define(ShapeRendererView) { ecs_access_write(DebugShapeRendererComp); }
+ecs_view_define(CanvasView) { ecs_access_write(DebugShapeCanvasComp); }
+ecs_view_define(DrawView) { ecs_access_write(RendDrawComp); }
+
+static AssetManagerComp* ui_asset_manager(EcsWorld* world) {
+  EcsView*     globalView = ecs_world_view_t(world, GlobalAssetsView);
+  EcsIterator* globalItr  = ecs_view_maybe_at(globalView, ecs_world_global(world));
+  return globalItr ? ecs_view_write_t(globalItr, AssetManagerComp) : null;
+}
+
+static DebugShapeRendererComp* debug_shape_renderer(EcsWorld* world) {
+  EcsView*     globalView = ecs_world_view_t(world, ShapeRendererView);
+  EcsIterator* globalItr  = ecs_view_maybe_at(globalView, ecs_world_global(world));
+  return globalItr ? ecs_view_write_t(globalItr, DebugShapeRendererComp) : null;
+}
+
+static EcsEntityId
+debug_shape_draw_create(EcsWorld* world, AssetManagerComp* assets, const DebugShapeType shape) {
+  const EcsEntityId entity        = ecs_world_entity_create(world);
+  RendDrawComp*     draw          = rend_draw_create(world, entity, RendDrawFlags_None);
+  const EcsEntityId graphicEntity = asset_lookup(world, assets, g_debugGraphics[shape]);
+  rend_draw_set_graphic(draw, graphicEntity);
+  return entity;
+}
+
+void debug_shape_renderer_create(EcsWorld* world, AssetManagerComp* assets) {
+  DebugShapeRendererComp* renderer =
+      ecs_world_add_t(world, ecs_world_global(world), DebugShapeRendererComp);
+
+  for (DebugShapeType shape = 0; shape != DebugShapeType_Count; ++shape) {
+    renderer->drawEntities[shape] = debug_shape_draw_create(world, assets, shape);
+  }
+}
+
+ecs_system_define(DebugShapeRenderSys) {
+  AssetManagerComp* assets = ui_asset_manager(world);
+  if (!assets) {
+    return; // Asset manager hasn't been initialized yet.
+  }
+
+  DebugShapeRendererComp* renderer = debug_shape_renderer(world);
+  if (!renderer) {
+    debug_shape_renderer_create(world, assets);
+    debug_shape_canvas_create(world, ecs_world_global(world)); // Global canvas for convenience.
+    return;
+  }
+
+  EcsView*     drawView = ecs_world_view_t(world, DrawView);
+  EcsIterator* drawItr  = ecs_view_itr(drawView);
+
+  for (EcsIterator* itr = ecs_view_itr(ecs_world_view_t(world, CanvasView)); ecs_view_walk(itr);) {
+    DebugShapeCanvasComp* canvas = ecs_view_write_t(itr, DebugShapeCanvasComp);
+    dynarray_for_t(&canvas->shapes, DebugShape, shape) {
+      ecs_view_jump(drawItr, renderer->drawEntities[shape->type]);
+      RendDrawComp* draw = ecs_view_write_t(drawItr, RendDrawComp);
+
+      switch (shape->type) {
+      case DebugShapeType_Box:
+        rend_draw_add_instance(draw, mem_empty, SceneTags_Debug, shape->data_box.box);
+        continue;
+      case DebugShapeType_Count:
+        break;
+      }
+      diag_crash();
+    }
+    dynarray_clear(&canvas->shapes);
+  }
+}
+
+ecs_module_init(debug_shape_module) {
+  ecs_register_comp(DebugShapeRendererComp);
+  ecs_register_comp(DebugShapeCanvasComp, .destructor = ecs_destruct_canvas);
+
+  ecs_register_view(GlobalAssetsView);
+  ecs_register_view(ShapeRendererView);
+  ecs_register_view(CanvasView);
+  ecs_register_view(DrawView);
+
+  ecs_register_system(
+      DebugShapeRenderSys,
+      ecs_view_id(GlobalAssetsView),
+      ecs_view_id(ShapeRendererView),
+      ecs_view_id(CanvasView),
+      ecs_view_id(DrawView));
+
+  ecs_order(DebugShapeRenderSys, DebugOrder_ShapeRender);
+}
+
+DebugShapeCanvasComp* debug_shape_canvas_create(EcsWorld* world, const EcsEntityId entity) {
+  return ecs_world_add_t(
+      world,
+      entity,
+      DebugShapeCanvasComp,
+      .shapes = dynarray_create_t(g_alloc_heap, DebugShape, 64));
+}
+
+void debug_draw_shape_box(DebugShapeCanvasComp* comp, const GeoBox box, const GeoColor color) {
+  *dynarray_push_t(&comp->shapes, DebugShape) = (DebugShape){
+      .type     = DebugShapeType_Box,
+      .data_box = {.box = box, .color = color},
+  };
+}
