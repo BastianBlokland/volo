6dce0ff09a7bd7fc7bcb5c71b8456bf1b05bcfb6
Bastian Blokland <mail@bastian.tech>
2022-05-07 16:03:03 +0300

    ui: Add extension shape

--- libs/ui/include/ui_shape.h
+++ libs/ui/include/ui_shape.h
@@ -4,35 +4,36 @@
 
 #define UI_SHAPES                                                                                  \
   X(0x0020, Empty)                                                                                 \
+  X(0xE069, WebAsset)                                                                              \
+  X(0xE1DB, Storage)                                                                               \
+  X(0xE25E, FormatShapes)                                                                          \
+  X(0xE312, Keyboard)                                                                              \
+  X(0xE338, VideogameAsset)                                                                        \
+  X(0xE3AE, Brush)                                                                                 \
+  X(0xE412, PhotoCamera)                                                                           \
+  X(0xE4FC, QueryStats)                                                                            \
+  X(0xE53B, Layers)                                                                                \
+  X(0xE53C, LayersClear)                                                                           \
+  X(0xE5CA, Check)                                                                                 \
+  X(0xE5CD, Close)                                                                                 \
+  X(0xE5CE, ExpandLess)                                                                            \
+  X(0xE5CF, ExpandMore)                                                                            \
+  X(0xE5D0, Fullscreen)                                                                            \
+  X(0xE5D1, FullscreenExit)                                                                        \
+  X(0xE5D6, UnfoldLess)                                                                            \
+  X(0xE5D7, UnfoldMore)                                                                            \
+  X(0xE80E, Whatshot)                                                                              \
+  X(0xE87B, Extension)                                                                             \
+  X(0xE89E, OpenInNew)                                                                             \
+  X(0xE9BA, Logout)                                                                                \
+  X(0xE9E4, Speed)                                                                                 \
+  X(0xE9FE, ViewInAr)                                                                              \
+  X(0xEA5F, Calculate)                                                                             \
+  X(0xEF5B, Monitor)                                                                               \
   X(0xF000, Square)                                                                                \
   X(0xF001, Circle)                                                                                \
   X(0xF002, CursorVertialBar)                                                                      \
-  X(0xE5CA, Check)                                                                                 \
-  X(0xE5CF, ExpandMore)                                                                            \
-  X(0xE5CE, ExpandLess)                                                                            \
-  X(0xE5D7, UnfoldMore)                                                                            \
-  X(0xE5D6, UnfoldLess)                                                                            \
-  X(0xE9E4, Speed)                                                                                 \
-  X(0xE9BA, Logout)                                                                                \
-  X(0xE5D0, Fullscreen)                                                                            \
-  X(0xE5D1, FullscreenExit)                                                                        \
-  X(0xE89E, OpenInNew)                                                                             \
-  X(0xE53B, Layers)                                                                                \
-  X(0xE53C, LayersClear)                                                                           \
-  X(0xEF5B, Monitor)                                                                               \
-  X(0xE3AE, Brush)                                                                                 \
-  X(0xE1DB, Storage)                                                                               \
-  X(0xEA5F, Calculate)                                                                             \
-  X(0xE069, WebAsset)                                                                              \
-  X(0xE338, VideogameAsset)                                                                        \
-  X(0xE4FC, QueryStats)                                                                            \
-  X(0xE80E, Whatshot)                                                                              \
-  X(0xE312, Keyboard)                                                                              \
-  X(0xE5CD, Close)                                                                                 \
-  X(0xF016, Grid4x4)                                                                               \
-  X(0xE412, PhotoCamera)                                                                           \
-  X(0xE25E, FormatShapes)                                                                          \
-  X(0xE9FE, ViewInAr)
+  X(0xF016, Grid4x4)
 
 enum {
 #define X(_UNICODE_, _NAME_) UiShape_##_NAME_ = _UNICODE_,
