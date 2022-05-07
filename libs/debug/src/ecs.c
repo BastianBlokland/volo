1aba6220b6eb2b68a70ff311f8ebd9a4455e880d
Bastian Blokland <mail@bastian.tech>
2022-05-07 16:50:17 +0300

    debug: Add component listing

--- libs/debug/src/ecs.c
+++ libs/debug/src/ecs.c
@@ -1,21 +1,82 @@
+#include "core_alloc.h"
 #include "debug_ecs.h"
 #include "debug_register.h"
 #include "ecs_world.h"
 #include "ui.h"
 
+typedef struct {
+  EcsCompId id;
+  String    name;
+  u32       size, align;
+} DebugEcsCompInfo;
+
 ecs_comp_define(DebugEcsPanelComp) {
-  UiPanel     panel;
-  EcsEntityId window;
+  UiPanel      panel;
+  UiScrollview scrollview;
+  DynArray     components; // DebugEcsCompInfo[]
 };
+
+static void ecs_destruct_ecs_panel(void* data) {
+  DebugEcsPanelComp* comp = data;
+  dynarray_destroy(&comp->components);
+}
 
 ecs_view_define(PanelUpdateView) {
   ecs_access_write(DebugEcsPanelComp);
   ecs_access_write(UiCanvasComp);
 }
 
+static void comp_info_query(DebugEcsPanelComp* panelComp, EcsWorld* world) {
+  dynarray_clear(&panelComp->components);
+
+  const EcsDef* def = ecs_world_def(world);
+  for (EcsCompId id = 0; id != ecs_def_comp_count(def); ++id) {
+    *dynarray_push_t(&panelComp->components, DebugEcsCompInfo) = (DebugEcsCompInfo){
+        .id    = id,
+        .name  = ecs_def_comp_name(def, id),
+        .size  = (u32)ecs_def_comp_size(def, id),
+        .align = (u32)ecs_def_comp_align(def, id),
+    };
+  }
+}
+
 static void physics_panel_draw(UiCanvasComp* canvas, DebugEcsPanelComp* panelComp) {
   const String title = fmt_write_scratch("{} Ecs Debug", fmt_ui_shape(Extension));
   ui_panel_begin(canvas, &panelComp->panel, .title = title);
+
+  UiTable table = ui_table(.spacing = ui_vector(10, 5));
+  ui_table_add_column(&table, UiTableColumn_Fixed, 50);
+  ui_table_add_column(&table, UiTableColumn_Fixed, 300);
+  ui_table_add_column(&table, UiTableColumn_Fixed, 50);
+  ui_table_add_column(&table, UiTableColumn_Fixed, 50);
+
+  ui_table_draw_header(
+      canvas,
+      &table,
+      (const UiTableColumnName[]){
+          {string_lit("Id"), string_lit("Component identifier.")},
+          {string_lit("Name"), string_lit("Component name.")},
+          {string_lit("Size"), string_lit("Component size (in bytes).")},
+          {string_lit("Align"), string_lit("Component required minimum alignment (in bytes).")},
+      });
+
+  const u32 numComps = (u32)panelComp->components.size;
+  ui_scrollview_begin(canvas, &panelComp->scrollview, ui_table_height(&table, numComps));
+
+  dynarray_for_t(&panelComp->components, DebugEcsCompInfo, compInfo) {
+    ui_table_next_row(canvas, &table);
+    ui_table_draw_row_bg(canvas, &table, ui_color(48, 48, 48, 192));
+
+    ui_label(canvas, fmt_write_scratch("{}", fmt_int(compInfo->id)));
+    ui_table_next_column(canvas, &table);
+    ui_label(canvas, compInfo->name, .selectable = true);
+    ui_table_next_column(canvas, &table);
+    ui_label(canvas, fmt_write_scratch("{}", fmt_int(compInfo->size)));
+    ui_table_next_column(canvas, &table);
+    ui_label(canvas, fmt_write_scratch("{}", fmt_int(compInfo->align)));
+  }
+
+  ui_scrollview_end(canvas, &panelComp->scrollview);
   ui_panel_end(canvas, &panelComp->panel);
 }
 
@@ -25,6 +86,8 @@ ecs_system_define(DebugPhysicsUpdatePanelSys) {
     const EcsEntityId  entity    = ecs_view_entity(itr);
     DebugEcsPanelComp* panelComp = ecs_view_write_t(itr, DebugEcsPanelComp);
     UiCanvasComp*      canvas    = ecs_view_write_t(itr, UiCanvasComp);
+
+    comp_info_query(panelComp, world);
 
     ui_canvas_reset(canvas);
     physics_panel_draw(canvas, panelComp);
@@ -39,7 +102,7 @@ ecs_system_define(DebugPhysicsUpdatePanelSys) {
 }
 
 ecs_module_init(debug_ecs_module) {
-  ecs_register_comp(DebugEcsPanelComp);
+  ecs_register_comp(DebugEcsPanelComp, .destructor = ecs_destruct_ecs_panel);
 
   ecs_register_view(PanelUpdateView);
 
@@ -52,7 +115,7 @@ EcsEntityId debug_ecs_panel_open(EcsWorld* world, const EcsEntityId window) {
       world,
       panelEntity,
       DebugEcsPanelComp,
-      .panel  = ui_panel(ui_vector(330, 255)),
-      .window = window);
+      .panel      = ui_panel(ui_vector(800, 500)),
+      .components = dynarray_create_t(g_alloc_heap, DebugEcsCompInfo, 256));
   return panelEntity;
 }
