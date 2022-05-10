d7cc127bdba9f916532850e76fea6896bb2d35b3
Bastian Blokland <mail@bastian.tech>
2022-05-10 20:26:30 +0300

    rend: Add api to query draw information

--- libs/rend/src/draw_internal.h
+++ libs/rend/src/draw_internal.h
@@ -4,6 +4,5 @@
 #include "rvk/pass_internal.h"
 #include "view_internal.h"
 
-EcsEntityId rend_draw_graphic(const RendDrawComp*);
 bool        rend_draw_gather(RendDrawComp*, const RendView*, const RendSettingsComp*);
 RvkPassDraw rend_draw_output(const RendDrawComp*, RvkGraphic* graphic);
