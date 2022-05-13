f9745a145e068095b0c5f6cc4953da6f2f4cd910
Bastian Blokland <bastianblokland@gmail.com>
2022-05-13 23:01:44 +0300

    geo: Add doc comments to vec load / store

--- libs/geo/src/simd_sse_internal.h
+++ libs/geo/src/simd_sse_internal.h
@@ -5,10 +5,18 @@
 
 typedef __m128 SimdVec;
 
+/**
+ * Load 4 (128 bit aligned) float values into a Simd vector.
+ * Pre-condition: bits_aligned_ptr(values, 16)
+ */
 INLINE_HINT static SimdVec simd_vec_load(const f32 values[4]) { return _mm_load_ps(values); }
 
+/**
+ * Store a Simd vector to 4 (128 bit aligned) float values.
+ * Pre-condition: bits_aligned_ptr(values, 16)
+ */
 INLINE_HINT static void simd_vec_store(const SimdVec vec, f32 values[4]) {
-  return _mm_store_ps(values, vec);
+  _mm_store_ps(values, vec);
 }
 
 INLINE_HINT static SimdVec simd_vec_add(const SimdVec a, const SimdVec b) {
