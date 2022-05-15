27009d4ff48f600b46736a5970caed3ea28754e5
Bastian Blokland <bastianblokland@gmail.com>
2022-05-15 08:50:49 +0300

    core: Move math intrinsics to shared header

--- libs/core/src/intrinsic_internal.h
+++ libs/core/src/intrinsic_internal.h
@@ -3,14 +3,72 @@
 
 #if defined(VOLO_MSVC)
 #include <intrin.h>
+#include <math.h>
 #pragma intrinsic(_BitScanForward)
-#pragma intrinsic(_BitScanReverse)
 #pragma intrinsic(_BitScanForward64)
+#pragma intrinsic(_BitScanReverse)
 #pragma intrinsic(_BitScanReverse64)
+#pragma intrinsic(acosf)
+#pragma intrinsic(asinf)
+#pragma intrinsic(atan2f)
+#pragma intrinsic(atanf)
+#pragma intrinsic(ceil)
+#pragma intrinsic(ceilf)
+#pragma intrinsic(cosf)
+#pragma intrinsic(floor)
+#pragma intrinsic(floorf)
+#pragma intrinsic(fmodf)
+#pragma intrinsic(logf)
+#pragma intrinsic(powf)
+#pragma intrinsic(sinf)
+#pragma intrinsic(sqrt)
+#pragma intrinsic(sqrtf)
+#pragma intrinsic(tanf)
+
+#define intrinsic_acosf acosf
+#define intrinsic_asinf asinf
+#define intrinsic_atan2f atan2f
+#define intrinsic_atanf atanf
+#define intrinsic_ceil ceil
+#define intrinsic_ceilf ceilf
+#define intrinsic_cosf cosf
+#define intrinsic_floor floor
+#define intrinsic_floorf floorf
+#define intrinsic_fmodf fmodf
+#define intrinsic_logf logf
+#define intrinsic_powf powf
+#define intrinsic_round round
+#define intrinsic_roundf roundf
+#define intrinsic_sinf sinf
+#define intrinsic_sqrt sqrt
+#define intrinsic_sqrtf sqrtf
+#define intrinsic_tanf tanf
+
+#else
+
+#define intrinsic_acosf __builtin_acosf
+#define intrinsic_asinf __builtin_asinf
+#define intrinsic_atan2f __builtin_atan2f
+#define intrinsic_atanf __builtin_atanf
+#define intrinsic_ceil __builtin_ceil
+#define intrinsic_ceilf __builtin_ceilf
+#define intrinsic_cosf __builtin_cosf
+#define intrinsic_floor __builtin_floor
+#define intrinsic_floorf __builtin_floorf
+#define intrinsic_fmodf __builtin_fmodf
+#define intrinsic_logf __builtin_logf
+#define intrinsic_powf __builtin_powf
+#define intrinsic_round __builtin_round
+#define intrinsic_roundf __builtin_roundf
+#define intrinsic_sinf __builtin_sinf
+#define intrinsic_sqrt __builtin_sqrt
+#define intrinsic_sqrtf __builtin_sqrtf
+#define intrinsic_tanf __builtin_tanf
+
 #endif
 
-#define intrinsic_popcnt_32(_MASK_) _mm_popcnt_u32(_MASK_)
-#define intrinsic_popcnt_64(_MASK_) _mm_popcnt_u64(_MASK_)
+#define intrinsic_popcnt_32 _mm_popcnt_u32
+#define intrinsic_popcnt_64 _mm_popcnt_u64
 
 /**
  * Pre-condition: mask != 0.
