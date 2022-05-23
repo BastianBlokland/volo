57755509c7faca8036f377dde74f16a009fa5df1
Bastian Blokland <mail@bastian.tech>
2022-05-23 21:08:45 +0300

    asset: Parse gltf json file

--- libs/asset/src/loader_mesh_gltf.c
+++ libs/asset/src/loader_mesh_gltf.c
@@ -1,7 +1,10 @@
 #include "asset_mesh.h"
 #include "core_alloc.h"
+#include "core_array.h"
 #include "core_diag.h"
 #include "ecs_world.h"
+#include "json_read.h"
+#include "log_logger.h"
 
 #include "mesh_utils_internal.h"
 #include "repo_internal.h"
@@ -11,11 +14,50 @@
  * Format specification: https://www.khronos.org/registry/glTF/specs/2.0/glTF-2.0.html
  */
 
+typedef enum {
+  GltfError_None = 0,
+  GltfError_InvalidJson,
+
+  GltfError_Count,
+} GltfError;
+
+static String gltf_error_str(const GltfError err) {
+  static const String g_msgs[] = {
+      string_static("None"),
+      string_static("Invalid json"),
+  };
+  ASSERT(array_elems(g_msgs) == GltfError_Count, "Incorrect number of gltf-error messages");
+  return g_msgs[err];
+}
+
+static void gltf_load_fail_msg(
+    EcsWorld* world, const EcsEntityId entity, const GltfError err, const String msg) {
+  log_e(
+      "Failed to parse gltf mesh",
+      log_param("code", fmt_int(err)),
+      log_param("error", fmt_text(msg)));
+  ecs_world_add_empty_t(world, entity, AssetFailedComp);
+}
+
+static void gltf_load_fail(EcsWorld* world, const EcsEntityId entity, const GltfError err) {
+  gltf_load_fail_msg(world, entity, err, gltf_error_str(err));
+}
+
 ecs_module_init(asset_gltf_module) {}
 
 void asset_load_gltf(EcsWorld* world, const String id, const EcsEntityId entity, AssetSource* src) {
-  (void)world;
   (void)id;
-  (void)entity;
-  (void)src;
+
+  JsonDoc*   doc = json_create(g_alloc_heap, 512);
+  JsonResult jsonRes;
+  json_read(doc, src->data, &jsonRes);
+  asset_repo_source_close(src);
+
+  if (jsonRes.type != JsonResultType_Success) {
+    gltf_load_fail_msg(world, entity, GltfError_InvalidJson, json_error_str(jsonRes.error));
+    json_destroy(doc);
+    return;
+  }
+
+  (void)gltf_load_fail;
 }
