1a6af1ed057187c1f4313bea896e2c7f320f0b02
Bastian Blokland <mail@bastian.tech>
2022-06-01 20:07:50 +0300

    shaders: Add skinned instance struct

--- assets/shaders/include/instance.glsl
+++ assets/shaders/include/instance.glsl
@@ -4,10 +4,17 @@
 #include "types.glsl"
 
 const u32 c_maxInstances = 2048;
+const u32 c_maxJoints    = 16; // Needs to match the maximum in asset_mesh.h
 
 struct InstanceData {
   f32v4 posAndScale; // x, y, z position, w scale
   f32v4 rot;         // x, y, z, w rotation quaternion
+};
+
+struct InstanceSkinnedData {
+  f32v4 posAndScale;                  // x, y, z position, w scale
+  f32v4 rot;                          // x, y, z, w rotation quaternion
+  f32m4 jointTransforms[c_maxJoints]; // Transformation matrix per joint.
 };
 
 #endif // INCLUDE_INSTANCE
