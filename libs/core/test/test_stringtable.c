af7ea0aa53e61bc376bb117a02675847726dfc87
Bastian Blokland <mail@bastian.tech>
2022-06-19 18:19:47 +0300

    core: Test many strings in StringTable

--- libs/core/test/test_stringtable.c
+++ libs/core/test/test_stringtable.c
@@ -15,5 +15,22 @@ spec(stringtable) {
     check_eq_string(stringtable_lookup(table, hash), str);
   }
 
+  it("can store many strings") {
+    const u32 count = 64;
+
+    // Add all strings.
+    for (u32 i = 0; i != count; ++i) {
+      const String str = fmt_write_scratch("My String {}", fmt_int(i));
+      stringtable_add(table, str);
+    }
+
+    // Retrieve all strings.
+    for (u32 i = 0; i != count; ++i) {
+      const String     str  = fmt_write_scratch("My String {}", fmt_int(i));
+      const StringHash hash = string_hash(str);
+      check_eq_string(stringtable_lookup(table, hash), str);
+    }
+  }
+
   teardown() { stringtable_destroy(table); }
 }
