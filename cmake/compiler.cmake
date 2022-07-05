bfabb4ba32f8d30814fa339c7bced98e89fd817e
Bastian Blokland <mail@bastian.tech>
2022-07-05 10:40:46 +0300

    cmake: Fix typo

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -79,7 +79,7 @@ macro(set_gcc_compile_options)
   # Optimization settings.
   add_compile_options(-O3)
   # add_compile_options(-ffast-math) # Enable (potentially lossy) floating point optimizations.
-  add_compile_options(-mf16c) # Enable output of f16c (f32 <-> f16 converions)
+  add_compile_options(-mf16c) # Enable output of f16c (f32 <-> f16 conversions)
   # add_compile_options(-mfma) # Enable output of 'fused multiply-add' instructions.
 
   # Debug options.
@@ -110,7 +110,7 @@ macro(set_clang_compile_options)
   # Optimization settings.
   add_compile_options(-O3)
   # add_compile_options(-ffast-math) # Enable (potentially lossy) floating point optimizations.
-  add_compile_options(-mf16c) # Enable output of f16c (f32 <-> f16 converions)
+  add_compile_options(-mf16c) # Enable output of f16c (f32 <-> f16 conversions)
   # add_compile_options(-mfma) # Enable output of 'fused multiply-add' instructions.
   # add_compile_options(-flto=full) # Enable link-time optimization.
   # add_link_options(-fuse-ld=lld -flto=full) # Enable link-time optimization.
