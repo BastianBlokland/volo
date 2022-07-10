283259f9867b3d3a080ccf1ebb81f2189cde3b3e
Bastian Blokland <mail@bastian.tech>
2022-07-10 17:26:25 +0300

    geo: Add insert api's to query environment

--- libs/geo/include/geo_query.h
+++ libs/geo/include/geo_query.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "geo_capsule.h"
+#include "geo_sphere.h"
 
 // Forward declare from 'core_alloc.h'.
 typedef struct sAllocator Allocator;
@@ -6,15 +8,26 @@ typedef struct sAllocator Allocator;
 /**
  * Environment for querying against.
  */
-typedef struct sQueryEnv QueryEnv;
+typedef struct sGeoQueryEnv GeoQueryEnv;
 
 /**
- * Create a new QueryEnv instance.
+ * Create a new GeoQueryEnv instance.
  * Destroy using 'geo_query_env_destroy()'.
  */
-QueryEnv* geo_query_env_create(Allocator*);
+GeoQueryEnv* geo_query_env_create(Allocator*);
 
 /**
- * Destroy a QueryEnv instance.
+ * Destroy a GeoQueryEnv instance.
  */
-void geo_query_env_destroy(QueryEnv*);
+void geo_query_env_destroy(GeoQueryEnv*);
+
+/**
+ * Clear all shapes from the environment.
+ */
+void geo_query_env_clear(GeoQueryEnv*);
+
+/**
+ * Insert geometric shapes into the environment.
+ */
+void geo_query_insert_sphere(GeoQueryEnv*, GeoSphere, u64 id);
+void geo_query_insert_capsule(GeoQueryEnv*, GeoCapsule, u64 id);
--- libs/geo/src/query.c
+++ libs/geo/src/query.c
@@ -1,17 +1,49 @@
 #include "core_alloc.h"
+#include "core_dynarray.h"
 #include "geo_query.h"
 
-struct sQueryEnv {
+struct sGeoQueryEnv {
   Allocator* alloc;
+  DynArray   spheres;    // GeoSphere[].
+  DynArray   sphereIds;  // u64[].
+  DynArray   capsules;   // GeoCapsule[].
+  DynArray   capsuleIds; // u64[].
 };
 
-QueryEnv* geo_query_env_create(Allocator* alloc) {
-  QueryEnv* env = alloc_alloc_t(alloc, QueryEnv);
+GeoQueryEnv* geo_query_env_create(Allocator* alloc) {
+  GeoQueryEnv* env = alloc_alloc_t(alloc, GeoQueryEnv);
 
-  *env = (QueryEnv){
-      .alloc = alloc,
+  *env = (GeoQueryEnv){
+      .alloc      = alloc,
+      .spheres    = dynarray_create_t(alloc, GeoSphere, 512),
+      .sphereIds  = dynarray_create_t(alloc, u64, 512),
+      .capsules   = dynarray_create_t(alloc, GeoCapsule, 512),
+      .capsuleIds = dynarray_create_t(alloc, u64, 512),
   };
   return env;
 }
 
-void geo_query_env_destroy(QueryEnv* env) { alloc_free_t(env->alloc, env); }
+void geo_query_env_destroy(GeoQueryEnv* env) {
+  dynarray_destroy(&env->spheres);
+  dynarray_destroy(&env->sphereIds);
+  dynarray_destroy(&env->capsules);
+  dynarray_destroy(&env->capsuleIds);
+  alloc_free_t(env->alloc, env);
+}
+
+void geo_query_env_clear(GeoQueryEnv* env) {
+  dynarray_clear(&env->spheres);
+  dynarray_clear(&env->sphereIds);
+  dynarray_clear(&env->capsules);
+  dynarray_clear(&env->capsuleIds);
+}
+
+void geo_query_insert_sphere(GeoQueryEnv* env, const GeoSphere sphere, const u64 id) {
+  *dynarray_push_t(&env->spheres, GeoSphere) = sphere;
+  *dynarray_push_t(&env->sphereIds, u64)     = id;
+}
+
+void geo_query_insert_capsule(GeoQueryEnv* env, const GeoCapsule capsule, const u64 id) {
+  *dynarray_push_t(&env->capsules, GeoCapsule) = capsule;
+  *dynarray_push_t(&env->capsuleIds, u64)      = id;
+}
