656911104fee9d2690b5fa7e7fb3b900e794eb51
Bastian Blokland <mail@bastian.tech>
2022-07-17 17:15:28 +0300

    scene: Add api to query tag name

--- libs/scene/include/scene_tag.h
+++ libs/scene/include/scene_tag.h
@@ -9,6 +9,7 @@ typedef enum {
   SceneTags_Debug      = 1 << 2,
   SceneTags_Selected   = 1 << 3,
 
+  SceneTags_Count   = 4,
   SceneTags_Default = SceneTags_Geometry,
 } SceneTags;
 
@@ -17,6 +18,12 @@ typedef struct {
 } SceneTagFilter;
 
 ecs_comp_extern_public(SceneTagComp) { SceneTags tags; };
+
+/**
+ * Lookup the name of the given tag.
+ * Pre-condition: Only a single bit is set.
+ */
+String scene_tag_name(SceneTags);
 
 void scene_tag_add(EcsWorld* world, EcsEntityId, SceneTags);
 
