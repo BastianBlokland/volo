511b9bbbbf1e8255a8df4dc9d80eefcbe4495105
Bastian Blokland <mail@bastian.tech>
2022-07-21 10:53:15 +0300

    scripts: Cleanup in run-win32.bat script (#278)

--- scripts/run-win32.bat
+++ scripts/run-win32.bat
@@ -1,5 +1,6 @@
-@ECHO OFF
+:: Convenience wrapper around around the run-win32.ps1 powershell script.
+@echo off
 setlocal
 cd /d %~dp0
-powershell.exe -ExecutionPolicy Bypass -NoLogo -NoProfile .\run-win32.ps1
-PAUSE
+powershell.exe -ExecutionPolicy Bypass -NoLogo -NoProfile .\run-win32.ps1 %*
+pause
