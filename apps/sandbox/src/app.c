9f8c9f8200cef009341d605271830529e5395a40
Bastian Blokland <mail@bastian.tech>
2022-07-23 17:05:27 +0300

    sandbox: Add new window input

--- apps/sandbox/src/app.c
+++ apps/sandbox/src/app.c
@@ -15,9 +15,6 @@
 
 static const GapVector g_windowSize = {1920, 1080};
 
-ecs_view_define(WindowExistenceView) { ecs_access_with(GapWindowComp); }
-ecs_module_init(sandbox_app_module) { ecs_register_view(WindowExistenceView); }
-
 static void app_window_create(EcsWorld* world) {
   const EcsEntityId window = gap_window_create(world, GapWindowFlags_Default, g_windowSize);
   debug_menu_create(world, window);
@@ -36,6 +33,29 @@ static void app_window_create(EcsWorld* world) {
       SceneTransformComp,
       .position = {0, 1.2f, -3.5f},
       .rotation = geo_quat_angle_axis(geo_right, 5 * math_deg_to_rad));
+}
+
+ecs_view_define(GlobalUpdateView) { ecs_access_read(InputManagerComp); }
+ecs_view_define(WindowExistenceView) { ecs_access_with(GapWindowComp); }
+
+ecs_system_define(AppUpdateSys) {
+  EcsView*     globalView = ecs_world_view_t(world, GlobalUpdateView);
+  EcsIterator* globalItr  = ecs_view_maybe_at(globalView, ecs_world_global(world));
+  if (!globalItr) {
+    return;
+  }
+  const InputManagerComp* input = ecs_view_read_t(globalItr, InputManagerComp);
+
+  if (input_triggered_lit(input, "SandboxNewWindow")) {
+    app_window_create(world);
+  }
+}
+
+ecs_module_init(sandbox_app_module) {
+  ecs_register_view(GlobalUpdateView);
+  ecs_register_view(WindowExistenceView);
+
+  ecs_register_system(AppUpdateSys, ecs_view_id(GlobalUpdateView));
 }
 
 static CliId g_assetFlag;
