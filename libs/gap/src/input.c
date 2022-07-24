0971a19a1bf4f7ab808b8e640420a387c8b9e070
Bastian Blokland <mail@bastian.tech>
2022-07-24 11:57:52 +0300

    gap: Track the pre-fullscreen window size

--- libs/gap/src/input.c
+++ libs/gap/src/input.c
@@ -39,6 +39,7 @@ String gap_key_str(const GapKey key) {
 
 static const String g_paramStrs[] = {
     string_static("window-size"),
+    string_static("window-size-pre-fullscreen"),
     string_static("cursor-pos"),
     string_static("cursor-delta"),
     string_static("scroll-delta"),
