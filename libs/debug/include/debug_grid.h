740db11268a2d91597e3493af87c448ec63cc584
Bastian Blokland <mail@bastian.tech>
2022-07-26 22:49:53 +0300

    debug: Translation gizmo grid snapping support (#293)
    * debug: Add grid snap api
    
    * debug: Add snap feature to translation gizmo

--- libs/debug/include/debug_grid.h
+++ libs/debug/include/debug_grid.h
@@ -1,5 +1,19 @@
 #pragma once
 #include "ecs_entity.h"
 #include "ecs_module.h"
+#include "geo_vector.h"
+
+/**
+ * Debug grid.
+ * NOTE: Initialized on each window entity.
+ */
+ecs_comp_extern(DebugGridComp);
+
+/**
+ * Snap the given position to the grid.
+ * Output is written back to the position pointer..
+ */
+void debug_grid_snap(const DebugGridComp*, GeoVector* position);
+void debug_grid_snap_axis(const DebugGridComp*, GeoVector* position, u8 axis);
 
 EcsEntityId debug_grid_panel_open(EcsWorld*, EcsEntityId window);
