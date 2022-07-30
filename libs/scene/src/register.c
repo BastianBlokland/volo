fcf7dcbacc4b8272dec0ac3351df9209442f46a1
Bastian Blokland <mail@bastian.tech>
2022-07-30 16:01:37 +0300

    scene: Remove velocity module

--- libs/scene/src/register.c
+++ libs/scene/src/register.c
@@ -14,5 +14,4 @@ void scene_register(EcsDef* def) {
   ecs_register_module(def, scene_tag_module);
   ecs_register_module(def, scene_time_module);
   ecs_register_module(def, scene_transform_module);
-  ecs_register_module(def, scene_velocity_module);
 }
