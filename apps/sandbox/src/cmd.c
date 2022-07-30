1d56bf2986d4784f94e7d530f08356e667389e37
Bastian Blokland <mail@bastian.tech>
2022-07-30 14:58:22 +0300

    sandbox: Implement move command

--- apps/sandbox/src/cmd.c
+++ apps/sandbox/src/cmd.c
@@ -2,6 +2,7 @@
 #include "core_diag.h"
 #include "core_dynarray.h"
 #include "ecs_world.h"
+#include "scene_locomotion.h"
 #include "scene_selection.h"
 
 #include "cmd_internal.h"
@@ -10,8 +11,9 @@
 typedef enum {
   Cmd_Select,
   Cmd_Deselect,
-  Cmd_Destroy,
+  Cmd_Move,
   Cmd_SpawnUnit,
+  Cmd_Destroy,
 } CmdType;
 
 typedef struct {
@@ -20,18 +22,24 @@ typedef struct {
 
 typedef struct {
   EcsEntityId object;
-} CmdDestroy;
+  GeoVector   position;
+} CmdMove;
 
 typedef struct {
   GeoVector position;
 } CmdSpawnUnit;
 
 typedef struct {
+  EcsEntityId object;
+} CmdDestroy;
+
+typedef struct {
   CmdType type;
   union {
     CmdSelect    select;
-    CmdDestroy   destroy;
+    CmdMove      move;
     CmdSpawnUnit spawnUnit;
+    CmdDestroy   destroy;
   };
 } Cmd;
 
@@ -51,10 +59,19 @@ ecs_view_define(GlobalUpdateView) {
   ecs_access_write(SceneSelectionComp);
 }
 
+ecs_view_define(MoveView) { ecs_access_write(SceneLocomotionComp); }
+
 static CmdControllerComp* cmd_controller_get(EcsWorld* world) {
   EcsView*     view = ecs_world_view_t(world, ControllerWriteView);
   EcsIterator* itr  = ecs_view_maybe_at(view, ecs_world_global(world));
   return itr ? ecs_view_write_t(itr, CmdControllerComp) : null;
+}
+
+static void cmd_execute_move(EcsWorld* world, const CmdMove* cmdMove) {
+  EcsIterator* moveItr = ecs_view_maybe_at(ecs_world_view_t(world, MoveView), cmdMove->object);
+  if (moveItr) {
+    ecs_view_write_t(moveItr, SceneLocomotionComp)->target = cmdMove->position;
+  }
 }
 
 static void cmd_execute(
@@ -70,12 +87,15 @@ static void cmd_execute(
   case Cmd_Deselect:
     scene_deselect(selection);
     break;
-  case Cmd_Destroy:
-    diag_assert_msg(ecs_world_exists(world, cmd->destroy.object), "Destroying non-existing obj");
-    ecs_world_entity_destroy(world, cmd->destroy.object);
+  case Cmd_Move:
+    cmd_execute_move(world, &cmd->move);
     break;
   case Cmd_SpawnUnit:
     unit_spawn(world, unitDb, cmd->spawnUnit.position);
+    break;
+  case Cmd_Destroy:
+    diag_assert_msg(ecs_world_exists(world, cmd->destroy.object), "Destroying non-existing obj");
+    ecs_world_entity_destroy(world, cmd->destroy.object);
     break;
   }
 }
@@ -106,9 +126,13 @@ ecs_module_init(sandbox_cmd_module) {
 
   ecs_register_view(ControllerWriteView);
   ecs_register_view(GlobalUpdateView);
+  ecs_register_view(MoveView);
 
   ecs_register_system(
-      CmdControllerUpdateSys, ecs_view_id(GlobalUpdateView), ecs_view_id(ControllerWriteView));
+      CmdControllerUpdateSys,
+      ecs_view_id(GlobalUpdateView),
+      ecs_view_id(ControllerWriteView),
+      ecs_view_id(MoveView));
 
   ecs_order(CmdControllerUpdateSys, AppOrder_CommandUpdate);
 }
@@ -126,16 +150,24 @@ void cmd_push_deselect(CmdControllerComp* controller) {
   };
 }
 
+void cmd_push_move(
+    CmdControllerComp* controller, const EcsEntityId object, const GeoVector position) {
+  *dynarray_push_t(&controller->commands, Cmd) = (Cmd){
+      .type = Cmd_Move,
+      .move = {.object = object, .position = position},
+  };
+}
+
+void cmd_push_spawn_unit(CmdControllerComp* controller, const GeoVector position) {
+  *dynarray_push_t(&controller->commands, Cmd) = (Cmd){
+      .type      = Cmd_SpawnUnit,
+      .spawnUnit = {.position = position},
+  };
+}
+
 void cmd_push_destroy(CmdControllerComp* controller, const EcsEntityId object) {
   *dynarray_push_t(&controller->commands, Cmd) = (Cmd){
       .type    = Cmd_Destroy,
       .destroy = {.object = object},
-  };
-}
-
-void cmd_push_spawn_unit(CmdControllerComp* controller, GeoVector position) {
-  *dynarray_push_t(&controller->commands, Cmd) = (Cmd){
-      .type      = Cmd_SpawnUnit,
-      .spawnUnit = {.position = position},
   };
 }
