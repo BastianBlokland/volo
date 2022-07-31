2e9f138f7391430c89be87efc65c8bb5092fd7fd
Bastian Blokland <mail@bastian.tech>
2022-07-31 20:28:21 +0300

    geo: Add rotatedbox-ray intersect utility

--- libs/geo/include/geo_box_rotated.h
+++ libs/geo/include/geo_box_rotated.h
@@ -10,3 +10,11 @@ typedef struct {
   GeoBox  box;
   GeoQuat rotation;
 } GeoBoxRotated;
+
+/**
+ * Compute the intersection of the rotated box with the given ray.
+ * Returns the time along the ray at which the intersection occurred or negative if no intersection
+ * occurred.
+ * NOTE: Writes the surface-normal at the point of intersection to 'outNormal'.
+ */
+f32 geo_box_rotated_intersect_ray(const GeoBoxRotated*, const GeoRay*, GeoVector* outNormal);
