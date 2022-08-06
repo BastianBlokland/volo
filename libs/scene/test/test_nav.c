ba3079a9bcf938c5dd9c0bca6ba02480d675d780
Bastian Blokland <mail@bastian.tech>
2022-08-07 00:01:50 +0300

    scene: Add pathing around obstacle testcase

--- libs/scene/test/test_nav.c
+++ libs/scene/test/test_nav.c
@@ -2,6 +2,7 @@
 #include "core_alloc.h"
 #include "ecs.h"
 #include "ecs_utils.h"
+#include "scene_collision.h"
 #include "scene_nav.h"
 #include "scene_register.h"
 #include "scene_transform.h"
@@ -13,11 +14,20 @@ static EcsEntityId test_create_agent(EcsWorld* world, const GeoVector pos, const
   return e;
 }
 
+static EcsEntityId test_create_blocker(EcsWorld* world, const GeoVector pos) {
+  const EcsEntityId e = ecs_world_entity_create(world);
+  ecs_world_add_t(world, e, SceneTransformComp, .position = pos, .rotation = geo_quat_ident);
+  scene_collision_add_sphere(world, e, (SceneCollisionSphere){.radius = .25f});
+  scene_nav_add_blocker(world, e);
+  return e;
+}
+
 static void test_check_path(
     CheckTestContext*       _testCtx,
     const SceneNavPathComp* comp,
     const GeoNavCell        expected[],
     const u32               expectedCount) {
+  check_eq_int(comp->cellCount, expectedCount);
   check_require(comp->cellCount == expectedCount);
   for (u32 i = 0; i != expectedCount; ++i) {
     check_eq_int(comp->cells[i].x, expected[i].x);
@@ -52,11 +62,20 @@ spec(nav) {
     ecs_run_sync(runner);
   }
 
-  it("computes an optimal straight line path") {
+  it("can compute a straight line path") {
     const EcsEntityId global = ecs_world_global(world);
     const EcsEntityId agent  = test_create_agent(world, geo_vector(-2, 0, 0), geo_vector(2, 0, 0));
     ecs_run_sync(runner); // Tick to create the agent.
     ecs_run_sync(runner); // Tick to query the path.
+
+    /**
+     * Verify the path.
+     * Expected: (1 is an output cell, 0 is an enqueued neighbor).
+     *
+     *  0000
+     * 011111
+     *  0000
+     */
 
     const SceneNavPathComp* path = ecs_utils_read_t(world, PathView, agent, SceneNavPathComp);
     test_check_path(
@@ -71,18 +90,51 @@ spec(nav) {
         },
         5);
 
-    /**
-     * Verify the amount of cells checked and enqueued.
-     *
-     *  0000
-     * 011111
-     *  0000
-     */
     const SceneNavStatsComp* stats = ecs_utils_read_t(world, StatsView, global, SceneNavStatsComp);
     check_eq_int(stats->pathCount, 1);
     check_eq_int(stats->pathItrCells, 5);
     check_eq_int(stats->pathOutputCells, 5);
     check_eq_int(stats->pathItrEnqueues, 14);
+  }
+
+  it("can compute a path around an obstacle") {
+    const EcsEntityId global = ecs_world_global(world);
+    const EcsEntityId agent  = test_create_agent(world, geo_vector(-2, 0, 0), geo_vector(2, 0, 0));
+    test_create_blocker(world, geo_vector(0, 0, 0));
+    ecs_run_sync(runner); // Tick to create the agent and the blocker.
+    ecs_run_sync(runner); // Tick to register the blocker.
+    ecs_run_sync(runner); // Tick to query the path.
+
+    /**
+     * Verify the path.
+     * Expected: (1 is an output cell, x is blocked and 0 is an enqueued neighbor).
+     *
+     *  00 0
+     * 011x11
+     *  01110
+     *   000
+     */
+
+    const SceneNavPathComp* path = ecs_utils_read_t(world, PathView, agent, SceneNavPathComp);
+    test_check_path(
+        _testCtx,
+        path,
+        (GeoNavCell[]){
+            {.x = halfGridSize - 2, .y = halfGridSize},
+            {.x = halfGridSize - 1, .y = halfGridSize},
+            {.x = halfGridSize - 1, .y = halfGridSize - 1},
+            {.x = halfGridSize + 0, .y = halfGridSize - 1},
+            {.x = halfGridSize + 1, .y = halfGridSize - 1},
+            {.x = halfGridSize + 1, .y = halfGridSize},
+            {.x = halfGridSize + 2, .y = halfGridSize},
+        },
+        7);
+
+    const SceneNavStatsComp* stats = ecs_utils_read_t(world, StatsView, global, SceneNavStatsComp);
+    check_eq_int(stats->pathCount, 1);
+    check_eq_int(stats->pathItrCells, 7);
+    check_eq_int(stats->pathOutputCells, 7);
+    check_eq_int(stats->pathItrEnqueues, 16);
   }
 
   teardown() {
