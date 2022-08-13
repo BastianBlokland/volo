413a028f83b73a8dc9a5848d7200f98290c282f6
Bastian Blokland <mail@bastian.tech>
2022-08-13 16:39:24 +0300

    scene: Minor cleanup

--- libs/scene/include/scene_selection.h
+++ libs/scene/include/scene_selection.h
@@ -8,34 +8,16 @@
 ecs_comp_extern(SceneSelectionComp);
 
 /**
- * Retrieve the main selected entity or 0 if no entity is currently selected.
+ * Check the current selection.
  */
 EcsEntityId scene_selection_main(const SceneSelectionComp*);
+bool        scene_selection_contains(const SceneSelectionComp*, EcsEntityId);
+bool        scene_selection_empty(const SceneSelectionComp*);
 
 /**
- * Check if the given entity is currently selected.
- */
-bool scene_selection_contains(const SceneSelectionComp*, EcsEntityId);
-
-/**
- * Check if the selection is currently empty.
- */
-bool scene_selection_empty(const SceneSelectionComp*);
-
-/**
- * Request the given entity to be selected.
- * NOTE: Is deferred until the next 'SceneOrder_SelectionUpdate'.
+ * Modify the current selection..
+ * NOTE: Deferred until the next 'SceneOrder_SelectionUpdate'.
  */
 void scene_selection_add(SceneSelectionComp*, EcsEntityId);
-
-/**
- * Request the given entity to be deselected.
- * NOTE: Is deferred until the next 'SceneOrder_SelectionUpdate'.
- */
 void scene_selection_remove(SceneSelectionComp*, EcsEntityId);
-
-/**
- * Request to clear the selection (if any).
- * NOTE: Is deferred until the next 'SceneOrder_SelectionUpdate'.
- */
 void scene_selection_clear(SceneSelectionComp*);
