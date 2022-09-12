248b8a0298e4225cdef507b8e2a1fc5f081a04b2
Bastian Blokland <mail@bastian.tech>
2022-09-12 17:55:33 +0300

    ai: Add basic blackboard tests

--- libs/ai/test/test_blackboard.c
+++ libs/ai/test/test_blackboard.c
@@ -1,10 +1,33 @@
 #include "ai_blackboard.h"
 #include "check_spec.h"
+#include "core_alloc.h"
 
 spec(blackboard) {
-  setup() {}
 
-  it("works") {}
+  AiBlackboard* bb = null;
 
-  teardown() {}
+  setup() { bb = ai_blackboard_create(g_alloc_heap); }
+
+  it("returns the fallback if knowledge is unset") {
+    check_eq_float(ai_blackboard_get_f64(bb, string_hash_lit("test"), 42), 42, 1e-6f);
+  }
+
+  it("returns the stored knowledge") {
+    ai_blackboard_set_f64(bb, string_hash_lit("test"), 42);
+    check_eq_float(ai_blackboard_get_f64(bb, string_hash_lit("test"), 1337), 42, 1e-6f);
+  }
+
+  it("can store many knowledge keys") {
+    const u32 keyCount = 1337;
+    for (u32 i = 0; i != keyCount; ++i) {
+      const String key = fmt_write_scratch("test_{}", fmt_int(i));
+      ai_blackboard_set_f64(bb, string_hash(key), i);
+    }
+    for (u32 i = 0; i != keyCount; ++i) {
+      const String key = fmt_write_scratch("test_{}", fmt_int(i));
+      check_eq_float(ai_blackboard_get_f64(bb, string_hash(key), -1), i, 1e-6f);
+    }
+  }
+
+  teardown() { ai_blackboard_destroy(bb); }
 }
