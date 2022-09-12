23747f0c912483ba0b4c7b92e005e113f9ff2484
Bastian Blokland <mail@bastian.tech>
2022-09-12 16:46:54 +0300

    ai: Blackboard skeleton

--- libs/ai/include/ai_blackboard.h
+++ libs/ai/include/ai_blackboard.h
@@ -1,2 +1,21 @@
 #pragma once
 #include "ai_knowledge.h"
+
+// Forward declare from 'core_alloc.h'.
+typedef struct sAllocator Allocator;
+
+/**
+ * Blackboard instance for storing knowledge.
+ */
+typedef struct sAiBlackboard AiBlackboard;
+
+/**
+ * Create a new AiBlackboard instance.
+ * Destroy using 'ai_blackboard_destroy()'.
+ */
+AiBlackboard* ai_blackboard_create(Allocator*);
+
+/**
+ * Destroy a AiBlackboard instance.
+ */
+void ai_blackboard_destroy(AiBlackboard*);
--- libs/ai/src/blackboard.c
+++ libs/ai/src/blackboard.c
@@ -1,1 +1,17 @@
 #include "ai_blackboard.h"
+#include "core_alloc.h"
+
+struct sAiBlackboard {
+  Allocator* alloc;
+};
+
+AiBlackboard* ai_blackboard_create(Allocator* alloc) {
+  AiBlackboard* bb = alloc_alloc_t(alloc, AiBlackboard);
+
+  *bb = (AiBlackboard){
+      .alloc = alloc,
+  };
+  return bb;
+}
+
+void ai_blackboard_destroy(AiBlackboard* bb) { alloc_free_t(bb->alloc, bb); }
