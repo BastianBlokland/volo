55f48a4719beb73dfa79fecf783aa981848f164c
Bastian Blokland <mail@bastian.tech>
2022-09-25 17:14:52 +0300

    ai: Add ai_result_str util

--- libs/ai/include/ai_result.h
+++ libs/ai/include/ai_result.h
@@ -1,6 +1,9 @@
 #pragma once
+#include "core_string.h"
 
 typedef enum {
   AiResult_Success,
   AiResult_Failure,
 } AiResult;
+
+String ai_result_str(AiResult);
---
+++ libs/ai/src/result.c
@@ -0,0 +1,12 @@
+#include "ai_result.h"
+#include "core_annotation.h"
+#include "core_array.h"
+
+String ai_result_str(const AiResult res) {
+  static const String g_names[] = {
+      string_static("Success"),
+      string_static("Failure"),
+  };
+  ASSERT(array_elems(g_names) == 2, "Incorrect number of names");
+  return g_names[res];
+}
