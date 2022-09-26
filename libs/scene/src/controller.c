d37b96ee32a6404064b09dbf08633d4aba682398
Bastian Blokland <mail@bastian.tech>
2022-09-26 20:30:07 +0300

    scene: Add attack target command

--- libs/scene/src/controller.c
+++ libs/scene/src/controller.c
@@ -1,17 +1,20 @@
 #include "ai_blackboard.h"
 #include "core_stringtable.h"
 #include "ecs_world.h"
+#include "scene_attack.h"
 #include "scene_brain.h"
 #include "scene_nav.h"
 
 // clang-format off
 
 static StringHash g_blackboardKeyNavTarget,
-                  g_blackboardKeyNavStop;
+                  g_blackboardKeyNavStop,
+                  g_blackboardKeyAttackTarget;
 
 // clang-format on
 
 ecs_view_define(BrainView) {
+  ecs_access_maybe_write(SceneAttackComp);
   ecs_access_maybe_write(SceneNavAgentComp);
   ecs_access_write(SceneBrainComp);
 }
@@ -27,7 +30,6 @@ ecs_system_define(SceneControllerUpdateSys) {
 
     SceneNavAgentComp* navAgent = ecs_view_write_t(itr, SceneNavAgentComp);
     if (navAgent) {
-
       // Start moving when the nav-target knowledge is set.
       if (ai_blackboard_exists(bb, g_blackboardKeyNavTarget)) {
         const GeoVector target = ai_blackboard_get_vector(bb, g_blackboardKeyNavTarget);
@@ -45,12 +47,20 @@ ecs_system_define(SceneControllerUpdateSys) {
         ai_blackboard_unset(bb, g_blackboardKeyNavStop);
       }
     }
+
+    // Set attack target.
+    SceneAttackComp* attack = ecs_view_write_t(itr, SceneAttackComp);
+    if (attack) {
+      attack->attackTarget = ai_blackboard_get_entity(bb, g_blackboardKeyAttackTarget);
+      ai_blackboard_unset(bb, g_blackboardKeyAttackTarget);
+    }
   }
 }
 
 ecs_module_init(scene_controller_module) {
-  g_blackboardKeyNavTarget = stringtable_add(g_stringtable, string_lit("cmd-nav-target"));
-  g_blackboardKeyNavStop   = stringtable_add(g_stringtable, string_lit("cmd-nav-stop"));
+  g_blackboardKeyNavTarget    = stringtable_add(g_stringtable, string_lit("cmd-nav-target"));
+  g_blackboardKeyNavStop      = stringtable_add(g_stringtable, string_lit("cmd-nav-stop"));
+  g_blackboardKeyAttackTarget = stringtable_add(g_stringtable, string_lit("cmd-attack-target"));
 
   ecs_register_view(BrainView);
 
