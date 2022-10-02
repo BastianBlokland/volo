7a3127f208c1fa1a1b74c0ffaee281cc34d1111c
Bastian Blokland <mail@bastian.tech>
2022-10-02 16:10:51 +0300

    vfx: Lookup particle graphic

--- libs/vfx/src/register.c
+++ libs/vfx/src/register.c
@@ -1,4 +1,7 @@
 #include "ecs_def.h"
 #include "vfx_register.h"
 
-void vfx_register(EcsDef* def) { ecs_register_module(def, vfx_emitter_module); }
+void vfx_register(EcsDef* def) {
+  ecs_register_module(def, vfx_emitter_module);
+  ecs_register_module(def, vfx_resource_module);
+}
