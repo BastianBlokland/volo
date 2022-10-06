befbe73829a6b351209127052d9faed90c3e13b1
Bastian Blokland <mail@bastian.tech>
2022-10-06 20:46:03 +0300

    asset: Refactor vfx asset loader

--- libs/asset/include/asset_vfx.h
+++ libs/asset/include/asset_vfx.h
@@ -3,6 +3,6 @@
 #include "geo_color.h"
 
 ecs_comp_extern_public(AssetVfxComp) {
-  String   atlasEntry;
-  GeoColor color;
+  StringHash atlasEntry;
+  GeoColor   color;
 };
--- libs/asset/src/loader_vfx.c
+++ libs/asset/src/loader_vfx.c
@@ -9,7 +9,16 @@
 #include "repo_internal.h"
 
 static DataReg* g_dataReg;
-static DataMeta g_dataVfxCompMeta;
+static DataMeta g_dataVfxDefMeta;
+
+typedef struct {
+  f32 r, g, b, a;
+} AssetVfxColorDef;
+
+typedef struct {
+  String           atlasEntry;
+  AssetVfxColorDef color;
+} AssetVfxDef;
 
 static void vfx_datareg_init() {
   static ThreadSpinLock g_initLock;
@@ -21,28 +30,23 @@ static void vfx_datareg_init() {
     g_dataReg = data_reg_create(g_alloc_persist);
 
     // clang-format off
-    data_reg_struct_t(g_dataReg, GeoColor);
-    data_reg_field_t(g_dataReg, GeoColor, r, data_prim_t(f32));
-    data_reg_field_t(g_dataReg, GeoColor, g, data_prim_t(f32));
-    data_reg_field_t(g_dataReg, GeoColor, b, data_prim_t(f32));
-    data_reg_field_t(g_dataReg, GeoColor, a, data_prim_t(f32));
+    data_reg_struct_t(g_dataReg, AssetVfxColorDef);
+    data_reg_field_t(g_dataReg, AssetVfxColorDef, r, data_prim_t(f32));
+    data_reg_field_t(g_dataReg, AssetVfxColorDef, g, data_prim_t(f32));
+    data_reg_field_t(g_dataReg, AssetVfxColorDef, b, data_prim_t(f32));
+    data_reg_field_t(g_dataReg, AssetVfxColorDef, a, data_prim_t(f32));
 
-    data_reg_struct_t(g_dataReg, AssetVfxComp);
-    data_reg_field_t(g_dataReg, AssetVfxComp, atlasEntry, data_prim_t(String), .flags = DataFlags_NotEmpty);
-    data_reg_field_t(g_dataReg, AssetVfxComp, color, t_GeoColor);
+    data_reg_struct_t(g_dataReg, AssetVfxDef);
+    data_reg_field_t(g_dataReg, AssetVfxDef, atlasEntry, data_prim_t(String), .flags = DataFlags_NotEmpty);
+    data_reg_field_t(g_dataReg, AssetVfxDef, color, t_AssetVfxColorDef);
     // clang-format on
 
-    g_dataVfxCompMeta = data_meta_t(t_AssetVfxComp);
+    g_dataVfxDefMeta = data_meta_t(t_AssetVfxDef);
   }
   thread_spinlock_unlock(&g_initLock);
 }
 
 ecs_comp_define_public(AssetVfxComp);
-
-static void ecs_destruct_vfx_comp(void* data) {
-  AssetVfxComp* comp = data;
-  string_maybe_free(g_alloc_heap, comp->atlasEntry);
-}
 
 ecs_view_define(VfxUnloadView) {
   ecs_access_with(AssetVfxComp);
@@ -60,10 +64,19 @@ ecs_system_define(VfxUnloadAssetSys) {
   }
 }
 
+static GeoColor asset_vfx_build_color(const AssetVfxColorDef* def) {
+  return geo_color(def->r, def->g, def->b, def->a);
+}
+
+static void asset_vfx_build(const AssetVfxDef* def, AssetVfxComp* out) {
+  out->atlasEntry = string_hash(def->atlasEntry);
+  out->color      = asset_vfx_build_color(&def->color);
+}
+
 ecs_module_init(asset_vfx_module) {
   vfx_datareg_init();
 
-  ecs_register_comp(AssetVfxComp, .destructor = ecs_destruct_vfx_comp);
+  ecs_register_comp(AssetVfxComp);
 
   ecs_register_view(VfxUnloadView);
 
@@ -73,16 +86,17 @@ ecs_module_init(asset_vfx_module) {
 void asset_load_vfx(EcsWorld* world, const String id, const EcsEntityId entity, AssetSource* src) {
   (void)id;
 
-  AssetVfxComp   vfxComp;
+  AssetVfxDef    vfxDef;
   String         errMsg;
   DataReadResult readRes;
-  data_read_json(g_dataReg, src->data, g_alloc_heap, g_dataVfxCompMeta, mem_var(vfxComp), &readRes);
+  data_read_json(g_dataReg, src->data, g_alloc_heap, g_dataVfxDefMeta, mem_var(vfxDef), &readRes);
   if (UNLIKELY(readRes.error)) {
     errMsg = readRes.errorMsg;
     goto Error;
   }
 
-  *ecs_world_add_t(world, entity, AssetVfxComp) = vfxComp;
+  AssetVfxComp* result = ecs_world_add_t(world, entity, AssetVfxComp);
+  asset_vfx_build(&vfxDef, result);
 
   ecs_world_add_empty_t(world, entity, AssetLoadedComp);
   goto Cleanup;
@@ -92,5 +106,6 @@ Error:
   ecs_world_add_empty_t(world, entity, AssetFailedComp);
 
 Cleanup:
+  data_destroy(g_dataReg, g_alloc_heap, g_dataVfxDefMeta, mem_var(vfxDef));
   asset_repo_source_close(src);
 }
