4c7b8dd7f72f8d453fd4ae1a3c3e811b2f483adc
Bastian Blokland <bastianblokland@gmail.com>
2022-10-22 09:15:43 +0300

    core: Add 'NO_INLINE_HINT' annotation

--- libs/core/include/core_annotation.h
+++ libs/core/include/core_annotation.h
@@ -70,6 +70,17 @@ ASSERT(false, "Unsupported compiler");
 #endif
 
 /**
+ * Hint to the compiler that this function should not be inlined.
+ */
+#if defined(VOLO_CLANG) || defined(VOLO_GCC)
+#define NO_INLINE_HINT __attribute__((noinline))
+#elif defined(VOLO_MSVC)
+#define NO_INLINE_HINT __declspec(noinline)
+#else
+#define NO_INLINE_HINT
+#endif
+
+/**
  * Mark a variable as having thread storage duration.
  * Which means it is created when the thread starts and cleaned up when the thread ends.
  */
