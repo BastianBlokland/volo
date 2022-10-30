d7ec9962909138e493e13e63b353c6c50e274b2f
Bastian Blokland <mail@bastian.tech>
2022-10-30 16:10:49 +0200

    script: Initial lexer tests

--- libs/script/test/test_lex.c
+++ libs/script/test/test_lex.c
@@ -1,5 +1,6 @@
 #include "check_spec.h"
 #include "core_array.h"
+#include "script_error.h"
 #include "script_lex.h"
 
 spec(lex) {
@@ -65,6 +66,42 @@ spec(lex) {
             script_token_fmt(&testData[i].a),
             script_token_fmt(&testData[i].b));
       }
+    }
+  }
+
+  it("can identify tokens") {
+    const struct {
+      String      input;
+      ScriptToken expected;
+    } testData[] = {
+        {string_static("=="), {.type = ScriptTokenType_OpEqEq}},
+        {string_static("!="), {.type = ScriptTokenType_OpBangEq}},
+        {string_static("<"), {.type = ScriptTokenType_OpLe}},
+        {string_static("<="), {.type = ScriptTokenType_OpLeEq}},
+        {string_static(">"), {.type = ScriptTokenType_OpGt}},
+        {string_static(">="), {.type = ScriptTokenType_OpGtEq}},
+        {string_static("null"), {.type = ScriptTokenType_LitNull}},
+        {string_static("42"), {.type = ScriptTokenType_LitNumber, .val_number = 42}},
+        {string_static("true"), {.type = ScriptTokenType_LitBool, .val_bool = true}},
+        {string_static("false"), {.type = ScriptTokenType_LitBool, .val_bool = false}},
+        {
+            string_static("$hello"),
+            {.type = ScriptTokenType_LitKey, .val_key = string_hash_lit("hello")},
+        },
+        {string_static("|"), {.type = ScriptTokenType_Error, .val_error = ScriptError_InvalidChar}},
+        {string_static(""), {.type = ScriptTokenType_End}},
+    };
+
+    for (u32 i = 0; i != array_elems(testData); ++i) {
+      ScriptToken  token;
+      const String rem = script_lex(testData[i].input, null, &token);
+
+      check_msg(string_is_empty(rem), "Unexpected remaining input: '{}'", fmt_text(rem));
+      check_msg(
+          script_token_equal(&token, &testData[i].expected),
+          "{} == {}",
+          script_token_fmt(&token),
+          script_token_fmt(&testData[i].expected));
     }
   }
 }
