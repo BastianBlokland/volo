7a10c2e335dcce7446c6529dfa06970d33114666
Bastian Blokland <mail@bastian.tech>
2022-10-30 17:16:16 +0200

    script: Consume whole word on lex error

--- libs/script/src/lex.c
+++ libs/script/src/lex.c
@@ -1,12 +1,13 @@
 #include "core_diag.h"
 #include "core_format.h"
+#include "core_math.h"
 #include "core_utf8.h"
 #include "script_lex.h"
 
 #define script_token_err(_ERR_)                                                                    \
   (ScriptToken) { .type = ScriptTokenType_Error, .val_error = (_ERR_) }
 
-static bool script_is_seperator(const u8 c) {
+static bool script_is_word_seperator(const u8 c) {
   switch (c) {
   case '\0':
   case '\t':
@@ -49,6 +50,18 @@ static bool script_is_seperator(const u8 c) {
   }
 }
 
+static u32 script_scan_word_end(const String str) {
+  u32 end = 0;
+  for (; end < str.size && !script_is_word_seperator(*string_at(str, end)); ++end)
+    ;
+  return end;
+}
+
+static String script_consume_word_or_char(const String str) {
+  diag_assert(!string_is_empty(str));
+  return string_consume(str, math_max(script_scan_word_end(str), 1));
+}
+
 static u8 script_peek(const String str, const u32 ahead) {
   return UNLIKELY(str.size <= ahead) ? '\0' : *string_at(str, ahead);
 }
@@ -59,7 +72,7 @@ static String script_lex_null(String str, ScriptToken* out) {
     return string_consume(str, 4);
   }
   *out = script_token_err(ScriptError_InvalidCharInNull);
-  return string_consume(str, 1);
+  return script_consume_word_or_char(str);
 }
 
 static String script_lex_lit_number(String str, ScriptToken* out) {
@@ -74,7 +87,7 @@ static String script_lex_true(String str, ScriptToken* out) {
     return string_consume(str, 4);
   }
   *out = script_token_err(ScriptError_InvalidCharInTrue);
-  return string_consume(str, 1);
+  return script_consume_word_or_char(str);
 }
 
 static String script_lex_false(String str, ScriptToken* out) {
@@ -84,16 +97,14 @@ static String script_lex_false(String str, ScriptToken* out) {
     return string_consume(str, 5);
   }
   *out = script_token_err(ScriptError_InvalidCharInFalse);
-  return string_consume(str, 1);
+  return script_consume_word_or_char(str);
 }
 
 static String script_lex_key(String str, StringTable* stringtable, ScriptToken* out) {
   diag_assert(*string_begin(str) == '$');
   str = string_consume(str, 1); // Skip the leading '$'.
 
-  u32 end = 0;
-  for (; end < str.size && !script_is_seperator(*string_at(str, end)); ++end)
-    ;
+  const u32 end = script_scan_word_end(str);
   if (UNLIKELY(!end)) {
     *out = script_token_err(ScriptError_KeyIdentifierEmpty);
     return str;
@@ -172,7 +183,7 @@ String script_lex(String str, StringTable* stringtable, ScriptToken* out) {
       continue;
     default:
       *out = script_token_err(ScriptError_InvalidChar);
-      return string_consume(str, 1);
+      return script_consume_word_or_char(str);
     }
   }
 
