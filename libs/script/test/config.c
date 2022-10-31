0eecc6bc5d169d390bd24e4a5f46a957790cfb61
Bastian Blokland <mail@bastian.tech>
2022-10-31 20:38:06 +0200

    script: Add memory instance

--- libs/script/test/config.c
+++ libs/script/test/config.c
@@ -3,6 +3,7 @@
 void app_check_configure(CheckDef* check) {
   register_spec(check, doc);
   register_spec(check, lex);
+  register_spec(check, mem);
   register_spec(check, operation);
   register_spec(check, read);
   register_spec(check, val);
