6d4b963a737e0ebd5eefdf8dc16006d8fb851c2c
Bastian Blokland <mail@bastian.tech>
2022-11-13 16:31:34 +0200

    scene: Minor cleanup in scene_attack header

--- libs/scene/include/scene_attack.h
+++ libs/scene/include/scene_attack.h
@@ -3,16 +3,16 @@
 #include "ecs_entity.h"
 #include "ecs_module.h"
 
-typedef enum {
-  SceneAttackFlags_Firing = 1 << 0,
-} SceneAttackFlags;
-
 ecs_comp_extern_public(SceneWeaponComp) {
   TimeDuration intervalMin, intervalMax;
   StringHash   animAim, animFire;
   EcsEntityId  vfxMuzzleFlash, vfxProjectile, vfxImpact;
   f32          spreadAngleMax;
 };
+
+typedef enum {
+  SceneAttackFlags_Firing = 1 << 0,
+} SceneAttackFlags;
 
 ecs_comp_extern_public(SceneAttackComp) {
   SceneAttackFlags flags;
