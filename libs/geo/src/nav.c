98c24bab00e870d2fd4a8217db7b3aac3f32b83c
Bastian Blokland <mail@bastian.tech>
2022-11-15 22:22:32 +0200

    geo: Increase max cell occupants to 5

--- libs/geo/src/nav.c
+++ libs/geo/src/nav.c
@@ -12,7 +12,7 @@
 
 #define geo_nav_workers_max 64
 #define geo_nav_occupants_max 2048
-#define geo_nav_occupants_per_cell 4
+#define geo_nav_occupants_per_cell 5
 #define geo_nav_blockers_max 512
 #define geo_nav_blocker_max_cells 128
 
