784f5b694985ed4fcaa1f070f4eed43c675a0d7a
Bastian Blokland <mail@bastian.tech>
2022-11-19 13:26:49 +0200

    scene: Add unit module

--- libs/scene/src/register.c
+++ libs/scene/src/register.c
@@ -25,6 +25,7 @@ void scene_register(EcsDef* def) {
   ecs_register_module(def, scene_target_module);
   ecs_register_module(def, scene_time_module);
   ecs_register_module(def, scene_transform_module);
+  ecs_register_module(def, scene_unit_module);
   ecs_register_module(def, scene_vfx_module);
   ecs_register_module(def, scene_weapon_module);
 }
