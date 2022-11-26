1cf61396ae60758a7fe6057b1a5d567ec215ce81
Bastian Blokland <mail@bastian.tech>
2022-11-26 19:35:53 +0200

    scene: Move velocity update to the end of the frame

--- libs/scene/include/scene_register.h
+++ libs/scene/include/scene_register.h
@@ -3,9 +3,9 @@
 
 enum {
   SceneOrder_TimeUpdate       = -100,
-  SceneOrder_VelocityUpdate   = -10,
   SceneOrder_AttachmentUpdate = 10,
   SceneOrder_LocomotionUpdate = 10,
+  SceneOrder_VelocityUpdate   = 100,
   SceneOrder_CollisionUpdate  = 100,
   SceneOrder_SelectionUpdate  = 100,
 };
