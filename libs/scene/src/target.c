b7e66ddee5a4af7625af0f461689c1a1c39e876e
Bastian Blokland <mail@bastian.tech>
2022-11-27 14:10:34 +0200

    scene: Decrease max target los dist

--- libs/scene/src/target.c
+++ libs/scene/src/target.c
@@ -12,7 +12,7 @@
 #define target_refresh_time_min time_seconds(1)
 #define target_refresh_time_max time_seconds(2.5)
 #define target_los_dist_min 1.0f
-#define target_los_dist_max 75.0f
+#define target_los_dist_max 50.0f
 
 ecs_comp_define_public(SceneTargetFinderComp);
 
