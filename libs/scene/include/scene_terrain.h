7451711a8cd5cda9c840f83bfb226f872142dd01
Bastian Blokland <mail@bastian.tech>
2022-12-11 16:36:24 +0200

    scene: Add terrain init api

--- libs/scene/include/scene_terrain.h
+++ libs/scene/include/scene_terrain.h
@@ -2,3 +2,5 @@
 #include "ecs_module.h"
 
 ecs_comp_extern(SceneTerrainComp);
+
+void scene_terrain_init(EcsWorld*, String terrainGraphic);
--- libs/scene/src/terrain.c
+++ libs/scene/src/terrain.c
@@ -1,5 +1,28 @@
+#include "core_alloc.h"
+#include "core_diag.h"
+#include "ecs_world.h"
 #include "scene_renderable.h"
 
-ecs_comp_define(SceneTerrainComp) { f32 size; };
+ecs_comp_define(SceneTerrainComp) {
+  String graphicId;
+  f32    size;
+};
 
-ecs_module_init(scene_terrain_module) { ecs_register_comp(SceneTerrainComp); }
+static void ecs_destruct_terrain(void* data) {
+  SceneTerrainComp* comp = data;
+  string_free(g_alloc_heap, comp->graphicId);
+}
+
+ecs_module_init(scene_terrain_module) {
+  ecs_register_comp(SceneTerrainComp, .destructor = ecs_destruct_terrain);
+}
+
+void scene_terrain_init(EcsWorld* world, const String graphicId) {
+  diag_assert_msg(graphicId.size, "Invalid terrain graphicId");
+
+  ecs_world_add_t(
+      world,
+      ecs_world_global(world),
+      SceneTerrainComp,
+      .graphicId = string_dup(g_alloc_heap, graphicId));
+}
