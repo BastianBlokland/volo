09afbedd15eeb7ce42553845895d32b799f65026
Bastian Blokland <mail@bastian.tech>
2022-12-13 00:00:37 +0200

    scene: Increase nav-grid size

--- libs/scene/src/nav.c
+++ libs/scene/src/nav.c
@@ -15,9 +15,10 @@
 
 ASSERT(sizeof(EcsEntityId) == sizeof(u64), "EntityId's have to be interpretable as 64bit integers");
 
-static const f32 g_sceneNavSize       = 200.0f;
-static const f32 g_sceneNavDensity    = 1.25f;
-static const f32 g_sceneNavCellHeight = 3.0f;
+static const f32 g_sceneNavSize            = 250.0f;
+static const f32 g_sceneNavDensity         = 1.25f;
+static const f32 g_sceneNavCellHeight      = 5.0f;
+static const f32 g_sceneNavCellBlockHeight = 1.5f;
 
 #define path_max_cells 64
 #define path_max_queries_per_task 25
@@ -47,8 +48,12 @@ static void ecs_destruct_nav_path_comp(void* data) {
 }
 
 static void scene_nav_env_create(EcsWorld* world) {
-  GeoNavGrid* grid =
-      geo_nav_grid_create(g_alloc_heap, g_sceneNavSize, g_sceneNavDensity, g_sceneNavCellHeight);
+  GeoNavGrid* grid = geo_nav_grid_create(
+      g_alloc_heap,
+      g_sceneNavSize,
+      g_sceneNavDensity,
+      g_sceneNavCellHeight,
+      g_sceneNavCellBlockHeight);
 
   ecs_world_add_t(world, ecs_world_global(world), SceneNavEnvComp, .navGrid = grid);
   ecs_world_add_t(world, ecs_world_global(world), SceneNavStatsComp);
