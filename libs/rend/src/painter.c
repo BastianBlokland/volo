6a3a148e2f5cc3bbdc7a49856b0a7b37f3cdfec2
Bastian Blokland <mail@bastian.tech>
2022-12-18 20:14:37 +0200

    rend: Refactor painter draw submission

--- libs/rend/src/painter.c
+++ libs/rend/src/painter.c
@@ -80,89 +80,131 @@ static GeoMatrix painter_view_proj_matrix(
   return geo_matrix_proj_ortho(2.0f, 2.0f / aspect, -100.0f, 100.0f);
 }
 
-static void painter_draw_override_dynmesh(
-    RendPainterComp*      painter,
-    RvkPass*              pass,
-    const RvkRepositoryId graphicId,
-    RendDrawComp*         orgDraw,
-    RvkMesh*              dynMesh) {
-  RvkRepository* repository = rvk_canvas_repository(painter->canvas);
-  RvkGraphic*    gra        = rvk_repository_graphic_get(repository, graphicId);
-
-  if (gra && rvk_pass_prepare(pass, gra) && rvk_pass_prepare_mesh(pass, dynMesh)) {
-    *dynarray_push_t(&painter->drawBuffer, RvkPassDraw) = rend_draw_output(orgDraw, gra, dynMesh);
-  }
+static void painter_push(RendPainterComp* painter, const RvkPassDraw draw) {
+  *dynarray_push_t(&painter->drawBuffer, RvkPassDraw) = draw;
 }
 
-static void painter_draw_forward(
-    RendPainterComp*             painter,
-    const RendSettingsComp*      settings,
-    const RendPainterGlobalData* globalData,
-    const RendView*              view,
-    RvkPass*                     forwardPass,
-    EcsView*                     drawView,
-    EcsView*                     graphicView) {
+static void painter_push_forward(
+    RendPainterComp*        painter,
+    const RendSettingsComp* settings,
+    const RendView*         view,
+    RvkPass*                pass,
+    EcsView*                drawView,
+    EcsView*                graphicView) {
 
-  dynarray_clear(&painter->drawBuffer);
-
-  // Prepare draws.
   EcsIterator* graphicItr = ecs_view_itr(graphicView);
   for (EcsIterator* drawItr = ecs_view_itr(drawView); ecs_view_walk(drawItr);) {
     RendDrawComp* draw = ecs_view_write_t(drawItr, RendDrawComp);
     if (!rend_draw_gather(draw, view, settings)) {
-      continue;
+      continue; // Draw culled.
     }
-    if (!ecs_view_contains(graphicView, rend_draw_graphic(draw))) {
-      continue;
+    if (!ecs_view_maybe_jump(graphicItr, rend_draw_graphic(draw))) {
+      continue; // Graphic not loaded.
     }
-    ecs_view_jump(graphicItr, rend_draw_graphic(draw));
     RvkGraphic* graphic = ecs_view_write_t(graphicItr, RendResGraphicComp)->graphic;
-
-    if (rvk_pass_prepare(forwardPass, graphic)) {
-      *dynarray_push_t(&painter->drawBuffer, RvkPassDraw) = rend_draw_output(draw, graphic, null);
-    }
-
-    if (graphic->mesh) {
-      const bool isStandardGeometry = (rend_draw_flags(draw) & RendDrawFlags_StandardGeometry) != 0;
-      const bool isSkinned          = (rend_draw_flags(draw) & RendDrawFlags_Skinned) != 0;
-      const bool isTerrain          = (rend_draw_flags(draw) & RendDrawFlags_Terrain) != 0;
-
-      diag_assert_msg(
-          isSkinned == ((graphic->mesh->flags & RvkMeshFlags_Skinned) != 0),
-          "Skinned meshes can only be drawn using skinned draws");
-
-      if (settings->flags & RendFlags_DebugSkinning && isStandardGeometry && isSkinned) {
-        const RvkRepositoryId graphicId = RvkRepositoryId_DebugSkinningGraphic;
-        painter_draw_override_dynmesh(painter, forwardPass, graphicId, draw, graphic->mesh);
-      }
-      if (settings->flags & RendFlags_Wireframe && (isStandardGeometry || isTerrain)) {
-        RvkRepositoryId graphicId;
-        if (isStandardGeometry && isSkinned) {
-          graphicId = RvkRepositoryId_WireframeSkinnedGraphic;
-        } else if (isTerrain) {
-          graphicId = RvkRepositoryId_WireframeTerrainGraphic;
-        } else {
-          diag_assert(isStandardGeometry && !isSkinned);
-          graphicId = RvkRepositoryId_WireframeGraphic;
-        }
-        painter_draw_override_dynmesh(painter, forwardPass, graphicId, draw, graphic->mesh);
-      }
+    if (rvk_pass_prepare(pass, graphic)) {
+      painter_push(painter, rend_draw_output(draw, graphic, null));
     }
   }
+}
 
-  // Sort draws.
+static void painter_push_wireframe(
+    RendPainterComp*        painter,
+    const RendSettingsComp* settings,
+    const RendView*         view,
+    RvkPass*                pass,
+    EcsView*                drawView,
+    EcsView*                graphicView) {
+
+  RvkRepository* repository = rvk_canvas_repository(painter->canvas);
+  EcsIterator*   graphicItr = ecs_view_itr(graphicView);
+
+  for (EcsIterator* drawItr = ecs_view_itr(drawView); ecs_view_walk(drawItr);) {
+    RendDrawComp* draw = ecs_view_write_t(drawItr, RendDrawComp);
+    if ((rend_draw_flags(draw) & (RendDrawFlags_StandardGeometry | RendDrawFlags_Terrain)) == 0) {
+      continue; // Not a draw we can render a wireframe for.
+    }
+    if (!rend_draw_gather(draw, view, settings)) {
+      continue; // Draw culled.
+    }
+    if (!ecs_view_maybe_jump(graphicItr, rend_draw_graphic(draw))) {
+      continue; // Graphic not loaded.
+    }
+    RvkGraphic* graphicOriginal = ecs_view_write_t(graphicItr, RendResGraphicComp)->graphic;
+    RvkMesh*    mesh            = graphicOriginal->mesh;
+    if (!mesh) {
+      continue; // Graphic does not have a mesh to draw a wireframe for.
+    }
+    RvkRepositoryId graphicId;
+    if (rend_draw_flags(draw) & RendDrawFlags_Terrain) {
+      graphicId = RvkRepositoryId_WireframeTerrainGraphic;
+    } else if (rend_draw_flags(draw) & RendDrawFlags_Skinned) {
+      graphicId = RvkRepositoryId_WireframeSkinnedGraphic;
+    } else {
+      graphicId = RvkRepositoryId_WireframeGraphic;
+    }
+    RvkGraphic* graphicWireframe = rvk_repository_graphic_get(repository, graphicId);
+    if (!graphicWireframe) {
+      continue; // Wireframe graphic not loaded.
+    }
+    if (rvk_pass_prepare(pass, graphicWireframe) && rvk_pass_prepare_mesh(pass, mesh)) {
+      painter_push(painter, rend_draw_output(draw, graphicWireframe, mesh));
+    }
+  }
+}
+
+static void painter_push_debugskinning(
+    RendPainterComp*        painter,
+    const RendSettingsComp* settings,
+    const RendView*         view,
+    RvkPass*                pass,
+    EcsView*                drawView,
+    EcsView*                graphicView) {
+
+  RvkRepository*        repository     = rvk_canvas_repository(painter->canvas);
+  const RvkRepositoryId debugGraphicId = RvkRepositoryId_DebugSkinningGraphic;
+  RvkGraphic*           debugGraphic   = rvk_repository_graphic_get(repository, debugGraphicId);
+  if (!debugGraphic || !rvk_pass_prepare(pass, debugGraphic)) {
+    return; // Debug graphic not ready to be drawn.
+  }
+
+  EcsIterator* graphicItr = ecs_view_itr(graphicView);
+  for (EcsIterator* drawItr = ecs_view_itr(drawView); ecs_view_walk(drawItr);) {
+    RendDrawComp*       draw      = ecs_view_write_t(drawItr, RendDrawComp);
+    const RendDrawFlags drawFlags = rend_draw_flags(draw);
+    if ((drawFlags & RendDrawFlags_Skinned) == 0) {
+      continue; // Not a skinned draw.
+    }
+    if (!rend_draw_gather(draw, view, settings)) {
+      continue; // Draw culled.
+    }
+    if (!ecs_view_maybe_jump(graphicItr, rend_draw_graphic(draw))) {
+      continue; // Graphic not loaded.
+    }
+    RvkGraphic* graphicOriginal = ecs_view_write_t(graphicItr, RendResGraphicComp)->graphic;
+    RvkMesh*    mesh            = graphicOriginal->mesh;
+    diag_assert(mesh);
+
+    if (rvk_pass_prepare_mesh(pass, mesh)) {
+      painter_push(painter, rend_draw_output(draw, debugGraphic, mesh));
+    }
+  }
+}
+
+static void
+painter_flush(RendPainterComp* painter, RvkPass* pass, const RendPainterGlobalData* globalData) {
   dynarray_sort(&painter->drawBuffer, painter_compare_pass_draw);
 
-  // Execute draws.
-  rvk_pass_begin(forwardPass, geo_color_black);
   rvk_pass_draw(
-      forwardPass,
+      pass,
       mem_var(*globalData),
       (RvkPassDrawList){
           .values = dynarray_begin_t(&painter->drawBuffer, RvkPassDraw),
           .count  = painter->drawBuffer.size,
       });
-  rvk_pass_end(forwardPass);
+  rvk_pass_end(pass);
+
+  dynarray_clear(&painter->drawBuffer);
 }
 
 static bool painter_draw(
@@ -192,7 +234,17 @@ static bool painter_draw(
     };
 
     RvkPass* forwardPass = rvk_canvas_pass(painter->canvas, RvkRenderPass_Forward);
-    painter_draw_forward(painter, settings, &globalData, &view, forwardPass, drawView, graphicView);
+    rvk_pass_begin(forwardPass, geo_color_black);
+    painter_push_forward(painter, settings, &view, forwardPass, drawView, graphicView);
+    if (settings->flags & RendFlags_Wireframe) {
+      painter_push_wireframe(painter, settings, &view, forwardPass, drawView, graphicView);
+    }
+    if (settings->flags & RendFlags_DebugSkinning) {
+      painter_push_debugskinning(painter, settings, &view, forwardPass, drawView, graphicView);
+    }
+    painter_flush(painter, forwardPass, &globalData);
+    rvk_pass_end(forwardPass);
+
     rvk_canvas_end(painter->canvas);
   }
   return draw;
