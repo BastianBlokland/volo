616b053a80a18d95a4df3f0a587dbaab46662748
Bastian Blokland <mail@bastian.tech>
2022-12-23 21:45:38 +0200

    rend: Additional global light params

--- libs/rend/include/rend_light.h
+++ libs/rend/include/rend_light.h
@@ -1,5 +1,14 @@
 #pragma once
 #include "ecs_module.h"
 #include "geo_color.h"
+#include "geo_quat.h"
 
-ecs_comp_extern_public(RendLightGlobalComp) { GeoColor sunLight; };
+ecs_comp_extern_public(RendLightGlobalComp) {
+  GeoColor sunLight;
+  f32      sunShininess;
+  GeoQuat  sunRotation;
+  f32      ambientIntensity;
+  f32      reflectFrac;
+};
+
+void rend_light_global_to_default(RendLightGlobalComp*);
--- libs/rend/src/light.c
+++ libs/rend/src/light.c
@@ -6,11 +6,10 @@ ecs_comp_define_public(RendLightGlobalComp);
 ecs_view_define(LightGlobalView) { ecs_access_maybe_write(RendLightGlobalComp); }
 
 static RendLightGlobalComp* rend_light_global_create(EcsWorld* world) {
-  return ecs_world_add_t(
-      world,
-      ecs_world_global(world),
-      RendLightGlobalComp,
-      .sunLight = geo_color(1.0f, 0.9f, 0.7f, 0));
+  const EcsEntityId    global      = ecs_world_global(world);
+  RendLightGlobalComp* lightGlobal = ecs_world_add_t(world, global, RendLightGlobalComp);
+  rend_light_global_to_default(lightGlobal);
+  return lightGlobal;
 }
 
 ecs_system_define(RendLightInitSys) {
@@ -25,4 +24,12 @@ ecs_module_init(rend_light_module) {
   ecs_register_view(LightGlobalView);
 
   ecs_register_system(RendLightInitSys, ecs_view_id(LightGlobalView));
+}
+
+void rend_light_global_to_default(RendLightGlobalComp* lightGlobal) {
+  lightGlobal->sunLight         = geo_color(1.0f, 0.9f, 0.7f, 0);
+  lightGlobal->sunShininess     = 16;
+  lightGlobal->sunRotation      = geo_quat_from_euler(geo_vector(65, 15, 0));
+  lightGlobal->ambientIntensity = 0.1f;
+  lightGlobal->reflectFrac      = 0.25f;
 }
