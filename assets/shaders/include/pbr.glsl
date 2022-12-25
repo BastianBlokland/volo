8c79f9c525ef965868d8f29152f23b4c349a51be
Bastian Blokland <mail@bastian.tech>
2022-12-25 20:18:12 +0200

    assets: Rename light.glsl to pbr.glsl

--- assets/shaders/include/pbr.glsl
+++ assets/shaders/include/pbr.glsl
@@ -1,5 +1,5 @@
-#ifndef INCLUDE_LIGHT
-#define INCLUDE_LIGHT
+#ifndef INCLUDE_PBR
+#define INCLUDE_PBR
 
 #include "math.glsl"
 #include "types.glsl"
@@ -16,7 +16,7 @@
  * Statistically approximates the relative surface area of microfacets exactly aligned to the
  * halfway vector.
  */
-f32 light_distribution_ggx(const f32v3 normal, const f32v3 halfDir, const f32 roughness) {
+f32 pbr_distribution_ggx(const f32v3 normal, const f32v3 halfDir, const f32 roughness) {
   const f32 a            = roughness * roughness;
   const f32 a2           = a * a;
   const f32 normDotHalf  = max(dot(normal, halfDir), 0.0);
@@ -28,7 +28,7 @@ f32 light_distribution_ggx(const f32v3 normal, const f32v3 halfDir, const f32 ro
   return nom / (c_pi * denom * denom);
 }
 
-f32 light_geometry_schlick_ggx(const f32 normDotView, const f32 roughness) {
+f32 pbr_geometry_schlick_ggx(const f32 normDotView, const f32 roughness) {
   const f32 r = (roughness + 1.0);
   const f32 k = (r * r) / 8.0;
 
@@ -42,30 +42,30 @@ f32 light_geometry_schlick_ggx(const f32 normDotView, const f32 roughness) {
  * Statistically approximates the relative surface area where its micro surface-details overshadow
  * each other, causing light rays to be occluded.
  */
-f32 light_geometry_smith(
+f32 pbr_geometry_smith(
     const f32v3 normal, const f32v3 view, const f32v3 lightDir, const f32 roughness) {
   const f32 normDotView  = max(dot(normal, view), 0.0);
   const f32 normDotLight = max(dot(normal, lightDir), 0.0);
-  const f32 ggx2         = light_geometry_schlick_ggx(normDotView, roughness);
-  const f32 ggx1         = light_geometry_schlick_ggx(normDotLight, roughness);
+  const f32 ggx2         = pbr_geometry_schlick_ggx(normDotView, roughness);
+  const f32 ggx1         = pbr_geometry_schlick_ggx(normDotLight, roughness);
   return ggx1 * ggx2;
 }
 
 /**
  * Compute the ratio of light that gets reflected over the light that gets refracted.
  */
-f32v3 light_fresnel_schlick(const f32 cosTheta, const f32v3 reflectance) {
+f32v3 pbr_fresnel_schlick(const f32 cosTheta, const f32v3 reflectance) {
   return reflectance + (1.0 - reflectance) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
 }
 
-struct Surface {
+struct PbrSurface {
   f32v3 color;
   f32v3 normal;
   f32   roughness;
   f32   metallicness;
 };
 
-f32v3 light_surf_reflectance(const Surface surf) {
+f32v3 pbr_surf_reflectance(const PbrSurface surf) {
   /**
    * Calculate reflectance at normal incidence; if dia-electric (like plastic) use uniform
    * reflectance of of 0.04 and if it's a metal, use the albedo color (metallic workflow).
@@ -73,16 +73,16 @@ f32v3 light_surf_reflectance(const Surface surf) {
   return mix(f32v3(0.04), surf.color, surf.metallicness);
 }
 
-f32v3 light_resolve_directional(
-    const f32v3 radiance, const f32v3 dir, const f32v3 viewDir, const Surface surf) {
+f32v3 pbr_light_dir(
+    const f32v3 radiance, const f32v3 dir, const f32v3 viewDir, const PbrSurface surf) {
 
   const f32v3 halfDir     = normalize(viewDir - dir);
-  const f32v3 reflectance = light_surf_reflectance(surf);
+  const f32v3 reflectance = pbr_surf_reflectance(surf);
 
   // Cook-Torrance BRDF.
-  const f32   normDistFrac = light_distribution_ggx(surf.normal, halfDir, surf.roughness);
-  const f32   geoFrac      = light_geometry_smith(surf.normal, viewDir, -dir, surf.roughness);
-  const f32v3 fresnelFrac  = light_fresnel_schlick(max(dot(halfDir, viewDir), 0.0), reflectance);
+  const f32   normDistFrac = pbr_distribution_ggx(surf.normal, halfDir, surf.roughness);
+  const f32   geoFrac      = pbr_geometry_smith(surf.normal, viewDir, -dir, surf.roughness);
+  const f32v3 fresnelFrac  = pbr_fresnel_schlick(max(dot(halfDir, viewDir), 0.0), reflectance);
 
   const f32v3 numerator = normDistFrac * geoFrac * fresnelFrac;
   f32 denominator = 4.0 * max(dot(surf.normal, viewDir), 0.0) * max(dot(surf.normal, -dir), 0.0);
@@ -107,4 +107,4 @@ f32v3 light_resolve_directional(
   return (kD * surf.color / c_pi + specular) * radiance * normDotDir;
 }
 
-#endif // INCLUDE_LIGHT
+#endif // INCLUDE_PBR
--- assets/shaders/shade_base.frag
+++ assets/shaders/shade_base.frag
@@ -3,7 +3,7 @@
 
 #include "binding.glsl"
 #include "global.glsl"
-#include "light.glsl"
+#include "pbr.glsl"
 #include "tags.glsl"
 #include "texture.glsl"
 
@@ -33,7 +33,7 @@ void main() {
   const f32v4 normalTags = texture(u_texGeoNormalTags, in_texcoord);
   const f32   depth      = texture(u_texGeoDepth, in_texcoord).r;
 
-  Surface surf;
+  PbrSurface surf;
   surf.color        = colorRough.rgb;
   surf.normal       = normalTags.xyz;
   surf.roughness    = colorRough.a;
@@ -44,9 +44,8 @@ void main() {
   const f32v3 worldPos = clip_to_world(clipPos);
   const f32v3 viewDir  = normalize(u_global.camPosition.xyz - worldPos);
 
-  const f32v3 ambient = surf.color * u_draw.ambient;
-  const f32v3 sunLight =
-      light_resolve_directional(u_draw.sunRadiance.rgb, u_draw.sunDir.xyz, viewDir, surf);
+  const f32v3 ambient  = surf.color * u_draw.ambient;
+  const f32v3 sunLight = pbr_light_dir(u_draw.sunRadiance.rgb, u_draw.sunDir.xyz, viewDir, surf);
 
   out_color = f32v4(ambient + sunLight, 1.0);
 
