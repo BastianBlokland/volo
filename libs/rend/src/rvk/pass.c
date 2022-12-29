0900dbe0b06eac73820226d70871ea72ae9e63c6
Bastian Blokland <mail@bastian.tech>
2022-12-29 02:04:20 +0200

    rend: Increase max global image count to 4

--- libs/rend/src/rvk/pass.c
+++ libs/rend/src/rvk/pass.c
@@ -17,7 +17,7 @@
 #define pass_attachment_color_max 2
 #define pass_attachment_max (pass_attachment_color_max + 1)
 #define pass_dependencies_max 8
-#define pass_global_image_max 3
+#define pass_global_image_max 4
 
 typedef RvkGraphic* RvkGraphicPtr;
 
