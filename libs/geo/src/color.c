402d720b3c0054538b59b6de41f66a651361b800
Bastian Blokland <mail@bastian.tech>
2023-01-20 22:14:50 +0200

    geo: Add SIMD support for geo_color_lerp

--- libs/geo/src/color.c
+++ libs/geo/src/color.c
@@ -3,6 +3,12 @@
 #include "core_math.h"
 #include "geo_color.h"
 
+#define geo_color_simd_enable 1
+
+#if geo_color_simd_enable
+#include "simd_sse_internal.h"
+#endif
+
 GeoColor geo_color_get(const u64 idx) {
   // TODO: Consider replacing this with generating a random hue and then converting from hsv to rgb.
   static const GeoColor g_colors[] = {
@@ -26,12 +32,20 @@ GeoColor geo_color_get(const u64 idx) {
 }
 
 GeoColor geo_color_lerp(const GeoColor x, const GeoColor y, const f32 t) {
-  return (GeoColor){
-      .r = math_lerp(x.r, y.r, t),
-      .g = math_lerp(x.g, y.g, t),
-      .b = math_lerp(x.b, y.b, t),
-      .a = math_lerp(x.a, y.a, t),
-  };
+#if geo_color_simd_enable
+  const SimdVec vX = simd_vec_load(x.data);
+  const SimdVec vY = simd_vec_load(y.data);
+  const SimdVec vT = simd_vec_broadcast(t);
+  GeoColor      res;
+  simd_vec_store(simd_vec_add(vX, simd_vec_mul(simd_vec_sub(vY, vX), vT)), res.data);
+  return res;
+#else
+  return geo_color(
+      math_lerp(x.r, y.r, t),
+      math_lerp(x.g, y.g, t),
+      math_lerp(x.b, y.b, t),
+      math_lerp(x.a, y.a, t));
+#endif
 }
 
 void geo_color_pack_f16(const GeoColor color, f16 out[4]) {
