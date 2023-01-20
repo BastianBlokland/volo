f8bc2b08c4d9bb8b52bb614296aeb17cdd632f99
Bastian Blokland <mail@bastian.tech>
2023-01-20 22:16:06 +0200

    geo: Add color add / mul tests

--- libs/geo/test/test_color.c
+++ libs/geo/test/test_color.c
@@ -4,6 +4,19 @@
 
 spec(color) {
 
+  it("sums all components when adding") {
+    check_eq_color(
+        geo_color_add(geo_color(1, -2.1f, 3, 4), geo_color(2, 3.2f, 4, 5)),
+        geo_color(3, 1.1f, 7, 9));
+    check_eq_color(
+        geo_color_add(geo_color(1, 2, 3, 0), geo_color(4, 5, 6, 0)), geo_color(5, 7, 9, 0));
+  }
+
+  it("multiplies each component by the scalar when multiplying") {
+    check_eq_color(geo_color_mul(geo_color(5, -2.1f, 6, 8), 2), geo_color(10, -4.2f, 12, 16));
+    check_eq_color(geo_color_mul(geo_color(1, 2, 3, 0), -2), geo_color(-2, -4, -6, 0));
+  }
+
   it("can linearly interpolate colors") {
     const GeoColor c1 = geo_color(10, 20, 10, 1);
     const GeoColor c2 = geo_color(20, 40, 20, 1);
