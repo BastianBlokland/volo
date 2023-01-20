2beae832952d54756584dad45b60444c280a972d
Bastian Blokland <mail@bastian.tech>
2023-01-20 22:12:57 +0200

    geo: Add geo_color_lerp test

--- libs/geo/test/test_color.c
+++ libs/geo/test/test_color.c
@@ -4,6 +4,13 @@
 
 spec(color) {
 
+  it("can linearly interpolate colors") {
+    const GeoColor c1 = geo_color(10, 20, 10, 1);
+    const GeoColor c2 = geo_color(20, 40, 20, 1);
+    const GeoColor c3 = geo_color(15, 30, 15, 1);
+    check_eq_color(geo_color_lerp(c1, c2, .5f), c3);
+  }
+
   it("lists all components when formatted") {
     check_eq_string(
         fmt_write_scratch("{}", geo_color_fmt(geo_color_white)), string_lit("1, 1, 1, 1"));
