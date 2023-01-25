3f7f3d47a43c76404814d4e9362dbe890acbe3f0
Bastian Blokland <mail@bastian.tech>
2023-01-26 00:07:30 +0200

    rend: Disable resource viewer on reset

--- libs/rend/src/settings.c
+++ libs/rend/src/settings.c
@@ -23,20 +23,21 @@ ecs_module_init(rend_settings_module) {
 void rend_settings_to_default(RendSettingsComp* s) {
   s->flags = RendFlags_FrustumCulling | RendFlags_AmbientOcclusion |
              RendFlags_AmbientOcclusionBlur | RendFlags_Bloom;
-  s->presentMode       = RendPresentMode_VSyncRelaxed;
-  s->ambientMode       = RendAmbientMode_DiffuseIrradiance;
-  s->exposure          = 1.0f;
-  s->tonemapper        = RendTonemapper_LinearSmooth;
-  s->resolutionScale   = 1.0f;
-  s->aoAngle           = 75 * math_deg_to_rad;
-  s->aoRadius          = 0.2f;
-  s->aoRadiusPower     = 3.5f;
-  s->aoPower           = 3.5f;
-  s->aoResolutionScale = 0.75f;
-  s->shadowResolution  = 2048;
-  s->bloomIntensity    = 0.04f;
-  s->bloomSteps        = 5;
-  s->bloomRadius       = 0.003f;
+  s->presentMode         = RendPresentMode_VSyncRelaxed;
+  s->ambientMode         = RendAmbientMode_DiffuseIrradiance;
+  s->exposure            = 1.0f;
+  s->tonemapper          = RendTonemapper_LinearSmooth;
+  s->resolutionScale     = 1.0f;
+  s->aoAngle             = 75 * math_deg_to_rad;
+  s->aoRadius            = 0.2f;
+  s->aoRadiusPower       = 3.5f;
+  s->aoPower             = 3.5f;
+  s->aoResolutionScale   = 0.75f;
+  s->shadowResolution    = 2048;
+  s->bloomIntensity      = 0.04f;
+  s->bloomSteps          = 5;
+  s->bloomRadius         = 0.003f;
+  s->debugViewerResource = 0;
 
   rend_settings_generate_ao_kernel(s);
 }
