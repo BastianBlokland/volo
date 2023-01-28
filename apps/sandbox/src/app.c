b39943c428c0829d49e430c0084748066efe8c19
Bastian Blokland <mail@bastian.tech>
2023-01-28 14:32:48 +0200

    sandbox: Add plant prop

--- apps/sandbox/src/app.c
+++ apps/sandbox/src/app.c
@@ -23,7 +23,7 @@
 #include "cmd_internal.h"
 
 static const GapVector g_appWindowSize = {1920, 1080};
-static const u32       g_appPropCount  = 300;
+static const u32       g_appPropCount  = 350;
 static const u64       g_appRngSeed    = 42;
 
 static void app_window_create(EcsWorld* world) {
@@ -60,9 +60,10 @@ static void app_scene_create_props(EcsWorld* world, Rng* rng) {
     StringHash prefabId;
     f32        weight;
   } g_props[] = {
-      {string_hash_lit("PropFence"), .weight = 0.8f},
-      {string_hash_lit("PropBarrel"), .weight = 0.1f},
-      {string_hash_lit("PropTree"), .weight = 0.1f},
+      {string_hash_lit("PropFence"), .weight = 0.6f},
+      {string_hash_lit("PropBarrel"), .weight = 0.05f},
+      {string_hash_lit("PropTree"), .weight = 0.05f},
+      {string_hash_lit("PropPlant"), .weight = 0.3f},
   };
 
   for (u32 instIdx = 0; instIdx != g_appPropCount; ++instIdx) {
