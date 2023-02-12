33177a3218563345922b488fd59e863ec618da4c
Bastian Blokland <mail@bastian.tech>
2023-02-12 21:43:02 +0200

    rend: Discretize shadow proj
    Reduces the visible shimmering

--- libs/rend/src/light.c
+++ libs/rend/src/light.c
@@ -17,7 +17,8 @@
 
 #include "light_internal.h"
 
-static const f32 g_lightDirMaxShadowDist = 250.0f;
+static const f32 g_lightDirMaxShadowDist  = 250.0f;
+static const f32 g_lightDirShadowStepSize = 15.0f;
 
 // TODO: Dynamically compute the world bounds based on content.
 static const GeoBox g_lightWorldBounds = {
@@ -205,6 +206,12 @@ static void rend_clip_frustum_far_to_bounds(GeoVector frustum[8], const GeoBox* 
   rend_clip_frustum_far_to_plane(frustum, &(GeoPlane){geo_backward, -clipBounds->min.z});
 }
 
+static GeoBox rend_light_shadow_discretize(GeoBox box, const f32 step) {
+  box.min = geo_vector_mul(geo_vector_round_nearest(geo_vector_div(box.min, step)), step);
+  box.max = geo_vector_mul(geo_vector_round_nearest(geo_vector_div(box.max, step)), step);
+  return geo_box_dilate(&box, geo_vector(step * 0.5f, step * 0.5f, step * 0.5f));
+}
+
 static GeoMatrix rend_light_dir_shadow_proj(
     const GapWindowComp*      win,
     const SceneCameraComp*    cam,
@@ -227,6 +234,12 @@ static GeoMatrix rend_light_dir_shadow_proj(
     const GeoVector localCorner = geo_matrix_transform3_point(lightViewMatrix, frustum[i]);
     bounds                      = geo_box_encapsulate(&bounds, localCorner);
   }
+
+  /**
+   * Discretize the bounds so the shadow projection stays the same for small movements, this reduces
+   * the visible shadow 'shimmering'.
+   */
+  bounds = rend_light_shadow_discretize(bounds, g_lightDirShadowStepSize);
 
   return geo_matrix_proj_ortho_box(
       bounds.min.x, bounds.max.x, bounds.min.y, bounds.max.y, bounds.min.z, bounds.max.z);
