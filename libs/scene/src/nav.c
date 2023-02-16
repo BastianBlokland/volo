dacbc3d97b376255fb58f7f6f814bd168128f88a
Bastian Blokland <mail@bastian.tech>
2023-02-16 20:03:35 +0200

    scene: Add scene_nav_closest_free_n util

--- libs/scene/src/nav.c
+++ libs/scene/src/nav.c
@@ -473,6 +473,11 @@ GeoNavIsland scene_nav_island(const SceneNavEnvComp* env, const GeoNavCell cell)
   return geo_nav_island(env->navGrid, cell);
 }
 
+u32 scene_nav_closest_free_n(
+    const SceneNavEnvComp* env, const GeoNavCell cell, const GeoNavCellContainer out) {
+  return geo_nav_closest_free_n(env->navGrid, cell, out);
+}
+
 bool scene_nav_reachable(const SceneNavEnvComp* env, const GeoNavCell from, const GeoNavCell to) {
   return geo_nav_reachable(env->navGrid, from, to);
 }
