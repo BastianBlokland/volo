3bbde4734597def1a60e48029d83c86eea56b06f
Bastian Blokland <mail@bastian.tech>
2023-02-23 23:09:45 +0200

    core: Flush denormals to zero

--- libs/core/src/float.c
+++ libs/core/src/float.c
@@ -29,6 +29,11 @@ MAYBE_UNUSED static void float_enable_exceptions() {
 #endif
 }
 
+static void float_enable_denorm_to_zero() {
+  _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+  _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+}
+
 static void float_cpu_id(const i32 functionId, i32 output[4]) {
 #if defined(VOLO_MSVC)
   __cpuid(output, functionId);
@@ -137,6 +142,7 @@ void float_init_thread() {
 #if VOLO_FLOAT_DEBUG
   float_enable_exceptions();
 #endif
+  float_enable_denorm_to_zero();
 }
 
 f16 float_f32_to_f16(const f32 val) { return g_floatF32ToF16Impl(val); }
