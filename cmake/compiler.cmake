07ea765804a68ca03911aead7cc06560ab50dd1e
Bastian Blokland <mail@bastian.tech>
2023-02-25 12:33:06 +0200

    cmake: Target native arch on gcc and clang

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -88,7 +88,8 @@ macro(set_gcc_compile_options)
   add_compile_options(-fno-strict-aliasing)
 
   # Optimization settings.
-  add_compile_options(-O3)
+  add_compile_options(-O3) # Optimization level 3.
+  add_compile_options(-march=native) # Optimize for the native cpu architecture (non portable).
   # add_compile_options(-ffast-math) # Enable (potentially lossy) floating point optimizations.
   add_compile_options(-mf16c) # Enable output of f16c (f32 <-> f16 conversions)
   # add_compile_options(-mfma) # Enable output of 'fused multiply-add' instructions.
@@ -126,7 +127,8 @@ macro(set_clang_compile_options)
   add_compile_options(-fno-strict-aliasing)
 
   # Optimization settings.
-  add_compile_options(-O3)
+  add_compile_options(-O3) # Optimization level 3.
+  add_compile_options(-march=native) # Optimize for the native cpu architecture (non portable).
   # add_compile_options(-ffast-math) # Enable (potentially lossy) floating point optimizations.
   add_compile_options(-mf16c) # Enable output of f16c (f32 <-> f16 conversions)
   # add_compile_options(-mfma) # Enable output of 'fused multiply-add' instructions.
