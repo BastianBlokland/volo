9dbf67cb0f7fd3d25079d0b7344b230d5d47034e
Bastian Blokland <mail@bastian.tech>
2023-02-25 13:14:27 +0200

    cmake: Enable fast-math optimizations

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -91,7 +91,7 @@ macro(set_gcc_compile_options)
   add_compile_options(-O3) # Optimization level 3.
   add_compile_options(-march=native) # Optimize for the native cpu architecture (non portable).
   add_compile_options(-funroll-loops) # Enable loop unrolling.
-  # add_compile_options(-ffast-math) # Enable (potentially lossy) floating point optimizations.
+  add_compile_options(-ffast-math) # Enable (potentially lossy) floating point optimizations.
   add_compile_options(-mf16c) # Enable output of f16c (f32 <-> f16 conversions)
   # add_compile_options(-mfma) # Enable output of 'fused multiply-add' instructions.
 
@@ -131,7 +131,7 @@ macro(set_clang_compile_options)
   add_compile_options(-O3) # Optimization level 3.
   add_compile_options(-march=native) # Optimize for the native cpu architecture (non portable).
   add_compile_options(-funroll-loops) # Enable loop unrolling.
-  # add_compile_options(-ffast-math) # Enable (potentially lossy) floating point optimizations.
+  add_compile_options(-ffast-math) # Enable (potentially lossy) floating point optimizations.
   add_compile_options(-mf16c) # Enable output of f16c (f32 <-> f16 conversions)
   # add_compile_options(-mfma) # Enable output of 'fused multiply-add' instructions.
 
@@ -200,7 +200,7 @@ macro(set_msvc_compile_options)
   add_compile_options(/O2) # Optimization level 2.
   add_compile_options(/Oi) # Enable intrinsic functions.
   add_compile_options(/Gv) # Use the 'vectorcall' calling convention.
-  # add_compile_options(/fp:fast)  # Enable (potentially lossy) floating point optimizations.
+  add_compile_options(/fp:fast)  # Enable (potentially lossy) floating point optimizations.
   add_compile_options(/GS-) # Disable 'Buffer Security Check'.
 
   # Debug options.
