be16e83e0e6b7133f990322cf6c68102adc60382
Bastian Blokland <mail@bastian.tech>
2023-03-12 16:29:00 +0200

    vfx: Create decal draw

--- libs/vfx/src/decal.c
+++ libs/vfx/src/decal.c
@@ -2,8 +2,13 @@
 #include "asset_manager.h"
 #include "ecs_world.h"
 #include "log_logger.h"
+#include "rend_draw.h"
 
-static const String g_vfxDecalAtlasColor = string_static("textures/vfx/decal_color.atl");
+// clang-format off
+static const String         g_vfxDecalGraphic    = string_static("graphics/vfx/decal.gra");
+static const String         g_vfxDecalAtlasColor = string_static("textures/vfx/decal_color.atl");
+static const RendDrawFlags  g_vfxDecalDrawFlags  = /* RendDrawFlags_Decal | */ RendDrawFlags_Preload;
+// clang-format on
 
 typedef enum {
   VfxRenderer_AtlasAcquired  = 1 << 0,
@@ -13,7 +18,18 @@ typedef enum {
 ecs_comp_define(VfxDecalRendererComp) {
   VfxRendererFlags flags;
   EcsEntityId      atlasColor;
+  EcsEntityId      drawEntity;
 };
+
+ecs_comp_define(VfxDecalDrawComp);
+
+static EcsEntityId vfx_decal_draw_create(EcsWorld* world, AssetManagerComp* assets) {
+  const EcsEntityId entity = asset_lookup(world, assets, g_vfxDecalGraphic);
+  ecs_world_add_empty_t(world, entity, VfxDecalDrawComp);
+  RendDrawComp* draw = rend_draw_create(world, entity, g_vfxDecalDrawFlags);
+  rend_draw_set_graphic(draw, entity); // Graphic is on the same entity as the draw.
+  return entity;
+}
 
 ecs_view_define(GlobalView) {
   ecs_access_maybe_write(VfxDecalRendererComp);
@@ -32,6 +48,7 @@ ecs_system_define(VfxDecalRendererInitSys) {
   if (!renderer) {
     renderer             = ecs_world_add_t(world, ecs_world_global(world), VfxDecalRendererComp);
     renderer->atlasColor = asset_lookup(world, assets, g_vfxDecalAtlasColor);
+    renderer->drawEntity = vfx_decal_draw_create(world, assets);
   }
 
   if (!(renderer->flags & (VfxRenderer_AtlasAcquired | VfxRenderer_AtlasUnloading))) {
@@ -71,6 +88,7 @@ ecs_system_define(VfxDecalUnloadChangedAtlasSys) {
 
 ecs_module_init(vfx_decal_module) {
   ecs_register_comp(VfxDecalRendererComp);
+  ecs_register_comp_empty(VfxDecalDrawComp);
 
   ecs_register_view(GlobalView);
 
