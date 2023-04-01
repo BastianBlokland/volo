4681f8440eb0b7904eb0865040937c53c45bbbb3
Bastian Blokland <mail@bastian.tech>
2023-04-01 13:22:20 +0300

    snd: Cleanup alsa device init

--- libs/snd/src/device_pal_linux_alsa.c
+++ libs/snd/src/device_pal_linux_alsa.c
@@ -20,45 +20,6 @@ typedef struct {
 
 static String alsa_error_str(const int err) { return string_from_null_term(snd_strerror(err)); }
 
-static String alsa_pcm_type_str(const snd_pcm_type_t type) {
-  // clang-format off
-  switch (type) {
-  case SND_PCM_TYPE_HW:           return string_lit("Kernel level PCM");
-  case SND_PCM_TYPE_HOOKS:        return string_lit("Hooked PCM");
-  case SND_PCM_TYPE_MULTI:        return string_lit("One or more linked PCM with exclusive access to selected channels");
-  case SND_PCM_TYPE_FILE:         return string_lit("File writing plugin");
-  case SND_PCM_TYPE_NULL:         return string_lit("Null endpoint PCM");
-  case SND_PCM_TYPE_SHM:          return string_lit("Shared memory client PCM");
-  case SND_PCM_TYPE_INET:         return string_lit("INET client PCM");
-  case SND_PCM_TYPE_COPY:         return string_lit("Copying plugin");
-  case SND_PCM_TYPE_LINEAR:       return string_lit("Linear format conversion PCM");
-  case SND_PCM_TYPE_ALAW:         return string_lit("A-Law format conversion PCM");
-  case SND_PCM_TYPE_MULAW:        return string_lit("Mu-Law format conversion PCM");
-  case SND_PCM_TYPE_ADPCM:        return string_lit("IMA-ADPCM format conversion PCM");
-  case SND_PCM_TYPE_RATE:         return string_lit("Rate conversion PCM");
-  case SND_PCM_TYPE_ROUTE:        return string_lit("Attenuated static route PCM");
-  case SND_PCM_TYPE_PLUG:         return string_lit("Format adjusted PCM");
-  case SND_PCM_TYPE_SHARE:        return string_lit("Sharing PCM");
-  case SND_PCM_TYPE_METER:        return string_lit("Meter plugin");
-  case SND_PCM_TYPE_MIX:          return string_lit("Mixing PCM");
-  case SND_PCM_TYPE_DROUTE:       return string_lit("Attenuated dynamic route PCM");
-  case SND_PCM_TYPE_LBSERVER:     return string_lit("Loopback server plugin");
-  case SND_PCM_TYPE_LINEAR_FLOAT: return string_lit("Linear Integer <-> Linear Float format conversion PCM");
-  case SND_PCM_TYPE_LADSPA:       return string_lit("LADSPA integration plugin");
-  case SND_PCM_TYPE_DMIX:         return string_lit("Direct Mixing plugin");
-  case SND_PCM_TYPE_JACK:         return string_lit("Jack Audio Connection Kit plugin");
-  case SND_PCM_TYPE_DSNOOP:       return string_lit("Direct Snooping plugin");
-  case SND_PCM_TYPE_DSHARE:       return string_lit("Direct Sharing plugin");
-  case SND_PCM_TYPE_IEC958:       return string_lit("IEC958 subframe plugin");
-  case SND_PCM_TYPE_SOFTVOL:      return string_lit("Soft volume plugin");
-  case SND_PCM_TYPE_IOPLUG:       return string_lit("External I/O plugin");
-  case SND_PCM_TYPE_EXTPLUG:      return string_lit("External filter plugin");
-  case SND_PCM_TYPE_MMAP_EMUL:    return string_lit("Mmap-emulation plugin");
-  default:                        return string_lit("unknown");
-  }
-  // clang-format on
-}
-
 static snd_pcm_t* alsa_pcm_open() {
   snd_pcm_t* pcm = null;
   const i32  err = snd_pcm_open(&pcm, snd_pcm_device, SND_PCM_STREAM_PLAYBACK, SND_PCM_NONBLOCK);
@@ -157,16 +118,18 @@ SndDevice* snd_device_create(Allocator* alloc) {
     config = alsa_pcm_initialize(pcm);
   }
   if (config.valid) {
-    const snd_pcm_type_t  type = snd_pcm_type(pcm);
-    const snd_pcm_info_t* info = alsa_pcm_info_scratch(pcm);
-    const i32             card = info ? snd_pcm_info_get_card(info) : -1;
+    const snd_pcm_type_t  type  = snd_pcm_type(pcm);
+    const snd_pcm_state_t state = snd_pcm_state(pcm);
+    const snd_pcm_info_t* info  = alsa_pcm_info_scratch(pcm);
+    const i32             card  = info ? snd_pcm_info_get_card(info) : -1;
     const String id = info ? string_from_null_term(snd_pcm_info_get_id(info)) : string_empty;
 
     log_i(
         "Alsa sound device created",
         log_param("id", fmt_text(id)),
         log_param("card", fmt_int(card)),
-        log_param("type", fmt_text(alsa_pcm_type_str(type))),
+        log_param("type", fmt_text(string_from_null_term(snd_pcm_type_name(type)))),
+        log_param("state", fmt_text(string_from_null_term(snd_pcm_state_name(state)))),
         log_param("buffer", fmt_size(config.bufferSize)));
   }
 
