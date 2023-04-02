0e53c554bef0862c65c87ff0c4d3be96a312f739
Bastian Blokland <mail@bastian.tech>
2023-04-02 17:24:44 +0300

    snd: Add output history

--- libs/snd/src/output.c
+++ libs/snd/src/output.c
@@ -9,6 +9,9 @@
 #include "constants_internal.h"
 #include "device_internal.h"
 
+#define snd_output_history_frames 4096
+ASSERT((snd_output_history_frames & (snd_output_history_frames - 1u)) == 0, "Non power-of-two")
+
 typedef struct {
   f32 samples[snd_frame_channels];
 } SndSoundFrame;
@@ -18,11 +21,21 @@ typedef struct {
   usize          frameCount;
 } SndSoundView;
 
-ecs_comp_define(SndOutputComp) { SndDevice* device; };
+ecs_comp_define(SndOutputComp) {
+  SndDevice* device;
+  f32        volume;
+
+  /**
+   * Keep a history of the last N frames in a ring-buffer for analysis and debug purposes.
+   */
+  SndSoundFrame* historyBuffer;
+  usize          historyCursor;
+};
 
 static void ecs_destruct_output_comp(void* data) {
   SndOutputComp* comp = data;
   snd_device_destroy(comp->device);
+  alloc_free_array_t(g_alloc_heap, comp->historyBuffer, snd_output_history_frames);
 }
 
 ecs_view_define(GlobalView) {
@@ -31,8 +44,16 @@ ecs_view_define(GlobalView) {
 }
 
 static SndOutputComp* snd_output_create(EcsWorld* world) {
+  SndSoundFrame* historyBuf = alloc_array_t(g_alloc_heap, SndSoundFrame, snd_output_history_frames);
+  mem_set(mem_create(historyBuf, sizeof(SndSoundFrame) * snd_output_history_frames), 0);
+
   return ecs_world_add_t(
-      world, ecs_world_global(world), SndOutputComp, .device = snd_device_create(g_alloc_heap));
+      world,
+      ecs_world_global(world),
+      SndOutputComp,
+      .device        = snd_device_create(g_alloc_heap),
+      .volume        = 0.25,
+      .historyBuffer = historyBuf);
 }
 
 static void snd_render_sine(SndSoundView out, const TimeSteady time, const f32 frequency) {
@@ -52,19 +73,29 @@ static void snd_render_sine(SndSoundView out, const TimeSteady time, const f32 f
 
 static void snd_render(SndSoundView out, const TimeSteady time) {
   snd_render_sine(out, time, 261.63f);
-  snd_render_sine(out, time, 329.63f);
-  snd_render_sine(out, time, 392.0f);
+  // snd_render_sine(out, time, 329.63f);
+  // snd_render_sine(out, time, 392.0f);
 }
 
-static void snd_output_device_period(
-    const SndDevicePeriod devicePeriod, const SndSoundView buffer, const f32 volume) {
+static void snd_output_history_add(SndOutputComp* outputComp, const u32 channel, const f32 value) {
+  outputComp->historyBuffer[outputComp->historyCursor].samples[channel] = value;
+  outputComp->historyCursor = (outputComp->historyCursor + 1) & (snd_output_history_frames - 1);
+}
+
+static void snd_output_fill_device_period(
+    SndOutputComp* outputComp, const SndDevicePeriod devicePeriod, const SndSoundView buffer) {
   diag_assert(devicePeriod.frameCount == buffer.frameCount);
 
   for (u32 frame = 0; frame != devicePeriod.frameCount; ++frame) {
     for (u32 channel = 0; channel != snd_frame_channels; ++channel) {
-      const f32 val     = buffer.frames[frame].samples[channel] * volume;
+      const f32 val     = buffer.frames[frame].samples[channel] * outputComp->volume;
       const f32 clipped = val > 1.0 ? 1.0f : (val < -1.0 ? -1.0f : val);
+
+      // Write to the device buffer.
       devicePeriod.samples[frame * snd_frame_channels + channel] = (i16)(clipped * i16_max);
+
+      // Add it to the output ring-buffer for analysis / debug purposes.
+      snd_output_history_add(outputComp, channel, clipped);
     }
   }
 }
@@ -75,23 +106,22 @@ ecs_system_define(SndOutputUpdateSys) {
   if (!globalItr) {
     return;
   }
-  SndOutputComp* output = ecs_view_write_t(globalItr, SndOutputComp);
-  if (!output) {
-    output = snd_output_create(world);
+  SndOutputComp* outputComp = ecs_view_write_t(globalItr, SndOutputComp);
+  if (!outputComp) {
+    outputComp = snd_output_create(world);
   }
 
-  if (snd_device_begin(output->device)) {
-    const SndDevicePeriod period = snd_device_period(output->device);
+  if (snd_device_begin(outputComp->device)) {
+    const SndDevicePeriod period = snd_device_period(outputComp->device);
 
     SndSoundFrame      soundFrames[snd_frame_count_max] = {0};
     const SndSoundView soundBuffer = {.frames = soundFrames, .frameCount = period.frameCount};
 
     snd_render(soundBuffer, period.timeBegin);
 
-    const f32 volume = 0.25f;
-    snd_output_device_period(period, soundBuffer, volume);
+    snd_output_fill_device_period(outputComp, period, soundBuffer);
 
-    snd_device_end(output->device);
+    snd_device_end(outputComp->device);
   }
 }
 
