1da8064a61a3012dc047dcf75a1e73cf21f66961
Bastian Blokland <mail@bastian.tech>
2023-04-02 17:04:43 +0300

    snd: Refactor render loop

--- libs/snd/src/output.c
+++ libs/snd/src/output.c
@@ -1,3 +1,5 @@
+#include "core_array.h"
+#include "core_diag.h"
 #include "core_math.h"
 #include "ecs_world.h"
 #include "scene_time.h"
@@ -6,6 +8,15 @@
 
 #include "constants_internal.h"
 #include "device_internal.h"
+
+typedef struct {
+  f32 samples[snd_frame_channels];
+} SndSoundFrame;
+
+typedef struct {
+  SndSoundFrame* frames;
+  usize          frameCount;
+} SndSoundView;
 
 ecs_comp_define(SndOutputComp) { SndDevice* device; };
 
@@ -24,17 +35,37 @@ static SndOutputComp* snd_output_create(EcsWorld* world) {
       world, ecs_world_global(world), SndOutputComp, .device = snd_device_create(g_alloc_heap));
 }
 
-static void snd_output_sine(const SndDevicePeriod period, const f32 frequency, const f32 volume) {
+static void snd_render_sine(SndSoundView out, const TimeSteady time, const f32 frequency) {
   const f64 stepPerSec   = 2.0f * math_pi_f64 * frequency;
   const f64 stepPerFrame = stepPerSec / snd_frame_rate;
 
-  f64 phase = period.timeBegin / (f64)time_second * stepPerSec;
-  for (u32 frame = 0; frame != period.frameCount; ++frame) {
-    const i16 val = (i16)(math_sin_f64(phase) * volume * i16_max);
-
-    period.samples[frame * snd_frame_channels + 0] = val; // Left sample.
-    period.samples[frame * snd_frame_channels + 1] = val; // Right sample.
+  f64 phase = time / (f64)time_second * stepPerSec;
+  for (u32 frame = 0; frame != out.frameCount; ++frame) {
+    const f32 val = (f32)math_sin_f64(phase);
     phase += stepPerFrame;
+
+    for (u32 channel = 0; channel != snd_frame_channels; ++channel) {
+      out.frames[frame].samples[channel] += val;
+    }
+  }
+}
+
+static void snd_render(SndSoundView out, const TimeSteady time) {
+  snd_render_sine(out, time, 261.63f);
+  snd_render_sine(out, time, 329.63f);
+  snd_render_sine(out, time, 392.0f);
+}
+
+static void snd_output_device_period(
+    const SndDevicePeriod devicePeriod, const SndSoundView buffer, const f32 volume) {
+  diag_assert(devicePeriod.frameCount == buffer.frameCount);
+
+  for (u32 frame = 0; frame != devicePeriod.frameCount; ++frame) {
+    for (u32 channel = 0; channel != snd_frame_channels; ++channel) {
+      const f32 val     = buffer.frames[frame].samples[channel] * volume;
+      const f32 clipped = val > 1.0 ? 1.0f : (val < -1.0 ? -1.0f : val);
+      devicePeriod.samples[frame * snd_frame_channels + channel] = (i16)(clipped * i16_max);
+    }
   }
 }
 
@@ -52,7 +83,14 @@ ecs_system_define(SndOutputUpdateSys) {
   if (snd_device_begin(output->device)) {
     const SndDevicePeriod period = snd_device_period(output->device);
 
-    snd_output_sine(period, 440, 0.1f);
+    SndSoundFrame      soundFrames[snd_frame_count_max] = {0};
+    const SndSoundView soundBuffer = {.frames = soundFrames, .frameCount = period.frameCount};
+
+    snd_render(soundBuffer, period.timeBegin);
+
+    const f32 volume = 0.25f;
+    snd_output_device_period(period, soundBuffer, volume);
+
     snd_device_end(output->device);
   }
 }
