20507aa271a0d19f8744cd1e163b07cb58350e52
Bastian Blokland <mail@bastian.tech>
2023-04-15 20:59:39 +0300

    snd: Support updating of pitch and gain

--- libs/snd/src/source.c
+++ libs/snd/src/source.c
@@ -2,57 +2,76 @@
 #include "ecs_world.h"
 #include "log_logger.h"
 #include "scene_sound.h"
+#include "scene_time.h"
 #include "snd_mixer.h"
 
 ecs_comp_define(SndSourceComp) { SndObjectId objectId; };
+ecs_comp_define(SndSourceBrokenComp);
 
-ecs_view_define(InitGlobalView) { ecs_access_write(SndMixerComp); }
-
-ecs_view_define(InitView) {
-  ecs_access_read(SceneSoundComp);
-  ecs_access_without(SndSourceComp);
+ecs_view_define(UpdateGlobalView) {
+  ecs_access_write(SndMixerComp);
+  ecs_access_read(SceneTimeSettingsComp);
+  ecs_access_without(SndSourceBrokenComp);
 }
 
-ecs_system_define(SndSourceInitSys) {
-  EcsView*     globalView = ecs_world_view_t(world, InitGlobalView);
+ecs_view_define(UpdateView) {
+  ecs_access_read(SceneSoundComp);
+  ecs_access_maybe_read(SndSourceComp);
+}
+
+ecs_system_define(SndSourceUpdateSys) {
+  EcsView*     globalView = ecs_world_view_t(world, UpdateGlobalView);
   EcsIterator* globalItr  = ecs_view_maybe_at(globalView, ecs_world_global(world));
   if (!globalItr) {
     return;
   }
-  SndMixerComp* mixer = ecs_view_write_t(globalItr, SndMixerComp);
+  SndMixerComp*                m            = ecs_view_write_t(globalItr, SndMixerComp);
+  const SceneTimeSettingsComp* timeSettings = ecs_view_read_t(globalItr, SceneTimeSettingsComp);
 
-  EcsView* initView = ecs_world_view_t(world, InitView);
-  for (EcsIterator* itr = ecs_view_itr(initView); ecs_view_walk(itr);) {
-    const EcsEntityId     entity  = ecs_view_entity(itr);
-    const SceneSoundComp* sndComp = ecs_view_read_t(itr, SceneSoundComp);
-    if (!ecs_entity_valid(sndComp->asset)) {
-      log_e("SceneSoundComp is missing an asset");
-      continue;
-    }
-    SndObjectId id;
-    if (snd_object_new(mixer, &id) == SndResult_Success) {
-      snd_object_set_asset(mixer, id, sndComp->asset);
-      if (sndComp->looping) {
-        snd_object_set_looping(mixer, id);
+  f32 timeScale;
+  if (timeSettings->flags & SceneTimeFlags_Paused) {
+    timeScale = 0.0;
+  } else {
+    timeScale = timeSettings->scale;
+  }
+
+  EcsView* updateView = ecs_world_view_t(world, UpdateView);
+  for (EcsIterator* itr = ecs_view_itr(updateView); ecs_view_walk(itr);) {
+    const EcsEntityId     entity     = ecs_view_entity(itr);
+    const SceneSoundComp* sndComp    = ecs_view_read_t(itr, SceneSoundComp);
+    const SndSourceComp*  sourceComp = ecs_view_read_t(itr, SndSourceComp);
+
+    if (!sourceComp) {
+      if (!ecs_entity_valid(sndComp->asset)) {
+        log_e("SceneSoundComp is missing an asset");
+        ecs_world_add_empty_t(world, entity, SndSourceBrokenComp);
+        continue;
       }
-      if (sndComp->pitch > f32_epsilon) {
-        snd_object_set_pitch(mixer, id, sndComp->pitch);
-      }
-      if (sndComp->gain > f32_epsilon) {
-        for (SndChannel chan = 0; chan != SndChannel_Count; ++chan) {
-          snd_object_set_gain(mixer, id, chan, sndComp->gain);
+      SndObjectId id;
+      if (snd_object_new(m, &id) == SndResult_Success) {
+        snd_object_set_asset(m, id, sndComp->asset);
+        if (sndComp->looping) {
+          snd_object_set_looping(m, id);
         }
+        sourceComp = ecs_world_add_t(world, entity, SndSourceComp, .objectId = id);
+      } else {
+        continue; // Failed to create a sound-object; retry next tick.
       }
-      ecs_world_add_t(world, entity, SndSourceComp, .objectId = id);
+    }
+
+    snd_object_set_pitch(m, sourceComp->objectId, sndComp->pitch * timeScale);
+    for (SndChannel chan = 0; chan != SndChannel_Count; ++chan) {
+      snd_object_set_gain(m, sourceComp->objectId, chan, sndComp->gain);
     }
   }
 }
 
 ecs_module_init(snd_source_module) {
   ecs_register_comp(SndSourceComp);
+  ecs_register_comp_empty(SndSourceBrokenComp);
 
-  ecs_register_view(InitGlobalView);
-  ecs_register_view(InitView);
+  ecs_register_view(UpdateGlobalView);
+  ecs_register_view(UpdateView);
 
-  ecs_register_system(SndSourceInitSys, ecs_view_id(InitGlobalView), ecs_view_id(InitView));
+  ecs_register_system(SndSourceUpdateSys, ecs_view_id(UpdateGlobalView), ecs_view_id(UpdateView));
 }
