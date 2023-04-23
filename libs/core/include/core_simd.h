bebd22d4391a84d6e939a61a6c09ebd2c6bf7ba8
Bastian Blokland <mail@bastian.tech>
2023-04-23 15:40:52 +0300

    core: Minor cleanup in intrinsics

--- libs/core/include/core_simd.h
+++ libs/core/include/core_simd.h
@@ -24,123 +24,140 @@ typedef __m128 SimdVec;
  * Load 4 (128 bit aligned) float values into a Simd vector.
  * Pre-condition: bits_aligned_ptr(values, 16)
  */
-INLINE_HINT static SimdVec simd_vec_load(const f32 values[4]) { return _mm_load_ps(values); }
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_load(const f32 values[static 4]) {
+  return _mm_load_ps(values);
+}
 
 /**
  * Store a Simd vector to 4 (128 bit aligned) float values.
  * Pre-condition: bits_aligned_ptr(values, 16)
  */
-INLINE_HINT static void simd_vec_store(const SimdVec vec, f32 values[4]) {
+MAYBE_UNUSED INLINE_HINT static void simd_vec_store(const SimdVec vec, f32 values[static 4]) {
   _mm_store_ps(values, vec);
 }
 
-INLINE_HINT static SimdVec simd_vec_zero() { return _mm_setzero_ps(); }
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_zero() { return _mm_setzero_ps(); }
 
-INLINE_HINT static f32 simd_vec_x(const SimdVec vec) { return _mm_cvtss_f32(vec); }
+MAYBE_UNUSED INLINE_HINT static f32 simd_vec_x(const SimdVec vec) { return _mm_cvtss_f32(vec); }
 
-INLINE_HINT static SimdVec simd_vec_set(const f32 a, const f32 b, const f32 c, const f32 d) {
+MAYBE_UNUSED INLINE_HINT static SimdVec
+simd_vec_set(const f32 a, const f32 b, const f32 c, const f32 d) {
   return _mm_set_ps(d, c, b, a);
 }
 
-INLINE_HINT static SimdVec simd_vec_broadcast(const f32 value) { return _mm_set1_ps(value); }
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_broadcast(const f32 value) {
+  return _mm_set1_ps(value);
+}
 
-INLINE_HINT static SimdVec simd_vec_clear_w(const SimdVec vec) {
-  static const u32 g_mask[4] = {~u32_lit(0), ~u32_lit(0), ~u32_lit(0), 0};
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_clear_w(const SimdVec vec) {
+  ALIGNAS(16) static const u32 g_mask[4] = {~u32_lit(0), ~u32_lit(0), ~u32_lit(0), 0};
   // NOTE: Can we do this without a memory load?
   return _mm_and_ps(vec, simd_vec_load((f32*)g_mask));
 }
 
-INLINE_HINT static SimdVec simd_vec_copy_w(const SimdVec dst, const SimdVec src) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_copy_w(const SimdVec dst, const SimdVec src) {
   return _mm_blend_ps(dst, src, 0b1000);
 }
 
-INLINE_HINT static SimdVec simd_vec_w_one(const SimdVec vec) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_w_one(const SimdVec vec) {
   return simd_vec_copy_w(vec, simd_vec_broadcast(1.0f));
 }
 
-INLINE_HINT static SimdVec simd_vec_w_all_ones(const SimdVec vec) {
-  static const u32 g_mask[4] = {0, 0, 0, ~u32_lit(0)};
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_w_all_ones(const SimdVec vec) {
+  ALIGNAS(16) static const u32 g_mask[4] = {0, 0, 0, ~u32_lit(0)};
   return simd_vec_copy_w(vec, simd_vec_load((f32*)g_mask));
 }
 
-INLINE_HINT static SimdVec simd_vec_add(const SimdVec a, const SimdVec b) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_add(const SimdVec a, const SimdVec b) {
   return _mm_add_ps(a, b);
 }
 
-INLINE_HINT static SimdVec simd_vec_sub(const SimdVec a, const SimdVec b) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_sub(const SimdVec a, const SimdVec b) {
   return _mm_sub_ps(a, b);
 }
 
-INLINE_HINT static SimdVec simd_vec_mul(const SimdVec a, const SimdVec b) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_mul(const SimdVec a, const SimdVec b) {
   return _mm_mul_ps(a, b);
 }
 
-INLINE_HINT static SimdVec simd_vec_div(const SimdVec a, const SimdVec b) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_div(const SimdVec a, const SimdVec b) {
   return _mm_div_ps(a, b);
 }
 
-INLINE_HINT static SimdVec simd_vec_min(const SimdVec a, const SimdVec b) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_min(const SimdVec a, const SimdVec b) {
   return _mm_min_ps(a, b);
 }
 
-INLINE_HINT static SimdVec simd_vec_max(const SimdVec a, const SimdVec b) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_max(const SimdVec a, const SimdVec b) {
   return _mm_max_ps(a, b);
 }
 
-INLINE_HINT static SimdVec simd_vec_less(const SimdVec a, const SimdVec b) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_less(const SimdVec a, const SimdVec b) {
   return _mm_cmplt_ps(a, b);
 }
 
-INLINE_HINT static SimdVec simd_vec_greater(const SimdVec a, const SimdVec b) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_greater(const SimdVec a, const SimdVec b) {
   return _mm_cmpgt_ps(a, b);
 }
 
-INLINE_HINT static SimdVec simd_vec_and(const SimdVec a, const SimdVec b) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_and(const SimdVec a, const SimdVec b) {
   return _mm_and_ps(a, b);
 }
 
-INLINE_HINT static bool simd_vec_any_true(const SimdVec a) { return _mm_movemask_ps(a) != 0b0000; }
-INLINE_HINT static bool simd_vec_any_false(const SimdVec a) { return _mm_movemask_ps(a) != 0b1111; }
-INLINE_HINT static bool simd_vec_all_true(const SimdVec a) { return _mm_movemask_ps(a) == 0b1111; }
-INLINE_HINT static bool simd_vec_all_false(const SimdVec a) { return _mm_movemask_ps(a) == 0b0000; }
+MAYBE_UNUSED INLINE_HINT static bool simd_vec_any_true(const SimdVec a) {
+  return _mm_movemask_ps(a) != 0b0000;
+}
 
-INLINE_HINT static SimdVec simd_vec_select(const SimdVec a, const SimdVec b, const SimdVec mask) {
+MAYBE_UNUSED INLINE_HINT static bool simd_vec_any_false(const SimdVec a) {
+  return _mm_movemask_ps(a) != 0b1111;
+}
+
+MAYBE_UNUSED INLINE_HINT static bool simd_vec_all_true(const SimdVec a) {
+  return _mm_movemask_ps(a) == 0b1111;
+}
+
+MAYBE_UNUSED INLINE_HINT static bool simd_vec_all_false(const SimdVec a) {
+  return _mm_movemask_ps(a) == 0b0000;
+}
+
+MAYBE_UNUSED INLINE_HINT static SimdVec
+simd_vec_select(const SimdVec a, const SimdVec b, const SimdVec mask) {
   return _mm_blendv_ps(a, b, mask);
 }
 
-INLINE_HINT static SimdVec simd_vec_abs(const SimdVec a) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_abs(const SimdVec a) {
   const SimdVec signBit = _mm_set1_ps(-0.0f);
   return _mm_andnot_ps(signBit, a);
 }
 
-INLINE_HINT static SimdVec simd_vec_round_nearest(const SimdVec a) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_round_nearest(const SimdVec a) {
   return _mm_round_ps(a, _MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC);
 }
 
-INLINE_HINT static SimdVec simd_vec_round_down(const SimdVec a) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_round_down(const SimdVec a) {
   return _mm_round_ps(a, _MM_FROUND_TO_NEG_INF | _MM_FROUND_NO_EXC);
 }
 
-INLINE_HINT static SimdVec simd_vec_round_up(const SimdVec a) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_round_up(const SimdVec a) {
   return _mm_round_ps(a, _MM_FROUND_TO_POS_INF | _MM_FROUND_NO_EXC);
 }
 
-INLINE_HINT static SimdVec simd_vec_dot4(const SimdVec a, const SimdVec b) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_dot4(const SimdVec a, const SimdVec b) {
   const SimdVec mul = _mm_mul_ps(a, b);
   const SimdVec t1  = _mm_hadd_ps(mul, mul);
   return _mm_hadd_ps(t1, t1);
 }
 
-INLINE_HINT static SimdVec simd_vec_dot3(const SimdVec a, const SimdVec b) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_dot3(const SimdVec a, const SimdVec b) {
   const SimdVec mul = _mm_mul_ps(a, b);
   const SimdVec t1  = simd_vec_clear_w(mul);
   const SimdVec t2  = _mm_hadd_ps(t1, t1);
   return _mm_hadd_ps(t2, t2);
 }
 
-INLINE_HINT static SimdVec simd_vec_sqrt(const SimdVec a) { return _mm_sqrt_ps(a); }
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_sqrt(const SimdVec a) { return _mm_sqrt_ps(a); }
 
-INLINE_HINT static SimdVec simd_vec_cross3(const SimdVec a, const SimdVec b) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_cross3(const SimdVec a, const SimdVec b) {
   const SimdVec t1  = simd_vec_permute(a, 3, 0, 2, 1);  // = (a.y, a.z, a.x, a.w)
   const SimdVec t2  = simd_vec_permute(b, 3, 1, 0, 2);  // = (b.z, b.x, b.y, b.w)
   SimdVec       res = _mm_mul_ps(t1, t2);               // Perform the left operation
@@ -149,7 +166,7 @@ INLINE_HINT static SimdVec simd_vec_cross3(const SimdVec a, const SimdVec b) {
   return _mm_sub_ps(res, _mm_mul_ps(t3, t4));           // Perform the right operation
 }
 
-INLINE_HINT static SimdVec simd_quat_mul(const SimdVec xyzw, const SimdVec abcd) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_quat_mul(const SimdVec xyzw, const SimdVec abcd) {
   /**
    * Multiply two quaternions.
    * Source: https://momchil-velikov.blogspot.com/2013/10/fast-sse-quternion-multiplication.html
@@ -177,7 +194,7 @@ INLINE_HINT static SimdVec simd_quat_mul(const SimdVec xyzw, const SimdVec abcd)
   return simd_vec_permute(XZWY, 2, 1, 3, 0);
 }
 
-INLINE_HINT static SimdVec simd_quat_rotate(const SimdVec quat, const SimdVec vec) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_quat_rotate(const SimdVec quat, const SimdVec vec) {
   const SimdVec scalar = simd_vec_splat(quat, 3);
   const SimdVec axis   = simd_vec_clear_w(quat);
   const SimdVec a      = simd_vec_cross3(axis, vec);
@@ -185,13 +202,13 @@ INLINE_HINT static SimdVec simd_quat_rotate(const SimdVec quat, const SimdVec ve
   return simd_vec_add(vec, simd_vec_mul(b, simd_vec_broadcast(2.0f)));
 }
 
-INLINE_HINT static SimdVec simd_quat_norm(const SimdVec quat) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_quat_norm(const SimdVec quat) {
   const SimdVec sqrMag = simd_vec_dot4(quat, quat);
   const SimdVec mag    = simd_vec_sqrt(sqrMag);
   return simd_vec_div(quat, mag);
 }
 
-INLINE_HINT static SimdVec simd_quat_norm_approx(const SimdVec quat) {
+MAYBE_UNUSED INLINE_HINT static SimdVec simd_quat_norm_approx(const SimdVec quat) {
   const SimdVec sqrMag = simd_vec_dot4(quat, quat);
   return simd_vec_mul(quat, _mm_rsqrt_ps(sqrMag));
 }
