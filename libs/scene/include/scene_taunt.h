a16fca8eb119998e4ac041cc26e6f29e163cb4de
Bastian Blokland <mail@bastian.tech>
2023-04-26 19:34:54 +0300

    scene: Hook up taunt dead event

--- libs/scene/include/scene_taunt.h
+++ libs/scene/include/scene_taunt.h
@@ -2,9 +2,16 @@
 #include "ecs_module.h"
 
 typedef enum {
+  SceneTauntRequests_Death = 1 << 0,
+} SceneTauntRequests;
+
+typedef enum {
   SceneTauntType_Death,
 
   SceneTauntType_Count,
 } SceneTauntType;
 
-ecs_comp_extern_public(SceneTauntComp) { StringHash tauntPrefabs[SceneTauntType_Count]; };
+ecs_comp_extern_public(SceneTauntComp) {
+  SceneTauntRequests requests;
+  StringHash         tauntPrefabs[SceneTauntType_Count];
+};
