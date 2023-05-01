ce61cbcb031c4fd2b9a343bf1fcb61774cfc0114
Bastian Blokland <mail@bastian.tech>
2023-05-01 15:13:54 +0300

    ui: Remove the layers shapes

--- libs/ui/include/ui_shape.h
+++ libs/ui/include/ui_shape.h
@@ -19,8 +19,6 @@
   X(0xE429, Tune)                                                                                  \
   X(0xE4FC, QueryStats)                                                                            \
   X(0xE518, Light)                                                                                 \
-  X(0xE53B, Layers)                                                                                \
-  X(0xE53C, LayersClear)                                                                           \
   X(0xE574, Category)                                                                              \
   X(0xE5CA, Check)                                                                                 \
   X(0xE5CD, Close)                                                                                 \
