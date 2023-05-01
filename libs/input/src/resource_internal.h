51bb9f23157264ee313db4e3c2d79125e6d71132
Bastian Blokland <mail@bastian.tech>
2023-05-01 21:02:42 +0300

    input: Increase maximum input maps

--- libs/input/src/resource_internal.h
+++ libs/input/src/resource_internal.h
@@ -2,7 +2,7 @@
 #include "ecs_entity.h"
 #include "input_resource.h"
 
-#define input_resource_max_maps 2
+#define input_resource_max_maps 3
 
 u32 input_resource_maps(
     const InputResourceComp*, EcsEntityId out[PARAM_ARRAY_SIZE(input_resource_max_maps)]);
