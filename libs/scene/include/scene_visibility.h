043e277b5fa87edbffa277b1119ea82e01c590f3
Bastian Blokland <mail@bastian.tech>
2023-06-04 17:59:52 +0300

    scene: Track visibility

--- libs/scene/include/scene_visibility.h
+++ libs/scene/include/scene_visibility.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "ecs_module.h"
+#include "geo_vector.h"
+#include "scene_faction.h"
 
 ecs_comp_extern_public(SceneVisibilityEnvComp);
 
 ecs_comp_extern_public(SceneVisionComp) { f32 radius; };
+
+/**
+ * Check if the specified position is visible for this faction.
+ */
+bool scene_visible(const SceneVisibilityEnvComp*, GeoVector pos, SceneFaction);
