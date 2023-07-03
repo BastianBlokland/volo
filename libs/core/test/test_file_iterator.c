1c4abe35cc81939134e9527944e071df8dcffaa3
Bastian Blokland <mail@bastian.tech>
2023-07-03 22:48:49 +0300

    core: Add multi-file file iterator test

--- libs/core/test/test_file_iterator.c
+++ libs/core/test/test_file_iterator.c
@@ -1,5 +1,7 @@
 #include "check_spec.h"
 #include "core_alloc.h"
+#include "core_array.h"
+#include "core_bits.h"
 #include "core_diag.h"
 #include "core_file_iterator.h"
 #include "core_path.h"
@@ -44,8 +46,8 @@ spec(file_iterator) {
     // Assert end of iterator.
     check_eq_int(file_iterator_next(itr, &entry), FileIteratorResult_End);
 
-    check_eq_int(file_delete_sync(filePath), FileResult_Success);
     file_iterator_destroy(itr);
+    check_eq_int(file_delete_sync(filePath), FileResult_Success);
   }
 
   it("finds a single entry in a directory with one sub-directory") {
@@ -64,8 +66,50 @@ spec(file_iterator) {
     // Assert end of iterator.
     check_eq_int(file_iterator_next(itr, &entry), FileIteratorResult_End);
 
-    check_eq_int(file_delete_dir_sync(subDirPath), FileResult_Success);
     file_iterator_destroy(itr);
+    check_eq_int(file_delete_dir_sync(subDirPath), FileResult_Success);
+  }
+
+  it("can find multiple files in a directory") {
+    static const String g_fileNames[] = {
+        string_static("a"),
+        string_static("b"),
+        string_static("c"),
+        string_static("d"),
+    };
+    array_for_t(g_fileNames, String, name) {
+      const String filePath = path_build_scratch(dirPath, *name);
+      check_eq_int(file_write_to_path_sync(filePath, *name), FileResult_Success);
+    }
+
+    FileIterator* itr = file_iterator_create(g_alloc_heap, dirPath);
+
+    // Try to find all files.
+    u32 foundFiles = 0;
+    for (u32 i = 0; i != array_elems(g_fileNames); ++i) {
+      check_eq_int(file_iterator_next(itr, &entry), FileIteratorResult_Found);
+      check_eq_int(entry.type, FileType_Regular);
+
+      for (u32 expectedIdx = 0; expectedIdx != array_elems(g_fileNames); ++expectedIdx) {
+        if (string_eq(entry.name, g_fileNames[expectedIdx])) {
+          foundFiles |= 1 << expectedIdx;
+          break;
+        }
+      }
+    }
+
+    // Assert all files are found.
+    check_eq_int(bits_popcnt(foundFiles), array_elems(g_fileNames));
+
+    // Assert end of iterator.
+    check_eq_int(file_iterator_next(itr, &entry), FileIteratorResult_End);
+
+    file_iterator_destroy(itr);
+
+    array_for_t(g_fileNames, String, name) {
+      const String filePath = path_build_scratch(dirPath, *name);
+      check_eq_int(file_delete_sync(filePath), FileResult_Success);
+    }
   }
 
   it("fails when iterating a directory that does not exist") {
