8492de21936c4668ec59f14cc7622039687dd1c0
Bastian Blokland <mail@bastian.tech>
2023-09-03 18:00:02 +0300

    Pack skinned vertices to 32 bytes (#572)
    * rend: Fix invalid unpack routine in dyn mesh viewer
    
    * rend: Pack skinned vertices to 32 bytes

--- assets/shaders/include/vertex.glsl
+++ assets/shaders/include/vertex.glsl
@@ -25,12 +25,10 @@ struct Vertex {
       f32v4((_VERT_).data4))
 
 struct VertexSkinnedPacked {
-  f16v4 data1; // x, y, z position
-  f16v4 data2; // x, y texcoord
-  f16v4 data3; // x, y, z normal
-  f16v4 data4; // x, y, z tangent, w tangent handedness
-  u16v4 data5; // x jointIndex0, y jointIndex1, z jointIndex2, w jointIndex3
-  f16v4 data6; // x jointWeight0, y jointWeight1, z jointWeight2, w jointWeight3
+  f16v4 data1; // x, y, z position, w texcoord x
+  f16v4 data2; // x, y, z normal , w texcoord y
+  f16v4 data3; // x, y, z tangent, w tangent handedness
+  u16v4 data4; // x jntIndexWeight0, y jntIndexWeight1, z jntIndexWeight2, w jntIndexWeight3,
 };
 
 struct VertexSkinned {
@@ -45,10 +43,18 @@ struct VertexSkinned {
 #define vert_skinned_unpack(_VERT_)                                                                \
   VertexSkinned(                                                                                   \
       f32v4((_VERT_).data1).xyz,                                                                   \
-      f32v2(f32v4((_VERT_).data2).xy),                                                             \
-      f32v4((_VERT_).data3).xyz,                                                                   \
-      f32v4((_VERT_).data4),                                                                       \
-      u32v4((_VERT_).data5),                                                                       \
-      f32v4((_VERT_).data6))
+      f32v2(f32v4((_VERT_).data1).w, f32v4((_VERT_).data2).w),                                     \
+      f32v4((_VERT_).data2).xyz,                                                                   \
+      f32v4((_VERT_).data3),                                                                       \
+      u32v4(                                                                                       \
+          u32v4((_VERT_).data4).x& u32(0xFF),                                                      \
+          u32v4((_VERT_).data4).y& u32(0xFF),                                                      \
+          u32v4((_VERT_).data4).z& u32(0xFF),                                                      \
+          u32v4((_VERT_).data4).w& u32(0xFF)),                                                     \
+      f32v4(                                                                                       \
+          (u32v4((_VERT_).data4).x >> 8) / 256.0,                                                  \
+          (u32v4((_VERT_).data4).y >> 8) / 256.0,                                                  \
+          (u32v4((_VERT_).data4).z >> 8) / 256.0,                                                  \
+          (u32v4((_VERT_).data4).w >> 8) / 256.0))
 
 #endif // INCLUDE_VERTEX
