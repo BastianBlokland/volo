c735ed4a4efd6ba865c645f7b1857ca1dc495e57
Bastian Blokland <mail@bastian.tech>
2023-09-14 21:59:04 +0300

    script: Support var declare in binder

--- libs/script/include/script_binder.h
+++ libs/script/include/script_binder.h
@@ -1,7 +1,10 @@
 #pragma once
+#include "core_string.h"
 
 // Forward declare from 'core_alloc.h'.
 typedef struct sAllocator Allocator;
+
+typedef u32 ScriptBindSlot;
 
 /**
  * Table of native functions to bind.
@@ -18,3 +21,23 @@ ScriptBinder* script_binder_create(Allocator*);
  * Destroy a ScriptBinder instance.
  */
 void script_binder_destroy(ScriptBinder*);
+
+/**
+ * Declare a function to bind.
+ * Pre-condition: Binder has not been prepared.
+ */
+void script_binder_declare(ScriptBinder*, StringHash name);
+
+/**
+ * Prepare the binder for linking.
+ * NOTE: No more declarations can be made after calling this.
+ * Pre-condition: Binder has not been prepared.
+ */
+void script_binder_prepare(ScriptBinder*);
+
+/**
+ * Lookup a declaration by name.
+ * NOTE: Returns sentinel_u32 if no declaration was found with the given name.
+ * Pre-condition: Binder has been prepared.
+ */
+ScriptBindSlot script_binder_lookup(const ScriptBinder*, StringHash name);
--- libs/script/src/binder.c
+++ libs/script/src/binder.c
@@ -1,9 +1,21 @@
 #include "core_alloc.h"
+#include "core_compare.h"
 #include "core_diag.h"
+#include "core_search.h"
+#include "core_sort.h"
 #include "script_binder.h"
 
+#define script_binder_max_funcs 64
+
+typedef enum {
+  ScriptBinderFlags_Prepared = 1 << 0,
+} ScriptBinderFlags;
+
 struct sScriptBinder {
-  Allocator* alloc;
+  Allocator*        alloc;
+  ScriptBinderFlags flags;
+  u32               count;
+  StringHash        names[script_binder_max_funcs];
 };
 
 ScriptBinder* script_binder_create(Allocator* alloc) {
@@ -15,3 +27,26 @@ ScriptBinder* script_binder_create(Allocator* alloc) {
 }
 
 void script_binder_destroy(ScriptBinder* binder) { alloc_free_t(binder->alloc, binder); }
+
+void script_binder_declare(ScriptBinder* binder, const StringHash name) {
+  diag_assert_msg(!(binder->flags & ScriptBinderFlags_Prepared), "Binder already prepared");
+  diag_assert_msg(binder->count < script_binder_max_funcs, "Bound function count exceeds max");
+
+  binder->names[binder->count++] = name;
+}
+
+void script_binder_prepare(ScriptBinder* binder) {
+  diag_assert_msg(!(binder->flags & ScriptBinderFlags_Prepared), "Binder already prepared");
+
+  sort_quicksort_t(binder->names, binder->names + binder->count, StringHash, compare_stringhash);
+  binder->flags |= ScriptBinderFlags_Prepared;
+}
+
+ScriptBindSlot script_binder_lookup(const ScriptBinder* binder, const StringHash name) {
+  diag_assert_msg(binder->flags & ScriptBinderFlags_Prepared, "Binder has not been prepared");
+
+  const StringHash* itr = search_binary_t(
+      binder->names, binder->names + binder->count, StringHash, compare_stringhash, &name);
+
+  return itr ? (u32)(itr - binder->names) : sentinel_u32;
+}
