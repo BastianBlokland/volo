41763c471f201280789515a542e9897bbbf4ead8
Bastian Blokland <mail@bastian.tech>
2023-10-01 16:26:14 +0300

    script: Add arg access apis

--- libs/script/include/script_args.h
+++ libs/script/include/script_args.h
@@ -1,5 +1,8 @@
 #pragma once
-#include "core_types.h"
+#include "core_string.h"
+#include "core_time.h"
+#include "ecs_entity.h"
+#include "geo_quat.h"
 
 // Forward declare from 'script_val.h'.
 typedef union uScriptVal ScriptVal;
@@ -8,5 +11,13 @@ typedef struct {
   const ScriptVal* values;
   usize            count;
 } ScriptArgs;
+
+f64          script_arg_number(ScriptArgs, u32 i, f64 fallback);
+bool         script_arg_bool(ScriptArgs, u32 i, bool fallback);
+GeoVector    script_arg_vector3(ScriptArgs, u32 i, GeoVector fallback);
+GeoQuat      script_arg_quat(ScriptArgs, u32 i, GeoQuat fallback);
+EcsEntityId  script_arg_entity(ScriptArgs, u32 i, EcsEntityId fallback);
+StringHash   script_arg_string(ScriptArgs, u32 i, StringHash fallback);
+TimeDuration script_arg_time(ScriptArgs, u32 i, TimeDuration fallback);
 
 ScriptVal script_arg_last_or_null(ScriptArgs);
--- libs/script/src/args.c
+++ libs/script/src/args.c
@@ -1,6 +1,34 @@
 #include "script_args.h"
 #include "script_val.h"
 
+f64 script_arg_number(const ScriptArgs args, const u32 i, const f64 fallback) {
+  return args.count > i ? script_get_number(args.values[i], fallback) : fallback;
+}
+
+bool script_arg_bool(const ScriptArgs args, const u32 i, const bool fallback) {
+  return args.count > i ? script_get_bool(args.values[i], fallback) : fallback;
+}
+
+GeoVector script_arg_vector3(const ScriptArgs args, const u32 i, const GeoVector fallback) {
+  return args.count > i ? script_get_vector3(args.values[i], fallback) : fallback;
+}
+
+GeoQuat script_arg_quat(const ScriptArgs args, const u32 i, const GeoQuat fallback) {
+  return args.count > i ? script_get_quat(args.values[i], fallback) : fallback;
+}
+
+EcsEntityId script_arg_entity(const ScriptArgs args, const u32 i, const EcsEntityId fallback) {
+  return args.count > i ? script_get_entity(args.values[i], fallback) : fallback;
+}
+
+StringHash script_arg_string(const ScriptArgs args, const u32 i, const StringHash fallback) {
+  return args.count > i ? script_get_string(args.values[i], fallback) : fallback;
+}
+
+TimeDuration script_arg_time(const ScriptArgs args, const u32 i, const TimeDuration fallback) {
+  return args.count > i ? script_get_time(args.values[i], fallback) : fallback;
+}
+
 ScriptVal script_arg_last_or_null(const ScriptArgs args) {
   return args.count ? args.values[args.count - 1] : script_null();
 }
