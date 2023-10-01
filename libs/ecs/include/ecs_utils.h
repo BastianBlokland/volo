1d18e7c083132198e880f1e377ac28760a6d51c7
Bastian Blokland <mail@bastian.tech>
2023-10-01 13:13:52 +0300

    ecs: Fix typo

--- libs/ecs/include/ecs_utils.h
+++ libs/ecs/include/ecs_utils.h
@@ -69,7 +69,7 @@ const void* ecs_utils_read_or_add(EcsWorld*, const EcsIterator*, EcsCompId);
  * Get a read/write pointer to an existing component or add a new component if the entity does not
  * have the requested component.
  *
- * Pre-condition: iterator has been intialized using ecs_view_walk() / ecs_view_jump().
+ * Pre-condition: iterator has been initalized using ecs_view_walk() / ecs_view_jump().
  * Pre-condition: view has 'Write' access to the given component type.
  */
 #define ecs_utils_write_or_add_t(_WORLD_, _ITR_, _TYPE_)                                           \
