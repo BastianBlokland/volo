9d40b2bdf7cae16956dcb84174bdcbb8c3abb692
Bastian Blokland <mail@bastian.tech>
2023-10-03 21:46:49 +0300

    lsp: Add stdio flag

--- apps/utilities/lsp.c
+++ apps/utilities/lsp.c
@@ -5,13 +5,17 @@
  * Language Server Protocol implementation for the Volo script language.
  */
 
-static CliId g_optHelp;
+static CliId g_optHelp, g_stdioFlag;
 
 void app_cli_configure(CliApp* app) {
   cli_app_register_desc(app, string_lit("Volo Script Language Server"));
 
+  g_stdioFlag = cli_register_flag(app, 0, string_lit("stdio"), CliOptionFlags_None);
+  cli_register_desc(app, g_stdioFlag, string_lit("Use stdin and stdout for communication."));
+
   g_optHelp = cli_register_flag(app, 'h', string_lit("help"), CliOptionFlags_None);
   cli_register_desc(app, g_optHelp, string_lit("Display this help page."));
+  cli_register_exclusions(app, g_optHelp, g_stdioFlag);
 }
 
 i32 app_cli_run(const CliApp* app, const CliInvocation* invoc) {
