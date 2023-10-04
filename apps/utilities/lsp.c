41cb033d3c8a0a21134f60a58f6c538b1c797135
Bastian Blokland <mail@bastian.tech>
2023-10-04 21:41:21 +0300

    lsp: Refactor read buffer handling

--- apps/utilities/lsp.c
+++ apps/utilities/lsp.c
@@ -28,6 +28,7 @@ static const String g_lspStatusMessage[LspStatus_Count] = {
 typedef struct {
   LspStatus  status;
   DynString* readBuffer;
+  usize      readCursor;
   File*      in;
   File*      out;
 } LspContext;
@@ -36,10 +37,20 @@ typedef struct {
   usize contentLength;
 } LspHeader;
 
-static void lsp_write_err(const String msg) {
+static void lsp_output_err(const String msg) {
   const String appName = path_filename(g_path_executable);
   const String text    = fmt_write_scratch("{}: {}\n", fmt_text(appName), fmt_text(msg));
   file_write_sync(g_file_stderr, text);
+}
+
+static void lsp_read_trim(LspContext* ctx) {
+  dynstring_erase_chars(ctx->readBuffer, 0, ctx->readCursor);
+  ctx->readCursor = 0;
+}
+
+static String lsp_read_remaining(LspContext* ctx) {
+  const String full = dynstring_view(ctx->readBuffer);
+  return string_slice(full, ctx->readCursor, full.size - ctx->readCursor);
 }
 
 static void lsp_read_chunk(LspContext* ctx) {
@@ -51,10 +62,10 @@ static void lsp_read_chunk(LspContext* ctx) {
 
 static String lsp_read_until(LspContext* ctx, const String pattern) {
   while (LIKELY(ctx->status == LspStatus_Running)) {
-    const String text = dynstring_view(ctx->readBuffer);
+    const String text = lsp_read_remaining(ctx);
     const usize  pos  = string_find_first(text, pattern);
     if (!sentinel_check(pos)) {
-      dynstring_erase_chars(ctx->readBuffer, 0, pos + pattern.size);
+      ctx->readCursor += pos + pattern.size;
       return string_slice(text, 0, pos + pattern.size);
     }
     lsp_read_chunk(ctx);
@@ -64,9 +75,9 @@ static String lsp_read_until(LspContext* ctx, const String pattern) {
 
 static String lsp_read_sized(LspContext* ctx, const usize size) {
   while (LIKELY(ctx->status == LspStatus_Running)) {
-    const String text = dynstring_view(ctx->readBuffer);
+    const String text = lsp_read_remaining(ctx);
     if (text.size >= size) {
-      dynstring_erase_chars(ctx->readBuffer, 0, size);
+      ctx->readCursor += size;
       return string_slice(text, 0, size);
     }
     lsp_read_chunk(ctx);
@@ -125,23 +136,25 @@ static i32 lsp_run_stdio() {
     const String    content = lsp_read_sized(&ctx, header.contentLength);
 
     JsonResult jsonResult;
-    json_clear(contentJson);
     json_read(contentJson, content, &jsonResult);
     if (UNLIKELY(jsonResult.type == JsonResultType_Fail)) {
       const String jsonErrMsg = json_error_str(jsonResult.error);
-      lsp_write_err(fmt_write_scratch("Json read failed: {}", fmt_text(jsonErrMsg)));
+      lsp_output_err(fmt_write_scratch("Json read failed: {}", fmt_text(jsonErrMsg)));
       ctx.status = LspStatus_ErrorInvalidJson;
       break;
     }
 
     lsp_handle_jrpc(&ctx, contentJson, jsonResult.val);
+
+    json_clear(contentJson);
+    lsp_read_trim(&ctx);
   }
 
   json_destroy(contentJson);
   dynstring_destroy(&readBuffer);
 
   if (ctx.status != LspStatus_Shutdown) {
-    lsp_write_err(g_lspStatusMessage[ctx.status]);
+    lsp_output_err(g_lspStatusMessage[ctx.status]);
     return 1;
   }
   return 0;
@@ -170,6 +183,6 @@ i32 app_cli_run(const CliApp* app, const CliInvocation* invoc) {
     return lsp_run_stdio();
   }
 
-  lsp_write_err(string_lit("No communication method specified."));
+  lsp_output_err(string_lit("No communication method specified."));
   return 1;
 }
