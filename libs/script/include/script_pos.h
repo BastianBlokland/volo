cd76697ed52d71ae83c1cb708b620580f3fb4bcc
Bastian Blokland <mail@bastian.tech>
2023-10-07 12:15:05 +0300

    script: Add position type

---
+++ libs/script/include/script_pos.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "core_string.h"
+
+typedef u32 ScriptPos; // Bytes into the source text.
+
+typedef struct {
+  ScriptPos start, end;
+} ScriptPosRange;
+
+typedef struct {
+  u16 line, column; // 0 based.
+} ScriptPosHuman;
+
+ScriptPosRange script_pos_range(ScriptPos start, ScriptPos end);
+ScriptPos      script_pos_trim(String text, ScriptPos);
+ScriptPosHuman script_pos_humanize(String text, ScriptPos);
