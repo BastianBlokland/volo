3a2f6e840068474df46fbe0357276d6ca0f935ea
Bastian Blokland <mail@bastian.tech>
2023-10-10 20:02:34 +0300

    core: Support up to 20 arguments in COUNT_VA_ARGS

--- libs/core/test/test_macro.c
+++ libs/core/test/test_macro.c
@@ -5,6 +5,7 @@
 spec(macro) {
 
   it("can count the number of VA_ARGs") {
+    // clang-format off
     ASSERT(COUNT_VA_ARGS() == 0, "test_macro failed");
     ASSERT(COUNT_VA_ARGS(1) == 1, "test_macro failed");
     ASSERT(COUNT_VA_ARGS(1, 2) == 2, "test_macro failed");
@@ -16,6 +17,17 @@ spec(macro) {
     ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8) == 8, "test_macro failed");
     ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9) == 9, "test_macro failed");
     ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) == 10, "test_macro failed");
+    ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11) == 11, "test_macro failed");
+    ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12) == 12, "test_macro failed");
+    ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13) == 13, "test_macro failed");
+    ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14) == 14, "test_macro failed");
+    ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15) == 15, "test_macro failed");
+    ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16) == 16, "test_macro failed");
+    ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17) == 17, "test_macro failed");
+    ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18) == 18, "test_macro failed");
+    ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19) == 19, "test_macro failed");
+    ASSERT(COUNT_VA_ARGS(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20) == 20, "test_macro failed");
+    // clang-format off
     ASSERT(COUNT_VA_ARGS(((1, 3, 4)), ("hello", {2})) == 2, "test_macro failed");
   }
 
