3401da4c9460e042a26b76b5569949cf2e7dde3f
Bastian Blokland <bastianblokland@gmail.com>
2023-10-15 18:54:59 +0300

    core: Inline string_consume in utf8.c

--- libs/core/src/utf8.c
+++ libs/core/src/utf8.c
@@ -1,4 +1,5 @@
 #include "core_annotation.h"
+#include "core_bits.h"
 #include "core_dynstring.h"
 #include "core_utf8.h"
 
@@ -8,7 +9,14 @@
 #define utf8_cp_triple_char ((Unicode)0xFFFF)
 #define utf8_cp_quad_char utf8_cp_max
 
-static bool utf8_cp_valid(const Unicode cp) { return cp <= utf8_cp_max; }
+INLINE_HINT static String string_consume_unchecked(const String str, const usize amount) {
+  return (String){
+      .ptr  = bits_ptr_offset(str.ptr, amount),
+      .size = str.size - amount,
+  };
+}
+
+INLINE_HINT static bool utf8_cp_valid(const Unicode cp) { return cp <= utf8_cp_max; }
 
 bool utf8_contchar(const u8 c) { return (c & 0b11000000) == 0b10000000; }
 
@@ -106,7 +114,7 @@ String utf8_cp_read(String utf8, Unicode* out) {
   const usize charCount = utf8_cp_bytes_from_first(chars[0]);
   if (UNLIKELY(!charCount)) {
     *out = 0;
-    return string_consume(utf8, 1);
+    return string_consume_unchecked(utf8, 1);
   }
 
   // Validate that the remaining characters are all valid utf8 continuation characters.
@@ -117,7 +125,7 @@ String utf8_cp_read(String utf8, Unicode* out) {
   for (u8 i = 1; i != charCount; ++i) {
     if (UNLIKELY(!utf8_contchar(chars[i]))) {
       *out = 0;
-      return string_consume(utf8, charCount);
+      return string_consume_unchecked(utf8, charCount);
     }
   }
 
@@ -137,5 +145,5 @@ String utf8_cp_read(String utf8, Unicode* out) {
            (chars[2] & 0b00111111) << 6 | (chars[3] & 0b00111111);
     break;
   }
-  return string_consume(utf8, charCount);
+  return string_consume_unchecked(utf8, charCount);
 }
