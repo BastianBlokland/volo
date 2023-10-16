ab055d8df3cb0503dce3411c2901197b6636c5dc
Bastian Blokland <mail@bastian.tech>
2023-10-16 20:03:07 +0300

    script: Placeholder format api

---
+++ libs/script/include/script_format.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "core_dynstring.h"
+
+/**
+ * Reformat the given script source text.
+ */
+void script_format(DynString* out, String input);
---
+++ libs/script/src/format.c
@@ -0,0 +1,3 @@
+#include "script_format.h"
+
+void script_format(DynString* out, const String input) { dynstring_append(out, input); }
