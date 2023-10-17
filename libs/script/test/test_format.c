d258948a9c24e17403e2b66794b4f27a67c2e183
Bastian Blokland <mail@bastian.tech>
2023-10-17 21:58:53 +0300

    script: Add first format test case

--- libs/script/test/test_format.c
+++ libs/script/test/test_format.c
@@ -13,11 +13,12 @@ spec(format) {
       String input, expect;
     } g_testData[] = {
         {string_static("\n"), string_static("\n")},
+        {string_static("42\n"), string_static("42\n")},
     };
 
     for (u32 i = 0; i != array_elems(g_testData); ++i) {
+      dynstring_clear(&bufferStr);
       script_format(&bufferStr, g_testData[i].input);
-
       check_eq_string(dynstring_view(&bufferStr), g_testData[i].expect);
     }
   }
