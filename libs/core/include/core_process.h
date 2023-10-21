93a4d962d5c430a1147b56b7c31bbc6058bdd29d
Bastian Blokland <mail@bastian.tech>
2023-10-21 13:56:54 +0300

    core: Document process api

--- libs/core/include/core_process.h
+++ libs/core/include/core_process.h
@@ -16,9 +16,9 @@ typedef struct sProcess Process;
 typedef i64 ProcessId;
 
 typedef enum {
-  ProcessFlags_PipeStdIn  = 1 << 0, // Create a pipe for writing to std in.
-  ProcessFlags_PipeStdOut = 1 << 1, // Create a pipe for reading from std out.
-  ProcessFlags_PipeStdErr = 1 << 2, // Create a pipe for reading from std err.
+  ProcessFlags_PipeStdIn  = 1 << 0, // Create a pipe for writing to std-in.
+  ProcessFlags_PipeStdOut = 1 << 1, // Create a pipe for reading from std-out.
+  ProcessFlags_PipeStdErr = 1 << 2, // Create a pipe for reading from std-err.
   ProcessFlags_NewGroup   = 1 << 3, // Create a new process group for the child proccess.
   ProcessFlags_Detached   = 1 << 4, // Leave the process running when closing the handle.
 } ProcessFlags;
@@ -38,9 +38,9 @@ typedef enum {
 
 typedef enum {
   ProcessExitCode_Success            = 0,
-  ProcessExitCode_InvalidProcess     = -100,
-  ProcessExitCode_TerminatedBySignal = -101,
-  ProcessExitCode_UnknownError       = -102,
+  ProcessExitCode_InvalidProcess     = -1000,
+  ProcessExitCode_TerminatedBySignal = -1001,
+  ProcessExitCode_UnknownError       = -1002,
 } ProcessExitCode;
 
 /**
@@ -49,22 +49,42 @@ typedef enum {
 String process_result_str(ProcessResult);
 
 /**
- * Create a new process.
+ * Create a new process handle.
  * Destroy using 'process_destroy()'.
  */
 Process* process_create(Allocator*, String file, const String args[], u32 argCount, ProcessFlags);
 
 /**
- * Destroy a process.
+ * Destroy a process handle.
+ * NOTE: Kills the process if its still running and the handle does not have the Detached flag.
  */
 void process_destroy(Process*);
 
+/**
+ * Retrieve the result enum for starting the process.
+ */
 ProcessResult process_start_result(const Process*);
-ProcessId     process_id(const Process*);
 
+/**
+ * Retrieve the OS process-id for this handle.
+ */
+ProcessId process_id(const Process*);
+
+/**
+ * Retrieve a file handle to process pipes.
+ * NOTE: Return null if the process was started without the corresponding pipe flag.
+ */
 File* process_pipe_in(Process*);
 File* process_pipe_out(Process*);
 File* process_pipe_err(Process*);
 
-ProcessResult   process_signal(Process*, Signal);
+/**
+ * Send a signal to the given process.
+ */
+ProcessResult process_signal(Process*, Signal);
+
+/**
+ * Block until the process has finished executing and return its exit-code.
+ * NOTE: -1000, -1001 and -1002 have special meaning given by this function.
+ */
 ProcessExitCode process_block(Process*);
