c127e8ceae014b3250540a2c3c57661be679052d
Bastian Blokland <mail@bastian.tech>
2023-10-29 13:04:32 +0200

    debug: Show null memory values by default

--- libs/debug/src/script.c
+++ libs/debug/src/script.c
@@ -688,11 +688,7 @@ ecs_module_init(debug_script_module) {
 EcsEntityId debug_script_panel_open(EcsWorld* world, const EcsEntityId window) {
   const EcsEntityId panelEntity = ui_canvas_create(world, window, UiCanvasCreateFlags_ToFront);
   ecs_world_add_t(
-      world,
-      panelEntity,
-      DebugScriptPanelComp,
-      .panel          = ui_panel(.size = ui_vector(800, 500)),
-      .hideNullMemory = true);
+      world, panelEntity, DebugScriptPanelComp, .panel = ui_panel(.size = ui_vector(800, 500)));
   return panelEntity;
 }
 
@@ -702,7 +698,6 @@ EcsEntityId debug_script_output_panel_open(EcsWorld* world, const EcsEntityId wi
       world,
       panelEntity,
       DebugScriptPanelComp,
-      .panel          = ui_panel(.size = ui_vector(800, 500), .activeTab = DebugScriptTab_Output),
-      .hideNullMemory = true);
+      .panel = ui_panel(.size = ui_vector(800, 500), .activeTab = DebugScriptTab_Output));
   return panelEntity;
 }
