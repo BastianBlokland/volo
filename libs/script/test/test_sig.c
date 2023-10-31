8658f969858df4e9db39bc797df3351461451246
Bastian Blokland <mail@bastian.tech>
2023-10-31 20:42:37 +0200

    script: Add zero argument sig test

--- libs/script/test/test_sig.c
+++ libs/script/test/test_sig.c
@@ -5,16 +5,23 @@
 #include "script_val.h"
 
 spec(sig) {
+  it("can store ret type") {
+    ScriptSig* sig = script_sig_create(g_alloc_scratch, script_mask_bool, null, 0);
+
+    check_eq_int(script_sig_ret(sig), script_mask_bool);
+
+    script_sig_destroy(sig);
+  }
+
   it("can store ret type and arguments") {
-    const ScriptMask   ret    = script_mask_bool;
     const ScriptSigArg args[] = {
         {.name = string_lit("argA"), .mask = script_mask_number},
         {.name = string_lit("argB"), .mask = script_mask_null},
         {.name = string_lit("argC"), .mask = script_mask_null | script_mask_vector3},
     };
-    ScriptSig* sig = script_sig_create(g_alloc_heap, ret, args, array_elems(args));
+    ScriptSig* sig = script_sig_create(g_alloc_scratch, script_mask_bool, args, array_elems(args));
 
-    check_eq_int(script_sig_ret(sig), ret);
+    check_eq_int(script_sig_ret(sig), script_mask_bool);
     check_eq_int(script_sig_arg_count(sig), array_elems(args));
     for (u8 i = 0; i != array_elems(args); ++i) {
       check_eq_string(script_sig_arg(sig, i).name, args[i].name);
