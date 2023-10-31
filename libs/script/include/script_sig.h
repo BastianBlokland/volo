a2667b7aaa3a521433857e09bcdc26b3fabcc784
Bastian Blokland <mail@bastian.tech>
2023-10-31 20:17:35 +0200

    script: Initial signature type

---
+++ libs/script/include/script_sig.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "core_string.h"
+
+// Forward declare from 'core_alloc.h'.
+typedef struct sAllocator Allocator;
+
+// Forward declare from 'script_val.h'.
+typedef u16 ScriptMask;
+
+#define script_sig_arg_count_max 10
+#define script_sig_arg_name_max 64
+
+typedef struct {
+  String     name;
+  ScriptMask mask; // Mask of accepted types.
+} ScriptSigArg;
+
+typedef struct sScriptSig ScriptSig;
+
+ScriptSig* script_sig_create(Allocator*, ScriptMask ret, const ScriptSigArg args[], u8 argCount);
+void       script_sig_destroy(ScriptSig*);
+
+ScriptMask   script_sig_ret(const ScriptSig*);
+u8           script_sig_arg_count(const ScriptSig*);
+ScriptSigArg script_sig_arg(const ScriptSig* sig, u8 index);
---
+++ libs/script/src/sig.c
@@ -0,0 +1,81 @@
+#include "core_alloc.h"
+#include "core_array.h"
+#include "core_bits.h"
+#include "core_diag.h"
+#include "core_sentinel.h"
+#include "script_sig.h"
+
+/**
+ * Signature, stores the return type and names and types for arguments.
+ *
+ * Memory layout is a ScriptSig header followed by the following data per argument:
+ * - ScriptMask (2 bytes)
+ * - nameLen (1 byte)
+ * - Name (nameLen bytes)
+ *
+ */
+
+ASSERT(script_sig_arg_name_max <= u8_max, "Argument name length has to be storable in a byte");
+
+static usize sig_arg_data_size(const ScriptSigArg arg) {
+  diag_assert_msg(arg.name.size <= script_sig_arg_name_max, "Argument name length exceeds max");
+  return sizeof(ScriptMask) + 1 /* nameLen */ + arg.name.size;
+}
+
+struct sScriptSig {
+  Allocator* alloc;
+  ScriptMask retMask;
+  u8         argCount;
+  u16        argOffsets[script_sig_arg_count_max];
+};
+
+ScriptSig* script_sig_create(
+    Allocator* alloc, const ScriptMask ret, const ScriptSigArg args[], const u8 argCount) {
+  diag_assert_msg(argCount <= script_sig_arg_count_max, "Argument count exceeds max");
+
+  usize allocSize = sizeof(ScriptSig);
+  for (u8 i = 0; i != argCount; ++i) {
+    allocSize += sig_arg_data_size(args[i]);
+  }
+  ScriptSig* sig = alloc_alloc(alloc, allocSize, alignof(ScriptSig)).ptr;
+
+  sig->alloc    = alloc;
+  sig->retMask  = ret;
+  sig->argCount = argCount;
+
+  usize offset = sizeof(ScriptSig);
+  for (u8 i = 0; i != argCount; ++i) {
+    diag_assert(offset < u16_max);
+    diag_assert(bits_aligned(offset, alignof(ScriptMask)));
+
+    sig->argOffsets[i] = (u16)offset;
+    offset += sig_arg_data_size(args[i]);
+  }
+
+  return sig;
+}
+
+void script_sig_destroy(ScriptSig* sig) {
+  usize allocSize = sizeof(ScriptSig);
+  for (u8 i = 0; i != sig->argCount; ++i) {
+    allocSize += sig_arg_data_size(script_sig_arg(sig, i));
+  }
+  alloc_free(sig->alloc, mem_create(sig, allocSize));
+}
+
+ScriptMask script_sig_ret(const ScriptSig* sig) { return sig->retMask; }
+
+u8 script_sig_arg_count(const ScriptSig* sig) { return sig->argCount; }
+
+ScriptSigArg script_sig_arg(const ScriptSig* sig, const u8 index) {
+  diag_assert_msg(index < script_sig_arg_count_max, "Argument index exceeds maximum");
+
+  const u16 offset = sig->argOffsets[index];
+  diag_assert_msg(!sentinel_check(offset), "Argument index out of bounds");
+
+  const ScriptMask mask    = *(ScriptMask*)bits_ptr_offset(sig, offset);
+  const u8         nameLen = *(u8*)bits_ptr_offset(sig, offset + sizeof(ScriptMask));
+  const String name = mem_create(bits_ptr_offset(sig, offset + sizeof(ScriptMask) + 1), nameLen);
+
+  return (ScriptSigArg){.mask = mask, .name = name};
+}
