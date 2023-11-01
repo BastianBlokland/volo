080ca591175babd6b47134f77f596aef8389856e
Bastian Blokland <mail@bastian.tech>
2023-11-01 19:56:21 +0200

    script: Add script_sym_clone_into util

--- libs/script/src/sym.c
+++ libs/script/src/sym.c
@@ -93,6 +93,30 @@ static ScriptSymId sym_find_by_mem_key(const ScriptSymBag* b, const StringHash m
   return script_sym_sentinel;
 }
 
+static void script_sym_clone_into(Allocator* alloc, ScriptSym* dst, const ScriptSym* src) {
+  dst->type  = src->type;
+  dst->label = string_dup(alloc, src->label);
+  dst->doc   = string_maybe_dup(alloc, src->doc);
+  switch (src->type) {
+  case ScriptSymType_BuiltinFunction:
+    dst->data.builtinFunc = src->data.builtinFunc;
+    break;
+  case ScriptSymType_ExternFunction:
+    dst->data.externFunc = src->data.externFunc;
+    break;
+  case ScriptSymType_Variable:
+    dst->data.var = src->data.var;
+    break;
+  case ScriptSymType_MemoryKey:
+    dst->data.memKey = src->data.memKey;
+    break;
+  case ScriptSymType_BuiltinConstant:
+  case ScriptSymType_Keyword:
+  case ScriptSymType_Count:
+    break;
+  }
+}
+
 ScriptSymBag* script_sym_bag_create(Allocator* alloc) {
   ScriptSymBag* bag = alloc_alloc_t(alloc, ScriptSymBag);
 
@@ -118,12 +142,7 @@ ScriptSymId script_sym_push(ScriptSymBag* bag, const ScriptSym* sym) {
     return script_sym_sentinel;
   }
 
-  *dynarray_push_t(&bag->symbols, ScriptSym) = (ScriptSym){
-      .type  = sym->type,
-      .label = string_dup(bag->alloc, sym->label),
-      .doc   = string_maybe_dup(bag->alloc, sym->doc),
-      .data  = sym->data,
-  };
+  script_sym_clone_into(bag->alloc, dynarray_push_t(&bag->symbols, ScriptSym), sym);
 
   return id;
 }
