5d58e47244927ed637c52c70658a7b300a54bc18
Bastian Blokland <mail@bastian.tech>
2023-11-09 22:49:59 +0200

    data: Remove tree-schema support

--- libs/data/test/config.c
+++ libs/data/test/config.c
@@ -4,7 +4,6 @@ void app_check_configure(CheckDef* check) {
   register_spec(check, jsonschema);
   register_spec(check, read_json);
   register_spec(check, registry);
-  register_spec(check, treeschema);
   register_spec(check, utils_clone);
   register_spec(check, utils_destroy);
   register_spec(check, write_json);
