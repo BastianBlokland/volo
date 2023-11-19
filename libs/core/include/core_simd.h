463956fe3f6d905652d48a99610e8a1f591ec7dd
Bastian Blokland <bastianblokland@gmail.com>
2023-11-19 23:40:15 +0200

    core: Add float to int casts in simd utils

--- libs/core/include/core_simd.h
+++ libs/core/include/core_simd.h
@@ -134,7 +134,7 @@ MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_greater(const SimdVec a, const 
 }
 
 MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_eq_u32(const SimdVec a, const SimdVec b) {
-  return _mm_cmpeq_epi32(a, b);
+  return _mm_castsi128_ps(_mm_cmpeq_epi32(_mm_castps_si128(a), _mm_castps_si128(b)));
 }
 
 MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_and(const SimdVec a, const SimdVec b) {
@@ -142,7 +142,7 @@ MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_and(const SimdVec a, const Simd
 }
 
 MAYBE_UNUSED INLINE_HINT static SimdVec simd_vec_pack_u32_to_u16(const SimdVec a, const SimdVec b) {
-  return _mm_packs_epi32(a, b);
+  return _mm_castsi128_ps(_mm_packs_epi32(_mm_castps_si128(a), _mm_castps_si128(b)));
 }
 
 MAYBE_UNUSED INLINE_HINT static u32 simd_vec_mask_u32(const SimdVec a) {
