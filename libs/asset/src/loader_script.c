36279fe7468e0167219f17c02a8c0847187c735f
Bastian Blokland <mail@bastian.tech>
2023-11-25 18:16:39 +0200

    scene: Add vfx_decal() script api

--- libs/asset/src/loader_script.c
+++ libs/asset/src/loader_script.c
@@ -377,6 +377,18 @@ static void asset_binder_init() {
       asset_bind(binder, name, doc, ret, args, array_elems(args));
     }
     {
+      const String       name   = string_lit("vfx_decal");
+      const String       doc    = string_lit("Place a decal.\n\n*Note*: Resulting entity is not automatically destroyed.\n\n*Note*: It takes one frame before it can be used with the 'vfx_param()' api.");
+      const ScriptMask   ret    = script_mask_entity;
+      const ScriptSigArg args[] = {
+          {string_lit("asset"), script_mask_entity},
+          {string_lit("pos"), script_mask_vec3},
+          {string_lit("rot"), script_mask_quat},
+          {string_lit("alpha"), script_mask_num | script_mask_null},
+      };
+      asset_bind(binder, name, doc, ret, args, array_elems(args));
+    }
+    {
       const String       name   = string_lit("vfx_param");
       const String       doc    = string_lit("Change or query a vfx parameter on the given entity.\n\nSupported parameters:\n\n-`Alpha`");
       const ScriptMask   ret    = script_mask_num | script_mask_null;
@@ -400,7 +412,7 @@ static void asset_binder_init() {
       };
       asset_bind(binder, name, doc, ret, args, array_elems(args));
     }
-        {
+    {
       const String       name   = string_lit("sound_param");
       const String       doc    = string_lit("Change or query a sound parameter on the given entity.\n\nSupported parameters:\n\n-`Gain`\n\n-`Pitch`");
       const ScriptMask   ret    = script_mask_num | script_mask_null;
