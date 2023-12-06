9c9be17f90c77f462ce18e178fefbb35000f1bbd
Bastian Blokland <mail@bastian.tech>
2023-12-06 16:44:19 +0200

    asset: Make all color components required

--- libs/asset/src/loader_prefab.c
+++ libs/asset/src/loader_prefab.c
@@ -264,10 +264,10 @@ static void prefab_datareg_init() {
     data_reg_field_t(reg, AssetPrefabVec3Def, z, data_prim_t(f32), .flags = DataFlags_Opt);
 
     data_reg_struct_t(reg, AssetPrefabColorDef);
-    data_reg_field_t(reg, AssetPrefabColorDef, r, data_prim_t(f32), .flags = DataFlags_Opt);
-    data_reg_field_t(reg, AssetPrefabColorDef, g, data_prim_t(f32), .flags = DataFlags_Opt);
-    data_reg_field_t(reg, AssetPrefabColorDef, b, data_prim_t(f32), .flags = DataFlags_Opt);
-    data_reg_field_t(reg, AssetPrefabColorDef, a, data_prim_t(f32), .flags = DataFlags_Opt);
+    data_reg_field_t(reg, AssetPrefabColorDef, r, data_prim_t(f32));
+    data_reg_field_t(reg, AssetPrefabColorDef, g, data_prim_t(f32));
+    data_reg_field_t(reg, AssetPrefabColorDef, b, data_prim_t(f32));
+    data_reg_field_t(reg, AssetPrefabColorDef, a, data_prim_t(f32));
 
     data_reg_struct_t(reg, AssetPrefabShapeSphereDef);
     data_reg_field_t(reg, AssetPrefabShapeSphereDef, offset, t_AssetPrefabVec3Def, .flags = DataFlags_Opt);
@@ -719,12 +719,12 @@ static void prefab_build(
       const String rallySoundId   = traitDef->data_production.rallySoundId;
       const f32    rallySoundGain = traitDef->data_production.rallySoundGain;
       outTrait->data_production   = (AssetPrefabTraitProduction){
-          .spawnPos        = prefab_build_vec3(&traitDef->data_production.spawnPos),
-          .rallyPos        = prefab_build_vec3(&traitDef->data_production.rallyPos),
-          .productSetId    = string_hash(traitDef->data_production.productSetId),
-          .rallySoundAsset = asset_maybe_lookup(ctx->world, ctx->assetManager, rallySoundId),
-          .rallySoundGain  = rallySoundGain <= 0 ? 1 : rallySoundGain,
-          .placementRadius = traitDef->data_production.placementRadius,
+            .spawnPos        = prefab_build_vec3(&traitDef->data_production.spawnPos),
+            .rallyPos        = prefab_build_vec3(&traitDef->data_production.rallyPos),
+            .productSetId    = string_hash(traitDef->data_production.productSetId),
+            .rallySoundAsset = asset_maybe_lookup(ctx->world, ctx->assetManager, rallySoundId),
+            .rallySoundGain  = rallySoundGain <= 0 ? 1 : rallySoundGain,
+            .placementRadius = traitDef->data_production.placementRadius,
       };
     } break;
     case AssetPrefabTrait_Scalable:
