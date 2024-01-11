75047aee22cc4f791c97441bfa0d4636afbbef54
Bastian Blokland <mail@bastian.tech>
2024-01-11 20:21:40 +0200

    rend: Support compression source mips

--- libs/rend/src/rvk/texture.c
+++ libs/rend/src/rvk/texture.c
@@ -51,10 +51,6 @@ static RvkTextureCompress rvk_texture_compression(const AssetTextureComp* asset)
      */
     return RvkTextureCompress_None;
   }
-  if (asset->srcMipLevels > 1) {
-    // TODO: Support compressed textures with source mip-maps.
-    return RvkTextureCompress_None;
-  }
   if (asset->channels == AssetTextureChannels_One) {
     return RvkTextureCompress_Bc4;
   }
@@ -199,15 +195,19 @@ static void rvk_texture_encode(
   u8*       outPtr = out.ptr;
 
   Bc0Block block;
-  for (u32 l = 0; l != math_max(asset->layers, 1); ++l) {
-    for (u32 y = 0; y < asset->height; y += 4, inPtr += asset->width * 4 * asset->channels) {
-      for (u32 x = 0; x < asset->width; x += 4) {
-        if (asset->channels == 1) {
-          bc0_extract1(inPtr + x, asset->width, &block);
-        } else {
-          bc0_extract4((const BcColor8888*)inPtr + x, asset->width, &block);
+  for (u32 mip = 0; mip != math_max(asset->srcMipLevels, 1); ++mip) {
+    const u32 mipWidth  = math_max(asset->width >> mip, 1);
+    const u32 mipHeight = math_max(asset->height >> mip, 1);
+    for (u32 l = 0; l != math_max(asset->layers, 1); ++l) {
+      for (u32 y = 0; y < mipHeight; y += 4, inPtr += mipWidth * 4 * asset->channels) {
+        for (u32 x = 0; x < mipWidth; x += 4) {
+          if (asset->channels == 1) {
+            bc0_extract1(inPtr + x, mipWidth, &block);
+          } else {
+            bc0_extract4((const BcColor8888*)inPtr + x, mipWidth, &block);
+          }
+          outPtr += rvk_texture_encode_block(&block, compress, outPtr);
         }
-        outPtr += rvk_texture_encode_block(&block, compress, outPtr);
       }
     }
   }
@@ -291,8 +291,8 @@ RvkTexture* rvk_texture_create(RvkDevice* dev, const AssetTextureComp* asset, St
 
   RvkTexture* tex = alloc_alloc_t(g_alloc_heap, RvkTexture);
   *tex            = (RvkTexture){
-                 .device  = dev,
-                 .dbgName = string_dup(g_alloc_heap, dbgName),
+      .device  = dev,
+      .dbgName = string_dup(g_alloc_heap, dbgName),
   };
   const RvkSize            size     = rvk_size(asset->width, asset->height);
   const RvkTextureCompress compress = rvk_texture_compression(asset);
