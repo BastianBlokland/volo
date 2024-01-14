70335d12f2c529a93e6026cbe06b548fb5e08e4f
Bastian Blokland <mail@bastian.tech>
2024-01-14 22:29:20 +0200

    rend: Add rvk_buffer_map api

--- libs/rend/src/rvk/buffer_internal.h
+++ libs/rend/src/rvk/buffer_internal.h
@@ -24,8 +24,22 @@ typedef struct sRvkBuffer {
 
 RvkBuffer rvk_buffer_create(RvkDevice*, u64 size, RvkBufferType);
 void      rvk_buffer_destroy(RvkBuffer*, RvkDevice*);
-void      rvk_buffer_upload(RvkBuffer*, Mem data, u64 offset);
 String    rvk_buffer_type_str(RvkBufferType);
+
+/**
+ * Map the buffer into cpu writable memory.
+ * NOTE: Requires the buffer to be host-visible.
+ * NOTE: Writes to the mapped memory require a flush to become visible to the driver.
+ */
+Mem  rvk_buffer_map(RvkBuffer*, const u64 offset);
+void rvk_buffer_flush(RvkBuffer*);
+
+/**
+ * Copies the given data to the buffer.
+ * NOTE: Requires the buffer to be host-visible.
+ * NOTE: Automatically performs a buffer flush.
+ */
+void rvk_buffer_upload(RvkBuffer*, Mem data, u64 offset);
 
 void rvk_buffer_transfer_ownership(
     const RvkBuffer*,
