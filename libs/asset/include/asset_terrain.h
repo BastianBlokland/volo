1092e298aba4f72bc928314b79944517296dd8aa
Bastian Blokland <mail@bastian.tech>
2024-01-23 23:14:18 +0200

    asset: Add terrain header

---
+++ libs/asset/include/asset_terrain.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "ecs_entity.h"
+#include "ecs_module.h"
+
+// Forward declare from 'core_dynstring.h'.
+typedef struct sDynArray DynString;
+
+ecs_comp_extern_public(AssetTerrainComp) {
+  String      graphicId, heightmapId;
+  EcsEntityId graphic, heightmap;
+};
+
+void asset_terrain_jsonschema_write(DynString*);
