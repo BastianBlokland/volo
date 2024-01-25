8c00587d66a34b4bd71c9514fcb8acbf53523c30
Bastian Blokland <mail@bastian.tech>
2024-01-25 22:28:20 +0200

    rend: Add error when loading an out of date asset

--- libs/rend/src/resource.c
+++ libs/rend/src/resource.c
@@ -298,6 +298,11 @@ static bool rend_res_asset_wait(EcsWorld* world, EcsIterator* resourceItr) {
   if (!ecs_world_has_t(world, entity, AssetLoadedComp)) {
     return false;
   }
+  if (ecs_world_has_t(world, entity, AssetChangedComp)) {
+    log_e(
+        "Loaded an out-of-date asset",
+        log_param("info", fmt_text("Usually indicates that a changed asset was not released")));
+  }
   return true;
 }
 
