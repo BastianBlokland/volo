12f888ba8a6d9325648ff04d9ca62300501bba68
Bastian Blokland <mail@bastian.tech>
2024-01-27 00:03:51 +0200

    asset: Update test

--- libs/asset/test/test_loader_terrain.c
+++ libs/asset/test/test_loader_terrain.c
@@ -12,6 +12,7 @@ static const AssetMemRecord g_testData[] = {
         .data = string_static("{"
                               "  \"graphicId\": \"test.graphic\","
                               "  \"heightmapId\": \"height.r16\","
+                              "  \"heightScale\": 1.0"
                               "}"),
     },
 };
