af973bd278c2b26e67c22ddf156dcd34bbe36e3d
Bastian Blokland <mail@bastian.tech>
2024-01-28 16:43:58 +0200

    scene: Document minimap color space

--- assets/shaders/include/minimap.glsl
+++ assets/shaders/include/minimap.glsl
@@ -2,9 +2,9 @@
 #define INCLUDE_MINIMAP
 
 struct MinimapData {
-  f32v4 data1; // x, y: position, z, w: size.
-  f32v4 data2; // x: alpha, y: zoomInv, z, w: unused.
-  f32v4 colorLow, colorHigh;
+  f32v4 data1;               // x, y: position, z, w: size.
+  f32v4 data2;               // x: alpha, y: zoomInv, z, w: unused.
+  f32v4 colorLow, colorHigh; // Colors in linear space.
 };
 
 #endif // INCLUDE_MINIMAP
