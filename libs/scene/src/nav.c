9cb1858e68440a601be252bf33325b917cfd5c03
Bastian Blokland <mail@bastian.tech>
2024-01-31 18:36:50 +0200

    scene: Change nav grid density to 1.0

--- libs/scene/src/nav.c
+++ libs/scene/src/nav.c
@@ -18,7 +18,7 @@
 ASSERT(sizeof(EcsEntityId) == sizeof(u64), "EntityId's have to be interpretable as 64bit integers");
 
 static const f32 g_sceneNavFallbackSize    = 500.0f;
-static const f32 g_sceneNavDensity         = 1.25f;
+static const f32 g_sceneNavDensity         = 1.0f;
 static const f32 g_sceneNavCellHeight      = 5.0f;
 static const f32 g_sceneNavCellBlockHeight = 3.0f;
 
