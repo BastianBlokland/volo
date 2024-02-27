2d3594862ddb994d41dc186fdea2db500177eaeb
Bastian Blokland <mail@bastian.tech>
2024-02-27 19:57:19 +0200

    vfx: Identity point warp test

--- libs/vfx/test/test_warp.c
+++ libs/vfx/test/test_warp.c
@@ -1,5 +1,6 @@
 #include "check_spec.h"
 #include "core_alloc.h"
+#include "core_diag.h"
 #include "core_float.h"
 #include "core_rng.h"
 #include "geo_vector.h"
@@ -19,6 +20,14 @@ typedef struct {
 } VfxWarpVec;
 
 #define vfx_warp_vec_fmt(_VEC_) fmt_list_lit(fmt_float((_VEC_).x), fmt_float((_VEC_).y))
+
+static VfxWarpVec vfx_warp_vec_add(const VfxWarpVec a, const VfxWarpVec b) {
+  return (VfxWarpVec){.x = a.x + b.x, .y = a.y + b.y};
+}
+
+static VfxWarpVec vfx_warp_vec_sub(const VfxWarpVec a, const VfxWarpVec b) {
+  return (VfxWarpVec){.x = a.x - b.x, .y = a.y - b.y};
+}
 
 static VfxWarpVec vfx_warp_vec_rand_in_box(Rng* rng, const f32 min, const f32 max) {
   return (VfxWarpVec){.x = rng_sample_range(rng, min, max), .y = rng_sample_range(rng, min, max)};
@@ -44,6 +53,22 @@ static VfxWarp vfx_warp_ident() {
       }};
 }
 
+static VfxWarp vfx_warp_to_points(const VfxWarpVec p[PARAM_ARRAY_SIZE(4)]) {
+  const VfxWarpVec d = vfx_warp_vec_add(vfx_warp_vec_sub(p[0], p[1]), vfx_warp_vec_sub(p[2], p[3]));
+  if (d.x < f32_epsilon && d.y < f32_epsilon) {
+    // Affine transformation.
+    const VfxWarpVec a = vfx_warp_vec_sub(p[1], p[0]);
+    const VfxWarpVec b = vfx_warp_vec_sub(p[2], p[1]);
+    return (VfxWarp){
+        .columns = {
+            {a.x, b.x, p[0].x},
+            {a.y, b.y, p[0].y},
+            {0.0f, 0.0f, 1.0f},
+        }};
+  }
+  diag_crash_msg("Non-affine point warping not supported");
+}
+
 void eq_warp_vec_impl(
     CheckTestContext* ctx, const VfxWarpVec a, const VfxWarpVec b, const SourceLoc src) {
 
@@ -64,7 +89,22 @@ spec(warp) {
   it("returns the same points when applying a identity warp") {
     const VfxWarp w = vfx_warp_ident();
     for (u32 i = 0; i != 100; ++i) {
-      const VfxWarpVec p       = vfx_warp_vec_rand_in_box(testRng, -100.0f, 100.0f);
+      const VfxWarpVec p       = vfx_warp_vec_rand_in_box(testRng, -10.0f, 10.0f);
+      const VfxWarpVec pWarped = vfx_warp_apply(&w, p);
+      check_eq_warp_vec(p, pWarped);
+    }
+  }
+
+  it("returns the same points when applying a unit points warp") {
+    const VfxWarpVec toPoints[4] = {
+        {0.0f, 0.0f},
+        {1.0f, 0.0f},
+        {1.0f, 1.0f},
+        {0.0f, 1.0f},
+    };
+    const VfxWarp w = vfx_warp_to_points(toPoints);
+    for (u32 i = 0; i != 100; ++i) {
+      const VfxWarpVec p       = vfx_warp_vec_rand_in_box(testRng, -10.0f, 10.0f);
       const VfxWarpVec pWarped = vfx_warp_apply(&w, p);
       check_eq_warp_vec(p, pWarped);
     }
