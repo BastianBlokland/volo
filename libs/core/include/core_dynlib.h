cb48fb434ef3ffaf91ad9270705e11bfa6c6e377
Bastian Blokland <mail@bastian.tech>
2024-04-04 00:31:48 +0300

    core: Update dynlib docs

--- libs/core/include/core_dynlib.h
+++ libs/core/include/core_dynlib.h
@@ -31,12 +31,14 @@ String dynlib_result_str(DynLibResult);
 
 /**
  * Create a new dynamic-library handle.
+ * NOTE: name can either be an absolute path to a .so / .dll or a path relative to the search paths.
  * Destroy using 'dynlib_destroy()'.
  */
-DynLibResult dynlib_load(Allocator*, String path, DynLib** out);
+DynLibResult dynlib_load(Allocator*, String name, DynLib** out);
 
 /**
  * Destroy a dynamic-library handle.
+ * NOTE: If this the last handle to the library it will be unloaded and invalidate retrieved syms.
  */
 void dynlib_destroy(DynLib*);
 
