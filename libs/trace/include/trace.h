9618ed3a3e05f1c30f32c478b1c85901f38ae0d6
Bastian Blokland <mail@bastian.tech>
2024-04-08 23:52:05 +0300

    trace: Initial mem sink header

--- libs/trace/include/trace.h
+++ libs/trace/include/trace.h
@@ -1,4 +1,5 @@
 #pragma once
 #include "trace_event.h"
 #include "trace_init.h"
+#include "trace_sink_mem.h"
 #include "trace_sink_superluminal.h"
---
+++ libs/trace/include/trace_sink_mem.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "core_annotation.h"
+#include "trace_sink.h"
+
+// Forward declare from 'core_time.h'.
+typedef i64 TimeSteady;
+
+/**
+ * InMemorySink - sink that outputs events to in-memory buffers for later inspection / dumping.
+ */
+
+typedef struct {
+  TimeSteady timeStart; // Nano-seconds since the start of the process steady clock.
+  u32        timeDur;   // Duration in nano-seconds (limits the max event dur to 4 seconds).
+  u8         id;        // Identifier label index.
+  u8         color;     // TraceColor
+  u8         msgLength;
+  u8         msgData[49];
+} TraceSinkMemEvent;
+
+ASSERT(sizeof(TraceSinkMemEvent) == 64, "Unexpected event size")
+
+/**
+ * Create a in-memory trace output sink.
+ * NOTE: Should be registered using 'trace_event_add_sink()'.
+ */
+TraceSink* trace_sink_mem(Allocator*);
