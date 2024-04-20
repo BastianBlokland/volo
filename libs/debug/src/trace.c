8b77606f86bf5447b8b1545c0c27d388405c5868
Bastian Blokland <mail@bastian.tech>
2024-04-20 17:01:05 +0300

    debug: Trace focus and refresh (#837)
    * debug: Implement trace focus
    
    * debug: Fix drawing trace rules out of bounds
    
    * debug: Reduce min trace window to 1 us
    
    * debug: Support trace refresh

--- libs/debug/src/trace.c
+++ libs/debug/src/trace.c
@@ -13,6 +13,7 @@
 // clang-format off
 
 static const String g_tooltipFreeze       = string_static("Freeze the data set (halts data collection).");
+static const String g_tooltipRefresh      = string_static("Refresh the data set.");
 static const String g_tooltipTraceDump    = string_static("Dump performance trace data to disk (in the 'logs' directory).");
 static const String g_messageNoStoreSink  = string_static("No store trace-sink found.\nNote: Was the binary compiled with the 'TRACE' option?");
 
@@ -30,8 +31,8 @@ typedef struct {
 
 ecs_comp_define(DebugTracePanelComp) {
   UiPanel        panel;
-  bool           freeze;
-  bool           hoverAny;
+  bool           freeze, refresh;
+  bool           hoverAny, panAny;
   TimeSteady     timeHead;
   TimeDuration   timeWindow;
   DebugTraceData threads[debug_trace_max_threads];
@@ -92,9 +93,10 @@ trace_options_draw(UiCanvasComp* c, DebugTracePanelComp* panel, const TraceSink*
 
   UiTable table = ui_table(.spacing = ui_vector(10, 5), .rowHeight = 20);
   ui_table_add_column(&table, UiTableColumn_Fixed, 200);
-  ui_table_add_column(&table, UiTableColumn_Fixed, 175);
+  ui_table_add_column(&table, UiTableColumn_Fixed, 160);
   ui_table_add_column(&table, UiTableColumn_Fixed, 75);
-  ui_table_add_column(&table, UiTableColumn_Fixed, 50);
+  ui_table_add_column(&table, UiTableColumn_Fixed, 40);
+  ui_table_add_column(&table, UiTableColumn_Fixed, 100);
 
   ui_table_next_row(c, &table);
 
@@ -112,6 +114,15 @@ trace_options_draw(UiCanvasComp* c, DebugTracePanelComp* panel, const TraceSink*
   ui_table_next_column(c, &table);
   ui_toggle(c, &panel->freeze, .tooltip = g_tooltipFreeze);
 
+  ui_table_next_column(c, &table);
+  if (ui_button(
+          c,
+          .label   = string_lit("Refresh"),
+          .tooltip = g_tooltipRefresh,
+          .flags   = panel->freeze ? UiWidget_Default : UiWidget_Disabled)) {
+    panel->refresh = true;
+  }
+
   ui_layout_pop(c);
 }
 
@@ -119,7 +130,7 @@ static void trace_data_input_zoom(UiCanvasComp* c, DebugTracePanelComp* panel, c
   const f64 zoomSpeed = 0.1;
   const f64 zoomFrac  = 1.0 - ui_canvas_input_scroll(c).y * zoomSpeed;
 
-  const TimeDuration min = time_microseconds(10);
+  const TimeDuration min = time_microsecond;
   const TimeDuration max = time_milliseconds(250);
   const TimeDuration new = math_clamp_i64((i64)((f64)panel->timeWindow * zoomFrac), min, max);
 
@@ -137,6 +148,26 @@ static void trace_data_input_pan(UiCanvasComp* c, DebugTracePanelComp* panel, co
     const f64 inputFrac = ui_canvas_input_delta(c).x / rect.width;
     panel->timeHead -= (TimeDuration)((f64)panel->timeWindow * inputFrac);
   }
+}
+
+static void trace_data_input_focus(DebugTracePanelComp* panel, const TraceStoreEvent* evt) {
+  panel->timeHead   = evt->timeStart + evt->timeDur;
+  panel->timeWindow = math_max(evt->timeDur, time_microsecond);
+}
+
+static void trace_data_tooltip_draw(
+    UiCanvasComp*          c,
+    const UiId             barId,
+    const TraceStoreEvent* evt,
+    const String           msg,
+    const String           id) {
+  DynString tooltipBuffer = dynstring_create_over(mem_stack(256));
+  if (msg.size) {
+    fmt_write(&tooltipBuffer, "\a.bMessage\ar:\a>12{}\n", fmt_text(msg));
+  }
+  fmt_write(&tooltipBuffer, "\a.bId\ar:\a>12{}\n", fmt_text(id));
+  fmt_write(&tooltipBuffer, "\a.bDuration\ar:\a>12{}\n", fmt_duration(evt->timeDur));
+  ui_tooltip(c, barId, dynstring_view(&tooltipBuffer));
 }
 
 static void trace_data_ruler_draw(UiCanvasComp* c, const f32 x, const UiRect bgRect) {
@@ -172,14 +203,16 @@ static void trace_data_events_draw(
     trace_data_input_zoom(c, panel, bgRect);
   }
   if (panel->freeze && blockStatus >= UiStatus_Pressed) {
-    trace_data_input_pan(c, panel, bgRect);
+    static const f32 g_panThreshold = 2.5f;
+    if (panel->panAny || math_abs(ui_canvas_input_delta(c).x) > g_panThreshold) {
+      panel->panAny = true;
+      trace_data_input_pan(c, panel, bgRect);
+    }
   }
 
   // NOTE: Timestamps are in nanoseconds.
   const f64 timeLeft  = (f64)(panel->timeHead - panel->timeWindow);
   const f64 timeRight = (f64)panel->timeHead;
-
-  DynString tooltipBuffer = dynstring_create_over(mem_stack(256));
 
   dynarray_for_t(&data->events, TraceStoreEvent, evt) {
     const f64 fracLeft  = math_unlerp(timeLeft, timeRight, (f64)evt->timeStart);
@@ -195,8 +228,8 @@ static void trace_data_events_draw(
     const f64      fracWidth = fracRightClamped - fracLeftClamped;
     const UiVector size      = {.width = (f32)fracWidth, .height = 0.2f};
     const UiVector pos       = {
-        .x = (f32)fracLeftClamped,
-        .y = 1.0f - size.height * (evt->stackDepth + 1),
+              .x = (f32)fracLeftClamped,
+              .y = 1.0f - size.height * (evt->stackDepth + 1),
     };
     ui_layout_set(c, ui_rect(pos, size), UiBase_Container);
 
@@ -210,16 +243,14 @@ static void trace_data_events_draw(
 
     const String id  = trace_sink_store_id(sinkStore, evt->id);
     const String msg = mem_create(evt->msgData, evt->msgLength);
-    if (barHovered) {
-      dynstring_clear(&tooltipBuffer);
-      if (msg.size) {
-        fmt_write(&tooltipBuffer, "\a.bMessage\ar:\a>12{}\n", fmt_text(msg));
+    if (barHovered && panel->freeze) {
+      ui_canvas_interact_type(c, UiInteractType_Action);
+      if (!panel->panAny && barStatus == UiStatus_Activated) {
+        trace_data_input_focus(panel, evt);
       }
-      fmt_write(&tooltipBuffer, "\a.bId\ar:\a>12{}\n", fmt_text(id));
-      fmt_write(&tooltipBuffer, "\a.bDuration\ar:\a>12{}\n", fmt_duration(evt->timeDur));
-      ui_tooltip(c, barId, dynstring_view(&tooltipBuffer));
+      trace_data_tooltip_draw(c, barId, evt, msg, id);
     } else {
-      ui_canvas_id_skip(c, 2); // NOTE: Tooltips consume two events.
+      ui_canvas_id_skip(c, 2); // NOTE: Tooltips consume two ids.
     }
 
     static const f32 g_minWidthForLabel = 100.0f;
@@ -227,11 +258,16 @@ static void trace_data_events_draw(
       ui_style_outline(c, 1);
       ui_style_color(c, ui_color_white);
       ui_canvas_draw_text(c, msg.size ? msg : id, 12, UiAlign_MiddleCenter, UiFlags_None);
+    } else {
+      ui_canvas_id_skip(c, 1);
     }
   }
 
-  if (panel->hoverAny) {
-    trace_data_ruler_draw(c, ui_canvas_input_pos(c).x, bgRect);
+  const f32 inputX = ui_canvas_input_pos(c).x;
+  if (panel->hoverAny && inputX > bgRect.x && inputX < (bgRect.x + bgRect.width)) {
+    trace_data_ruler_draw(c, inputX, bgRect);
+  } else {
+    ui_canvas_id_skip(c, 1);
   }
 
   ui_style_pop(c);
@@ -264,6 +300,10 @@ trace_panel_draw(UiCanvasComp* c, DebugTracePanelComp* panel, const TraceSink* s
 
     ui_layout_container_push(c, UiClip_None);
 
+    if (ui_canvas_status(c) < UiStatus_Pressed) {
+      panel->panAny = false;
+    }
+
     const UiId threadsBeginId = ui_canvas_id_peek(c);
 
     array_for_t(panel->threads, DebugTraceData, data) {
@@ -291,7 +331,7 @@ trace_panel_draw(UiCanvasComp* c, DebugTracePanelComp* panel, const TraceSink* s
     panel->hoverAny = ui_canvas_group_status(c, threadsBeginId, threadsEndId) == UiStatus_Hovered;
 
   } else {
-    panel->hoverAny = false;
+    panel->hoverAny = panel->panAny = false;
     ui_label(c, g_messageNoStoreSink, .align = UiAlign_MiddleCenter);
   }
 
@@ -318,10 +358,11 @@ ecs_system_define(DebugTracePanelQuerySys) {
       continue; // No need to query data for hidden panels.
     }
 
-    if (!panel->freeze) {
+    if (!panel->freeze || panel->refresh) {
       trace_data_clear(panel);
       panel->timeHead = time_steady_clock();
       trace_sink_store_visit(sinkStore, trace_data_visitor, panel);
+      panel->refresh = false;
     }
   }
 }
@@ -344,7 +385,7 @@ ecs_system_define(DebugTracePanelDrawSys) {
     ui_canvas_reset(canvas);
     const bool pinned = ui_panel_pinned(&panel->panel);
     if (debug_panel_hidden(ecs_view_read_t(itr, DebugPanelComp)) && !pinned) {
-      panel->hoverAny = false;
+      panel->hoverAny = panel->panAny = false;
       continue;
     }
 
