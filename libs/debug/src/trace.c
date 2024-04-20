e23dc45eea20cf9ebf30cdd24acac22e8b1756f1
Bastian Blokland <mail@bastian.tech>
2024-04-20 14:48:21 +0300

    debug: Tweak trace event display

--- libs/debug/src/trace.c
+++ libs/debug/src/trace.c
@@ -139,11 +139,13 @@ static void trace_data_input_pan(UiCanvasComp* c, DebugTracePanelComp* panel, co
   }
 }
 
-static void trace_data_ruler_draw(UiCanvasComp* c, const f32 x) {
+static void trace_data_ruler_draw(UiCanvasComp* c, const f32 x, const UiRect bgRect) {
   ui_style_push(c);
   ui_style_color(c, ui_color(255, 255, 255, 128));
   ui_style_outline(c, 0);
-  ui_line(c, ui_vector(x, 0.0f), ui_vector(x, 1.0f), .base = UiBase_Container, .width = 1.0f);
+  const UiVector from = ui_vector(x, bgRect.y);
+  const UiVector to   = ui_vector(x, bgRect.y + bgRect.height);
+  ui_line(c, from, to, .base = UiBase_Absolute, .width = 1.0f);
   ui_style_pop(c);
 }
 
@@ -153,7 +155,7 @@ static void trace_data_events_draw(
     const DebugTraceData* data,
     const TraceSink*      sinkStore) {
   ui_layout_push(c);
-  ui_layout_container_push(c, UiClip_Rect);
+  ui_layout_container_push(c, UiClip_None);
   ui_style_push(c);
 
   ui_canvas_id_block_next(c); // Start events on their own id-block.
@@ -187,16 +189,22 @@ static void trace_data_events_draw(
       ui_canvas_id_skip(c, 4); // 4: +1 for bar, +1 for label, +2 for tooltip.
       continue;                // Event outside of the visible region.
     }
-    const f64      fracWidth = fracRight - fracLeft;
+    const f64 fracLeftClamped  = math_max(fracLeft, 0.0);
+    const f64 fracRightClamped = math_min(fracRight, 1.0);
+
+    const f64      fracWidth = fracRightClamped - fracLeftClamped;
     const UiVector size      = {.width = (f32)fracWidth, .height = 0.2f};
-    const UiVector pos = {.x = (f32)fracLeft, .y = 1.0f - size.height * (evt->stackDepth + 1)};
+    const UiVector pos       = {
+        .x = (f32)fracLeftClamped,
+        .y = 1.0f - size.height * (evt->stackDepth + 1),
+    };
     ui_layout_set(c, ui_rect(pos, size), UiBase_Container);
 
     const UiId     barId      = ui_canvas_id_peek(c);
     const UiStatus barStatus  = ui_canvas_elem_status(c, barId);
     const bool     barHovered = barStatus >= UiStatus_Hovered;
 
-    ui_style_outline(c, barHovered ? 3 : 1);
+    ui_style_outline(c, barHovered ? 2 : 1);
     ui_style_color_with_mult(c, trace_event_color(evt->color), barHovered ? 2.0f : 1.0f);
     ui_canvas_draw_glyph(c, UiShape_Square, 5, UiFlags_Interactable);
 
@@ -223,7 +231,7 @@ static void trace_data_events_draw(
   }
 
   if (panel->hoverAny) {
-    trace_data_ruler_draw(c, (ui_canvas_input_pos(c).x - bgRect.x) / bgRect.width);
+    trace_data_ruler_draw(c, ui_canvas_input_pos(c).x, bgRect);
   }
 
   ui_style_pop(c);
