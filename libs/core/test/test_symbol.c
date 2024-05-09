06eed383acf86c9799d643dfd5ffb2273e2db240
Bastian Blokland <mail@bastian.tech>
2024-05-09 15:54:08 +0300

    core: Initial symbol tests

---
+++ libs/core/test/test_symbol.c
@@ -0,0 +1,18 @@
+#include "check_spec.h"
+#include "core_annotation.h"
+#include "core_symbol.h"
+
+volatile i32 g_preserveFunc;
+
+NO_INLINE_HINT static void test_symbol_func() { g_preserveFunc = 42; }
+
+spec(symbol) {
+
+  it("returns an empty string for a non-existent function") {
+    check_eq_string(symbol_name((Symbol)42), string_empty);
+  }
+
+  skip_it("can lookup the name of a function") {
+    check_eq_string(symbol_name(&test_symbol_func), string_lit("test_symbol_func"));
+  }
+}
