e1028c7208f8a9dd8cd65eb8b8af34e25f3633ba
Bastian Blokland <mail@bastian.tech>
2024-05-09 15:46:13 +0300

    core: Start dwarf session

--- libs/core/src/symbol_pal_linux.c
+++ libs/core/src/symbol_pal_linux.c
@@ -1,7 +1,11 @@
 #include "core_alloc.h"
+#include "core_diag.h"
 #include "core_dynlib.h"
+#include "core_file.h"
+#include "core_path.h"
 #include "core_thread.h"
 
+#include "file_internal.h"
 #include "symbol_internal.h"
 
 #define dwarf_cmd_read 0
@@ -18,6 +22,7 @@ typedef struct {
 } DwarfDie;
 
 typedef enum {
+  SymbolResolver_Init,
   SymbolResolver_Ready,
   SymbolResolver_Error,
 } SymbolResolverState;
@@ -25,10 +30,14 @@ typedef enum {
 typedef struct {
   Allocator*          alloc;
   SymbolResolverState state;
+  File*               exec; // Handle to our own executable.
 
-  DynLib* libDw;
+  DynLib* dwLib;
+  Dwarf*  dwSession;
+
   // clang-format off
   Dwarf*      (SYS_DECL* dwarf_begin)(int fildes, int cmd);
+  int         (SYS_DECL* dwarf_end)(Dwarf*);
   DwarfDie*   (SYS_DECL* dwarf_addrdie)(Dwarf*, uptr addr, DwarfDie* result);
   int         (SYS_DECL* dwarf_getscopes)(DwarfDie* cuDie, uptr addr, DwarfDie** scopes);
   const char* (SYS_DECL* dwarf_diename)(DwarfDie*);
@@ -43,21 +52,22 @@ typedef struct {
 static SymbolResolver* g_symbolResolver;
 static ThreadMutex     g_symbolResolverMutex;
 
-static bool libdw_load(SymbolResolver* resolver) {
-  DynLibResult loadRes = dynlib_load(resolver->alloc, string_lit("libdw.so"), &resolver->libDw);
+static bool dw_load(SymbolResolver* resolver) {
+  DynLibResult loadRes = dynlib_load(resolver->alloc, string_lit("libdw.so.1"), &resolver->dwLib);
   if (loadRes != DynLibResult_Success) {
     return false;
   }
 
 #define DW_LOAD_SYM(_NAME_)                                                                        \
   do {                                                                                             \
-    resolver->_NAME_ = dynlib_symbol(resolver->libDw, string_lit(#_NAME_));                        \
+    resolver->_NAME_ = dynlib_symbol(resolver->dwLib, string_lit(#_NAME_));                        \
     if (!resolver->_NAME_) {                                                                       \
       return false;                                                                                \
     }                                                                                              \
   } while (false)
 
   DW_LOAD_SYM(dwarf_begin);
+  DW_LOAD_SYM(dwarf_end);
   DW_LOAD_SYM(dwarf_addrdie);
   DW_LOAD_SYM(dwarf_getscopes);
   DW_LOAD_SYM(dwarf_diename);
@@ -66,21 +76,48 @@ static bool libdw_load(SymbolResolver* resolver) {
   return true;
 }
 
+static bool dw_begin(SymbolResolver* resolver) {
+  diag_assert(resolver->state == SymbolResolver_Init);
+  diag_assert(!resolver->dwSession);
+  resolver->dwSession = resolver->dwarf_begin(resolver->exec->handle, dwarf_cmd_read);
+  return resolver->dwSession != null;
+}
+
+static void dw_end(SymbolResolver* resolver) {
+  diag_assert(resolver->dwSession);
+  resolver->dwarf_end(resolver->dwSession);
+}
+
 static SymbolResolver* symbol_resolver_create(Allocator* alloc) {
   SymbolResolver* resolver = alloc_alloc_t(alloc, SymbolResolver);
   *resolver                = (SymbolResolver){.alloc = alloc};
 
-  if (libdw_load(resolver)) {
-    resolver->state = SymbolResolver_Ready;
-  } else {
-    resolver->state = SymbolResolver_Error;
+  if (file_create(alloc, g_path_executable, FileMode_Open, FileAccess_Read, &resolver->exec)) {
+    goto Error;
   }
+  if (!dw_load(resolver)) {
+    goto Error;
+  }
+  if (!dw_begin(resolver)) {
+    goto Error;
+  }
+  resolver->state = SymbolResolver_Ready;
+  return resolver;
+
+Error:
+  resolver->state = SymbolResolver_Error;
   return resolver;
 }
 
 static void symbol_resolver_destroy(SymbolResolver* resolver) {
-  if (resolver->libDw) {
-    dynlib_destroy(resolver->libDw);
+  if (resolver->dwSession) {
+    dw_end(resolver);
+  }
+  if (resolver->exec) {
+    file_destroy(resolver->exec);
+  }
+  if (resolver->dwLib) {
+    dynlib_destroy(resolver->dwLib);
   }
   alloc_free_t(resolver->alloc, resolver);
 }
