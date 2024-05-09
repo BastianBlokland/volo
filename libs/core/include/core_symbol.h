6453873913ca7218f1c6577e73338bfa13bb55d5
Bastian Blokland <mail@bastian.tech>
2024-05-09 14:56:23 +0300

    core: Update docs of symbol_name

--- libs/core/include/core_symbol.h
+++ libs/core/include/core_symbol.h
@@ -6,5 +6,6 @@ typedef void* Symbol;
 /**
  * Lookup the name of the given symbol, returns an empty string if no name was found.
  * NOTE: Requires the executable to be compiled with debug symbols.
+ * NOTE: Only (non-inlined) function symbols are supported at this time.
  */
 String symbol_name(Symbol);
