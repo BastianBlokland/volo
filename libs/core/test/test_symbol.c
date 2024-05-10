0dfa4ea8ef9083c4c7eec05e618f82b1b3e7e6d4
Bastian Blokland <mail@bastian.tech>
2024-05-10 20:36:29 +0300

    core: Test stack frame collection

--- libs/core/test/test_symbol.c
+++ libs/core/test/test_symbol.c
@@ -4,7 +4,23 @@
 
 volatile i32 g_preserveFunc;
 
+spec(symbol);
+
 NO_INLINE_HINT static void test_symbol_func() { g_preserveFunc = 42; }
+
+NO_INLINE_HINT static bool test_symbol_stack_valid() {
+  const SymbolStack stack = symbol_stack();
+  /**
+   * Topmost two frames should be this function and the 'symbol' spec routine.
+   */
+  if (symbol_base(stack.frames[0]) != symbol_addr_rel((SymbolAddr)&test_symbol_stack_valid)) {
+    return false;
+  }
+  if (symbol_base(stack.frames[1]) != symbol_addr_rel((SymbolAddr)&spec_name(symbol))) {
+    return false;
+  }
+  return true;
+}
 
 spec(symbol) {
 
@@ -17,4 +33,6 @@ spec(symbol) {
     // NOTE: Requires the test executable to be build with debug info.
     check_eq_string(symbol_name((SymbolAddr)&test_symbol_func), string_lit("test_symbol_func"));
   }
+
+  it("can collect stack traces") { check(test_symbol_stack_valid()); }
 }
