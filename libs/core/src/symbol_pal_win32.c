780725f4dd0d91cfaab5388e2b9650bd1eb16b08
Bastian Blokland <bastianblokland@gmail.com>
2024-05-10 10:39:03 +0300

    core: Add win32 symbol resolver object

--- libs/core/src/symbol_pal_win32.c
+++ libs/core/src/symbol_pal_win32.c
@@ -3,16 +3,37 @@
 
 #include "symbol_internal.h"
 
-static ThreadMutex g_symResolverMutex;
+typedef struct {
+  Allocator* alloc;
+} SymResolver;
+
+static SymResolver* g_symResolver;
+static ThreadMutex  g_symResolverMutex;
+
+static SymResolver* resolver_create(Allocator* alloc) {
+  SymResolver* r = alloc_alloc_t(alloc, SymResolver);
+  *r             = (SymResolver){.alloc = alloc};
+  return r;
+}
+
+static void resolver_destroy(SymResolver* r) { alloc_free_t(r->alloc, r); }
 
 void symbol_pal_init(void) { g_symResolverMutex = thread_mutex_create(g_alloc_persist); }
 
-void symbol_pal_teardown(void) { thread_mutex_destroy(g_symResolverMutex); }
+void symbol_pal_teardown(void) {
+  if (g_symResolver) {
+    resolver_destroy(g_symResolver);
+  }
+  thread_mutex_destroy(g_symResolverMutex);
+}
 
 String symbol_pal_name(Symbol symbol) {
   String result = string_empty;
   thread_mutex_lock(g_symResolverMutex);
   {
+    if (!g_symResolver) {
+      g_symResolver = resolver_create(g_alloc_heap);
+    }
     // TODO: Implement symbol resolution.
   }
   thread_mutex_unlock(g_symResolverMutex);
