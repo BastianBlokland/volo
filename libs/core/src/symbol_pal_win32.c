25601f752dc41c26b3e6d9a59cd729b0d9971baa
Bastian Blokland <bastianblokland@gmail.com>
2024-05-11 17:37:01 +0300

    core: Implement symbol_pal_dbg_init for win32

--- libs/core/src/symbol_pal_win32.c
+++ libs/core/src/symbol_pal_win32.c
@@ -1,27 +1,14 @@
 #include "core_alloc.h"
-#include "core_diag.h"
 #include "core_dynlib.h"
 #include "core_path.h"
-#include "core_search.h"
-#include "core_thread.h"
 
 #include "symbol_internal.h"
 
 #include <Windows.h>
 #include <psapi.h>
 
-// #define VOLO_SYMBOL_RESOLVER_VERBOSE
-
-#define symbol_aux_chunk_size (4 * usize_kibibyte)
-#define symbol_name_length_max 64
-
-typedef struct {
-  SymbolAddrRel begin, end;
-  String        name;
-} SymInfo;
-
 /**
- * To lookup symbol debug information we use the DbgHelp library.
+ * To retrieve symbol debug information we use the DbgHelp library.
  * NOTE: Is only available when a PDB file is found or debug symbols are embedded in the executable.
  */
 
@@ -48,19 +35,8 @@ typedef struct {
 
 typedef BOOL(SYS_DECL* DbgHelpSymEnumCallback)(const DbgHelpSymInfo*, ULONG size, void* ctx);
 
-typedef enum {
-  SymResolver_Init,
-  SymResolver_Ready,
-  SymResolver_Error,
-} SymResolverState;
-
 typedef struct {
-  Allocator*       alloc;
-  Allocator*       allocAux; // (chunked) bump allocator for axillary data (eg symbol names).
-  SymResolverState state;
-  HANDLE           process;
-
-  DynArray syms; // SymInfo[], kept sorted on address.
+  HANDLE process;
 
   DynLib*    dbgHelp;
   bool       dbgHelpActive;
@@ -73,10 +49,7 @@ typedef struct {
   DWORD64 (SYS_DECL* SymLoadModuleEx)(HANDLE process, HANDLE file, PCSTR imageName, PCSTR moduleName, DWORD64 baseOfDll, DWORD dllSize, void* data, DWORD flags);
   BOOL    (SYS_DECL* SymEnumSymbolsEx)(HANDLE process, ULONG64 baseOfDll, PCSTR mask, DbgHelpSymEnumCallback callback, void* ctx, DWORD options);
   // clang-format on
-} SymResolver;
-
-static SymResolver* g_symResolver;
-static ThreadMutex  g_symResolverMutex;
+} SymDbg;
 
 static const char* to_null_term_scratch(const String str) {
   const Mem scratchMem = alloc_alloc(g_alloc_scratch, str.size + 1, 1);
@@ -85,53 +58,16 @@ static const char* to_null_term_scratch(const String str) {
   return scratchMem.ptr;
 }
 
-static i8 resolver_sym_compare(const void* a, const void* b) {
-  return compare_u32(field_ptr(a, SymInfo, begin), field_ptr(b, SymInfo, begin));
-}
-
-static bool resolver_sym_contains(const SymInfo* sym, const SymbolAddrRel addr) {
-  return addr >= sym->begin && addr < sym->end;
-}
-
-static void resolver_sym_register(
-    SymResolver* r, const SymbolAddrRel begin, const SymbolAddrRel end, const String name) {
-  const SymInfo sym = {.begin = begin, .end = end, .name = string_dup(r->allocAux, name)};
-  *dynarray_insert_sorted_t(&r->syms, SymInfo, resolver_sym_compare, &sym) = sym;
-
-#ifdef VOLO_SYMBOL_RESOLVER_VERBOSE
-  diag_print(
-      "[{}:{}] {}\n",
-      fmt_int(begin, .base = 16, .minDigits = 6),
-      fmt_int(end, .base = 16, .minDigits = 6),
-      fmt_text(name));
-#endif
-}
-
-static const SymInfo* resolver_sym_find(SymResolver* r, const SymbolAddrRel addr) {
-  if (!r->syms.size) {
-    return null; // No symbols known.
-  }
-  const SymInfo* begin = dynarray_begin_t(&r->syms, SymInfo);
-  const SymInfo* end   = dynarray_end_t(&r->syms, SymInfo);
-
-  const SymInfo  tgt     = {.begin = addr};
-  const SymInfo* greater = search_binary_greater_t(begin, end, SymInfo, resolver_sym_compare, &tgt);
-  const SymInfo* greaterOrEnd = greater ? greater : end;
-  const SymInfo* candidate    = greaterOrEnd - 1;
-
-  return resolver_sym_contains(candidate, addr) ? candidate : null;
-}
-
-static bool resolver_dbghelp_load(SymResolver* r) {
-  DynLibResult loadRes = dynlib_load(r->alloc, string_lit("Dbghelp.dll"), &r->dbgHelp);
+static bool sym_dbg_lib_load(SymDbg* dbg, Allocator* alloc) {
+  DynLibResult loadRes = dynlib_load(alloc, string_lit("Dbghelp.dll"), &dbg->dbgHelp);
   if (loadRes != DynLibResult_Success) {
     return false;
   }
 
 #define DBG_LOAD_SYM(_NAME_)                                                                       \
   do {                                                                                             \
-    r->_NAME_ = dynlib_symbol(r->dbgHelp, string_lit(#_NAME_));                                    \
-    if (!r->_NAME_) {                                                                              \
+    dbg->_NAME_ = dynlib_symbol(dbg->dbgHelp, string_lit(#_NAME_));                                \
+    if (!dbg->_NAME_) {                                                                            \
       return false;                                                                                \
     }                                                                                              \
   } while (false)
@@ -149,12 +85,12 @@ static bool resolver_dbghelp_load(SymResolver* r) {
  * Debug info search path.
  * NOTE: We only include the executable's own directory.
  */
-static const char* resolver_dbghelp_searchpath() {
+static const char* sym_dbg_searchpath() {
   const String execParentPath = path_parent(g_path_executable);
   return to_null_term_scratch(execParentPath);
 }
 
-static DWORD resolver_dbghelp_options() {
+static DWORD sym_dbg_options() {
   DWORD options = 0;
   options |= 0x00000004; /* SYMOPT_DEFERRED_LOADS */
   options |= 0x00000200; /* SYMOPT_FAIL_CRITICAL_ERRORS */
@@ -165,114 +101,66 @@ static DWORD resolver_dbghelp_options() {
   return options;
 }
 
-static bool resolver_dbghelp_begin(SymResolver* r) {
-  diag_assert(r->state == SymResolver_Init);
+static bool sym_dbg_lib_begin(SymDbg* dbg) {
   const BOOL invadeProcess = false; // Do not automatically load dbg-info for all modules.
-  if (!r->SymInitialize(r->process, resolver_dbghelp_searchpath(), invadeProcess)) {
+  if (!dbg->SymInitialize(dbg->process, sym_dbg_searchpath(), invadeProcess)) {
     return false;
   }
-  r->SymSetOptions(resolver_dbghelp_options());
+  dbg->SymSetOptions(sym_dbg_options());
+  dbg->dbgHelpActive = true;
 
   const char* imageName = to_null_term_scratch(g_path_executable);
-  r->dbgHelpActive      = true;
-  r->dbgHelpBaseAddr    = r->SymLoadModuleEx(r->process, null, imageName, null, 0, 0, null, 0);
-  return r->dbgHelpBaseAddr != 0;
+  dbg->dbgHelpBaseAddr  = dbg->SymLoadModuleEx(dbg->process, null, imageName, null, 0, 0, null, 0);
+  return dbg->dbgHelpBaseAddr != 0;
 }
 
-static void resolver_dbghelp_end(SymResolver* r) {
-  diag_assert(r->dbgHelpActive);
-  r->SymCleanup(r->process);
-  r->dbgHelpActive = false;
+static void sym_dbg_lib_end(SymDbg* dbg) {
+  dbg->SymCleanup(dbg->process);
+  dbg->dbgHelpActive = false;
 }
 
-static BOOL SYS_DECL
-resolver_dbghelp_enum_proc(const DbgHelpSymInfo* info, const ULONG size, void* ctx) {
+typedef struct {
+  SymDbg*    dbg;
+  SymbolReg* reg;
+} SymDbgEnumCtx;
+
+static BOOL SYS_DECL sym_dbg_enum_proc(const DbgHelpSymInfo* info, const ULONG size, void* ctx) {
   (void)size;
-  SymResolver* r = ctx;
+  SymDbgEnumCtx*   enumCtx  = ctx;
+  const SymbolAddr baseAddr = enumCtx->dbg->dbgHelpBaseAddr;
   if (info->Tag != dbghelp_symtag_function) {
     goto Continue; // Only (non-inlined) function symbols are supported at this time.
   }
-  if (!info->NameLen || info->NameLen > symbol_name_length_max) {
-    goto Continue; // When static-linking the CRT we get long c++ symbol names we can ignore.
+  if (!info->NameLen) {
+    goto Continue;
   }
-  if (info->Address < r->dbgHelpBaseAddr) {
+  if (info->Address < baseAddr) {
     goto Continue; // Symbol is outside of the executable space.
   }
   if (!size) {
     goto Continue;
   }
-  const SymbolAddrRel addrBegin = (SymbolAddrRel)((SymbolAddr)info->Address - r->dbgHelpBaseAddr);
+  const SymbolAddrRel addrBegin = (SymbolAddrRel)((SymbolAddr)info->Address - baseAddr);
   const SymbolAddrRel addrEnd   = addrBegin + (SymbolAddrRel)size;
-  const String        name      = mem_create(info->Name, info->NameLen);
-  resolver_sym_register(r, addrBegin, addrEnd, name);
+
+  symbol_reg_add(enumCtx->reg, addrBegin, addrEnd, mem_create(info->Name, info->NameLen));
 
 Continue:
   return TRUE; // Continue enumerating.
 }
 
-static bool resolver_init(SymResolver* r) {
+static bool sym_dbg_query(SymDbg* dbg, SymbolReg* reg) {
   const DWORD                  options  = 1; /* SYMENUM_OPTIONS_DEFAULT */
-  const DbgHelpSymEnumCallback callback = &resolver_dbghelp_enum_proc;
-  if (r->SymEnumSymbolsEx(r->process, r->dbgHelpBaseAddr, "*", callback, r, options)) {
+  const DbgHelpSymEnumCallback callback = &sym_dbg_enum_proc;
+
+  SymDbgEnumCtx ctx = {
+      .dbg = dbg,
+      .reg = reg,
+  };
+  if (dbg->SymEnumSymbolsEx(dbg->process, dbg->dbgHelpBaseAddr, "*", callback, &ctx, options)) {
     return true;
   }
   return false;
-}
-
-static SymResolver* resolver_create(Allocator* alloc) {
-  SymResolver* r = alloc_alloc_t(alloc, SymResolver);
-
-  *r = (SymResolver){
-      .alloc    = alloc,
-      .allocAux = alloc_chunked_create(alloc, alloc_bump_create, symbol_aux_chunk_size),
-      .process  = GetCurrentProcess(),
-      .syms     = dynarray_create_t(alloc, SymInfo, 1024),
-  };
-
-  if (!resolver_dbghelp_load(r)) {
-    goto Error;
-  }
-  if (!resolver_dbghelp_begin(r)) {
-    goto Error;
-  }
-  if (!resolver_init(r)) {
-    goto Error;
-  }
-  resolver_dbghelp_end(r);
-  r->state = SymResolver_Ready;
-  return r;
-
-Error:
-  r->state = SymResolver_Error;
-  return r;
-}
-
-static void resolver_destroy(SymResolver* r) {
-  dynarray_destroy(&r->syms);
-  if (r->dbgHelpActive) {
-    resolver_dbghelp_end(r);
-  }
-  if (r->dbgHelp) {
-    dynlib_destroy(r->dbgHelp);
-  }
-  alloc_chunked_destroy(r->allocAux);
-  alloc_free_t(r->alloc, r);
-}
-
-static const SymInfo* resolver_lookup(SymResolver* r, const SymbolAddrRel addr) {
-  if (r->state != SymResolver_Ready) {
-    return null;
-  }
-  return resolver_sym_find(r, addr);
-}
-
-void symbol_pal_init(void) { g_symResolverMutex = thread_mutex_create(g_alloc_persist); }
-
-void symbol_pal_teardown(void) {
-  if (g_symResolver) {
-    resolver_destroy(g_symResolver);
-  }
-  thread_mutex_destroy(g_symResolverMutex);
 }
 
 SymbolAddr symbol_pal_prog_begin(void) {
@@ -290,34 +178,25 @@ SymbolAddr symbol_pal_prog_end(void) {
   return programBegin + (SymbolAddr)moduleInfo.SizeOfImage;
 }
 
-String symbol_pal_dbg_name(const SymbolAddrRel addr) {
-  String result = string_empty;
-  thread_mutex_lock(g_symResolverMutex);
-  {
-    if (!g_symResolver) {
-      g_symResolver = resolver_create(g_alloc_heap);
-    }
-    const SymInfo* info = resolver_lookup(g_symResolver, addr);
-    if (info) {
-      result = info->name;
-    }
-  }
-  thread_mutex_unlock(g_symResolverMutex);
-  return result;
-}
+void symbol_pal_dbg_init(SymbolReg* reg) {
+  Allocator* bumpAlloc = alloc_bump_create_stack(4 * usize_kibibyte);
 
-SymbolAddrRel symbol_pal_dbg_base(const SymbolAddrRel addr) {
-  SymbolAddrRel result = sentinel_u32;
-  thread_mutex_lock(g_symResolverMutex);
-  {
-    if (!g_symResolver) {
-      g_symResolver = resolver_create(g_alloc_heap);
-    }
-    const SymInfo* info = resolver_lookup(g_symResolver, addr);
-    if (info) {
-      result = info->begin;
-    }
+  SymDbg dbg = {.process = GetCurrentProcess()};
+  if (!sym_dbg_lib_load(&dbg, bumpAlloc)) {
+    goto Done;
   }
-  thread_mutex_unlock(g_symResolverMutex);
-  return result;
+  if (!sym_dbg_lib_begin(&dbg)) {
+    goto Done;
+  }
+  if (!sym_dbg_query(&dbg, reg)) {
+    goto Done;
+  }
+
+Done:
+  if (dbg.dbgHelpActive) {
+    sym_dbg_lib_end(&dbg);
+  }
+  if (dbg.dbgHelp) {
+    dynlib_destroy(dbg.dbgHelp);
+  }
 }
