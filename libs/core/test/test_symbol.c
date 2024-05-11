3592b6e10c6dcc5b8bb344f09bd22cab6ea3a015
Bastian Blokland <bastianblokland@gmail.com>
2024-05-11 11:51:40 +0300

    core: Skip tests that require dbg info on MinGW

--- libs/core/test/test_symbol.c
+++ libs/core/test/test_symbol.c
@@ -2,9 +2,7 @@
 #include "core_annotation.h"
 #include "core_symbol.h"
 
-volatile i32 g_preserveFunc;
-
-spec(symbol);
+static volatile i32 g_preserveFunc;
 
 NO_INLINE_HINT static void test_symbol_func() { g_preserveFunc = 42; }
 
@@ -30,6 +28,15 @@ NO_INLINE_HINT static bool test_symbol_stack_func3() {
   return true;
 }
 
+static CheckTestFlags test_requires_dbg_info_flags(void) {
+  CheckTestFlags flags = CheckTestFlags_None;
+#ifdef __MINGW32__
+  flags |= CheckTestFlags_Skip; // MinGW (gcc port for Windows) doesn't emit PDB files at this time.
+#endif
+  // TODO: Skip if the executable was build without debug information?
+  return flags;
+}
+
 spec(symbol) {
 
   it("returns an empty string for a non-existent function") {
@@ -37,17 +44,21 @@ spec(symbol) {
     check_eq_string(symbol_name((SymbolAddr)uptr_max), string_empty);
   }
 
-  it("can lookup the name of a function") {
+  it("can lookup the name of a function", .flags = test_requires_dbg_info_flags()) {
     // NOTE: Requires the test executable to be build with debug info.
     check_eq_string(symbol_name((SymbolAddr)&test_symbol_func), string_lit("test_symbol_func"));
   }
 
-  it("can lookup the base address of a function") {
+  it("can lookup the base address of a function", .flags = test_requires_dbg_info_flags()) {
+    // NOTE: Requires the test executable to be build with debug info.
     const SymbolAddr    addr    = (SymbolAddr)&test_symbol_func;
     const SymbolAddrRel addrRel = symbol_addr_rel(addr);
     check_eq_int(symbol_base(addrRel), addrRel);
     check_eq_int(symbol_base(addrRel + 4), addrRel);
   }
 
-  it("can collect stack traces") { check(test_symbol_stack_func1()); }
+  it("can collect stack traces", .flags = test_requires_dbg_info_flags()) {
+    // NOTE: Requires the test executable to be build with debug info.
+    check(test_symbol_stack_func1());
+  }
 }
