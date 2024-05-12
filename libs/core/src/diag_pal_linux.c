d1550f74e15cf9ea09bf2bffc763a03c83bedf4f
Bastian Blokland <mail@bastian.tech>
2024-05-12 13:20:29 +0300

    core: Minor cleanup in diag_break handling on linux

--- libs/core/src/diag_pal_linux.c
+++ libs/core/src/diag_pal_linux.c
@@ -1,3 +1,5 @@
+#include "core_thread.h"
+
 #include "diag_internal.h"
 
 #include <signal.h>
@@ -6,17 +8,19 @@
 
 #define diag_crash_exit_code 1
 
-static bool g_debuggerPresent;
-
-static void diag_sigtrap_handler(int signum) {
-  (void)signum;
-
-  g_debuggerPresent = false;
+static void diag_trap_handler(MAYBE_UNUSED const int posixSignal) {
+  /**
+   * Normally do nothing when a break-point is hit, the debugger will catch it if its present.
+   */
 }
 
 void diag_pal_break(void) {
-  g_debuggerPresent = true;
-  signal(SIGTRAP, diag_sigtrap_handler);
+  static i32 g_trapHandlerInstalled;
+  if (!thread_atomic_exchange_i32(&g_trapHandlerInstalled, true)) {
+    struct sigaction action = {.sa_handler = diag_trap_handler};
+    sigemptyset(&action.sa_mask);
+    sigaction(SIGTRAP, &action, null);
+  }
   raise(SIGTRAP);
 }
 
