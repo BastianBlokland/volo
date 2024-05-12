807a2993538bad713b6af440072d27a573573eb9
Bastian Blokland <bastianblokland@gmail.com>
2024-05-12 18:34:15 +0300

    core: Dummy win32 exception intercept routines

--- libs/core/src/diag_pal_win32.c
+++ libs/core/src/diag_pal_win32.c
@@ -4,6 +4,35 @@
 
 #define diag_crash_exit_code 1
 
+static THREAD_LOCAL jmp_buf* g_exceptAnchor;
+
+void diag_pal_except_enable(jmp_buf* anchor, const i32 exceptionCode) {
+  static i32 g_exceptHandlerInstalled;
+
+  if (exceptionCode) {
+    /**
+     * An exception has occurred, report the crash with the recorded stack.
+     */
+    diag_assert(!g_exceptAnchor); // Anchors should be removed when an exception occurs.
+
+    // TODO: Report the crash.
+    diag_pal_crash();
+  } else {
+    /**
+     * Enable exception interception with the new anchor.
+     */
+    diag_assert_msg(!g_exceptAnchor, "Exception interception was already active for this thread");
+    g_exceptAnchor = anchor;
+
+    // TODO: Setup exception handler.
+  }
+}
+
+void diag_pal_except_disable(void) {
+  diag_assert_msg(g_exceptAnchor, "Exception interception was not active for this thread");
+  g_exceptAnchor = null;
+}
+
 void diag_pal_break(void) {
   if (IsDebuggerPresent()) {
     DebugBreak();
