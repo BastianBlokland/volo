7983dbd543b266fc4ab67474358ddbe843c776c9
Bastian Blokland <mail@bastian.tech>
2024-05-18 17:45:38 +0300

    core: Tweak alloc tracker output

--- libs/core/src/alloc_tracker.c
+++ libs/core/src/alloc_tracker.c
@@ -191,7 +191,7 @@ static void tracker_report_sort(TrackerReport* report) {
 }
 
 static void tracker_report_write(TrackerReport* report, DynString* out) {
-  fmt_write(out, "Active allocations:\n");
+  fmt_write(out, "Active allocations (inclusive):\n");
   for (u32 i = 0; i != report->entries.size; ++i) {
     const TrackerReportEntry* entry = dynarray_at_t(&report->entries, i, TrackerReportEntry);
 
@@ -201,7 +201,7 @@ static void tracker_report_write(TrackerReport* report, DynString* out) {
       const u32 offset = entry->addr - funcAddr;
       fmt_write(
           out,
-          " x{>4} {>5} {} {} +{}\n",
+          " x{>4} {>8} {} {} +{}\n",
           fmt_int(entry->count, .minDigits = 3),
           fmt_size(entry->size),
           fmt_int(funcAddr, .base = 16, .minDigits = 8),
@@ -211,7 +211,7 @@ static void tracker_report_write(TrackerReport* report, DynString* out) {
       const SymbolAddr addrAbs = symbol_addr_abs(entry->addr);
       fmt_write(
           out,
-          " x{>4} {>5} {} {}\n",
+          " x{>4} {>8} {} {}\n",
           fmt_int(entry->count, .minDigits = 3),
           fmt_size(entry->size),
           fmt_int(entry->addr, .base = 16, .minDigits = 8),
