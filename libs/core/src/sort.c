8ba04670a6c72763ab2bdc30ff0dacb0d97b6ce2
Bastian Blokland <bastianblokland@gmail.com>
2024-06-02 19:37:43 +0300

    core: Fast-path for 64 bit sort swaps

--- libs/core/src/sort.c
+++ libs/core/src/sort.c
@@ -1,4 +1,5 @@
 #include "core_array.h"
+#include "core_bits.h"
 #include "core_diag.h"
 #include "core_sort.h"
 
@@ -6,7 +7,12 @@
 
 #define sort_quicksort_elems_min 10
 
-INLINE_HINT static void sort_swap(u8* a, u8* b, u16 bytes) {
+typedef enum {
+  SortSwapType_u8,
+  SortSwapType_u64,
+} SortSwapType;
+
+INLINE_HINT static void sort_swap_u8(u8* a, u8* b, u16 bytes) {
   do {
     const u8 tmp = *a;
     *a++         = *b;
@@ -14,14 +20,44 @@ INLINE_HINT static void sort_swap(u8* a, u8* b, u16 bytes) {
   } while (--bytes);
 }
 
+INLINE_HINT static void sort_swap_u64(u8* a, u8* b, u16 bytes) {
+  u64* a64 = (u64*)a;
+  u64* b64 = (u64*)b;
+  do {
+    const u64 tmp = *a64;
+    *a64++        = *b64;
+    *b64++        = tmp;
+  } while (bytes -= sizeof(u64));
+}
+
+INLINE_HINT static void sort_swap(u8* a, u8* b, const u16 bytes, const SortSwapType type) {
+  switch (type) {
+  case SortSwapType_u8:
+    sort_swap_u8(a, b, bytes);
+    return;
+  case SortSwapType_u64:
+    sort_swap_u64(a, b, bytes);
+    return;
+  }
+  UNREACHABLE
+}
+
+INLINE_HINT static SortSwapType sort_swap_type(u8* ptr, const u16 bytes) {
+  if (bits_aligned_ptr(ptr, sizeof(u64)) && bits_aligned(bytes, sizeof(u64))) {
+    return SortSwapType_u64;
+  }
+  return SortSwapType_u8;
+}
+
 /**
  * Sort the given range using a basic insertion-sort scheme.
  * https://en.wikipedia.org/wiki/Insertion_sort
  */
-static void sort_insert(u8* begin, u8* end, const u16 stride, CompareFunc compare) {
+static void
+sort_insert(u8* begin, u8* end, const u16 stride, CompareFunc compare, const SortSwapType type) {
   for (u8* a = begin + stride; a < end; a += stride) {
     for (u8* b = a; b != begin && compare(b, b - stride) < 0; b -= stride) {
-      sort_swap(b, b - stride, stride);
+      sort_swap(b, b - stride, stride, type);
     }
   }
 }
@@ -30,19 +66,20 @@ static void sort_insert(u8* begin, u8* end, const u16 stride, CompareFunc compar
  * Select a pivot to partition on using the median-of-three scheme.
  * NOTE: Makes sure the first and last elements are sorted with respect to the pivot.
  */
-INLINE_HINT static u8* quicksort_pivot(u8* begin, u8* end, const u16 stride, CompareFunc compare) {
+INLINE_HINT static u8* quicksort_pivot(
+    u8* begin, u8* end, const u16 stride, CompareFunc compare, const SortSwapType type) {
   const usize elems  = (end - begin) / stride;
   u8*         center = begin + elems / 2 * stride;
   if (compare(center, begin) < 0) {
-    sort_swap(center, begin, stride);
+    sort_swap(center, begin, stride, type);
   }
   if (compare(end - stride, center) < 0) {
-    sort_swap(center, end - stride, stride);
+    sort_swap(center, end - stride, stride, type);
   } else {
     return center;
   }
   if (compare(center, begin) < 0) {
-    sort_swap(center, begin, stride);
+    sort_swap(center, begin, stride, type);
   }
   return center;
 }
@@ -54,9 +91,10 @@ INLINE_HINT static u8* quicksort_pivot(u8* begin, u8* end, const u16 stride, Com
  * Hoare's partition scheme:
  * - https://en.wikipedia.org/wiki/Quicksort#Hoare_partition_scheme
  */
-static u8* quicksort_partition(u8* begin, u8* end, const u16 stride, CompareFunc compare) {
+static u8* quicksort_partition(
+    u8* begin, u8* end, const u16 stride, CompareFunc compare, const SortSwapType type) {
   // Choose a pivot.
-  u8* pivot = quicksort_pivot(begin, end, stride, compare);
+  u8* pivot = quicksort_pivot(begin, end, stride, compare, type);
 
   // First and last elements are already sorted by 'quicksort_pivot' so can be skipped.
   begin += stride;
@@ -80,7 +118,7 @@ static u8* quicksort_partition(u8* begin, u8* end, const u16 stride, CompareFunc
     }
 
     // Begin is less then end, so swap them.
-    sort_swap(begin, end, stride);
+    sort_swap(begin, end, stride, type);
 
     // Patch up the pivot pointer in case it was moved.
     if (begin == pivot) {
@@ -108,16 +146,17 @@ void sort_quicksort(u8* begin, u8* end, const u16 stride, CompareFunc compare) {
 
   stack[stackSize++] = (QuickSortSection){.begin = begin, .end = end};
 
+  const SortSwapType swapType = sort_swap_type(begin, stride);
   while (stackSize) {
     const QuickSortSection section = stack[--stackSize];
 
     if ((section.end - section.begin) < (stride * sort_quicksort_elems_min)) {
       // Small section; use insertion sort.
-      sort_insert(section.begin, section.end, stride, compare);
+      sort_insert(section.begin, section.end, stride, compare, swapType);
       continue;
     }
 
-    u8* partition      = quicksort_partition(section.begin, section.end, stride, compare);
+    u8* partition      = quicksort_partition(section.begin, section.end, stride, compare, swapType);
     stack[stackSize++] = (QuickSortSection){.begin = section.begin, .end = partition};
     stack[stackSize++] = (QuickSortSection){.begin = partition, .end = section.end};
     diag_assert(stackSize < array_elems(stack));
@@ -139,6 +178,8 @@ void sort_bubblesort(u8* begin, u8* end, const u16 stride, CompareFunc compare) 
    * Not an efficient algorithm, but it useful for testing other sorting algorithms against.
    */
 
+  const SortSwapType swapType = sort_swap_type(begin, stride);
+
   usize len = (end - begin) / stride;
   while (len) {
     usize newLen = 0;
@@ -146,7 +187,7 @@ void sort_bubblesort(u8* begin, u8* end, const u16 stride, CompareFunc compare) 
       u8* a = begin + (i - 1) * stride;
       u8* b = begin + i * stride;
       if (compare(a, b) > 0) {
-        sort_swap(a, b, stride);
+        sort_swap(a, b, stride, swapType);
         newLen = i;
       }
     }
