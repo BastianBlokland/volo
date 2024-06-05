0eb4ecc023dd347af3747f9cb2ff8fede282f86a
Bastian Blokland <mail@bastian.tech>
2024-06-05 22:16:30 +0300

    geo: Simd impl for geo_box_intersect_ray

--- libs/geo/src/box.c
+++ libs/geo/src/box.c
@@ -411,10 +411,25 @@ GeoBox geo_box_from_frustum(const GeoVector frustum[PARAM_ARRAY_SIZE(8)]) {
 }
 
 f32 geo_box_intersect_ray(const GeoBox* box, const GeoRay* ray) {
-  /**
-   * Find the intersection of the axis-aligned box the given ray using Cyrus-Beck clipping.
-   * More information: https://izzofinal.wordpress.com/2012/11/09/ray-vs-box-round-1/
-   */
+/**
+ * Find the intersection of the axis-aligned box the given ray using Cyrus-Beck clipping.
+ * More information: https://izzofinal.wordpress.com/2012/11/09/ray-vs-box-round-1/
+ */
+#ifdef VOLO_SIMD
+  const SimdVec epsVec    = simd_vec_broadcast(f32_epsilon);
+  const SimdVec pointVec  = simd_vec_load(ray->point.comps);
+  const SimdVec dirVec    = simd_vec_load(ray->dir.comps);
+  const SimdVec dirVecInv = simd_vec_div(simd_vec_broadcast(1.0f), simd_vec_add(dirVec, epsVec));
+
+  const SimdVec boxMinVec = simd_vec_load(box->min.comps);
+  const SimdVec boxMaxVec = simd_vec_load(box->max.comps);
+
+  const SimdVec t0 = simd_vec_mul(simd_vec_sub(boxMinVec, pointVec), dirVecInv);
+  const SimdVec t1 = simd_vec_mul(simd_vec_sub(boxMaxVec, pointVec), dirVecInv);
+
+  const f32 tMin = simd_vec_x(simd_vec_max_comp3(simd_vec_min(t0, t1)));
+  const f32 tMax = simd_vec_x(simd_vec_min_comp3(simd_vec_max(t0, t1)));
+#else
   const f32 dirXInv = 1.0f / (ray->dir.x + f32_epsilon);
   const f32 dirYInv = 1.0f / (ray->dir.y + f32_epsilon);
   const f32 dirZInv = 1.0f / (ray->dir.z + f32_epsilon);
@@ -435,17 +450,10 @@ f32 geo_box_intersect_ray(const GeoBox* box, const GeoRay* ray) {
   const f32 maxB = math_max(t3, t4);
   const f32 maxC = math_max(t5, t6);
   const f32 tMax = math_min(math_min(maxA, maxB), maxC);
-
-  // if tMax < 0: ray intersects the box, but whole box is behind us.
-  if (tMax < 0) {
+#endif
+  if (tMax < 0 || tMin > tMax) {
     return -1.0f;
   }
-
-  // if tMin > tMax: ray misses the box.
-  if (tMin > tMax) {
-    return -1.0f;
-  }
-
   return tMin >= 0 ? tMin : tMax;
 }
 
