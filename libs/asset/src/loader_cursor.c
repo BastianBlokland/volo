eabacbc1e971240288ed641e7490e38a5066b153
Bastian Blokland <mail@bastian.tech>
2024-06-23 18:59:16 +0300

    asset: Support re-coloring cursors

--- libs/asset/src/loader_cursor.c
+++ libs/asset/src/loader_cursor.c
@@ -17,9 +17,14 @@ static DataReg* g_dataReg;
 static DataMeta g_dataCursorDefMeta;
 
 typedef struct {
-  String texture;
-  u32    hotspotX, hotspotY;
-  f32    scale;
+  f32 r, g, b, a;
+} CursorColorDef;
+
+typedef struct {
+  String          texture;
+  u32             hotspotX, hotspotY;
+  f32             scale;
+  CursorColorDef* color;
 } CursorDef;
 
 static void cursor_datareg_init(void) {
@@ -32,11 +37,18 @@ static void cursor_datareg_init(void) {
     DataReg* reg = data_reg_create(g_allocPersist);
 
     // clang-format off
+    data_reg_struct_t(reg, CursorColorDef);
+    data_reg_field_t(reg, CursorColorDef, r, data_prim_t(f32));
+    data_reg_field_t(reg, CursorColorDef, g, data_prim_t(f32));
+    data_reg_field_t(reg, CursorColorDef, b, data_prim_t(f32));
+    data_reg_field_t(reg, CursorColorDef, a, data_prim_t(f32));
+
     data_reg_struct_t(reg, CursorDef);
     data_reg_field_t(reg, CursorDef, texture, data_prim_t(String), .flags = DataFlags_NotEmpty);
     data_reg_field_t(reg, CursorDef, hotspotX, data_prim_t(u32));
     data_reg_field_t(reg, CursorDef, hotspotY, data_prim_t(u32));
     data_reg_field_t(reg, CursorDef, scale, data_prim_t(f32), .flags = DataFlags_NotEmpty | DataFlags_Opt);
+    data_reg_field_t(reg, CursorDef, color, t_CursorColorDef, .container = DataContainer_Pointer, .flags = DataFlags_Opt);
     // clang-format on
 
     g_dataCursorDefMeta = data_meta_t(t_CursorDef);
@@ -96,6 +108,10 @@ static AssetCursorPixel asset_cursor_pixel(const GeoColor color) {
   };
 }
 
+static GeoColor asset_cursor_color(const CursorColorDef* def) {
+  return geo_color(def->r, def->g, def->b, def->a);
+}
+
 static void asset_cursor_generate(
     const CursorDef* def, const AssetTextureComp* texture, AssetCursorComp* outCursor) {
 
@@ -107,8 +123,10 @@ static void asset_cursor_generate(
   const usize pixelMemSize = sizeof(AssetCursorPixel) * pixelCount;
   const Mem   pixelMem     = alloc_alloc(g_allocHeap, pixelMemSize, sizeof(AssetCursorPixel));
 
-  const bool needsResample = outWidth != texture->width || outHeight != texture->height;
-  if (needsResample) {
+  const bool scaled  = outWidth != texture->width || outHeight != texture->height;
+  const bool colored = def->color != null;
+  if (scaled || colored) {
+    const GeoColor    colorMul     = def->color ? asset_cursor_color(def->color) : geo_color_white;
     const f32         outWidthInv  = 1.0f / (f32)outWidth;
     const f32         outHeightInv = 1.0f / (f32)outHeight;
     AssetCursorPixel* outPixels    = pixelMem.ptr;
@@ -117,8 +135,12 @@ static void asset_cursor_generate(
       for (u32 x = 0; x != outWidth; ++x) {
         const f32 xNorm = (f32)(x + 0.5f) * outWidthInv;
 
-        const u32      layer       = 0;
-        const GeoColor colorLinear = asset_texture_sample(texture, xNorm, yNorm, layer);
+        const u32 layer       = 0;
+        GeoColor  colorLinear = asset_texture_sample(texture, xNorm, yNorm, layer);
+
+        if (colored) {
+          colorLinear = geo_color_mul_comps(colorLinear, colorMul);
+        }
 
         // Always output Srgb encoded pixels.
         outPixels[y * outWidth + x] = asset_cursor_pixel(geo_color_linear_to_srgb(colorLinear));
