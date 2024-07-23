38fb1e1396a0a5f360ec6a7c3676713352c458c2
Bastian Blokland <mail@bastian.tech>
2024-07-23 23:20:32 +0300

    data: Add data_hash util

--- libs/data/include/data_utils.h
+++ libs/data/include/data_utils.h
@@ -5,7 +5,7 @@
  * Deep-copy the original value into the given data memory.
  *
  * Pre-condition: original.size == clone.size.
- * Pre-condition: original memory contains an initialized value compatble with the given DataMeta.
+ * Pre-condition: original memory contains an initialized value compatible with the given DataMeta.
  * Pre-condition: original memory does not contain any cycles.
  * Pre-condition: DataMeta definition is not modified in parallel with this call.
  */
@@ -15,8 +15,20 @@ void data_clone(const DataReg*, Allocator* alloc, DataMeta, Mem original, Mem cl
  * Free the resources associated with the given value.
  *
  * Pre-condition: data resources where allocated from the given allocator.
- * Pre-condition: data memory contains an initialized value compatble with the given DataMeta.
+ * Pre-condition: data memory contains an initialized value compatible with the given DataMeta.
  * Pre-condition: data memory does not contain any cycles.
  * Pre-condition: DataMeta definition is not modified in parallel with this call.
  */
 void data_destroy(const DataReg*, Allocator* alloc, DataMeta, Mem data);
+
+typedef enum {
+  DataHashFlags_None       = 0,
+  DataHashFlags_ExcludeIds = 1 << 0,
+} DataHashFlags;
+
+/**
+ * Compute a hash of the data definition, useful for determining comptability of data.
+ *
+ * Pre-condition: DataMeta definition is not modified in parallel with this call.
+ */
+u32 data_hash(const DataReg*, DataMeta, DataHashFlags);
---
+++ libs/data/src/utils_hash.c
@@ -0,0 +1,122 @@
+#include "core_bits.h"
+#include "core_diag.h"
+#include "data_utils.h"
+
+#include "registry_internal.h"
+
+typedef struct {
+  const DataReg*      reg;
+  const DataHashFlags flags;
+  const DataMeta      meta;
+} HashCtx;
+
+static u32 data_hash_internal(const HashCtx*);
+
+static u32 data_hash_struct(const HashCtx* ctx) {
+  const DataDecl* decl = data_decl(ctx->reg, ctx->meta.type);
+  diag_assert(decl->kind == DataKind_Struct);
+
+  u32 hash = bits_hash_32_val((u32)decl->val_struct.fields.size);
+
+  dynarray_for_t(&decl->val_struct.fields, DataDeclField, fieldDecl) {
+    const HashCtx fieldCtx = {
+        .reg   = ctx->reg,
+        .flags = ctx->flags,
+        .meta  = fieldDecl->meta,
+    };
+    const u32 fieldHash = data_hash_internal(&fieldCtx);
+
+    if (!(ctx->flags & DataHashFlags_ExcludeIds)) {
+      hash = bits_hash_32_combine(hash, fieldDecl->id.hash);
+    }
+    hash = bits_hash_32_combine(hash, fieldHash);
+  }
+
+  return hash;
+}
+
+static u32 data_hash_union(const HashCtx* ctx) {
+  const DataDecl* decl = data_decl(ctx->reg, ctx->meta.type);
+  diag_assert(decl->kind == DataKind_Union);
+
+  u32 hash = bits_hash_32_val((u32)decl->val_union.choices.size);
+
+  dynarray_for_t(&decl->val_union.choices, DataDeclChoice, choiceDecl) {
+    const HashCtx choiceCtx = {
+        .reg   = ctx->reg,
+        .flags = ctx->flags,
+        .meta  = choiceDecl->meta,
+    };
+    const u32 choiceTagHash = bits_hash_32_val(choiceDecl->tag);
+    const u32 choiceValHash = data_hash_internal(&choiceCtx);
+
+    if (!(ctx->flags & DataHashFlags_ExcludeIds)) {
+      hash = bits_hash_32_combine(hash, choiceDecl->id.hash);
+    }
+    hash = bits_hash_32_combine(hash, choiceTagHash);
+    hash = bits_hash_32_combine(hash, choiceValHash);
+  }
+
+  return hash;
+}
+
+static u32 data_hash_enum(const HashCtx* ctx) {
+  const DataDecl* decl = data_decl(ctx->reg, ctx->meta.type);
+
+  u32 hash = bits_hash_32_val((u32)decl->val_enum.consts.size);
+
+  dynarray_for_t(&decl->val_enum.consts, DataDeclConst, constDecl) {
+    const u32 constValHash = bits_hash_32_val((u32)constDecl->value);
+
+    if (!(ctx->flags & DataHashFlags_ExcludeIds)) {
+      hash = bits_hash_32_combine(hash, constDecl->id.hash);
+    }
+    hash = bits_hash_32_combine(hash, constValHash);
+  }
+
+  return hash;
+}
+
+static u32 data_hash_single(const HashCtx* ctx) {
+  const DataKind kind = data_decl(ctx->reg, ctx->meta.type)->kind;
+  switch (kind) {
+  case DataKind_bool:
+  case DataKind_i8:
+  case DataKind_i16:
+  case DataKind_i32:
+  case DataKind_i64:
+  case DataKind_u8:
+  case DataKind_u16:
+  case DataKind_u32:
+  case DataKind_u64:
+  case DataKind_f32:
+  case DataKind_f64:
+  case DataKind_String:
+  case DataKind_Mem:
+    return bits_hash_32_val(kind);
+  case DataKind_Struct:
+    return data_hash_struct(ctx);
+  case DataKind_Union:
+    return data_hash_union(ctx);
+  case DataKind_Enum:
+    return data_hash_enum(ctx);
+  case DataKind_Invalid:
+  case DataKind_Count:
+    break;
+  }
+  diag_crash();
+}
+
+static u32 data_hash_internal(const HashCtx* ctx) {
+  const u32 containerHash = bits_hash_32_val(ctx->meta.container);
+  return bits_hash_32_combine(containerHash, data_hash_single(ctx));
+}
+
+u32 data_hash(const DataReg* reg, const DataMeta meta, const DataHashFlags flags) {
+  const HashCtx ctx = {
+      .reg   = reg,
+      .flags = flags,
+      .meta  = meta,
+  };
+  return data_hash_internal(&ctx);
+}
