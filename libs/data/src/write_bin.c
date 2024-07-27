70bf5744ec751ff4f2dcfbddf40eb78c22f608d0
Bastian Blokland <mail@bastian.tech>
2024-07-27 15:20:12 +0300

    data: Fix string and mem bin writing

--- libs/data/src/write_bin.c
+++ libs/data/src/write_bin.c
@@ -138,7 +138,6 @@ static void data_write_bin_val_single(const WriteCtx* ctx) {
     return;
   case DataKind_String:
   case DataKind_Mem:
-    return;
     bin_push_mem(ctx, *mem_as_t(ctx->data, Mem));
     return;
   case DataKind_Struct:
@@ -163,10 +162,10 @@ static void data_write_bin_val_pointer(const WriteCtx* ctx) {
   if (ptr) {
     const DataDecl* decl   = data_decl(ctx->reg, ctx->meta.type);
     const WriteCtx  subCtx = {
-        .reg  = ctx->reg,
-        .out  = ctx->out,
-        .meta = data_meta_base(ctx->meta),
-        .data = mem_create(ptr, decl->size),
+         .reg  = ctx->reg,
+         .out  = ctx->out,
+         .meta = data_meta_base(ctx->meta),
+         .data = mem_create(ptr, decl->size),
     };
     data_write_bin_val_single(&subCtx);
   }
