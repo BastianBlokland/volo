58a75f51b302207daf8a4fdc8b936782824f42e9
Bastian Blokland <mail@bastian.tech>
2024-08-03 20:02:53 +0300

    asset: Create cache dir

--- libs/asset/src/cache.c
+++ libs/asset/src/cache.c
@@ -1,12 +1,21 @@
 #include "core_alloc.h"
 #include "core_diag.h"
+#include "core_file.h"
 
 #include "cache_internal.h"
 
 struct sAssetCache {
   Allocator* alloc;
   String     path;
+  bool       error;
 };
+
+static void cache_ensure_dir(AssetCache* cache) {
+  const FileResult createRes = file_create_dir_sync(cache->path);
+  if (UNLIKELY(createRes != FileResult_Success && createRes != FileResult_AlreadyExists)) {
+    cache->error = true;
+  }
+}
 
 AssetCache* asset_cache_create(Allocator* alloc, const String path) {
   diag_assert(!string_is_empty(path));
@@ -17,6 +26,8 @@ AssetCache* asset_cache_create(Allocator* alloc, const String path) {
       .alloc = alloc,
       .path  = string_dup(alloc, path),
   };
+
+  cache_ensure_dir(cache);
 
   return cache;
 }
