d10f6821be026d87600fad93a066054b270d3cce
Bastian Blokland <mail@bastian.tech>
2024-08-03 20:45:55 +0300

    asset: Add cache registry open logic

--- libs/asset/src/cache.c
+++ libs/asset/src/cache.c
@@ -1,8 +1,13 @@
 #include "core_alloc.h"
 #include "core_diag.h"
 #include "core_file.h"
+#include "core_path.h"
+#include "data.h"
+#include "log_logger.h"
 
 #include "cache_internal.h"
+
+static const String g_assetCacheRegName = string_static("registry.blob");
 
 typedef struct {
   u32 dummy;
@@ -12,16 +17,65 @@ struct sAssetCache {
   Allocator*         alloc;
   String             path;
   bool               error;
-  AssetCacheRegistry registry;
+  AssetCacheRegistry reg;
+  File*              regFile;
 };
 
 DataMeta g_assetCacheDataDef;
 
-static void cache_ensure_dir(AssetCache* cache) {
+static bool cache_ensure_dir(AssetCache* cache) {
   const FileResult createRes = file_create_dir_sync(cache->path);
   if (UNLIKELY(createRes != FileResult_Success && createRes != FileResult_AlreadyExists)) {
-    cache->error = true;
+    log_e(
+        "Failed to create asset cache dir",
+        log_param("path", fmt_path(cache->path)),
+        log_param("error", fmt_text(file_result_str(createRes))));
+    return false;
   }
+  return true;
+}
+
+static bool cache_registry_open(AssetCache* cache) {
+  const String          regPath   = path_build_scratch(cache->path, g_assetCacheRegName);
+  const FileAccessFlags regAccess = FileAccess_Read | FileAccess_Write;
+
+  FileResult fileRes;
+  fileRes = file_create(cache->alloc, regPath, FileMode_Open, regAccess, &cache->regFile);
+  if (fileRes == FileResult_NotFound) {
+    return false;
+  }
+  if (UNLIKELY(fileRes != FileResult_Success)) {
+    log_w(
+        "Failed to open asset cache registry",
+        log_param("path", fmt_path(regPath)),
+        log_param("error", fmt_text(file_result_str(fileRes))));
+    return false;
+  }
+
+  String data;
+  fileRes = file_map(cache->regFile, &data);
+  if (UNLIKELY(fileRes != FileResult_Success)) {
+    log_w(
+        "Failed to map asset cache registry",
+        log_param("path", fmt_path(regPath)),
+        log_param("error", fmt_text(file_result_str(fileRes))));
+    file_destroy(cache->regFile);
+    return false;
+  }
+
+  DataReadResult readRes;
+  data_read_bin(g_dataReg, data, cache->alloc, g_assetCacheDataDef, mem_var(cache->reg), &readRes);
+  if (UNLIKELY(readRes.error)) {
+    log_w(
+        "Failed to read asset cache registry",
+        log_param("path", fmt_path(regPath)),
+        log_param("error", fmt_text(readRes.errorMsg)));
+    file_destroy(cache->regFile);
+    return false;
+  }
+
+  file_unmap(cache->regFile);
+  return true;
 }
 
 void asset_data_init_cache(void) {
@@ -43,12 +97,26 @@ AssetCache* asset_cache_create(Allocator* alloc, const String path) {
       .path  = string_dup(alloc, path),
   };
 
-  cache_ensure_dir(cache);
+  if (!cache_ensure_dir(cache)) {
+    cache->error = true;
+    goto Ret;
+  }
+  if (!cache_registry_open(cache)) {
+    cache->error = true;
+    goto Ret;
+  }
 
+Ret:
   return cache;
 }
 
 void asset_cache_destroy(AssetCache* cache) {
+  if (cache->regFile) {
+    // TODO: Flush changes.
+    file_destroy(cache->regFile);
+  }
+  data_destroy(g_dataReg, cache->alloc, g_assetCacheDataDef, mem_var(cache->reg));
+
   string_free(cache->alloc, cache->path);
   alloc_free_t(cache->alloc, cache);
 }
