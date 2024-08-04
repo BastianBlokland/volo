e0eae4ddeb5938c67ec8612f3009f90a19ab1e37
Bastian Blokland <mail@bastian.tech>
2024-08-04 12:33:13 +0300

    blob2j: Initial implementation

--- apps/utilities/blob2j.c
+++ apps/utilities/blob2j.c
@@ -1,17 +1,83 @@
 #include "app_cli.h"
 #include "asset.h"
+#include "core_alloc.h"
 #include "core_file.h"
+#include "core_format.h"
+#include "data.h"
 
 /**
  * Blob2J - Utility to convert Volo binary blobs to json.
  */
+
+static i32 blob2j_run(File* inputFile, File* outputFile) {
+  DynString buffer   = dynstring_create(g_allocHeap, usize_kibibyte);
+  Mem       data     = mem_empty;
+  i32       exitCode = 0;
+
+  if (file_read_to_end_sync(inputFile, &buffer)) {
+    file_write_sync(g_fileStdErr, string_lit("ERROR: Failed to read input.\n"));
+    exitCode = 1;
+    goto Ret;
+  }
+  const String input = dynstring_view(&buffer);
+
+  DataBinHeader  dataHeader;
+  DataReadResult readRes;
+  data_read_bin_header(input, &dataHeader, &readRes);
+  if (readRes.error) {
+    file_write_sync(
+        g_fileStdErr,
+        fmt_write_scratch("ERROR: Failed to read input: {}.\n", fmt_text(readRes.errorMsg)));
+    exitCode = 1;
+    goto Ret;
+  }
+
+  const DataType dataType = data_type_from_name_hash(g_dataReg, dataHeader.typeNameHash);
+  if (!dataType) {
+    file_write_sync(g_fileStdErr, string_lit("ERROR: Unknown input type.\n"));
+    exitCode = 1;
+    goto Ret;
+  }
+  const DataMeta dataMeta  = data_meta_t(dataType);
+  const usize    dataSize  = data_meta_size(g_dataReg, dataMeta);
+  const usize    dataAlign = data_meta_align(g_dataReg, dataMeta);
+  data                     = alloc_alloc(g_allocHeap, dataSize, dataAlign);
+
+  data_read_bin(g_dataReg, input, g_allocHeap, dataMeta, data, &readRes);
+  if (readRes.error) {
+    file_write_sync(
+        g_fileStdErr,
+        fmt_write_scratch("ERROR: Failed to read input: {}.\n", fmt_text(readRes.errorMsg)));
+    exitCode = 1;
+    goto Ret;
+  }
+
+  dynstring_clear(&buffer);
+  data_write_json(g_dataReg, &buffer, dataMeta, data, &data_write_json_opts());
+  dynstring_append_char(&buffer, '\n');
+
+  data_destroy(g_dataReg, g_allocHeap, dataMeta, data);
+
+  if (file_write_sync(outputFile, dynstring_view(&buffer))) {
+    file_write_sync(g_fileStdErr, string_lit("ERROR: Failed to write output.\n"));
+    exitCode = 1;
+    goto Ret;
+  }
+
+Ret:
+  if (mem_valid(data)) {
+    alloc_free(g_allocHeap, data);
+  }
+  dynstring_destroy(&buffer);
+  return exitCode;
+}
 
 static CliId g_optPath, g_optHelp;
 
 void app_cli_configure(CliApp* app) {
   cli_app_register_desc(app, string_lit("Utility to convert Volo binary blobs to json."));
 
-  g_optPath = cli_register_flag(app, 'p', string_lit("path"), CliOptionFlags_Required);
+  g_optPath = cli_register_arg(app, string_lit("path"), CliOptionFlags_Value);
   cli_register_desc(app, g_optPath, string_lit("Path to the binary blob."));
   cli_register_validator(app, g_optPath, cli_validate_file_regular);
 
@@ -28,5 +94,23 @@ i32 app_cli_run(const CliApp* app, const CliInvocation* invoc) {
     return 0;
   }
 
-  return 0;
+  File* inputFile      = null;
+  bool  inputFileOwned = false;
+
+  const String inputPath = cli_read_string(invoc, g_optPath, string_empty);
+  if (string_is_empty(inputPath)) {
+    inputFile = g_fileStdIn;
+  } else {
+    if (file_create(g_allocHeap, inputPath, FileMode_Open, FileAccess_Read, &inputFile)) {
+      file_write_sync(g_fileStdErr, string_lit("ERROR: Failed to open input file.\n"));
+      return 1;
+    }
+    inputFileOwned = true;
+  }
+
+  const i32 ret = blob2j_run(inputFile, g_fileStdOut);
+  if (inputFileOwned) {
+    file_destroy(inputFile);
+  }
+  return ret;
 }
