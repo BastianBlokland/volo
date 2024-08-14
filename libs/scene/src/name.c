bdf3b37efe1676aa17a956399a165f4f9847b161
Bastian Blokland <mail@bastian.tech>
2024-08-14 20:34:17 +0300

    scene: Assign debug name for vfx

--- libs/scene/src/name.c
+++ libs/scene/src/name.c
@@ -5,6 +5,7 @@
 #include "scene_level.h"
 #include "scene_name.h"
 #include "scene_renderable.h"
+#include "scene_vfx.h"
 
 ecs_comp_define_public(SceneNameComp);
 
@@ -12,6 +13,8 @@ ecs_view_define(InitDebugView) {
   ecs_access_with(SceneLevelInstanceComp);
   ecs_access_without(SceneNameComp);
   ecs_access_maybe_read(SceneRenderableComp);
+  ecs_access_maybe_read(SceneVfxDecalComp);
+  ecs_access_maybe_read(SceneVfxSystemComp);
 }
 
 ecs_view_define(AssetView) { ecs_access_read(AssetComp); }
@@ -23,6 +26,16 @@ static String scene_debug_name_from_asset(const AssetComp* assetComp) {
 static String scene_debug_name_find(EcsIterator* entityItr, EcsIterator* assetItr) {
   const SceneRenderableComp* renderable = ecs_view_read_t(entityItr, SceneRenderableComp);
   if (renderable && ecs_view_maybe_jump(assetItr, renderable->graphic)) {
+    const AssetComp* assetComp = ecs_view_read_t(assetItr, AssetComp);
+    return scene_debug_name_from_asset(assetComp);
+  }
+  const SceneVfxDecalComp* vfxDecal = ecs_view_read_t(entityItr, SceneVfxDecalComp);
+  if (vfxDecal && ecs_view_maybe_jump(assetItr, vfxDecal->asset)) {
+    const AssetComp* assetComp = ecs_view_read_t(assetItr, AssetComp);
+    return scene_debug_name_from_asset(assetComp);
+  }
+  const SceneVfxSystemComp* vfxSystem = ecs_view_read_t(entityItr, SceneVfxSystemComp);
+  if (vfxSystem && ecs_view_maybe_jump(assetItr, vfxSystem->asset)) {
     const AssetComp* assetComp = ecs_view_read_t(assetItr, AssetComp);
     return scene_debug_name_from_asset(assetComp);
   }
