3cc4c5dd7db955891ee8f623aeb2540b626913d0
Bastian Blokland <mail@bastian.tech>
2024-08-15 22:02:32 +0300

    vfx: Collect global stats

--- libs/vfx/src/stats.c
+++ libs/vfx/src/stats.c
@@ -1,6 +1,9 @@
+#include "ecs_view.h"
+#include "ecs_world.h"
 #include "vfx_stats.h"
 
 ecs_comp_define_public(VfxStatsComp);
+ecs_comp_define_public(VfxStatsGlobalComp);
 
 static void ecs_combine_vfx_stats(void* dataA, void* dataB) {
   VfxStatsComp* compA = dataA;
@@ -12,7 +15,40 @@ static void ecs_combine_vfx_stats(void* dataA, void* dataB) {
   }
 }
 
+ecs_view_define(GlobalStatsView) { ecs_access_write(VfxStatsGlobalComp); }
+ecs_view_define(StatsView) { ecs_access_write(VfxStatsComp); }
+
+static VfxStatsGlobalComp* vfx_stats_global_get_or_create(EcsWorld* world) {
+  EcsView*     view = ecs_world_view_t(world, GlobalStatsView);
+  EcsIterator* itr  = ecs_view_maybe_at(view, ecs_world_global(world));
+  return itr ? ecs_view_write_t(itr, VfxStatsGlobalComp)
+             : ecs_world_add_t(world, ecs_world_global(world), VfxStatsGlobalComp);
+}
+
+ecs_system_define(VfxStatsUpdateSys) {
+  VfxStatsGlobalComp* globalStats = vfx_stats_global_get_or_create(world);
+
+  mem_set(mem_var(globalStats->values), 0);
+
+  EcsView* statsView = ecs_world_view_t(world, StatsView);
+  for (EcsIterator* itr = ecs_view_itr(statsView); ecs_view_walk(itr);) {
+    VfxStatsComp* stats = ecs_view_write_t(itr, VfxStatsComp);
+
+    for (VfxStat stat = 0; stat != VfxStat_Count; ++stat) {
+      globalStats->values[stat] += stats->valuesNew[stat];
+
+      stats->valuesLast[stat] = stats->valuesNew[stat];
+      stats->valuesNew[stat]  = 0;
+    }
+  }
+}
+
 ecs_module_init(vfx_stats_module) {
   ecs_register_comp(VfxStatsComp, .combinator = ecs_combine_vfx_stats);
   ecs_register_comp(VfxStatsGlobalComp);
+
+  ecs_register_view(GlobalStatsView);
+  ecs_register_view(StatsView);
+
+  ecs_register_system(VfxStatsUpdateSys, ecs_view_id(GlobalStatsView), ecs_view_id(StatsView));
 }
