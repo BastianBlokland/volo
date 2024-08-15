b73eac667387167237c1d672afbf6d05cc4993d8
Bastian Blokland <mail@bastian.tech>
2024-08-15 21:21:03 +0300

    vfx: Add combinator for stats

--- libs/vfx/src/stats.c
+++ libs/vfx/src/stats.c
@@ -2,4 +2,16 @@
 
 ecs_comp_define_public(VfxStatsComp);
 
-ecs_module_init(vfx_stats_module) { ecs_register_comp(VfxStatsComp); }
+static void ecs_combine_vfx_stats(void* dataA, void* dataB) {
+  VfxStatsComp* compA = dataA;
+  VfxStatsComp* compB = dataB;
+
+  for (VfxStat stat = 0; stat != VfxStat_Count; ++stat) {
+    compA->valuesNew[stat] += compB->valuesNew[stat];
+    compA->valuesLast[stat] += compB->valuesLast[stat];
+  }
+}
+
+ecs_module_init(vfx_stats_module) {
+  ecs_register_comp(VfxStatsComp, .combinator = ecs_combine_vfx_stats);
+}
