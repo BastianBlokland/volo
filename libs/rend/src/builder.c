7681fcab2ee68119a19d224f1b73945d855684a7
Bastian Blokland <mail@bastian.tech>
2024-08-24 20:07:07 +0300

    rend: Add rend_builder_buffer api

--- libs/rend/src/builder.c
+++ libs/rend/src/builder.c
@@ -1,27 +1,42 @@
 #include "core_alloc.h"
+#include "core_diag.h"
 #include "core_dynarray.h"
+#include "jobs_executor.h"
 
 #include "builder_internal.h"
 #include "rvk/pass_internal.h"
 
+#define rend_builder_workers_max 8
+
+struct sRendBuilderBuffer {
+  DynArray draws; // RvkPassDraw[]
+};
+
 struct sRendBuilder {
-  Allocator* allocator;
-  DynArray   drawBuffer; // RvkPassDraw[]
+  Allocator*        allocator;
+  RendBuilderBuffer buffers[rend_builder_workers_max];
 };
 
 RendBuilder* rend_builder_create(Allocator* alloc) {
   RendBuilder* builder = alloc_alloc_t(alloc, RendBuilder);
 
-  *builder = (RendBuilder){
-      .allocator  = alloc,
-      .drawBuffer = dynarray_create_t(alloc, RvkPassDraw, 8),
-  };
+  *builder = (RendBuilder){.allocator = alloc};
+
+  for (u32 i = 0; i != rend_builder_workers_max; ++i) {
+    builder->buffers[i].draws = dynarray_create_t(alloc, RvkPassDraw, 8);
+  }
 
   return builder;
 }
 
 void rend_builder_destroy(RendBuilder* builder) {
-  dynarray_destroy(&builder->drawBuffer);
-
+  for (u32 i = 0; i != rend_builder_workers_max; ++i) {
+    dynarray_destroy(&builder->buffers[i].draws);
+  }
   alloc_free_t(builder->allocator, builder);
+}
+
+RendBuilderBuffer* rend_builder_buffer(const RendBuilder* builder) {
+  diag_assert(g_jobsWorkerId < rend_builder_workers_max);
+  return (RendBuilderBuffer*)&builder->buffers[g_jobsWorkerId];
 }
--- libs/rend/src/builder_internal.h
+++ libs/rend/src/builder_internal.h
@@ -12,7 +12,14 @@ typedef struct sRvkPass    RvkPass;
 /**
  * Utility to submit draws.
  */
-typedef struct sRendBuilder RendBuilder;
+typedef struct sRendBuilder       RendBuilder;
+typedef struct sRendBuilderBuffer RendBuilderBuffer;
 
 RendBuilder* rend_builder_create(Allocator*);
 void         rend_builder_destroy(RendBuilder*);
+
+/**
+ * Retrieve a thread-local buffer for the calling thread.
+ * NOTE: Buffers should not be stored and/or shared between threads.
+ */
+RendBuilderBuffer* rend_builder_buffer(const RendBuilder*);
