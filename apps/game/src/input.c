2b23fd2e4513debc7d7069f1303a79f0f6995a9c
Bastian Blokland <mail@bastian.tech>
2024-09-02 22:17:05 +0300

    game: Fix camera input overshoot (#998)
    * game: Fix camera zoom overshoot
    
    * game: Fix cam rotate overshoot

--- apps/game/src/input.c
+++ apps/game/src/input.c
@@ -267,7 +267,7 @@ static void update_camera_movement(
     state->camRotYTgt  = math_mod_f32(state->camRotYTgt + rotDelta, math_pi_f32 * 2.0f);
     lockCursor         = true;
   }
-  const f32 camRotEaseDelta = deltaSeconds * g_inputCamRotYEaseSpeed;
+  const f32 camRotEaseDelta = math_min(1.0f, deltaSeconds * g_inputCamRotYEaseSpeed);
   state->camRotY = math_lerp_angle_f32(state->camRotY, state->camRotYTgt, camRotEaseDelta);
 
   // Update zoom.
@@ -277,7 +277,7 @@ static void update_camera_movement(
       const f32 zoomDelta = input_scroll_y(input) * g_inputCamZoomMult;
       state->camZoomTgt   = math_clamp_f32(state->camZoomTgt + zoomDelta, 0.0f, 1.0f);
     }
-    const f32 camZoomEaseDelta = deltaSeconds * g_inputCamZoomEaseSpeed;
+    const f32 camZoomEaseDelta = math_min(1.0f, deltaSeconds * g_inputCamZoomEaseSpeed);
     state->camZoom             = math_lerp(state->camZoom, state->camZoomTgt, camZoomEaseDelta);
   }
 
