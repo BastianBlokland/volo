a85594d22d98c52eecb4569cae7c3c0f60f7add5
Bastian Blokland <mail@bastian.tech>
2024-09-06 21:39:04 +0300

    rend: Add global passes

--- libs/rend/src/platform_internal.h
+++ libs/rend/src/platform_internal.h
@@ -3,11 +3,28 @@
 
 // Internal forward declarations:
 typedef struct sRvkDevice   RvkDevice;
+typedef struct sRvkPass     RvkPass;
 typedef struct sRendBuilder RendBuilder;
+
+typedef enum {
+  RendPassId_Geometry,
+  RendPassId_Decal,
+  RendPassId_Fog,
+  RendPassId_FogBlur,
+  RendPassId_Shadow,
+  RendPassId_AmbientOcclusion,
+  RendPassId_Forward,
+  RendPassId_Distortion,
+  RendPassId_Bloom,
+  RendPassId_Post,
+
+  RendPassId_Count,
+} RendPassId;
 
 ecs_comp_extern_public(RendPlatformComp) {
   RvkDevice*   device;
   RendBuilder* builder;
+  RvkPass*     passes[RendPassId_Count];
 };
 
 void rend_platform_teardown(EcsWorld*);
