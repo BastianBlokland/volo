9a47fd46d88fef6c64dc23ed08f814d360cb50b9
Bastian Blokland <mail@bastian.tech>
2024-09-08 17:38:51 +0300

    rend: Add mesh support to repository

--- libs/rend/src/rvk/repository_internal.h
+++ libs/rend/src/rvk/repository_internal.h
@@ -1,8 +1,9 @@
 #pragma once
 
 // Internal forward declarations:
-typedef struct sRvkTexture RvkTexture;
 typedef struct sRvkGraphic RvkGraphic;
+typedef struct sRvkMesh    RvkMesh;
+typedef struct sRvkTexture RvkTexture;
 
 typedef enum {
   RvkRepositoryId_AmbientDebugGraphic,
@@ -20,6 +21,7 @@ typedef enum {
   RvkRepositoryId_FogBlurHorGraphic,
   RvkRepositoryId_FogBlurVerGraphic,
   RvkRepositoryId_FogGraphic,
+  RvkRepositoryId_MissingMesh,
   RvkRepositoryId_MissingTexture,
   RvkRepositoryId_MissingTextureCube,
   RvkRepositoryId_OutlineGraphic,
@@ -40,10 +42,12 @@ RvkRepository* rvk_repository_create(void);
 void           rvk_repository_destroy(RvkRepository*);
 
 void rvk_repository_texture_set(RvkRepository*, RvkRepositoryId, const RvkTexture*);
+void rvk_repository_mesh_set(RvkRepository*, RvkRepositoryId, const RvkMesh*);
 void rvk_repository_graphic_set(RvkRepository*, RvkRepositoryId, const RvkGraphic*);
 
 void rvk_repository_unset(RvkRepository*, RvkRepositoryId);
 
 const RvkTexture* rvk_repository_texture_get(const RvkRepository*, RvkRepositoryId);
+const RvkMesh*    rvk_repository_mesh_get(const RvkRepository*, RvkRepositoryId);
 const RvkGraphic* rvk_repository_graphic_get(const RvkRepository*, RvkRepositoryId);
 const RvkGraphic* rvk_repository_graphic_get_maybe(const RvkRepository*, RvkRepositoryId);
