1fcac8c76093a3b728fe1e59da088ac759290cb8
Bastian Blokland <mail@bastian.tech>
2024-09-19 19:53:59 +0300

    snd: Increase max period size

--- libs/snd/src/constants_internal.h
+++ libs/snd/src/constants_internal.h
@@ -4,4 +4,4 @@
 #define snd_frame_sample_depth 16
 #define snd_frame_sample_alignment 16
 #define snd_frame_count_alignment 4
-#define snd_frame_count_max 2048
+#define snd_frame_count_max (2048 * 3)
