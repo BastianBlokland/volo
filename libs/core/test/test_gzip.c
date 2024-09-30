f73c0527b6465ba852eff4e36860663fe32fe0e5
Bastian Blokland <mail@bastian.tech>
2024-09-30 21:00:58 +0300

    core: Add gzip decode test

---
+++ libs/core/test/test_gzip.c
@@ -0,0 +1,22 @@
+#include "check_spec.h"
+#include "core_alloc.h"
+#include "core_base64.h"
+#include "core_gzip.h"
+
+spec(gzip) {
+
+  it("can decode a hello-world file") {
+    const String dataB64 = string_lit("H4sICNne+mYAA3Rlc3QudHh0APNIzcnJVwjPL8pJUeQCAN3dFH0NAAAA");
+    const String data    = base64_decode_scratch(dataB64);
+
+    Mem       outputMem    = alloc_alloc(g_allocScratch, usize_kibibyte, 1);
+    DynString outputBuffer = dynstring_create_over(outputMem);
+
+    GzipError    err;
+    const String remaining = gzip_decode(data, &outputBuffer, &err);
+
+    check_eq_int(err, GzipError_None);
+    check_eq_string(remaining, string_empty);
+    check_eq_string(dynstring_view(&outputBuffer), string_lit("Hello World!\n"));
+  }
+}
