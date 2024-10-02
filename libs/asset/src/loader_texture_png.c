a8d0d233f1b5410965fa929e4f12acdc95c5f6d9
Bastian Blokland <mail@bastian.tech>
2024-10-02 21:20:35 +0300

    asset: Parse png chunks

--- libs/asset/src/loader_texture_png.c
+++ libs/asset/src/loader_texture_png.c
@@ -1,4 +1,5 @@
 #include "core_array.h"
+#include "core_bits.h"
 #include "ecs_world.h"
 #include "log_logger.h"
 
@@ -10,12 +11,21 @@
  * Spec: https://www.w3.org/TR/png-3/
  */
 
+#define png_max_chunks 64
+
 static const String g_pngMagic = string_lit("\x89\x50\x4E\x47\x0D\x0A\x1A\x0A");
+
+typedef struct {
+  u8  type[4];
+  Mem data;
+} PngChunk;
 
 typedef enum {
   PngError_None = 0,
+  PngError_MagicMismatch,
   PngError_Truncated,
-  PngError_Malformed,
+  PngError_ChunkLimitExceeded,
+  PngError_ChunkChecksumFailed,
 
   PngError_Count,
 } PngError;
@@ -23,11 +33,65 @@ typedef enum {
 static String png_error_str(const PngError err) {
   static const String g_msgs[] = {
       string_static("None"),
+      string_static("Data is not a png file"),
       string_static("Truncated png data"),
-      string_static("Malformed png data"),
+      string_static("Png exceeds chunk limit"),
+      string_static("Png chunk checksum failed"),
   };
   ASSERT(array_elems(g_msgs) == PngError_Count, "Incorrect number of png-error messages");
   return g_msgs[err];
+}
+
+static u32 png_load_chunks(Mem d, PngChunk out[PARAM_ARRAY_SIZE(png_max_chunks)], PngError* err) {
+  // Read magic bytes.
+  if (UNLIKELY(!string_starts_with(d, g_pngMagic))) {
+    *err = PngError_MagicMismatch;
+    return 0;
+  }
+  d = mem_consume(d, g_pngMagic.size);
+
+  // Read all chunks.
+  u32 chunkCount = 0;
+  while (!string_is_empty(d)) {
+    if (UNLIKELY(chunkCount == png_max_chunks)) {
+      *err = PngError_ChunkLimitExceeded;
+      return 0;
+    }
+    if (UNLIKELY(d.size < 4)) {
+      *err = PngError_Truncated;
+      return 0;
+    }
+    PngChunk* chunk = &out[chunkCount++];
+
+    // Read length.
+    u32 length;
+    d = mem_consume_be_u32(d, &length);
+
+    if (UNLIKELY(d.size < (length + 8))) {
+      *err = PngError_Truncated;
+      return 0;
+    }
+    const Mem typeAndDataMem = mem_slice(d, 0, length + 4);
+
+    // Read type.
+    mem_cpy(array_mem(chunk->type), mem_slice(d, 0, 4));
+    d = mem_consume(d, sizeof(chunk->type));
+
+    // Read data.
+    chunk->data = mem_slice(d, 0, length);
+    d           = mem_consume(d, length);
+
+    // Read checksum.
+    u32 crc;
+    d = mem_consume_be_u32(d, &crc);
+
+    // Validate checksum.
+    if (UNLIKELY(crc != bits_crc_32(0, typeAndDataMem))) {
+      *err = PngError_ChunkChecksumFailed;
+      return 0;
+    }
+  }
+  return chunkCount;
 }
 
 static void png_load_fail(EcsWorld* w, const EcsEntityId e, const String id, const PngError err) {
@@ -42,18 +106,17 @@ static void png_load_fail(EcsWorld* w, const EcsEntityId e, const String id, con
 void asset_load_tex_png(
     EcsWorld* world, const String id, const EcsEntityId entity, AssetSource* src) {
 
-  Mem data = src->data;
-  if (UNLIKELY(data.size < g_pngMagic.size)) {
-    png_load_fail(world, entity, id, PngError_Truncated);
-    goto Ret;
-  }
-  if (UNLIKELY(!string_starts_with(data, g_pngMagic))) {
-    png_load_fail(world, entity, id, PngError_Malformed);
+  PngError  err = PngError_None;
+  PngChunk  chunks[png_max_chunks];
+  const u32 chunkCount = png_load_chunks(src->data, chunks, &err);
+  if (UNLIKELY(err)) {
+    png_load_fail(world, entity, id, err);
     goto Ret;
   }
 
   // TODO: Implement png parsing.
-  png_load_fail(world, entity, id, PngError_Malformed);
+  (void)chunkCount;
+  png_load_fail(world, entity, id, PngError_Truncated);
 
 Ret:
   asset_repo_source_close(src);
