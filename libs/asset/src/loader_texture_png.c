a47d1e89eb062b88c945a5cd64789885fa31d754
Bastian Blokland <mail@bastian.tech>
2024-10-02 20:43:35 +0300

    asset: Validate png magic bytes

--- libs/asset/src/loader_texture_png.c
+++ libs/asset/src/loader_texture_png.c
@@ -10,8 +10,11 @@
  * Spec: https://www.w3.org/TR/png-3/
  */
 
+static const String g_pngMagic = string_lit("\x89\x50\x4E\x47\x0D\x0A\x1A\x0A");
+
 typedef enum {
   PngError_None = 0,
+  PngError_Truncated,
   PngError_Malformed,
 
   PngError_Count,
@@ -20,6 +23,7 @@ typedef enum {
 static String png_error_str(const PngError err) {
   static const String g_msgs[] = {
       string_static("None"),
+      string_static("Truncated png data"),
       string_static("Malformed png data"),
   };
   ASSERT(array_elems(g_msgs) == PngError_Count, "Incorrect number of png-error messages");
@@ -38,6 +42,19 @@ static void png_load_fail(EcsWorld* w, const EcsEntityId e, const String id, con
 void asset_load_tex_png(
     EcsWorld* world, const String id, const EcsEntityId entity, AssetSource* src) {
 
+  Mem data = src->data;
+  if (UNLIKELY(data.size < g_pngMagic.size)) {
+    png_load_fail(world, entity, id, PngError_Truncated);
+    goto Ret;
+  }
+  if (UNLIKELY(!string_starts_with(data, g_pngMagic))) {
+    png_load_fail(world, entity, id, PngError_Malformed);
+    goto Ret;
+  }
+
+  // TODO: Implement png parsing.
   png_load_fail(world, entity, id, PngError_Malformed);
+
+Ret:
   asset_repo_source_close(src);
 }
