666a88f65c9e2dc1cf037a08ab57d762c6eeff63
Bastian Blokland <mail@bastian.tech>
2024-10-06 14:51:39 +0300

    script: Add SIMD find enum name

--- libs/script/src/enum.c
+++ libs/script/src/enum.c
@@ -9,6 +9,28 @@
 
 ASSERT((script_enum_max_entries % 8) == 0, "Only multiple of 8 max entry counts are supported");
 
+INLINE_HINT static u32 script_enum_find_name(const ScriptEnum* e, const StringHash nameHash) {
+#ifdef VOLO_SIMD
+  const SimdVec targetVec = simd_vec_broadcast_u32(nameHash);
+  for (u32 i = 0; i < e->count; i += 8) {
+    const SimdVec eqA    = simd_vec_eq_u32(simd_vec_load(e->nameHashes + i), targetVec);
+    const SimdVec eqB    = simd_vec_eq_u32(simd_vec_load(e->nameHashes + i + 4), targetVec);
+    const u32     eqMask = simd_vec_mask_u8(simd_vec_pack_u32_to_u16(eqA, eqB));
+
+    if (eqMask) {
+      return i + intrinsic_ctz_32(eqMask) / 2; // Div 2 due to 16 bit entries.
+    }
+  }
+#else
+  for (u32 i = 0; i != e->count; ++i) {
+    if (e->nameHashes[i] == nameHash) {
+      return i;
+    }
+  }
+#endif
+  return sentinel_u32;
+}
+
 void script_enum_push(ScriptEnum* e, const String name, const i32 value) {
   const StringHash nameHash = stringtable_add(g_stringtable, name);
 
@@ -21,43 +43,21 @@ void script_enum_push(ScriptEnum* e, const String name, const i32 value) {
 }
 
 bool script_enum_contains_name(const ScriptEnum* e, const StringHash nameHash) {
-#ifdef VOLO_SIMD
-  const SimdVec targetVec = simd_vec_broadcast_u32(nameHash);
-  for (u32 i = 0; i < e->count; i += 8) {
-    const SimdVec eqA    = simd_vec_eq_u32(simd_vec_load(e->nameHashes + i), targetVec);
-    const SimdVec eqB    = simd_vec_eq_u32(simd_vec_load(e->nameHashes + i + 4), targetVec);
-    const u32     eqMask = simd_vec_mask_u8(simd_vec_pack_u32_to_u16(eqA, eqB));
-
-    if (eqMask) {
-      const u32 eqIndex = i + intrinsic_ctz_32(eqMask) / 2; // Div 2 due to 16 bit entries.
-      return eqIndex < e->count;
-      break;
-    }
-  }
-#else
-  for (u32 i = 0; i != e->count; ++i) {
-    if (e->nameHashes[i] == nameHash) {
-      return e->values[i];
-    }
-  }
-#endif
-  return false;
+  return script_enum_find_name(e, nameHash) < e->count;
 }
 
 i32 script_enum_lookup_value(const ScriptEnum* e, const StringHash nameHash, ScriptError* err) {
-  for (u32 i = 0; i != e->count; ++i) {
-    if (e->nameHashes[i] == nameHash) {
-      return e->values[i];
-    }
+  const u32 index = script_enum_find_name(e, nameHash);
+  if (index < e->count) {
+    return e->values[index];
   }
   return *err = script_error(ScriptError_EnumInvalidEntry), 0;
 }
 
 i32 script_enum_lookup_maybe_value(const ScriptEnum* e, const StringHash nameHash, const i32 def) {
-  for (u32 i = 0; i != e->count; ++i) {
-    if (e->nameHashes[i] == nameHash) {
-      return e->values[i];
-    }
+  const u32 index = script_enum_find_name(e, nameHash);
+  if (index < e->count) {
+    return e->values[index];
   }
   return def;
 }
