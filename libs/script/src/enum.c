acb36bbc8a55c1a5919ed4b677e3ec94a8dc6155
Bastian Blokland <mail@bastian.tech>
2024-10-06 14:55:02 +0300

    script: Add SIMD impl of script_enum_lookup_name

--- libs/script/src/enum.c
+++ libs/script/src/enum.c
@@ -16,7 +16,6 @@ INLINE_HINT static u32 script_enum_find_name(const ScriptEnum* e, const StringHa
     const SimdVec eqA    = simd_vec_eq_u32(simd_vec_load(e->nameHashes + i), targetVec);
     const SimdVec eqB    = simd_vec_eq_u32(simd_vec_load(e->nameHashes + i + 4), targetVec);
     const u32     eqMask = simd_vec_mask_u8(simd_vec_pack_u32_to_u16(eqA, eqB));
-
     if (eqMask) {
       return i + intrinsic_ctz_32(eqMask) / 2; // Div 2 due to 16 bit entries.
     }
@@ -24,6 +23,27 @@ INLINE_HINT static u32 script_enum_find_name(const ScriptEnum* e, const StringHa
 #else
   for (u32 i = 0; i != e->count; ++i) {
     if (e->nameHashes[i] == nameHash) {
+      return i;
+    }
+  }
+#endif
+  return sentinel_u32;
+}
+
+INLINE_HINT static u32 script_enum_find_value(const ScriptEnum* e, const i32 value) {
+#ifdef VOLO_SIMD
+  const SimdVec targetVec = simd_vec_broadcast_i32(value);
+  for (u32 i = 0; i < e->count; i += 8) {
+    const SimdVec eqA    = simd_vec_eq_u32(simd_vec_load(e->values + i), targetVec);
+    const SimdVec eqB    = simd_vec_eq_u32(simd_vec_load(e->values + i + 4), targetVec);
+    const u32     eqMask = simd_vec_mask_u8(simd_vec_pack_u32_to_u16(eqA, eqB));
+    if (eqMask) {
+      return i + intrinsic_ctz_32(eqMask) / 2; // Div 2 due to 16 bit entries.
+    }
+  }
+#else
+  for (u32 i = 0; i != e->count; ++i) {
+    if (e->values[i] == value) {
       return i;
     }
   }
@@ -63,10 +83,9 @@ i32 script_enum_lookup_maybe_value(const ScriptEnum* e, const StringHash nameHas
 }
 
 StringHash script_enum_lookup_name(const ScriptEnum* e, const i32 value) {
-  for (u32 i = 0; i != e->count; ++i) {
-    if (e->values[i] == value) {
-      return e->nameHashes[i];
-    }
+  const u32 index = script_enum_find_value(e, value);
+  if (index < e->count) {
+    return e->nameHashes[index];
   }
   return 0;
 }
