7de64d5ded4349875a16f14ba9478c5ee25a33b8
Bastian Blokland <mail@bastian.tech>
2024-10-10 22:38:52 +0300

    script: Minor cleanup in compile.c

--- libs/script/src/compile.c
+++ libs/script/src/compile.c
@@ -21,11 +21,11 @@ typedef struct {
   DynString*       out;
   u64              regAvailability; // Bitmask of available registers.
   RegId            varRegisters[script_var_count];
-} CompileContext;
+} Context;
 
-static u32 reg_available(CompileContext* ctx) { return bits_popcnt_64(ctx->regAvailability); }
+static u32 reg_available(Context* ctx) { return bits_popcnt_64(ctx->regAvailability); }
 
-static RegId reg_alloc(CompileContext* ctx) {
+static RegId reg_alloc(Context* ctx) {
   if (UNLIKELY(!ctx->regAvailability)) {
     return sentinel_u8; // No registers are available.
   }
@@ -34,46 +34,48 @@ static RegId reg_alloc(CompileContext* ctx) {
   return res;
 }
 
-static void reg_free(CompileContext* ctx, const RegId reg) {
+static void reg_free(Context* ctx, const RegId reg) {
+  diag_assert(reg < script_vm_regs);
   diag_assert_msg(!(ctx->regAvailability & (u64_lit(1) << reg)), "Register already freed");
   ctx->regAvailability |= u64_lit(1) << reg;
 }
 
-static void reg_free_all(CompileContext* ctx) {
+static void reg_free_all(Context* ctx) {
   ASSERT(script_vm_regs <= 63, "Register allocator only supports up to 63 registers");
   ctx->regAvailability = (u64_lit(1) << script_vm_regs) - 1;
 }
 
-static void emit_simple(CompileContext* ctx, const ScriptOp op) {
-  dynstring_append_char(ctx->out, op);
-}
+static void emit_simple(Context* ctx, const ScriptOp op) { dynstring_append_char(ctx->out, op); }
 
-static void emit_value(CompileContext* ctx, const RegId dst, const u8 valId) {
+static void emit_value(Context* ctx, const RegId dst, const u8 valId) {
+  diag_assert(dst < script_vm_regs && valId < ctx->doc->values.size);
   dynstring_append_char(ctx->out, ScriptOp_Value);
   dynstring_append_char(ctx->out, dst);
   dynstring_append_char(ctx->out, valId);
 }
 
-static void emit_unary(CompileContext* ctx, const ScriptOp op, const RegId dst) {
+static void emit_unary(Context* ctx, const ScriptOp op, const RegId dst) {
+  diag_assert(dst < script_vm_regs);
   dynstring_append_char(ctx->out, op);
   dynstring_append_char(ctx->out, dst);
 }
 
-static void emit_binary(CompileContext* ctx, const ScriptOp op, const RegId dst, const RegId src) {
+static void emit_binary(Context* ctx, const ScriptOp op, const RegId dst, const RegId src) {
+  diag_assert(dst < script_vm_regs && src < script_vm_regs);
   dynstring_append_char(ctx->out, op);
   dynstring_append_char(ctx->out, dst);
   dynstring_append_char(ctx->out, src);
 }
 
-static void emit_move(CompileContext* ctx, const RegId dst, const RegId src) {
+static void emit_move(Context* ctx, const RegId dst, const RegId src) {
   if (dst != src) {
     emit_binary(ctx, ScriptOp_Move, dst, src);
   }
 }
 
-static ScriptCompileError compile_expr(CompileContext*, RegId dst, ScriptExpr);
+static ScriptCompileError compile_expr(Context*, RegId dst, ScriptExpr);
 
-static ScriptCompileError compile_value(CompileContext* ctx, const RegId dst, const ScriptExpr e) {
+static ScriptCompileError compile_value(Context* ctx, const RegId dst, const ScriptExpr e) {
   const ScriptExprValue* data = &expr_data(ctx->doc, e)->value;
   if (UNLIKELY(data->valId > u8_max)) {
     return ScriptCompileError_TooManyValues;
@@ -82,16 +84,15 @@ static ScriptCompileError compile_value(CompileContext* ctx, const RegId dst, co
   return ScriptCompileError_None;
 }
 
-static ScriptCompileError
-compile_var_load(CompileContext* ctx, const RegId dst, const ScriptExpr e) {
+static ScriptCompileError compile_var_load(Context* ctx, const RegId dst, const ScriptExpr e) {
   const ScriptExprVarLoad* data = &expr_data(ctx->doc, e)->var_load;
   diag_assert(!sentinel_check(ctx->varRegisters[data->var]));
   emit_move(ctx, dst, ctx->varRegisters[data->var]);
   return ScriptCompileError_None;
 }
 
-static ScriptCompileError compile_intr_unary(
-    CompileContext* ctx, const RegId dst, const ScriptOp op, const ScriptExpr* args) {
+static ScriptCompileError
+compile_intr_unary(Context* ctx, const RegId dst, const ScriptOp op, const ScriptExpr* args) {
   ScriptCompileError err = ScriptCompileError_None;
   if ((err = compile_expr(ctx, dst, args[0]))) {
     return err;
@@ -100,8 +101,8 @@ static ScriptCompileError compile_intr_unary(
   return err;
 }
 
-static ScriptCompileError compile_intr_binary(
-    CompileContext* ctx, const RegId dst, const ScriptOp op, const ScriptExpr* args) {
+static ScriptCompileError
+compile_intr_binary(Context* ctx, const RegId dst, const ScriptOp op, const ScriptExpr* args) {
   ScriptCompileError err = ScriptCompileError_None;
   if ((err = compile_expr(ctx, dst, args[0]))) {
     return err;
@@ -119,7 +120,7 @@ static ScriptCompileError compile_intr_binary(
   return err;
 }
 
-static ScriptCompileError compile_intr(CompileContext* ctx, const RegId dst, const ScriptExpr e) {
+static ScriptCompileError compile_intr(Context* ctx, const RegId dst, const ScriptExpr e) {
   const ScriptExprIntrinsic* data = &expr_data(ctx->doc, e)->intrinsic;
   const ScriptExpr*          args = expr_set_data(ctx->doc, data->argSet);
   switch (data->intrinsic) {
@@ -195,7 +196,7 @@ static ScriptCompileError compile_intr(CompileContext* ctx, const RegId dst, con
   UNREACHABLE
 }
 
-static ScriptCompileError compile_expr(CompileContext* ctx, const RegId dst, const ScriptExpr e) {
+static ScriptCompileError compile_expr(Context* ctx, const RegId dst, const ScriptExpr e) {
   switch (expr_kind(ctx->doc, e)) {
   case ScriptExprKind_Value:
     return compile_value(ctx, dst, e);
@@ -223,7 +224,7 @@ String script_compile_error_str(const ScriptCompileError res) {
 }
 
 ScriptCompileError script_compile(const ScriptDoc* doc, const ScriptExpr expr, DynString* out) {
-  CompileContext ctx = {
+  Context ctx = {
       .doc = doc,
       .out = out,
   };
