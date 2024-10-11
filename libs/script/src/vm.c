de35fdf4590958a0785981bb5915598cb847b167
Bastian Blokland <mail@bastian.tech>
2024-10-11 19:25:07 +0300

    vm: Extern func op

--- libs/script/src/vm.c
+++ libs/script/src/vm.c
@@ -2,6 +2,7 @@
 #include "core_array.h"
 #include "core_diag.h"
 #include "script_binder.h"
+#include "script_error.h"
 #include "script_mem.h"
 #include "script_vm.h"
 
@@ -21,8 +22,17 @@ INLINE_HINT static bool vm_reg_valid(ScriptVmContext* ctx, const u8 regId) {
   return regId < array_elems(ctx->regs);
 }
 
+INLINE_HINT static bool vm_reg_set_valid(ScriptVmContext* ctx, const u8 regId, const u8 regCount) {
+  return regId + regCount <= array_elems(ctx->regs);
+}
+
 INLINE_HINT static bool vm_val_valid(ScriptVmContext* ctx, const u8 valId) {
   return valId < ctx->doc->values.size;
+}
+
+INLINE_HINT static u16 vm_read_u16(const u8 data[]) {
+  // NOTE: Input data is not required to be aligned to 16 bit.
+  return (u16)data[0] << 8 | (u16)data[1];
 }
 
 INLINE_HINT static u32 vm_read_u32(const u8 data[]) {
@@ -66,6 +76,20 @@ static ScriptVal vm_run(ScriptVmContext* ctx, const String code) {
       if (UNLIKELY(!vm_reg_valid(ctx, ip[-5]))) goto Corrupt;
       script_mem_store(ctx->m, vm_read_u32(&ip[-4]), ctx->regs[ip[-5]]);
       continue;
+    case ScriptOp_Extern: {
+      if (UNLIKELY((ip += 6) >= ipEnd)) goto Corrupt;
+      if (UNLIKELY(!vm_reg_valid(ctx, ip[-5]))) goto Corrupt;
+      if (UNLIKELY(!vm_reg_set_valid(ctx, ip[-2], ip[-1]))) goto Corrupt;
+      const ScriptBinderSlot funcSlot = vm_read_u16(&ip[4]);
+      if (UNLIKELY(funcSlot >= script_binder_count(ctx->binder))) goto Corrupt;
+      const ScriptArgs args = {.values = &ctx->regs[ip[-2]], .count = ip[-1]};
+      ScriptError      err  = {0};
+      ctx->regs[ip[-5]] = script_binder_exec(ctx->binder, funcSlot, ctx->bindCtx, args, &err);
+      if (UNLIKELY(err.kind)) {
+        ctx->panic = (ScriptPanic){.kind = script_error_to_panic(err.kind)};
+        return script_null();
+      }
+    } continue;
 #define OP_BINARY_SIMPLE(_OP_, _FUNC_)                                                             \
     case ScriptOp_##_OP_:                                                                          \
       if (UNLIKELY((ip += 3) >= ipEnd)) goto Corrupt;                                              \
@@ -135,37 +159,41 @@ void script_vm_disasm_write(const ScriptDoc* doc, const String code, DynString* 
     // clang-format off
     switch ((ScriptOp)*ip) {
     case ScriptOp_Fail:
-      if (UNLIKELY((ip += 1) > ipEnd)) { return; }
+      if (UNLIKELY((ip += 1) > ipEnd)) return;
       fmt_write(out, "[Fail]\n");
       break;
     case ScriptOp_Return:
-      if (UNLIKELY((ip += 2) > ipEnd)) { return; }
+      if (UNLIKELY((ip += 2) > ipEnd)) return;
       fmt_write(out, "[Return r{}]\n", fmt_int(ip[-1]));
       break;
     case ScriptOp_Move:
-      if (UNLIKELY((ip += 3) > ipEnd)) { return; }
+      if (UNLIKELY((ip += 3) > ipEnd)) return;
       fmt_write(out, "[Move r{} r{}]\n", fmt_int(ip[-2]), fmt_int(ip[-1]));
       break;
     case ScriptOp_Value:
-      if (UNLIKELY((ip += 3) > ipEnd)) { return; }
+      if (UNLIKELY((ip += 3) > ipEnd)) return;
       fmt_write(out, "[Value r{} v{}]\n", fmt_int(ip[-2]), fmt_int(ip[-1]));
       break;
     case ScriptOp_MemLoad:
-      if (UNLIKELY((ip += 6) > ipEnd)) { return; }
+      if (UNLIKELY((ip += 6) > ipEnd)) return;
       fmt_write(out, "[MemLoad r{} #{}]\n", fmt_int(ip[-5]), fmt_int(vm_read_u32(&ip[-4])));
       break;
     case ScriptOp_MemStore:
-      if (UNLIKELY((ip += 6) > ipEnd)) { return; }
+      if (UNLIKELY((ip += 6) > ipEnd)) return;
       fmt_write(out, "[MemStore r{} #{}]\n", fmt_int(ip[-5]), fmt_int(vm_read_u32(&ip[-4])));
+      break;
+    case ScriptOp_Extern:
+      if (UNLIKELY((ip += 6) > ipEnd)) return;
+      fmt_write(out, "[Extern r{} f{} r{} c{}]\n", fmt_int(ip[-5]), fmt_int(vm_read_u16(&ip[-4])), fmt_int(ip[-2]), fmt_int(ip[-1]));
       break;
 #define OP_BINARY_SIMPLE(_OP_)                                                                     \
     case ScriptOp_##_OP_:                                                                          \
-      if (UNLIKELY((ip += 3) > ipEnd)) { return; }                                                 \
+      if (UNLIKELY((ip += 3) > ipEnd)) return;                                                     \
       fmt_write(out, "[" #_OP_ " r{} r{}]\n", fmt_int(ip[-2]), fmt_int(ip[-1]));                   \
       break;
 #define OP_UNARY_SIMPLE(_OP_)                                                                      \
     case ScriptOp_##_OP_:                                                                          \
-      if (UNLIKELY((ip += 2) > ipEnd)) { return; }                                                 \
+      if (UNLIKELY((ip += 2) > ipEnd)) return;                                                     \
       fmt_write(out, "[" #_OP_ " r{}]\n", fmt_int(ip[-1]));                                        \
       break;
 
