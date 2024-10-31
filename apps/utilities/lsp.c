395c51ace078da2f508c0fee4ee5d81beb4e14c4
Bastian Blokland <mail@bastian.tech>
2024-10-31 22:17:35 +0200

    lsp: Document lack of utf8 support

--- apps/utilities/lsp.c
+++ apps/utilities/lsp.c
@@ -1938,6 +1938,7 @@ static i32 lsp_run_stdio(const ScriptBinder* scriptBinder) {
     const String    content = lsp_read_sized(&ctx, header.contentLength);
 
     JsonResult jsonResult;
+    // TODO: JRpc spec only supports utf8 encoding while our json lexer only supports Ascii.
     json_read(jDoc, content, JsonReadFlags_None, &jsonResult);
     if (UNLIKELY(jsonResult.type == JsonResultType_Fail)) {
       const String jsonErr = json_error_str(jsonResult.error);
