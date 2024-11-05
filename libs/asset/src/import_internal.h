5e7dc2b484d798cf45df2a9b13c207600dbbbb14
Bastian Blokland <mail@bastian.tech>
2024-11-05 22:41:06 +0200

    asset: Document asset_import_ready

--- libs/asset/src/import_internal.h
+++ libs/asset/src/import_internal.h
@@ -8,5 +8,6 @@ ecs_comp_extern(AssetImportEnvComp);
 
 /**
  * Check if we are ready to import assets.
+ * NOTE: Ready state is only valid this frame as due to hot-loading it can become un-ready.
  */
 bool asset_import_ready(const AssetImportEnvComp*);
