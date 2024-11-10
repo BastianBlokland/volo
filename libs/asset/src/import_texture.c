cfb4200e2221d3f71c5619e7829ff7b53dc853d1
Bastian Blokland <mail@bastian.tech>
2024-11-10 16:46:06 +0200

    asset: Add texture import flags

--- libs/asset/src/import_texture.c
+++ libs/asset/src/import_texture.c
@@ -2,13 +2,29 @@
 #include "core_array.h"
 #include "script_args.h"
 #include "script_binder.h"
+#include "script_enum.h"
 #include "script_sig.h"
 
 #include "import_texture_internal.h"
 
 ScriptBinder* g_assetScriptImportTextureBinder;
 
+static ScriptEnum g_assetImportTextureFlags;
+
+static void import_init_enum_flags(void) {
+#define PUSH_FLAG(_ENUM_, _NAME_)                                                                  \
+  script_enum_push((_ENUM_), string_lit(#_NAME_), AssetImportTextureFlags_##_NAME_);
+
+  PUSH_FLAG(&g_assetImportTextureFlags, NormalMap);
+  PUSH_FLAG(&g_assetImportTextureFlags, Lossless);
+  PUSH_FLAG(&g_assetImportTextureFlags, Linear);
+
+#undef PUSH_FLAG
+}
+
 void asset_data_init_import_texture(void) {
+  import_init_enum_flags();
+
   const ScriptBinderFlags flags = ScriptBinderFlags_DisallowMemoryAccess;
   ScriptBinder* binder = script_binder_create(g_allocPersist, string_lit("import-texture"), flags);
   script_binder_filter_set(binder, string_lit("import/texture/*.script"));
@@ -20,8 +36,6 @@ void asset_data_init_import_texture(void) {
 }
 
 bool asset_import_texture(const AssetImportEnvComp* env, const String id, AssetImportTexture* out) {
-  *out = (AssetImportTexture){
-      .dummy = 42,
-  };
+  *out = (AssetImportTexture){0};
   return asset_import_eval(env, g_assetScriptImportTextureBinder, id, out);
 }
--- libs/asset/src/import_texture_internal.h
+++ libs/asset/src/import_texture_internal.h
@@ -1,8 +1,14 @@
 #pragma once
 #include "import_internal.h"
 
+typedef enum {
+  AssetImportTextureFlags_NormalMap = 1 << 0,
+  AssetImportTextureFlags_Lossless  = 1 << 1,
+  AssetImportTextureFlags_Linear    = 1 << 2,
+} AssetImportTextureFlags;
+
 typedef struct {
-  u32 dummy;
+  AssetImportTextureFlags flags;
 } AssetImportTexture;
 
 bool asset_import_texture(const AssetImportEnvComp*, String id, AssetImportTexture*);
