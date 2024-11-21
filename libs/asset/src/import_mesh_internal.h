7bf295bf61e1c58ff6022c47b70d0da297a52907
Bastian Blokland <mail@bastian.tech>
2024-11-21 19:04:47 +0200

    asset: Add anim data to import data

--- libs/asset/src/import_mesh_internal.h
+++ libs/asset/src/import_mesh_internal.h
@@ -8,9 +8,15 @@ typedef struct {
 } AssetImportJoint;
 
 typedef struct {
+  StringHash nameHash; // Interned in the global string table.
+} AssetImportAnim;
+
+typedef struct {
   f32              vertexScale;
   AssetImportJoint joints[asset_mesh_joints_max];
   u32              jointCount;
+  AssetImportAnim  anims[asset_mesh_anims_max];
+  u32              animCount;
 } AssetImportMesh;
 
 bool asset_import_mesh(const AssetImportEnvComp*, String id, AssetImportMesh*);
