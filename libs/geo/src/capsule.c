d9ae490ef6c7a2738c9bcf6eb64bfbebf6e3f143
Bastian Blokland <mail@bastian.tech>
2024-12-07 16:47:43 +0200

    geo: Add Simd impl of geo_capsule_transform3

--- libs/geo/src/capsule.c
+++ libs/geo/src/capsule.c
@@ -2,17 +2,45 @@
 #include "geo_quat.h"
 #include "geo_sphere.h"
 
+#ifdef VOLO_SIMD
+#include "core_simd.h"
+#endif
+
 GeoCapsule geo_capsule_dilate(const GeoCapsule* capsule, const f32 radius) {
   return (GeoCapsule){.line = capsule->line, .radius = capsule->radius + radius};
 }
 
 GeoCapsule geo_capsule_transform3(
     const GeoCapsule* capsule, const GeoVector offset, const GeoQuat rotation, const f32 scale) {
+#ifdef VOLO_SIMD
+  const SimdVec offsetVec = simd_vec_load(offset.comps);
+  const SimdVec rotVec    = simd_vec_load(rotation.comps);
+  const SimdVec scaleVec  = simd_vec_broadcast(scale);
 
+  SimdVec aVec = simd_vec_load(capsule->line.a.comps);
+  aVec         = simd_vec_mul(aVec, scaleVec);
+  aVec         = simd_quat_rotate(rotVec, aVec);
+  aVec         = simd_vec_add(aVec, offsetVec);
+
+  SimdVec bVec = simd_vec_load(capsule->line.b.comps);
+  bVec         = simd_vec_mul(bVec, scaleVec);
+  bVec         = simd_quat_rotate(rotVec, bVec);
+  bVec         = simd_vec_add(bVec, offsetVec);
+
+  SimdVec radiusVec = simd_vec_broadcast(capsule->radius);
+  radiusVec         = simd_vec_mul(radiusVec, scaleVec);
+
+  GeoCapsule res;
+  simd_vec_store(aVec, res.line.a.comps);
+  simd_vec_store(bVec, res.line.b.comps);
+  res.radius = simd_vec_x(radiusVec);
+  return res;
+#else
   return (GeoCapsule){
       .line   = geo_line_transform3(&capsule->line, offset, rotation, scale),
       .radius = capsule->radius * scale,
   };
+#endif
 }
 
 f32 geo_capsule_intersect_ray(const GeoCapsule* capsule, const GeoRay* ray) {
