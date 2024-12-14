0f7dfd87adbea425c32ab280c59b32ac8dd4455a
Bastian Blokland <mail@bastian.tech>
2024-12-14 15:32:46 +0200

    data: Early out array visit

--- libs/data/src/utils_visit.c
+++ libs/data/src/utils_visit.c
@@ -13,6 +13,59 @@ typedef struct {
   DataVisitor    visitor;
 } VisitCtx;
 
+static bool data_visit_needed(const DataReg* reg, const DataMeta meta, const DataType visitType) {
+  if (meta.type == visitType) {
+    return true;
+  }
+  const DataDecl* decl = data_decl(reg, meta.type);
+  switch (decl->kind) {
+  case DataKind_bool:
+  case DataKind_i8:
+  case DataKind_i16:
+  case DataKind_i32:
+  case DataKind_i64:
+  case DataKind_u8:
+  case DataKind_u16:
+  case DataKind_u32:
+  case DataKind_u64:
+  case DataKind_f16:
+  case DataKind_f32:
+  case DataKind_f64:
+  case DataKind_TimeDuration:
+  case DataKind_Angle:
+  case DataKind_Enum:
+  case DataKind_StringHash:
+  case DataKind_Opaque:
+  case DataKind_String:
+  case DataKind_DataMem:
+    return false;
+  case DataKind_Struct: {
+    dynarray_for_t(&decl->val_struct.fields, DataDeclField, fieldDecl) {
+      if (data_visit_needed(reg, fieldDecl->meta, visitType)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  case DataKind_Union: {
+    dynarray_for_t(&decl->val_union.choices, DataDeclChoice, choice) {
+      const bool emptyChoice = choice->meta.type == 0;
+      if (emptyChoice) {
+        continue;
+      }
+      if (data_visit_needed(reg, choice->meta, visitType)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  case DataKind_Invalid:
+  case DataKind_Count:
+    break;
+  }
+  diag_crash();
+}
+
 static void data_visit_internal(const VisitCtx*);
 
 static void data_visit_struct(const VisitCtx* ctx) {
@@ -90,11 +143,15 @@ static void data_visit_inline_array(const VisitCtx* ctx) {
   if (UNLIKELY(ctx->data.size != expectedSize)) {
     diag_crash_msg("Unexpected data-size for inline array");
   }
+  const DataMeta baseMeta = data_meta_base(ctx->meta);
+  if (!data_visit_needed(ctx->reg, baseMeta, ctx->visitType)) {
+    return;
+  }
   const DataDecl* decl = data_decl(ctx->reg, ctx->meta.type);
   for (u16 i = 0; i != ctx->meta.fixedCount; ++i) {
     const VisitCtx elemCtx = {
         .reg       = ctx->reg,
-        .meta      = data_meta_base(ctx->meta),
+        .meta      = baseMeta,
         .data      = mem_create(bits_ptr_offset(ctx->data.ptr, decl->size * i), decl->size),
         .visitType = ctx->visitType,
         .visitCtx  = ctx->visitCtx,
@@ -105,13 +162,16 @@ static void data_visit_inline_array(const VisitCtx* ctx) {
 }
 
 static void data_visit_heap_array(const VisitCtx* ctx) {
-  const DataDecl*  decl  = data_decl(ctx->reg, ctx->meta.type);
-  const HeapArray* array = mem_as_t(ctx->data, HeapArray);
-
+  const DataDecl*  decl     = data_decl(ctx->reg, ctx->meta.type);
+  const HeapArray* array    = mem_as_t(ctx->data, HeapArray);
+  const DataMeta   baseMeta = data_meta_base(ctx->meta);
+  if (!data_visit_needed(ctx->reg, baseMeta, ctx->visitType)) {
+    return;
+  }
   for (usize i = 0; i != array->count; ++i) {
     const VisitCtx elemCtx = {
         .reg       = ctx->reg,
-        .meta      = data_meta_base(ctx->meta),
+        .meta      = baseMeta,
         .data      = data_elem_mem(decl, array, i),
         .visitType = ctx->visitType,
         .visitCtx  = ctx->visitCtx,
@@ -122,12 +182,15 @@ static void data_visit_heap_array(const VisitCtx* ctx) {
 }
 
 static void data_visit_dynarray(const VisitCtx* ctx) {
-  const DynArray* array = mem_as_t(ctx->data, DynArray);
-
+  const DynArray* array    = mem_as_t(ctx->data, DynArray);
+  const DataMeta  baseMeta = data_meta_base(ctx->meta);
+  if (!data_visit_needed(ctx->reg, baseMeta, ctx->visitType)) {
+    return;
+  }
   for (usize i = 0; i != array->size; ++i) {
     const VisitCtx elemCtx = {
         .reg       = ctx->reg,
-        .meta      = data_meta_base(ctx->meta),
+        .meta      = baseMeta,
         .data      = dynarray_at(array, i, 1),
         .visitType = ctx->visitType,
         .visitCtx  = ctx->visitCtx,
