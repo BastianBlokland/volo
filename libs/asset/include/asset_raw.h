1017c595c4b471faa1d52f4a24ab22bc28337642
Bastian Blokland <mail@bastian.tech>
2024-12-15 17:47:36 +0200

    asset: Fix typo

--- libs/asset/include/asset_raw.h
+++ libs/asset/include/asset_raw.h
@@ -2,6 +2,6 @@
 #include "ecs_module.h"
 
 /**
- * Raw assets are not proccessed by the asset-system and contain the exact source content.
+ * Raw assets are not processed by the asset-system and contain the exact source content.
  */
 ecs_comp_extern_public(AssetRawComp) { String data; };
