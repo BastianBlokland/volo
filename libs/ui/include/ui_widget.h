a177a0d23bb42376795457e65eb1f354b1104bdc
Bastian Blokland <mail@bastian.tech>
2025-01-11 15:21:33 +0200

    debug: Tweak dropdown placeholder

--- libs/ui/include/ui_widget.h
+++ libs/ui/include/ui_widget.h
@@ -182,7 +182,7 @@ typedef struct {
     .maxHeight      = 150,                                                                         \
     .frameColor     = ui_color(32, 32, 32, 192),                                                   \
     .dropFrameColor = ui_color(64, 64, 64, 235),                                                   \
-    .placeholder    = string_lit("- Select -"),                                                    \
+    .placeholder    = string_lit("< None >"),                                                      \
     __VA_ARGS__}))
 
 /**
