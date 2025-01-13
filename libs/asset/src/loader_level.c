2ac2c1a6676ab6f7e0596736e21acfe1cede3440
Bastian Blokland <mail@bastian.tech>
2025-01-13 20:14:11 +0200

    asset: Optional pos / rot in levels

--- libs/asset/src/loader_level.c
+++ libs/asset/src/loader_level.c
@@ -71,8 +71,8 @@ void asset_data_init_level(void) {
   data_reg_field_t(g_dataReg, AssetLevelObject, id, data_prim_t(u32), .flags = DataFlags_Opt | DataFlags_NotEmpty);
   data_reg_field_t(g_dataReg, AssetLevelObject, prefab, data_prim_t(StringHash), .flags = DataFlags_NotEmpty);
   data_reg_field_t(g_dataReg, AssetLevelObject, faction, t_AssetLevelFaction, .flags = DataFlags_Opt);
-  data_reg_field_t(g_dataReg, AssetLevelObject, position, g_assetGeoVec3Type);
-  data_reg_field_t(g_dataReg, AssetLevelObject, rotation, g_assetGeoQuatType);
+  data_reg_field_t(g_dataReg, AssetLevelObject, position, g_assetGeoVec3Type, .flags = DataFlags_Opt);
+  data_reg_field_t(g_dataReg, AssetLevelObject, rotation, g_assetGeoQuatType, .flags = DataFlags_Opt);
   data_reg_field_t(g_dataReg, AssetLevelObject, scale, data_prim_t(f32), .flags = DataFlags_Opt | DataFlags_NotEmpty);
   data_reg_field_t(g_dataReg, AssetLevelObject, properties, g_assetPropertyType, .container = DataContainer_HeapArray, .flags = DataFlags_Opt);
   data_reg_field_t(g_dataReg, AssetLevelObject, sets, data_prim_t(StringHash), .container = DataContainer_InlineArray, .fixedCount = asset_level_sets_max, .flags = DataFlags_Opt);
