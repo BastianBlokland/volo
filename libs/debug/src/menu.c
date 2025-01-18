9ea341a43e1b11d1a540972c29cbe7ba41ad50eb
Bastian Blokland <mail@bastian.tech>
2025-01-18 15:36:55 +0200

    debug: Use construction icon for dev menu

--- libs/debug/src/menu.c
+++ libs/debug/src/menu.c
@@ -295,7 +295,7 @@ static void menu_action_bar_draw(
   ui_table_draw_bg(canvas, &table, rows, ui_color(178, 0, 0, 192));
 
   ui_table_next_row(canvas, &table);
-  ui_canvas_draw_glyph(canvas, UiShape_Bug, 0, UiFlags_Interactable);
+  ui_canvas_draw_glyph(canvas, UiShape_Construction, 0, UiFlags_Interactable);
 
   // Panel open / close.
   for (u32 childIndex = 0; childIndex != array_elems(g_menuChildConfig); ++childIndex) {
