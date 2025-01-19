10850978da89161113ef06c319445139e80e8f11
Bastian Blokland <mail@bastian.tech>
2025-01-19 16:17:37 +0200

    net: Initial dns tests

--- libs/net/test/test_dns.c
+++ libs/net/test/test_dns.c
@@ -1,6 +1,25 @@
 #include "check_spec.h"
+#include "net_addr.h"
 #include "net_dns.h"
 
 spec(dns) {
-  it("works") {}
+  it("fails to resolve an empty host") {
+    NetAddr            addr;
+    const NetDnsResult res = net_dns_resolve_sync(string_empty, NetDnsService_Http, &addr);
+    check_eq_int(res, NetDnsResult_InvalidHost);
+  }
+
+  it("can resolve localhost") {
+    NetAddr            addr;
+    const NetDnsResult res =
+        net_dns_resolve_sync(string_lit("localhost"), NetDnsService_Http, &addr);
+    check_eq_int(res, NetDnsResult_Success);
+  }
+
+  it("can resolve loopback") {
+    NetAddr            addr;
+    const NetDnsResult res =
+        net_dns_resolve_sync(string_lit("127.0.0.1"), NetDnsService_Http, &addr);
+    check_eq_int(res, NetDnsResult_Success);
+  }
 }
