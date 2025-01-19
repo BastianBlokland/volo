b80645205e972ff9303ab3e3ae5934bdd1cf2f00
Bastian Blokland <mail@bastian.tech>
2025-01-19 14:41:53 +0200

    net: Load AddrInfo apis

--- libs/net/src/pal_win32.c
+++ libs/net/src/pal_win32.c
@@ -6,11 +6,13 @@
 #include "pal_internal.h"
 
 #include <Windows.h>
+#include <ws2tcpip.h>
 
 typedef struct {
   DynLib* lib;
   // clang-format off
-  // TODO: Add needed apis.
+  int  (SYS_DECL* GetAddrInfoW)(const wchar_t* nodeName, const wchar_t* serviceName, const ADDRINFOW* hints, ADDRINFOW** out);
+  void (SYS_DECL* FreeAddrInfoW)(ADDRINFOW*);
   // clang-format on
 } NetWinSockLib;
 
@@ -23,21 +25,32 @@ static bool net_ws_init(NetWinSockLib* ws, Allocator* alloc) {
   }
   log_i("WinSock library loaded", log_param("path", fmt_path(dynlib_path(ws->lib))));
 
-  (void)ws;
-  (void)alloc;
+#define WS_LOAD_SYM(_NAME_)                                                                        \
+  do {                                                                                             \
+    ws->_NAME_ = dynlib_symbol(ws->lib, string_lit(#_NAME_));                                      \
+    if (!ws->_NAME_) {                                                                             \
+      log_w("WinSock symbol '{}' missing", log_param("sym", fmt_text(string_lit(#_NAME_))));       \
+      return false;                                                                                \
+    }                                                                                              \
+  } while (false)
+
+  WS_LOAD_SYM(GetAddrInfoW);
+  WS_LOAD_SYM(FreeAddrInfoW);
+
+#undef WS_LOAD_SYM
   return true;
 }
 
 static NetWinSockLib g_netWsLib;
-static bool          g_netWsInit;
+static bool          g_netWsReady;
 
 void net_pal_init(void) {
-  diag_assert(!g_netWsInit);
-  g_netWsInit = net_ws_init(&g_netWsLib, g_allocPersist);
+  diag_assert(!g_netWsReady);
+  g_netWsReady = net_ws_init(&g_netWsLib, g_allocPersist);
 }
 
 void net_pal_teardown(void) {
-  if (g_netWsInit) {
+  if (g_netWsLib.lib) {
     dynlib_destroy(g_netWsLib.lib);
   }
 }
