fedd4438bc4e447b64737aa576353af37396899f
Bastian Blokland <mail@bastian.tech>
2025-01-19 19:15:26 +0200

    net: Fixup test data initializers

--- libs/net/test/test_addr.c
+++ libs/net/test/test_addr.c
@@ -9,52 +9,50 @@ spec(addr) {
       String expected;
     } g_testData[] = {
         {
-            .ip       = (NetIp){.type = NetIpType_V4, .v4 = {.data = {0, 0, 0, 1}}},
+            .ip       = {.type = NetIpType_V4, .v4 = {.data = {0, 0, 0, 1}}},
             .expected = string_static("0.0.0.1"),
         },
         {
-            .ip       = (NetIp){.type = NetIpType_V4, .v4 = {.data = {127, 0, 0, 1}}},
+            .ip       = {.type = NetIpType_V4, .v4 = {.data = {127, 0, 0, 1}}},
             .expected = string_static("127.0.0.1"),
         },
         {
-            .ip       = (NetIp){.type = NetIpType_V4, .v4 = {.data = {192, 168, 42, 1}}},
+            .ip       = {.type = NetIpType_V4, .v4 = {.data = {192, 168, 42, 1}}},
             .expected = string_static("192.168.42.1"),
         },
         {
-            .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {0, 0, 0, 0, 0, 0, 0, 0}}},
+            .ip       = {.type = NetIpType_V6, .v6 = {.groups = {0, 0, 0, 0, 0, 0, 0, 0}}},
             .expected = string_static("::"),
         },
         {
-            .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {0, 0, 0, 0, 0, 0, 0, 1}}},
+            .ip       = {.type = NetIpType_V6, .v6 = {.groups = {0, 0, 0, 0, 0, 0, 0, 1}}},
             .expected = string_static("::1"),
         },
         {
-            .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {1, 2, 3, 4, 5, 6, 7, 8}}},
+            .ip       = {.type = NetIpType_V6, .v6 = {.groups = {1, 2, 3, 4, 5, 6, 7, 8}}},
             .expected = string_static("1:2:3:4:5:6:7:8"),
         },
         {
-            .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {1, 0, 0, 0, 0, 0, 0, 8}}},
+            .ip       = {.type = NetIpType_V6, .v6 = {.groups = {1, 0, 0, 0, 0, 0, 0, 8}}},
             .expected = string_static("1::8"),
         },
         {
-            .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {0, 2, 3, 4, 5, 6, 7, 8}}},
+            .ip       = {.type = NetIpType_V6, .v6 = {.groups = {0, 2, 3, 4, 5, 6, 7, 8}}},
             .expected = string_static("::2:3:4:5:6:7:8"),
         },
         {
-            .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {0, 0, 3, 0, 0, 6, 7, 8}}},
+            .ip       = {.type = NetIpType_V6, .v6 = {.groups = {0, 0, 3, 0, 0, 6, 7, 8}}},
             .expected = string_static("::3:0:0:6:7:8"),
         },
         {
-            .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {1, 0, 0, 0, 0, 0, 0, 0}}},
+            .ip       = {.type = NetIpType_V6, .v6 = {.groups = {1, 0, 0, 0, 0, 0, 0, 0}}},
             .expected = string_static("1::"),
         },
         {
             .ip =
-                (NetIp){
-                    .type = NetIpType_V6,
-                    .v6 =
-                        {.groups =
-                             {0x2001, 0x0DB8, 0x0000, 0x0000, 0x0000, 0xFF00, 0x0042, 0x8329}}},
+                {.type = NetIpType_V6,
+                 .v6 =
+                     {.groups = {0x2001, 0x0DB8, 0x0000, 0x0000, 0x0000, 0xFF00, 0x0042, 0x8329}}},
             .expected = string_static("2001:DB8::FF00:42:8329"),
         },
     };
