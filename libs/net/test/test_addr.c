17e1ac39315b8001ae024f48b871ef0f8388cd74
Bastian Blokland <mail@bastian.tech>
2025-01-19 19:08:15 +0200

    net: Fix non-const initializer

--- libs/net/test/test_addr.c
+++ libs/net/test/test_addr.c
@@ -10,43 +10,43 @@ spec(addr) {
     } g_testData[] = {
         {
             .ip       = (NetIp){.type = NetIpType_V4, .v4 = {.data = {0, 0, 0, 1}}},
-            .expected = string_lit("0.0.0.1"),
+            .expected = string_static("0.0.0.1"),
         },
         {
             .ip       = (NetIp){.type = NetIpType_V4, .v4 = {.data = {127, 0, 0, 1}}},
-            .expected = string_lit("127.0.0.1"),
+            .expected = string_static("127.0.0.1"),
         },
         {
             .ip       = (NetIp){.type = NetIpType_V4, .v4 = {.data = {192, 168, 42, 1}}},
-            .expected = string_lit("192.168.42.1"),
+            .expected = string_static("192.168.42.1"),
         },
         {
             .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {0, 0, 0, 0, 0, 0, 0, 0}}},
-            .expected = string_lit("::"),
+            .expected = string_static("::"),
         },
         {
             .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {0, 0, 0, 0, 0, 0, 0, 1}}},
-            .expected = string_lit("::1"),
+            .expected = string_static("::1"),
         },
         {
             .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {1, 2, 3, 4, 5, 6, 7, 8}}},
-            .expected = string_lit("1:2:3:4:5:6:7:8"),
+            .expected = string_static("1:2:3:4:5:6:7:8"),
         },
         {
             .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {1, 0, 0, 0, 0, 0, 0, 8}}},
-            .expected = string_lit("1::8"),
+            .expected = string_static("1::8"),
         },
         {
             .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {0, 2, 3, 4, 5, 6, 7, 8}}},
-            .expected = string_lit("::2:3:4:5:6:7:8"),
+            .expected = string_static("::2:3:4:5:6:7:8"),
         },
         {
             .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {0, 0, 3, 0, 0, 6, 7, 8}}},
-            .expected = string_lit("::3:0:0:6:7:8"),
+            .expected = string_static("::3:0:0:6:7:8"),
         },
         {
             .ip       = (NetIp){.type = NetIpType_V6, .v6 = {.groups = {1, 0, 0, 0, 0, 0, 0, 0}}},
-            .expected = string_lit("1::"),
+            .expected = string_static("1::"),
         },
         {
             .ip =
@@ -55,7 +55,7 @@ spec(addr) {
                     .v6 =
                         {.groups =
                              {0x2001, 0x0DB8, 0x0000, 0x0000, 0x0000, 0xFF00, 0x0042, 0x8329}}},
-            .expected = string_lit("2001:DB8::FF00:42:8329"),
+            .expected = string_static("2001:DB8::FF00:42:8329"),
         },
     };
     for (u32 i = 0; i != array_elems(g_testData); ++i) {
@@ -74,7 +74,7 @@ spec(addr) {
                     .ip   = {.type = NetIpType_V4, .v4 = {.data = {0, 0, 0, 1}}},
                     .port = 42,
                 },
-            .expected = string_lit("0.0.0.1:42"),
+            .expected = string_static("0.0.0.1:42"),
         },
         {
             .addr =
@@ -82,7 +82,7 @@ spec(addr) {
                     .ip   = {.type = NetIpType_V6, .v6 = {.groups = {0, 0, 0, 0, 0, 0, 0, 1}}},
                     .port = 42,
                 },
-            .expected = string_lit("[::1]:42"),
+            .expected = string_static("[::1]:42"),
         },
     };
     for (u32 i = 0; i != array_elems(g_testData); ++i) {
