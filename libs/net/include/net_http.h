1c0abd37e585a3f3f447ebf1e762ffd2a4aff4ee
Bastian Blokland <mail@bastian.tech>
2025-01-26 23:51:56 +0200

    net: Document http api

--- libs/net/include/net_http.h
+++ libs/net/include/net_http.h
@@ -8,37 +8,41 @@ typedef enum {
 } NetHttpFlags;
 
 /**
- * TODO:
+ * Http (Hypertext Transfer Protocol) connection.
  */
 typedef struct sNetHttp NetHttp;
 
 /**
- * TODO:
+ * Establish a Http connection to a remote server.
+ * NOTE: Multiple requests can be made serially over the same connection.
+ * Should be cleaned up using 'net_http_destroy()'.
  */
 NetHttp* net_http_connect_sync(Allocator*, String host, NetHttpFlags);
 
 /**
- * TODO:
+ * Destroy the given Http connection.
+ * NOTE: For gracefully ended sessions call 'net_http_shutdown_sync()' before destroying.
  */
 void net_http_destroy(NetHttp*);
 
 /**
- * TODO:
+ * Query the status of the given Http connection.
  */
 NetResult net_http_status(const NetHttp*);
 
 /**
- * TODO:
+ * Query information about the remote server.
  */
 const NetAddr* net_http_remote(const NetHttp*);
 String         net_http_remote_name(const NetHttp*);
 
 /**
- * TODO:
+ * Synchonously perform a 'GET' request for the given resource.
+ * NOTE: Response body is written to the output DynString.
  */
 NetResult net_http_get_sync(NetHttp*, String uri, DynString* out);
 
 /**
- * TODO:
+ * Synchonously shutdown the Http connection.
  */
 NetResult net_http_shutdown_sync(NetHttp*);
