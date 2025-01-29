45db95badaa9f075e2d5258d67ce449622eb2975
Bastian Blokland <mail@bastian.tech>
2025-01-29 19:26:37 +0200

    httpu: Disable console logging when piping

--- apps/utilities/httpu.c
+++ apps/utilities/httpu.c
@@ -126,7 +126,9 @@ i32 app_cli_run(const CliApp* app, const CliInvocation* invoc) {
     return 0;
   }
 
-  log_add_sink(g_logger, log_sink_pretty_default(g_allocHeap, LogMask_All));
+  if (tty_isatty(g_fileStdOut)) {
+    log_add_sink(g_logger, log_sink_pretty_default(g_allocHeap, LogMask_All));
+  }
   log_add_sink(g_logger, log_sink_json_default(g_allocHeap, LogMask_All));
 
   HttpuContext ctx = {
