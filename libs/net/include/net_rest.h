a63b41ef7a6827ae6d69bd064369d96100e04908
Bastian Blokland <mail@bastian.tech>
2025-01-31 21:57:56 +0200

    net: Initial rest header

---
+++ libs/net/include/net_rest.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "net.h"
+
+#define net_rest_workers_max 8
+
+/**
+ * Rest (REpresentational State Transfer) session.
+ */
+typedef struct sNetRest NetRest;
+
+/**
+ * Create a rest session.
+ * Should be cleaned up using 'net_rest_destroy()'.
+ */
+NetRest* net_rest_create(Allocator*, u32 workers);
+
+/**
+ * Destroy the given rest session.
+ */
+void net_rest_destroy(NetRest*);
