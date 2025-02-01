e00b74096ced6829762479b21136593aa81d43b3
Bastian Blokland <mail@bastian.tech>
2025-02-01 10:09:19 +0200

    net: Create worker and request structures

--- libs/net/include/net_rest.h
+++ libs/net/include/net_rest.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "net.h"
 
-#define net_rest_workers_max 8
+typedef u32 NetRestId;
 
 /**
  * Rest (REpresentational State Transfer) session.
@@ -12,9 +12,18 @@ typedef struct sNetRest NetRest;
  * Create a rest session.
  * Should be cleaned up using 'net_rest_destroy()'.
  */
-NetRest* net_rest_create(Allocator*, u32 workerCount);
+NetRest* net_rest_create(Allocator*, u32 workerCount, u32 requestCount);
 
 /**
  * Destroy the given rest session.
  */
 void net_rest_destroy(NetRest*);
+
+NetRestId net_rest_head(NetRest*, String host, String uri, const NetHttpAuth*, const NetHttpEtag*);
+NetRestId net_rest_get(NetRest*, String host, String uri, const NetHttpAuth*, const NetHttpEtag*);
+
+bool               net_rest_done(NetRest*, NetRestId);
+NetResult          net_rest_result(NetRest*, NetRestId);
+String             net_rest_data(NetRest*, NetRestId);
+const NetHttpEtag* net_rest_etag(NetRest*, NetRestId);
+void               net_rest_release(NetRest*, NetRestId);
--- libs/net/src/rest.c
+++ libs/net/src/rest.c
@@ -1,16 +1,41 @@
 #include "core_alloc.h"
+#include "core_math.h"
 #include "net_rest.h"
 
+typedef struct {
+  u32 dummy;
+} NetRestWorker;
+
+typedef struct {
+  u32 dummy;
+} NetRestRequest;
+
 typedef struct sNetRest {
-  Allocator* alloc;
-  u32        workers[net_rest_workers_max];
-  u32        workerCount;
+  Allocator*      alloc;
+  NetRestWorker*  workers;
+  NetRestRequest* requests;
+  u32             workerCount, requestCount;
 } NetRest;
 
-NetRest* net_rest_create(Allocator* alloc, const u32 workerCount) {
+NetRest* net_rest_create(Allocator* alloc, u32 workerCount, u32 requestCount) {
+  workerCount  = math_max(1, workerCount);
+  requestCount = math_max(workerCount, requestCount);
+
   NetRest* rest = alloc_alloc_t(alloc, NetRest);
-  *rest         = (NetRest){.alloc = alloc, .workerCount = workerCount};
+
+  *rest = (NetRest){
+      .alloc        = alloc,
+      .workers      = alloc_array_t(alloc, NetRestWorker, workerCount),
+      .requests     = alloc_array_t(alloc, NetRestRequest, requestCount),
+      .workerCount  = workerCount,
+      .requestCount = requestCount,
+  };
+
   return rest;
 }
 
-void net_rest_destroy(NetRest* rest) { alloc_free_t(rest->alloc, rest); }
+void net_rest_destroy(NetRest* rest) {
+  alloc_free_array_t(rest->alloc, rest->workers, rest->workerCount);
+  alloc_free_array_t(rest->alloc, rest->requests, rest->requestCount);
+  alloc_free_t(rest->alloc, rest);
+}
