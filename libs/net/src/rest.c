5c9002c58a9505edaaad6ecd5e51b298cd0b5ce2
Bastian Blokland <mail@bastian.tech>
2025-02-01 10:44:31 +0200

    net: Basic rest worker loop

--- libs/net/src/rest.c
+++ libs/net/src/rest.c
@@ -1,21 +1,46 @@
 #include "core_alloc.h"
+#include "core_format.h"
 #include "core_math.h"
+#include "core_thread.h"
 #include "net_rest.h"
 
-typedef struct {
-  u32 dummy;
-} NetRestWorker;
+typedef enum {
+  NetRestState_Idle,
+  NetRestState_Waiting,
+  NetRestState_Busy,
+  NetRestState_Finished,
+} NetRestState;
 
 typedef struct {
-  u32 dummy;
+  NetRestState state;
 } NetRestRequest;
 
 typedef struct sNetRest {
-  Allocator*      alloc;
-  NetRestWorker*  workers;
+  Allocator* alloc;
+
+  ThreadMutex     workerMutex;
+  ThreadCondition workerWakeCondition;
+  ThreadHandle*   workerThreads;
+  bool            workerShutdown;
+  u32             workerCount;
+
   NetRestRequest* requests;
-  u32             workerCount, requestCount;
+  u32             requestCount;
 } NetRest;
+
+static void rest_worker_thread(void* data) {
+  NetRest* rest = data;
+
+  bool shutdown = false;
+  while (!shutdown) {
+    thread_mutex_lock(rest->workerMutex);
+    {
+      thread_cond_wait(rest->workerWakeCondition, rest->workerMutex);
+      shutdown = rest->workerShutdown;
+    }
+    thread_mutex_unlock(rest->workerMutex);
+  }
+}
 
 NetRest* net_rest_create(Allocator* alloc, u32 workerCount, u32 requestCount) {
   workerCount  = math_max(1, workerCount);
@@ -24,18 +49,48 @@ NetRest* net_rest_create(Allocator* alloc, u32 workerCount, u32 requestCount) {
   NetRest* rest = alloc_alloc_t(alloc, NetRest);
 
   *rest = (NetRest){
-      .alloc        = alloc,
-      .workers      = alloc_array_t(alloc, NetRestWorker, workerCount),
+      .alloc = alloc,
+
+      .workerMutex         = thread_mutex_create(g_allocHeap),
+      .workerWakeCondition = thread_cond_create(g_allocHeap),
+      .workerThreads       = alloc_array_t(alloc, ThreadHandle, workerCount),
+      .workerCount         = workerCount,
+
       .requests     = alloc_array_t(alloc, NetRestRequest, requestCount),
-      .workerCount  = workerCount,
       .requestCount = requestCount,
   };
+
+  // Spawn workers.
+  for (u32 i = 0; i != workerCount; ++i) {
+    const String         threadName = fmt_write_scratch("volo_rest_{}", fmt_int(i));
+    const ThreadPriority threadPrio = ThreadPriority_Low;
+    rest->workerThreads[i] = thread_start(rest_worker_thread, (void*)rest, threadName, threadPrio);
+  }
 
   return rest;
 }
 
 void net_rest_destroy(NetRest* rest) {
-  alloc_free_array_t(rest->alloc, rest->workers, rest->workerCount);
+  // Signal workers to shutdown.
+  thread_mutex_lock(rest->workerMutex);
+  {
+    rest->workerShutdown = true;
+    thread_cond_broadcast(rest->workerWakeCondition);
+  }
+  thread_mutex_unlock(rest->workerMutex);
+
+  // Wait for workers to shutdown.
+  for (u32 i = 0; i != rest->workerCount; ++i) {
+    thread_join(rest->workerThreads[i]);
+  }
+
+  // Cleanup worker data.
+  thread_mutex_destroy(rest->workerMutex);
+  thread_mutex_destroy(rest->workerWakeCondition);
+  alloc_free_array_t(rest->alloc, rest->workerThreads, rest->workerCount);
+
+  // Cleanup requests.
   alloc_free_array_t(rest->alloc, rest->requests, rest->requestCount);
+
   alloc_free_t(rest->alloc, rest);
 }
