4457f0061dbfa1a59eec9ff211060576f19b7ae8
Bastian Blokland <mail@bastian.tech>
2025-02-07 20:31:12 +0200

    xml: Add string allocator

--- libs/xml/src/doc.c
+++ libs/xml/src/doc.c
@@ -2,21 +2,25 @@
 #include "core_dynarray.h"
 #include "xml_doc.h"
 
+#define xml_str_chunk_size (16 * usize_kibibyte)
+
 typedef struct {
   u32 dummy;
 } XmlNodeData;
 
 struct sXmlDoc {
   Allocator* alloc;
-  DynArray   nodes; // XmlNodeData[]
+  Allocator* allocStr; // (chunked) bump allocator for string data.
+  DynArray   nodes;    // XmlNodeData[]
 };
 
 XmlDoc* xml_create(Allocator* alloc, const usize nodeCapacity) {
   XmlDoc* doc = alloc_alloc_t(alloc, XmlDoc);
 
   *doc = (XmlDoc){
-      .alloc = alloc,
-      .nodes = dynarray_create_t(alloc, XmlNodeData, nodeCapacity),
+      .alloc    = alloc,
+      .allocStr = alloc_chunked_create(alloc, alloc_bump_create, xml_str_chunk_size),
+      .nodes    = dynarray_create_t(alloc, XmlNodeData, nodeCapacity),
   };
 
   return doc;
@@ -24,5 +28,6 @@ XmlDoc* xml_create(Allocator* alloc, const usize nodeCapacity) {
 
 void xml_destroy(XmlDoc* doc) {
   dynarray_destroy(&doc->nodes);
+  alloc_chunked_destroy(doc->allocStr); // Free all string data.
   alloc_free_t(doc->alloc, doc);
 }
