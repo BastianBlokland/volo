110db2ee41fd98e3cf10a1a571fe5c35290e4855
Bastian Blokland <mail@bastian.tech>
2025-02-07 17:50:28 +0200

    xml: Library skeleton

---
+++ libs/xml/include/xml.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "core.h"
+
+/**
+ * Forward header for the xml library.
+ */
+
+typedef struct sXmlDoc XmlDoc;
---
+++ libs/xml/include/xml_doc.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "core.h"
+
+/**
+ * Definition for a Xml Document.
+ * Supports a subset of Xml 1.0 (https://www.w3.org/TR/2008/REC-xml-20081126/).
+ */
+typedef struct sJsonDoc JsonDoc;
