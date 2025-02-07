0e42251cb6a3ebe1647b38f5b90974f422498bed
Bastian Blokland <mail@bastian.tech>
2025-02-07 17:54:56 +0200

    xml: Add create / destroy utils

--- libs/xml/include/xml_doc.h
+++ libs/xml/include/xml_doc.h
@@ -1,8 +1,20 @@
 #pragma once
-#include "core.h"
+#include "xml.h"
 
 /**
  * Definition for a Xml Document.
  * Supports a subset of Xml 1.0 (https://www.w3.org/TR/2008/REC-xml-20081126/).
  */
-typedef struct sJsonDoc JsonDoc;
+typedef struct sXmlDoc XmlDoc;
+
+/**
+ * Create a new Xml document.
+ *
+ * Should be destroyed using 'xml_destroy()'.
+ */
+XmlDoc* xml_create(Allocator*);
+
+/**
+ * Destroy a Xml document.
+ */
+void xml_destroy(XmlDoc*);
--- libs/xml/src/doc.c
+++ libs/xml/src/doc.c
@@ -1,1 +1,16 @@
+#include "core_alloc.h"
 #include "xml_doc.h"
+
+struct sXmlDoc {
+  Allocator* alloc;
+};
+
+XmlDoc* xml_create(Allocator* alloc) {
+  XmlDoc* doc = alloc_alloc_t(alloc, XmlDoc);
+
+  *doc = (XmlDoc){.alloc = alloc};
+
+  return doc;
+}
+
+void xml_destroy(XmlDoc* doc) { alloc_free_t(doc->alloc, doc); }
