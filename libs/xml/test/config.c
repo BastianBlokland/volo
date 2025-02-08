f8ae3280adb7e758c62a6e8ff9bc1d92069fb1e2
Bastian Blokland <mail@bastian.tech>
2025-02-08 20:58:37 +0200

    xml: Add equate tests

--- libs/xml/test/config.c
+++ libs/xml/test/config.c
@@ -1,5 +1,8 @@
 #include "app_check.h"
 
-void app_check_init(CheckDef* check) { register_spec(check, doc); }
+void app_check_init(CheckDef* check) {
+  register_spec(check, doc);
+  register_spec(check, eq);
+}
 
 void app_check_teardown(void) {}
