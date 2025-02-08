4d4dd041bb31221c648b715f1232a06192d1e25d
Bastian Blokland <mail@bastian.tech>
2025-02-08 13:47:35 +0200

    xml: Add error enum

--- libs/xml/include/xml.h
+++ libs/xml/include/xml.h
@@ -5,5 +5,6 @@
  * Forward header for the xml library.
  */
 
+typedef enum eXmlError       XmlError;
 typedef struct sXmlAttribute XmlAttribute;
 typedef struct sXmlDoc       XmlDoc;
---
+++ libs/xml/include/xml_read.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "xml.h"
+
+typedef enum eXmlError {
+  XmlError_Truncated,
+
+  XmlError_Count,
+} XmlError;
