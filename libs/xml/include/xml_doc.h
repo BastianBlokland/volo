befe54766668de12c1843911dfa55efc20b096da
Bastian Blokland <mail@bastian.tech>
2025-02-08 12:02:31 +0200

    xml: Added basic nodes to doc

--- libs/xml/include/xml_doc.h
+++ libs/xml/include/xml_doc.h
@@ -9,8 +9,8 @@
 typedef struct sXmlDoc XmlDoc;
 
 typedef enum eXmlType {
-  XmlType_Attribute,
   XmlType_Element,
+  XmlType_Attribute,
   XmlType_Text,
   XmlType_Comment,
 
@@ -38,16 +38,46 @@ XmlDoc* xml_create(Allocator*, usize nodeCapacity);
 void xml_destroy(XmlDoc*);
 
 /**
- * TODO:
+ * Clear a Xml document.
+ * NOTE: After clearing all previously added Xml nodes are invalided.
  */
-XmlNode xml_add_attr(XmlDoc*, XmlNode parent, String name, String value);
+void xml_clear(XmlDoc*);
 
 /**
- * TODO:
+ * Add a new element node to the document.
+ * Optionally provide a parent element node, provide 'sentinel_u32' to make a root element.
  */
 XmlNode xml_add_elem(XmlDoc*, XmlNode parent, String name);
 
 /**
- * TODO:
+ * Add a new attribute node to an element node.
+ * Returns 'sentinel_u32' when the parent element already had an attribute with the same name.
  */
-XmlNode xml_add_text(XmlDoc*, XmlNode parent, String text);
+XmlNode xml_add_attr(XmlDoc*, XmlNode parent, String name, String value);
+
+/**
+ * Add a new text node to an element node.
+ */
+XmlNode xml_add_text(XmlDoc*, XmlNode parent, String value);
+
+/**
+ * Add a new comment node to an element node.
+ */
+XmlNode xml_add_comment(XmlDoc*, XmlNode parent, String value);
+
+/**
+ * Query node data.
+ */
+XmlType xml_type(const XmlDoc*, XmlNode);
+String  xml_name(const XmlDoc*, XmlNode);
+String  xml_value(const XmlDoc*, XmlNode);
+bool    xml_attr_has(const XmlDoc*, XmlNode node, String name);
+String  xml_attr_get(const XmlDoc*, XmlNode node, String name);
+XmlNode xml_first_child(const XmlDoc*, XmlNode);
+XmlNode xml_first_attr(const XmlDoc*, XmlNode);
+
+/**
+ * Retrieve the next (sibling) node.
+ * Returns 'sentinel_u32' when there are no more sibling nodes.
+ */
+XmlNode xml_next(const XmlDoc*, XmlNode node);
