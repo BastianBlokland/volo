d9b265b597f68ee3653c2bbd846aeadc048adc48
Bastian Blokland <mail@bastian.tech>
2025-02-09 15:47:31 +0200

    xml: Add attribute tests

--- libs/xml/test/test_write.c
+++ libs/xml/test/test_write.c
@@ -25,6 +25,32 @@ spec(write) {
                    "<test/>"));
   }
 
+  it("can write a basic node without a declaration") {
+    const XmlNode node = xml_add_elem(doc, sentinel_u32, string_lit("test"));
+
+    xml_write(&buffer, doc, node, &xml_write_opts(.flags = XmlWriteFlags_SkipDeclaration));
+    check_eq_string(dynstring_view(&buffer), string_lit("<test/>"));
+  }
+
+  it("can write a basic node with an attribute") {
+    const XmlNode node = xml_add_elem(doc, sentinel_u32, string_lit("test"));
+    xml_add_attr(doc, node, string_lit("foo"), string_lit("bar"));
+
+    xml_write(&buffer, doc, node, &xml_write_opts(.flags = XmlWriteFlags_SkipDeclaration));
+    check_eq_string(dynstring_view(&buffer), string_lit("<test foo=\"bar\"/>"));
+  }
+
+  it("can write a basic node with multiple attributes") {
+    const XmlNode node = xml_add_elem(doc, sentinel_u32, string_lit("test"));
+    xml_add_attr(doc, node, string_lit("foo"), string_lit("bar"));
+    xml_add_attr(doc, node, string_lit("hello"), string_lit("world"));
+    xml_add_attr(doc, node, string_lit("test"), string_lit("world"));
+
+    xml_write(&buffer, doc, node, &xml_write_opts(.flags = XmlWriteFlags_SkipDeclaration));
+    check_eq_string(
+        dynstring_view(&buffer), string_lit("<test foo=\"bar\" hello=\"world\" test=\"world\"/>"));
+  }
+
   teardown() {
     xml_destroy(doc);
     dynstring_destroy(&buffer);
