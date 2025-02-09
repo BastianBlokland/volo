9774f8b3d09809df8c780a4312785160049408dd
Bastian Blokland <mail@bastian.tech>
2025-02-09 15:59:06 +0200

    xml: Test nested nodes

--- libs/xml/test/test_write.c
+++ libs/xml/test/test_write.c
@@ -15,7 +15,7 @@ spec(write) {
     buffer = dynstring_create(g_allocPage, usize_kibibyte * 4);
   }
 
-  it("can write a basic node") {
+  it("can write a node") {
     const XmlNode node = xml_add_elem(doc, sentinel_u32, string_lit("test"));
 
     xml_write(&buffer, doc, node, &xml_write_opts());
@@ -25,14 +25,14 @@ spec(write) {
                    "<test/>"));
   }
 
-  it("can write a basic node without a declaration") {
+  it("can write a node without a declaration") {
     const XmlNode node = xml_add_elem(doc, sentinel_u32, string_lit("test"));
 
     xml_write(&buffer, doc, node, &xml_write_opts(.flags = XmlWriteFlags_SkipDeclaration));
     check_eq_string(dynstring_view(&buffer), string_lit("<test/>"));
   }
 
-  it("can write a basic node with an attribute") {
+  it("can write a node with an attribute") {
     const XmlNode node = xml_add_elem(doc, sentinel_u32, string_lit("test"));
     xml_add_attr(doc, node, string_lit("foo"), string_lit("bar"));
 
@@ -40,7 +40,7 @@ spec(write) {
     check_eq_string(dynstring_view(&buffer), string_lit("<test foo=\"bar\"/>"));
   }
 
-  it("can write a basic node with multiple attributes") {
+  it("can write a node with multiple attributes") {
     const XmlNode node = xml_add_elem(doc, sentinel_u32, string_lit("test"));
     xml_add_attr(doc, node, string_lit("foo"), string_lit("bar"));
     xml_add_attr(doc, node, string_lit("hello"), string_lit("world"));
@@ -51,7 +51,7 @@ spec(write) {
         dynstring_view(&buffer), string_lit("<test foo=\"bar\" hello=\"world\" test=\"world\"/>"));
   }
 
-  it("can write a basic node with text") {
+  it("can write a node with text") {
     const XmlNode node = xml_add_elem(doc, sentinel_u32, string_lit("test"));
     xml_add_text(doc, node, string_lit("Hello World!"));
 
@@ -59,7 +59,7 @@ spec(write) {
     check_eq_string(dynstring_view(&buffer), string_lit("<test>Hello World!</test>"));
   }
 
-  it("can write a basic node with a child node") {
+  it("can write a node with a child node") {
     const XmlNode node = xml_add_elem(doc, sentinel_u32, string_lit("test"));
     xml_add_elem(doc, node, string_lit("foo"));
 
@@ -68,6 +68,58 @@ spec(write) {
         dynstring_view(&buffer),
         string_lit("<test>\n"
                    "  <foo/>\n"
+                   "</test>"));
+  }
+
+  it("can write a node with multiple child nodes") {
+    const XmlNode node = xml_add_elem(doc, sentinel_u32, string_lit("test"));
+    xml_add_elem(doc, node, string_lit("foo"));
+    xml_add_elem(doc, node, string_lit("bar"));
+    xml_add_elem(doc, node, string_lit("baz"));
+
+    xml_write(&buffer, doc, node, &xml_write_opts(.flags = XmlWriteFlags_SkipDeclaration));
+    check_eq_string(
+        dynstring_view(&buffer),
+        string_lit("<test>\n"
+                   "  <foo/>\n"
+                   "  <bar/>\n"
+                   "  <baz/>\n"
+                   "</test>"));
+  }
+
+  it("can write a node with mixed child nodes") {
+    const XmlNode node = xml_add_elem(doc, sentinel_u32, string_lit("test"));
+    xml_add_text(doc, node, string_lit("Hello"));
+    xml_add_comment(doc, node, string_lit("Test!"));
+    xml_add_elem(doc, node, string_lit("bar"));
+    xml_add_text(doc, node, string_lit("World"));
+
+    xml_write(&buffer, doc, node, &xml_write_opts(.flags = XmlWriteFlags_SkipDeclaration));
+    check_eq_string(
+        dynstring_view(&buffer),
+        string_lit("<test>\n"
+                   "  Hello\n"
+                   "  <!-- Test! -->\n"
+                   "  <bar/>\n"
+                   "  World\n"
+                   "</test>"));
+  }
+
+  it("can write nested nodes") {
+    const XmlNode node       = xml_add_elem(doc, sentinel_u32, string_lit("test"));
+    const XmlNode child      = xml_add_elem(doc, node, string_lit("foo"));
+    const XmlNode grandChild = xml_add_elem(doc, child, string_lit("bar"));
+    xml_add_elem(doc, grandChild, string_lit("baz"));
+
+    xml_write(&buffer, doc, node, &xml_write_opts(.flags = XmlWriteFlags_SkipDeclaration));
+    check_eq_string(
+        dynstring_view(&buffer),
+        string_lit("<test>\n"
+                   "  <foo>\n"
+                   "    <bar>\n"
+                   "      <baz/>\n"
+                   "    </bar>\n"
+                   "  </foo>\n"
                    "</test>"));
   }
 
