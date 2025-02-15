eedd77fcdff8bc57011be6ed66fa939dca88b398
Bastian Blokland <mail@bastian.tech>
2025-02-15 15:03:53 +0200

    core: Custom type-defs

--- libs/core/include/core_types.h
+++ libs/core/include/core_types.h
@@ -1,56 +1,82 @@
 #pragma once
-#include <stddef.h>
-#include <stdint.h>
 
-typedef int8_t    i8;
-typedef int16_t   i16;
-typedef int32_t   i32;
-typedef int64_t   i64;
-typedef uint8_t   u8;
-typedef uint16_t  u16;
-typedef uint32_t  u32;
-typedef uint64_t  u64;
-typedef size_t    usize;
-typedef intptr_t  iptr;
-typedef uintptr_t uptr;
+/**
+ * Primitive type definitions.
+ */
 
-typedef uint16_t f16;
-typedef float    f32;
-typedef double   f64;
+typedef signed char        i8;
+typedef signed short int   i16;
+typedef signed int         i32;
+typedef signed long int    i64;
+typedef unsigned char      u8;
+typedef unsigned short int u16;
+typedef unsigned int       u32;
+typedef unsigned long int  u64;
 
-typedef u8 bool;
-#define true UINT8_C(1)
-#define false UINT8_C(0)
+#ifdef VOLO_32_BIT
+typedef unsigned int usize;
+typedef signed int   iptr;
+typedef unsigned int uptr;
+#else
+typedef unsigned long int usize;
+typedef signed long int   iptr;
+typedef unsigned long int uptr;
+#endif
+
+typedef unsigned short int f16;
+typedef float              f32;
+typedef double             f64;
+
+typedef unsigned char bool;
+#define true 1
+#define false 0
 
 #define null ((void*)0)
 
-#define u8_lit(_LITERAL_) UINT8_C(_LITERAL_)
-#define i8_lit(_LITERAL_) INT8_C(_LITERAL_)
-#define u16_lit(_LITERAL_) UINT16_C(_LITERAL_)
-#define i16_lit(_LITERAL_) INT16_C(_LITERAL_)
-#define u32_lit(_LITERAL_) UINT32_C(_LITERAL_)
-#define i32_lit(_LITERAL_) INT32_C(_LITERAL_)
-#define u64_lit(_LITERAL_) UINT64_C(_LITERAL_)
-#define i64_lit(_LITERAL_) INT64_C(_LITERAL_)
-#define usize_lit(_LITERAL_) ((usize)UINT64_C(_LITERAL_))
+#define u8_lit(_LITERAL_) _LITERAL_
+#define i8_lit(_LITERAL_) _LITERAL_
+#define u16_lit(_LITERAL_) _LITERAL_
+#define i16_lit(_LITERAL_) _LITERAL_
+#define u32_lit(_LITERAL_) _LITERAL_##U
+#define i32_lit(_LITERAL_) _LITERAL_
+#define u64_lit(_LITERAL_) _LITERAL_##UL
+#define i64_lit(_LITERAL_) _LITERAL_##L
 
-#define i8_min INT8_MIN
-#define i16_min INT16_MIN
-#define i32_min INT32_MIN
-#define i64_min INT64_MIN
-#define iptr_min INTPTR_MIN
+#ifdef VOLO_32_BIT
+#define usize_lit(_LITERAL_) _LITERAL_##U
+#else
+#define usize_lit(_LITERAL_) _LITERAL_##UL
+#endif
 
-#define i8_max INT8_MAX
-#define i16_max INT16_MAX
-#define i32_max INT32_MAX
-#define i64_max INT64_MAX
-#define u8_max UINT8_MAX
-#define u16_max UINT16_MAX
-#define u32_max UINT32_MAX
-#define u64_max UINT64_MAX
-#define usize_max SIZE_MAX
-#define iptr_max INTPTR_MAX
-#define uptr_max UINTPTR_MAX
+#define i8_min (-128)
+#define i16_min (-32767 - 1)
+#define i32_min (-2147483647 - 1)
+#define i64_min (-i64_lit(9223372036854775807) - 1)
+
+#ifdef VOLO_32_BIT
+#define iptr_min (-2147483647 - 1)
+#else
+#define iptr_min (-i64_lit(9223372036854775807) - 1)
+#endif
+
+#define i8_max (127)
+#define i16_max (32767)
+#define i32_max (2147483647)
+#define i64_max (i64_lit(9223372036854775807))
+#define u8_max (255)
+#define u16_max (65535)
+#define u32_max (4294967295U)
+#define u64_max (u64_lit(18446744073709551615))
+
+#ifdef VOLO_32_BIT
+#define usize_max (4294967295U)
+#define iptr_max (2147483647)
+#define uptr_max (4294967295U)
+#else
+#define usize_max (u64_lit(18446744073709551615))
+#define iptr_max (i64_lit(9223372036854775807))
+#define uptr_max (u64_lit(18446744073709551615))
+#endif
 
 #define usize_byte ((usize)1)
 #define usize_kibibyte (usize_byte * 1024)
@@ -58,6 +84,11 @@ typedef u8 bool;
 #define usize_gibibyte (usize_mebibyte * 1024)
 #define usize_tebibyte (usize_gibibyte * 1024)
 #define usize_pebibyte (usize_tebibyte * 1024)
+
+/**
+ * Retrieve the offset (in bytes) of the given member in the type.
+ */
+#define offsetof(_TYPE_, __MEMBER__) __builtin_offsetof(_TYPE_, __MEMBER__)
 
 /**
  * Return the alignment required for the given type.
