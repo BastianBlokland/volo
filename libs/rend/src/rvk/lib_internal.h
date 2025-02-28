f85b8a346ac71f26c414d6adba805cd3fe4cb5f6
Bastian Blokland <mail@bastian.tech>
2025-02-28 20:01:50 +0200

    rend: Add instance create logic to RvkLib

--- libs/rend/src/rvk/lib_internal.h
+++ libs/rend/src/rvk/lib_internal.h
@@ -2,7 +2,13 @@
 #include "rend_settings.h"
 #include "vulkan_api.h"
 
+typedef enum {
+  RvkLibFlags_Validation = 1 << 0,
+  RvkLibFlags_Debug      = 1 << 1,
+} RvkLibFlags;
+
 typedef struct sRvkLib {
+  RvkLibFlags           flags;
   VkInstance            vkInst;
   VkAllocationCallbacks vkAlloc;
 } RvkLib;
