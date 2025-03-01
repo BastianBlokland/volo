bdfa1c7ecabd97a927dd7faa9c10151e03af2594
Bastian Blokland <mail@bastian.tech>
2025-03-01 13:21:10 +0200

    rend: Remove func load utils

--- libs/rend/src/rvk/vulkan_internal.h
+++ libs/rend/src/rvk/vulkan_internal.h
@@ -3,22 +3,8 @@
 #include "vulkan_api.h"
 
 /**
- * Load a Vulkan instance api by name.
- */
-#define rvk_func_load_instance(_INSTANCE_, _API_)                                                  \
-  ((PFN_##_API_)rvk_func_load_instance_internal((_INSTANCE_), string_lit(#_API_)))
-
-/**
- * Load a Vulkan device api by name.
- */
-#define rvk_func_load_device(_DEVICE_, _API_)                                                      \
-  ((PFN_##_API_)rvk_func_load_device_internal((_DEVICE_), string_lit(#_API_)))
-
-/**
  * Call a Vulkan api and check its result.
  */
 #define rvk_call(_API_, _FUNC_, ...) rvk_check(string_lit(#_FUNC_), (_API_)._FUNC_(__VA_ARGS__))
 
-void* rvk_func_load_instance_internal(VkInstance, String api);
-void* rvk_func_load_device_internal(VkDevice, String api);
-void  rvk_check(String func, VkResult);
+void rvk_check(String func, VkResult);
