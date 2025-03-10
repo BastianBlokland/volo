1f8932187e2febba200ffcf6ce09a0fb9839d6e5
Bastian Blokland <mail@bastian.tech>
2025-03-10 07:03:19 +0200

    core: Minor cleanup in thread_cond_wait_timeout

--- libs/core/src/thread_pal_win32.c
+++ libs/core/src/thread_pal_win32.c
@@ -358,11 +358,11 @@ void thread_cond_wait(const ThreadCondition condHandle, const ThreadMutex mutexH
 }
 
 void thread_cond_wait_timeout(
-    const ThreadCondition condHandle, const ThreadMutex mutexHandle, TimeDuration timeout) {
+    const ThreadCondition condHandle, const ThreadMutex mutexHandle, const TimeDuration timeout) {
   ThreadConditionData* condData  = (ThreadConditionData*)condHandle;
   ThreadMutexData*     mutexData = (ThreadMutexData*)mutexHandle;
 
-  DWORD milliseconds = (DWORD)(timeout / time_millisecond);
+  const DWORD milliseconds = (DWORD)(timeout / time_millisecond);
 
   const BOOL sleepRes = SleepConditionVariableCS(&condData->impl, &mutexData->impl, milliseconds);
   if (UNLIKELY(!sleepRes && GetLastError() != ERROR_TIMEOUT)) {
