9b58072051e456bdb1b647d85a601fce86ea41f7
Bastian Blokland <mail@bastian.tech>
2025-03-29 17:11:54 +0200

    vfx: Configurable emissive mul

--- libs/vfx/src/stamp.c
+++ libs/vfx/src/stamp.c
@@ -99,7 +99,7 @@ void vfx_stamp_output(RendObjectComp* obj, const VfxStamp* params) {
 
   out->data4[0] = vfx_combine_u16(params->atlasColorIndex, 0);
   out->data4[1] = vfx_combine_u16(params->atlasNormalIndex, 0);
-  out->data4[2] = vfx_combine_u16(params->atlasEmissiveIndex, 0);
+  out->data4[2] = vfx_combine_u16(params->atlasEmissiveIndex, vfx_frac_u8(params->emissive));
   out->data4[3] = vfx_combine_u16(vfx_frac_u8(params->alphaBegin), vfx_frac_u8(params->alphaEnd));
 
   geo_vector_pack_f16(
