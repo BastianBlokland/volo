faca9656a31359b7cad6b49cd5cc652f42548c11
Bastian Blokland <mail@bastian.tech>
2025-06-23 21:43:09 +0300

    check: Add error limit (#1359)

--- libs/check/src/result.c
+++ libs/check/src/result.c
@@ -3,12 +3,16 @@
 
 #include "result_internal.h"
 
+static const u32 g_checkMaxErrors = 1000;
+
 CheckResult* check_result_create(Allocator* alloc) {
   CheckResult* result = alloc_alloc_t(alloc, CheckResult);
-  *result             = (CheckResult){
+
+  *result = (CheckResult){
       .alloc  = alloc,
       .errors = dynarray_create_t(alloc, CheckError, 0),
   };
+
   return result;
 }
 
@@ -19,12 +23,29 @@ void check_result_destroy(CheckResult* result) {
 }
 
 void check_result_error(CheckResult* result, String msg, const SourceLoc source) {
-  diag_assert_msg(!result->finished, "Result is already finished");
+  if (UNLIKELY(result->finished)) {
+    diag_crash_msg("Result is already finished");
+  }
+  static THREAD_LOCAL bool g_checkBusy;
+  if (UNLIKELY(g_checkBusy)) {
+    /**
+     * Re-entered this function while reporting the error; system is likely out of memory.
+     */
+    result->errorsTruncated = true;
+    return;
+  }
+  g_checkBusy = true;
 
-  *dynarray_push_t(&result->errors, CheckError) = (CheckError){
-      .msg    = string_dup(result->alloc, msg),
-      .source = source,
-  };
+  if (LIKELY(result->errors.size != g_checkMaxErrors)) {
+    *dynarray_push_t(&result->errors, CheckError) = (CheckError){
+        .msg    = string_dup(result->alloc, msg),
+        .source = source,
+    };
+  } else {
+    result->errorsTruncated = true;
+  }
+
+  g_checkBusy = false;
 }
 
 void check_result_finish(CheckResult* result, const TimeDuration duration) {
