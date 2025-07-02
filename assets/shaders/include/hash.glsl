734fe2957c7e2b973c6f70eac9d4bec55e11e69f
Bastian Blokland <mail@bastian.tech>
2025-07-02 21:28:08 +0300

    rend: Add shader float hash utils

--- assets/shaders/include/hash.glsl
+++ assets/shaders/include/hash.glsl
@@ -4,6 +4,37 @@
 #include "types.glsl"
 
 /**
+ * Compute number between 0 (inclusive) and 1 (exclusive) for the given value.
+ * Source 'Dave_Hoskins' from: https://www.shadertoy.com/view/4djSRW
+ */
+f32 hash_f32(f32 x) {
+  x = fract(x * 0.011);
+  x *= x + 7.5;
+  x *= x + x;
+  return fract(x);
+}
+
+/**
+ * Compute number between 0 (inclusive) and 1 (exclusive) for the given value.
+ * Source 'Dave_Hoskins' from: https://www.shadertoy.com/view/4djSRW
+ */
+f32 hash_f32v2(f32v2 x) {
+  f32v3 x3 = fract(f32v3(x.xyx) * 0.13);
+  x3 += dot(x3, x3.yzx + 3.333);
+  return fract((x3.x + x3.y) * x3.z);
+}
+
+/**
+ * Compute number between 0 (inclusive) and 1 (exclusive) for the given value.
+ * Source 'Dave_Hoskins' from: https://www.shadertoy.com/view/4djSRW
+ */
+f32 hash_f32v3(f32v3 x) {
+  x  = fract(x * 0.1031);
+  x += dot(x, x.zyx + 31.32);
+  return fract((x.x + x.y) * x.z);
+}
+
+/**
  * Compute a hash for the given unsigned 32 bit value.
  * bias: 0.17353355999581582 (very probably the best of its kind).
  * Source 'lowbias32' from: https://www.shadertoy.com/view/WttXWX
