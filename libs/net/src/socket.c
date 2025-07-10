ac3a5aa35c86d1f6b1069d4ae106b32d6b550bbf
Bastian Blokland <mail@bastian.tech>
2025-07-10 19:34:06 +0300

    net: Fix net_socket_connect_any_sync func name

--- libs/net/src/socket.c
+++ libs/net/src/socket.c
@@ -2,8 +2,8 @@
 #include "net_result.h"
 #include "net_socket.h"
 
-NetSocket* net_socket_connect_any_ip_sync(
-    Allocator* alloc, const NetIp* ips, const u32 ipCount, const u16 port) {
+NetSocket*
+net_socket_connect_any_sync(Allocator* alloc, const NetIp* ips, const u32 ipCount, const u16 port) {
   NetSocket* result = null;
   for (u32 i = 0; i != ipCount; ++i) {
     if (result) {
