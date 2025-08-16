e21a2fcff7109324eccefff726f57b58dc563411
Bastian Blokland <mail@bastian.tech>
2025-08-16 15:04:19 +0300

    core: Add a test for dynlib_symbol_global

--- libs/core/test/test_dynlib.c
+++ libs/core/test/test_dynlib.c
@@ -2,6 +2,8 @@
 #include "core_alloc.h"
 #include "core_dynlib.h"
 
+double SYS_DECL sin(double); // Libc sin function.
+
 spec(dynlib) {
 
   it("fails when opening an non-existent library") {
@@ -10,5 +12,14 @@ spec(dynlib) {
 
     DynLib* lib;
     check(dynlib_load(alloc, libName, &lib) == DynLibResult_LibraryNotFound);
+  }
+
+  it("returns null if a global symbol cannot be found") {
+    check(dynlib_symbol_global(string_lit("hello_world")) == null);
+  }
+
+  it("can lookup global symbols") {
+    // Check if we can lookup the libc sin function.
+    check(dynlib_symbol_global(string_lit("sin")) == &sin);
   }
 }
