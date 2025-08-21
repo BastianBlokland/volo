7ee060499608925519da58422497ca049885ddc4
Bastian Blokland <mail@bastian.tech>
2025-08-21 21:09:46 +0300

    rend: Make pass id unsigned

--- libs/rend/src/rvk/graphic.c
+++ libs/rend/src/rvk/graphic.c
@@ -987,7 +987,7 @@ bool rvk_graphic_finalize(
     const RvkPass*          pass,
     RendReport*             report) {
   diag_assert_msg(!gra->vkPipeline, "Graphic already finalized");
-  diag_assert(gra->passId == rvk_pass_config(pass)->id);
+  diag_assert((u32)gra->passId == rvk_pass_config(pass)->id);
 
   const TimeSteady startTime = time_steady_clock();
 
@@ -1094,7 +1094,10 @@ bool rvk_graphic_finalize(
 
   if (report) {
     rend_report_push_value(
-        report, string_lit("Pass"), string_empty, asset_graphic_pass_name(gra->passId));
+        report,
+        string_lit("Pass"),
+        string_empty,
+        asset_graphic_pass_name((AssetGraphicPass)gra->passId));
     rend_report_push_value(
         report,
         string_lit("Pass order"),
@@ -1153,7 +1156,7 @@ void rvk_graphic_bind(
   diag_assert_msg(rvk_graphic_is_ready(graphic, dev), "Graphic is not ready");
   diag_assert_msg(rvk_pass_active(pass), "Pass not active");
 #endif
-  diag_assert_msg(graphic->passId == rvk_pass_config(pass)->id, "Unsupported pass");
+  diag_assert_msg((u32)graphic->passId == rvk_pass_config(pass)->id, "Unsupported pass");
 
   rvk_call(dev, cmdBindPipeline, vkCmdBuf, VK_PIPELINE_BIND_POINT_GRAPHICS, graphic->vkPipeline);
 
