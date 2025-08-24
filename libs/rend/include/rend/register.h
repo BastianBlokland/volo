3b241467e3abf7551b922e2e31bb9b17ed2d8889
Bastian Blokland <mail@bastian.tech>
2025-08-24 13:03:42 +0300

    rend: Add 'EnableStats' register flag

--- libs/rend/include/rend/register.h
+++ libs/rend/include/rend/register.h
@@ -10,7 +10,12 @@ enum {
   RendOrder_Reset        = 3000,
 };
 
+typedef enum {
+  RendRegisterFlags_None        = 0,
+  RendRegisterFlags_EnableStats = 1 << 0,
+} RendRegisterFlags;
+
 /**
  * Register the ecs modules for the Renderer library.
  */
-void rend_register(EcsDef*);
+void rend_register(EcsDef*, RendRegisterFlags);
