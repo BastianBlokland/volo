cc199056eb16c94155116b297c1941e74d049db2
Bastian Blokland <mail@bastian.tech>
2025-08-25 18:56:44 +0300

    game: Add game state

---
+++ game/src/game.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "ecs/module.h"
+
+typedef enum {
+  GameState_MenuMain,
+  GameState_MenuLevel,
+  GameState_Loading,
+  GameState_Play,
+  GameState_Debug,
+  GameState_Edit,
+  GameState_Pause,
+} GameState;
+
+ecs_comp_extern(GameComp);
+
+GameState game_state(const GameComp*);
