54ac98be0b7b661a8a0874e40a46a587fb2cbb20
Bastian Blokland <mail@bastian.tech>
2025-08-28 18:48:53 +0300

    ui: Add arrow-left shape

--- libs/ui/include/ui/shape.h
+++ libs/ui/include/ui/shape.h
@@ -28,6 +28,7 @@
   X(0xE53B, Layers)                                                                                \
   X(0xE548, Hospital)                                                                              \
   X(0xE574, Category)                                                                              \
+  X(0xE5C4, ArrowLeft)                                                                             \
   X(0xE5CA, Check)                                                                                 \
   X(0xE5CD, Close)                                                                                 \
   X(0xE5CE, ExpandLess)                                                                            \
