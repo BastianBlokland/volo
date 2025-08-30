3377d000648ddf111ab99c13e2cbccdbba2778d8
Bastian Blokland <mail@bastian.tech>
2025-08-30 21:51:03 +0300

    dev: Add dev_menu_edit_panels_open util

--- libs/dev/include/dev/menu.h
+++ libs/dev/include/dev/menu.h
@@ -4,3 +4,6 @@
 ecs_comp_extern(DevMenuComp);
 
 EcsEntityId dev_menu_create(EcsWorld*, EcsEntityId window);
+
+void dev_menu_edit_panels_open(EcsWorld*, DevMenuComp*);
+void dev_menu_edit_panels_close(EcsWorld*, DevMenuComp*);
