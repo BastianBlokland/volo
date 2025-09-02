c9eaa60ba323008085aaa70d936694f91dae42d4
Bastian Blokland <mail@bastian.tech>
2025-09-02 21:52:58 +0300

    loc: Skeleton manager api

--- libs/loc/include/loc/manager.h
+++ libs/loc/include/loc/manager.h
@@ -10,3 +10,23 @@ ecs_comp_extern(LocManagerComp);
  * Initialize the locale manager.
  */
 LocManagerComp* loc_manager_init(EcsWorld*, String preferredLocale);
+
+/**
+ * Has the locale manager finished initializing.
+ */
+bool loc_manager_ready(const LocManagerComp*);
+
+/**
+ * Query the available locales.
+ * Pre-condition: loc_manager_ready().
+ */
+const String* loc_manager_locale_names(const LocManagerComp*); // String[loc_manager_locale_count];
+u32           loc_manager_locale_count(const LocManagerComp*);
+
+/**
+ * Query / update the active locale.
+ * Pre-condition: loc_manager_ready().
+ * Pre-condition: localeIndex < loc_manager_locale_count().
+ */
+u32  loc_manager_active_get(const LocManagerComp*);
+void loc_manager_active_set(const LocManagerComp*, u32 localeIndex);
