265e73e6a72e7ae910a920870e5a618e185e0931
Bastian Blokland <mail@bastian.tech>
2025-09-03 18:51:55 +0300

    loc: Add translate convenience overloads

--- libs/loc/include/loc/translate.h
+++ libs/loc/include/loc/translate.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "core/forward.h"
 
+#define loc_translate_lit(_KEY_LIT_) loc_translate(string_hash_lit(_KEY_LIT_))
+#define loc_translate_str(_KEY_STR_) loc_translate(string_hash(_KEY_STR_))
+
 /**
  * Retrieve the localized string for the given key.
  * NOTE: Returns an empty string when the key cannot be found.
