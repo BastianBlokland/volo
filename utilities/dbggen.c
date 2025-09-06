eea6e55f63a55ea7215c6015f659e79c92bcdf9d
Bastian Blokland <mail@bastian.tech>
2025-09-06 05:19:58 +0300

    utilities: Rename dbgsetup

--- utilities/dbggen.c
+++ utilities/dbggen.c
@@ -14,7 +14,7 @@
 #include "log/sink_pretty.h"
 
 /**
- * DebugSetup - Utility to generate debugger configuration files for a set of executables.
+ * DebugGen - Utility to generate debugger configuration files for a set of executables.
  *
  * For example a VsCode launch config file:
  * ```
@@ -35,20 +35,20 @@
  */
 
 typedef enum {
-  DbgSetupDbg_Lldb,
-  DbgSetupDbg_Cppvsdbg,
+  DbgGenDbg_Lldb,
+  DbgGenDbg_Cppvsdbg,
 
-  DbgSetupDbg_Count,
-  DbgSetupDbg_Default = DbgSetupDbg_Lldb,
-} DbgSetupDbg;
+  DbgGenDbg_Count,
+  DbgGenDbg_Default = DbgGenDbg_Lldb,
+} DbgGenDbg;
 
 static const String g_dbgStrs[] = {
     string_static("lldb"),
     string_static("cppvsdbg"),
 };
-ASSERT(array_elems(g_dbgStrs) == DbgSetupDbg_Count, "Incorrect number of dbg strings");
+ASSERT(array_elems(g_dbgStrs) == DbgGenDbg_Count, "Incorrect number of dbg strings");
 
-static bool dbgsetup_validate_dbg(const String input) {
+static bool dbggen_validate_dbg(const String input) {
   array_for_t(g_dbgStrs, String, cfg) {
     if (string_eq(*cfg, input)) {
       return true;
@@ -58,13 +58,13 @@ static bool dbgsetup_validate_dbg(const String input) {
 }
 
 typedef struct {
-  DbgSetupDbg dbg;
-  String      workspace;
-  String*     targets;
-  usize       targetCount;
-} DbgSetupCtx;
+  DbgGenDbg dbg;
+  String    workspace;
+  String*   targets;
+  usize     targetCount;
+} DbgGenCtx;
 
-static bool dbgsetup_write_json(String path, const JsonDoc* jsonDoc, const JsonVal jsonVal) {
+static bool dbggen_write_json(String path, const JsonDoc* jsonDoc, const JsonVal jsonVal) {
   DynString dynString = dynstring_create(g_allocHeap, 64 * usize_kibibyte);
   json_write(&dynString, jsonDoc, jsonVal, &json_write_opts(.mode = JsonWriteMode_Compact));
 
@@ -80,7 +80,7 @@ static bool dbgsetup_write_json(String path, const JsonDoc* jsonDoc, const JsonV
   return res == FileResult_Success;
 }
 
-static JsonVal dbgsetup_vscode_gen_launch_entry(DbgSetupCtx* ctx, JsonDoc* doc, String target) {
+static JsonVal dbggen_vscode_gen_launch_entry(DbgGenCtx* ctx, JsonDoc* doc, String target) {
   const JsonVal obj = json_add_object(doc);
   json_add_field_lit(
       doc,
@@ -95,7 +95,7 @@ static JsonVal dbgsetup_vscode_gen_launch_entry(DbgSetupCtx* ctx, JsonDoc* doc, 
   return obj;
 }
 
-static JsonVal dbgsetup_vscode_gen_attach_entry(DbgSetupCtx* ctx, JsonDoc* doc, String target) {
+static JsonVal dbggen_vscode_gen_attach_entry(DbgGenCtx* ctx, JsonDoc* doc, String target) {
   const JsonVal obj = json_add_object(doc);
   json_add_field_lit(
       doc,
@@ -108,24 +108,24 @@ static JsonVal dbgsetup_vscode_gen_attach_entry(DbgSetupCtx* ctx, JsonDoc* doc, 
   return obj;
 }
 
-static JsonVal dbgsetup_vscode_generate_json(DbgSetupCtx* ctx, JsonDoc* doc) {
+static JsonVal dbggen_vscode_generate_json(DbgGenCtx* ctx, JsonDoc* doc) {
   const JsonVal root = json_add_object(doc);
   json_add_field_lit(doc, root, "version", json_add_string_lit(doc, "0.2.0"));
 
   const JsonVal configs = json_add_array(doc);
   json_add_field_lit(doc, root, "configurations", configs);
   for (usize i = 0; i != ctx->targetCount; ++i) {
-    json_add_elem(doc, configs, dbgsetup_vscode_gen_launch_entry(ctx, doc, ctx->targets[i]));
-    json_add_elem(doc, configs, dbgsetup_vscode_gen_attach_entry(ctx, doc, ctx->targets[i]));
+    json_add_elem(doc, configs, dbggen_vscode_gen_launch_entry(ctx, doc, ctx->targets[i]));
+    json_add_elem(doc, configs, dbggen_vscode_gen_attach_entry(ctx, doc, ctx->targets[i]));
   }
   return root;
 }
 
-static bool dbgsetup_vscode_generate_launch_file(DbgSetupCtx* ctx) {
+static bool dbggen_vscode_generate_launch_file(DbgGenCtx* ctx) {
   JsonDoc* jsonDoc = json_create(g_allocHeap, 1024);
 
   const String path = path_build_scratch(ctx->workspace, string_lit(".vscode/launch.json"));
-  const bool res = dbgsetup_write_json(path, jsonDoc, dbgsetup_vscode_generate_json(ctx, jsonDoc));
+  const bool   res  = dbggen_write_json(path, jsonDoc, dbggen_vscode_generate_json(ctx, jsonDoc));
 
   if (res) {
     log_i("Generated VSCode launch config", log_param("path", fmt_path(path)));
@@ -142,8 +142,8 @@ AppType app_cli_configure(CliApp* app) {
 
   g_optDbg = cli_register_flag(app, 'd', string_lit("debugger"), CliOptionFlags_Value);
   cli_register_desc_choice_array(
-      app, g_optDbg, string_lit("What debugger to use."), g_dbgStrs, DbgSetupDbg_Default);
-  cli_register_validator(app, g_optDbg, dbgsetup_validate_dbg);
+      app, g_optDbg, string_lit("What debugger to use."), g_dbgStrs, DbgGenDbg_Default);
+  cli_register_validator(app, g_optDbg, dbggen_validate_dbg);
 
   g_optWorkspace = cli_register_flag(app, 'w', string_lit("workspace"), CliOptionFlags_Required);
   cli_register_desc(app, g_optWorkspace, string_lit("Project workspace."));
@@ -160,10 +160,10 @@ i32 app_cli_run(MAYBE_UNUSED const CliApp* app, const CliInvocation* invoc) {
   log_add_sink(g_logger, log_sink_pretty_default(g_allocHeap, g_fileStdOut, ~LogMask_Debug));
   log_add_sink(g_logger, log_sink_json_default(g_allocHeap, LogMask_All));
 
-  DbgSetupCtx ctx = {
-      .dbg = (DbgSetupDbg)cli_read_choice_array(invoc, g_optDbg, g_dbgStrs, DbgSetupDbg_Default),
-      .workspace   = cli_read_string(invoc, g_optWorkspace, string_empty),
-      .targets     = cli_parse_values(invoc, g_optTargets).values,
+  DbgGenCtx ctx = {
+      .dbg       = (DbgGenDbg)cli_read_choice_array(invoc, g_optDbg, g_dbgStrs, DbgGenDbg_Default),
+      .workspace = cli_read_string(invoc, g_optWorkspace, string_empty),
+      .targets   = cli_parse_values(invoc, g_optTargets).values,
       .targetCount = cli_parse_values(invoc, g_optTargets).count,
   };
 
@@ -177,5 +177,5 @@ i32 app_cli_run(MAYBE_UNUSED const CliApp* app, const CliInvocation* invoc) {
       log_param("debugger", fmt_text(g_dbgStrs[ctx.dbg])),
       log_param("targets", fmt_int(ctx.targetCount)));
 
-  return dbgsetup_vscode_generate_launch_file(&ctx) ? 0 : 1;
+  return dbggen_vscode_generate_launch_file(&ctx) ? 0 : 1;
 }
