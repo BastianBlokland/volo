f6620798e5d97b520b8540e97d3f1e659745b9f5
Bastian Blokland <mail@bastian.tech>
2025-09-06 07:07:27 +0300

    scene: Generate ids

---
+++ libs/scene/include/scene/id.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// clang-format off
+enum {
+  SceneId_STATUS_BURNING = 3417612726,
+  SceneId_STATUS_BLEEDING = 481228918,
+  SceneId_STATUS_HEALING = 293953976,
+  SceneId_STATUS_VETERAN = 3162569135,
+};
+// clang-format on
