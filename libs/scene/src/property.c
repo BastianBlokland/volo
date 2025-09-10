de040aeec187f6af079af8e53232dce1b0f07400
Bastian Blokland <mail@bastian.tech>
2025-09-10 20:57:32 +0300

    scene: Add scene_prop_clear util

--- libs/scene/src/property.c
+++ libs/scene/src/property.c
@@ -38,17 +38,19 @@ ecs_module_init(scene_property_module) {
   ecs_register_system(ScenePropertyInitSys);
 }
 
-ScriptVal scene_prop_load(const ScenePropertyComp* k, const StringHash key) {
-  return script_mem_load(&k->memory, key);
+ScriptVal scene_prop_load(const ScenePropertyComp* p, const StringHash key) {
+  return script_mem_load(&p->memory, key);
 }
 
-void scene_prop_store(ScenePropertyComp* k, const StringHash key, const ScriptVal value) {
-  script_mem_store(&k->memory, key, value);
+void scene_prop_store(ScenePropertyComp* p, const StringHash key, const ScriptVal value) {
+  script_mem_store(&p->memory, key, value);
 }
 
-const ScriptMem* scene_prop_memory(const ScenePropertyComp* k) { return &k->memory; }
+void scene_prop_clear(ScenePropertyComp* p) { script_mem_clear(&p->memory); }
 
-ScriptMem* scene_prop_memory_mut(ScenePropertyComp* k) { return &k->memory; }
+const ScriptMem* scene_prop_memory(const ScenePropertyComp* p) { return &p->memory; }
+
+ScriptMem* scene_prop_memory_mut(ScenePropertyComp* p) { return &p->memory; }
 
 ScenePropertyComp* scene_prop_add(EcsWorld* world, const EcsEntityId entity) {
   return ecs_world_add_t(world, entity, ScenePropertyComp, .memory = script_mem_create());
