d34c60647c1287c000c54f33a620b3836f67e8bf
Bastian Blokland <mail@bastian.tech>
2025-09-13 21:08:31 +0300

    geo: Assert ray query output time

--- libs/geo/src/query.c
+++ libs/geo/src/query.c
@@ -725,6 +725,7 @@ bool geo_query_ray(
   }
 
   if (foundHit) {
+    diag_assert(best.time <= maxDist);
     *outHit = best;
   }
   return foundHit;
@@ -799,6 +800,7 @@ bool geo_query_ray_fat(
   }
 
   if (foundHit) {
+    diag_assert(best.time <= maxDist);
     *outHit = best;
   }
   return foundHit;
