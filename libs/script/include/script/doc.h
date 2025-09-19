723539d8913610273127750ac6e509d95cacc148
Bastian Blokland <mail@bastian.tech>
2025-09-19 22:35:39 +0300

    script: Support up to 32 variables

--- libs/script/include/script/doc.h
+++ libs/script/include/script/doc.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "script/forward.h"
 
-#define script_var_count 16
+#define script_var_count 32
 #define script_var_sentinel sentinel_u8
 #define script_scope_sentinel sentinel_u32
 #define script_expr_sentinel sentinel_u32
