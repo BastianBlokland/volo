5511dd6f3ab96afed15349ab9f3205aa9cfdaeab
Bastian Blokland <mail@bastian.tech>
2025-09-19 18:56:11 +0300

    scene: Support attachment with rotation offset

--- libs/scene/src/action.c
+++ libs/scene/src/action.c
@@ -267,7 +267,8 @@ static void action_attach(ActionContext* ctx, const SceneActionAttach* a) {
   if (a->jointName) {
     attach->jointName = a->jointName;
   }
-  attach->offset = a->offset;
+  attach->offsetPos = a->offset;
+  attach->offsetRot = geo_quat_ident;
 }
 
 static void action_detach(ActionContext* ctx, const SceneActionDetach* a) {
