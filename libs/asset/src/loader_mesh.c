e4b7aa84932877f51b97f44adeafdbed1ee0b04f
Bastian Blokland <mail@bastian.tech>
2025-09-21 13:49:40 +0300

    asset: Only load joint-names when dev-support is enabled

--- libs/asset/src/loader_mesh.c
+++ libs/asset/src/loader_mesh.c
@@ -134,7 +134,6 @@ void asset_load_mesh_bin(
     const String              id,
     const EcsEntityId         entity,
     AssetSource*              src) {
-  (void)importEnv;
 
   AssetMeshBundle bundle;
   DataReadResult  result;
@@ -149,12 +148,14 @@ void asset_load_mesh_bin(
   *ecs_world_add_t(world, entity, AssetMeshComp) = bundle.mesh;
   if (bundle.skeleton) {
 
-    // Add the joint names to the string-table for debug purposes.
-    const u8* jointNamesItr = mem_at_u8(bundle.skeleton->data, bundle.skeleton->jointNames);
-    for (u32 joint = 0; joint != bundle.skeleton->jointCount; ++joint) {
-      const u8 size = *jointNamesItr++;
-      stringtable_add(g_stringtable, mem_create(jointNamesItr, size));
-      jointNamesItr += size;
+    if (asset_import_dev_support(importEnv)) {
+      // Add the joint names to the string-table for debug purposes.
+      const u8* jointNamesItr = mem_at_u8(bundle.skeleton->data, bundle.skeleton->jointNames);
+      for (u32 joint = 0; joint != bundle.skeleton->jointCount; ++joint) {
+        const u8 size = *jointNamesItr++;
+        stringtable_add(g_stringtable, mem_create(jointNamesItr, size));
+        jointNamesItr += size;
+      }
     }
 
     *ecs_world_add_t(world, entity, AssetMeshSkeletonComp) = *bundle.skeleton;
