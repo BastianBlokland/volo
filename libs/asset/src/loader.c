8754737cf82069ec0c7dbf9f8ccd35c4896897bd
Bastian Blokland <mail@bastian.tech>
2025-09-21 13:03:52 +0300

    asset: Dummy Gltf step / cublicspline interpolation (#1504)
    * asset: Parse gltf channel interpolation modes
    
    * asset: Validate anim channel output count
    
    * asset: Support pushing cubic-spline value data
    
    * asset: Bump gltf loader version

--- libs/asset/src/loader.c
+++ libs/asset/src/loader.c
@@ -21,8 +21,8 @@
   _X_(AssetFormat_Locale,           locale,             1  )                                       \
   _X_(AssetFormat_LocaleBin,        locale,             1  )                                       \
   _X_(AssetFormat_MeshBin,          mesh_bin,           3  )                                       \
-  _X_(AssetFormat_MeshGlb,          mesh_glb,           27 )                                       \
-  _X_(AssetFormat_MeshGltf,         mesh_gltf,          27 )                                       \
+  _X_(AssetFormat_MeshGlb,          mesh_glb,           28 )                                       \
+  _X_(AssetFormat_MeshGltf,         mesh_gltf,          28 )                                       \
   _X_(AssetFormat_MeshObj,          mesh_obj,           7  )                                       \
   _X_(AssetFormat_MeshProc,         mesh_proc,          2  )                                       \
   _X_(AssetFormat_Prefabs,          prefabs,            3  )                                       \
