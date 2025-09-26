53f68dd81206f8d3ad499d1b3ad81cb68cc1a0e2
Bastian Blokland <mail@bastian.tech>
2025-09-26 20:22:13 +0300

    dev: Show pressed keys in input window

--- libs/dev/src/input.c
+++ libs/dev/src/input.c
@@ -5,6 +5,7 @@
 #include "ecs/view.h"
 #include "ecs/world.h"
 #include "gap/input.h"
+#include "gap/window.h"
 #include "input/manager.h"
 #include "ui/canvas.h"
 #include "ui/layout.h"
@@ -28,7 +29,7 @@ ASSERT(array_elems(g_inputTabNames) == DevInputTab_Count, "Incorrect number of n
 ecs_comp_define(DevInputPanelComp) {
   UiPanel      panel;
   UiScrollview scrollview;
-  bool         pressedKeysOnly;
+  bool         downKeysOnly;
   u32          lastRowCount;
 };
 
@@ -36,25 +37,30 @@ static void platform_options_draw(UiCanvasComp* canvas, DevInputPanelComp* panel
   ui_layout_push(canvas);
 
   UiTable table = ui_table(.spacing = ui_vector(10, 5), .rowHeight = 20);
-  ui_table_add_column(&table, UiTableColumn_Fixed, 120);
+  ui_table_add_column(&table, UiTableColumn_Fixed, 100);
   ui_table_add_column(&table, UiTableColumn_Fixed, 25);
 
   ui_table_next_row(canvas, &table);
-  ui_label(canvas, string_lit("Pressed only:"));
+  ui_label(canvas, string_lit("Down only:"));
   ui_table_next_column(canvas, &table);
-  ui_toggle(canvas, &panelComp->pressedKeysOnly);
+  ui_toggle(canvas, &panelComp->downKeysOnly);
 
   ui_layout_pop(canvas);
 }
 
 static void platform_panel_tab_draw(
-    UiCanvasComp* c, DevInputPanelComp* panelComp, const GapPlatformComp* platform) {
+    UiCanvasComp*          c,
+    DevInputPanelComp*     panelComp,
+    const GapPlatformComp* platform,
+    const GapWindowComp*   activeWindow) {
   platform_options_draw(c, panelComp);
   ui_layout_grow(c, UiAlign_BottomCenter, ui_vector(0, -35), UiBase_Absolute, Ui_Y);
   ui_layout_container_push(c, UiClip_None, UiLayer_Normal);
 
   UiTable table = ui_table(.spacing = ui_vector(10, 5));
   ui_table_add_column(&table, UiTableColumn_Fixed, 150);
+  ui_table_add_column(&table, UiTableColumn_Fixed, 75);
+  ui_table_add_column(&table, UiTableColumn_Fixed, 75);
   ui_table_add_column(&table, UiTableColumn_Flexible, 0);
 
   ui_table_draw_header(
@@ -62,6 +68,8 @@ static void platform_panel_tab_draw(
       &table,
       (const UiTableColumnName[]){
           {string_lit("Key"), string_lit("Platform key.")},
+          {string_lit("Index"), string_lit("Platform key index.")},
+          {string_lit("Down"), string_lit("Is the key currently held down?")},
           {string_lit("Label"), string_lit("Platform key label.")},
       });
 
@@ -73,6 +81,10 @@ static void platform_panel_tab_draw(
   ui_canvas_id_block_next(c); // Start the list of keys on its own id block.
   panelComp->lastRowCount = 0;
   for (GapKey key = 0; key != GapKey_Count; ++key) {
+    const bool isDown = activeWindow && gap_window_key_down(activeWindow, key);
+    if (panelComp->downKeysOnly && !isDown) {
+      continue;
+    }
     const f32              y    = ui_table_height(&table, panelComp->lastRowCount++);
     const UiScrollviewCull cull = ui_scrollview_cull(&panelComp->scrollview, y, table.rowHeight);
     if (cull == UiScrollviewCull_After) {
@@ -86,8 +98,14 @@ static void platform_panel_tab_draw(
     gap_key_label(platform, key, &labelBuffer);
 
     ui_table_jump_row(c, &table, panelComp->lastRowCount - 1);
-    ui_table_draw_row_bg(c, &table, ui_color(48, 48, 48, 192));
+    ui_table_draw_row_bg(c, &table, isDown ? ui_color(16, 64, 16, 192) : ui_color(48, 48, 48, 192));
     ui_label(c, gap_key_str(key));
+
+    ui_table_next_column(c, &table);
+    ui_label(c, fmt_write_scratch("{}", fmt_int(key)));
+
+    ui_table_next_column(c, &table);
+    ui_label(c, isDown ? string_lit("yes") : string_lit("no"));
 
     ui_table_next_column(c, &table);
     ui_label(c, dynstring_view(&labelBuffer));
@@ -102,7 +120,8 @@ static void input_panel_draw(
     UiCanvasComp*           c,
     DevInputPanelComp*      panelComp,
     const InputManagerComp* input,
-    const GapPlatformComp*  platform) {
+    const GapPlatformComp*  platform,
+    const GapWindowComp*    activeWindow) {
   (void)input;
 
   const String title = fmt_write_scratch("{} Input Panel", fmt_ui_shape(Keyboard));
@@ -116,12 +135,14 @@ static void input_panel_draw(
 
   switch (panelComp->panel.activeTab) {
   case DevInputTab_Platform:
-    platform_panel_tab_draw(c, panelComp, platform);
+    platform_panel_tab_draw(c, panelComp, platform, activeWindow);
     break;
   }
 
   ui_panel_end(c, &panelComp->panel);
 }
+
+ecs_view_define(WindowView) { ecs_access_read(GapWindowComp); }
 
 ecs_view_define(PanelUpdateGlobalView) {
   ecs_access_read(InputManagerComp);
@@ -145,7 +166,13 @@ ecs_system_define(DevInputUpdatePanelSys) {
   const InputManagerComp* input    = ecs_view_read_t(globalItr, InputManagerComp);
   const GapPlatformComp*  platform = ecs_view_read_t(globalItr, GapPlatformComp);
 
-  EcsView* panelView = ecs_world_view_t(world, PanelUpdateView);
+  EcsView* windowView = ecs_world_view_t(world, WindowView);
+  EcsView* panelView  = ecs_world_view_t(world, PanelUpdateView);
+
+  const EcsEntityId    activeWindowEntity = input_active_window(input);
+  EcsIterator*         activeWindowItr    = ecs_view_maybe_at(windowView, activeWindowEntity);
+  const GapWindowComp* activeWindow =
+      activeWindowItr ? ecs_view_read_t(activeWindowItr, GapWindowComp) : null;
 
   for (EcsIterator* itr = ecs_view_itr(panelView); ecs_view_walk(itr);) {
     DevInputPanelComp* panelComp = ecs_view_write_t(itr, DevInputPanelComp);
@@ -156,7 +183,7 @@ ecs_system_define(DevInputUpdatePanelSys) {
     if (dev_panel_hidden(ecs_view_read_t(itr, DevPanelComp)) && !pinned) {
       continue;
     }
-    input_panel_draw(canvas, panelComp, input, platform);
+    input_panel_draw(canvas, panelComp, input, platform, activeWindow);
 
     if (ui_panel_closed(&panelComp->panel)) {
       ecs_world_entity_destroy(world, ecs_view_entity(itr));
@@ -172,6 +199,7 @@ ecs_module_init(dev_input_module) {
 
   ecs_register_system(
       DevInputUpdatePanelSys,
+      ecs_register_view(WindowView),
       ecs_register_view(PanelUpdateGlobalView),
       ecs_register_view(PanelUpdateView));
 }
