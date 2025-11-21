c6bb094518a3d2b8f51bcb71c3fa0c4aca31d49f
Bastian Blokland <mail@bastian.tech>
2025-11-21 15:37:53 +0200

    nix: Use rpath instead of LD_LIBRARY_PATH (#1534)
    * Format flake
    
    * Enable vulkan loader debug
    
    * Use rpath instead of LD_LIBRARY_PATH

--- flake.nix
+++ flake.nix
@@ -5,51 +5,58 @@
     nixpkgs.url = "nixpkgs/nixos-25.05";
   };
 
-  outputs = { nixpkgs, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    llvmPkg = pkgs.llvmPackages_21;
-  in
-  {
-    devShells.${system} = rec {
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      llvmPkg = pkgs.llvmPackages_21;
+    in
+    {
+      devShells.${system} = rec {
 
-      llvm = (pkgs.mkShellNoCC.override { stdenv = llvmPkg.stdenv; }) {
+        llvm = (pkgs.mkShellNoCC.override { stdenv = llvmPkg.stdenv; }) {
 
-        packages = [
-          pkgs.clang-tools
+          packages = [
+            pkgs.nixfmt-rfc-style
+            pkgs.clang-tools
 
-          llvmPkg.lld
-          llvmPkg.lldb
-          llvmPkg.clang
+            llvmPkg.lld
+            llvmPkg.lldb
+            llvmPkg.clang
 
-          pkgs.cmake
-          pkgs.ninja
+            pkgs.cmake
+            pkgs.ninja
 
-          pkgs.elfutils
-          pkgs.shaderc
-          pkgs.vulkan-tools
-          pkgs.openssl
-          pkgs.vulkan-loader
-          pkgs.xorg.libxcb
-          pkgs.xorg.xcbutilkeysyms
-          pkgs.libxkbcommon
-          pkgs.alsa-lib
-        ];
+            pkgs.elfutils
+            pkgs.shaderc
+            pkgs.vulkan-tools
+            pkgs.openssl
+            pkgs.vulkan-loader
+            pkgs.xorg.libxcb
+            pkgs.xorg.xcbutilkeysyms
+            pkgs.libxkbcommon
+            pkgs.alsa-lib
+          ];
 
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-          pkgs.elfutils
-          pkgs.openssl
-          pkgs.shaderc
-          pkgs.vulkan-loader
-          pkgs.xorg.libxcb
-          pkgs.xorg.xcbutilkeysyms
-          pkgs.libxkbcommon
-          pkgs.alsa-lib
-        ];
+          # Configure runtime dependencies.
+          NIX_LDFLAGS = "-rpath ${
+            pkgs.lib.makeLibraryPath [
+              pkgs.elfutils
+              pkgs.openssl
+              pkgs.shaderc
+              pkgs.vulkan-loader
+              pkgs.xorg.libxcb
+              pkgs.xorg.xcbutilkeysyms
+              pkgs.libxkbcommon
+              pkgs.alsa-lib
+            ]
+          }";
+
+          VK_LOADER_DEBUG = "error"; # error,warn,info
+        };
+
+        default = llvm;
       };
-
-      default = llvm;
     };
-  };
 }
