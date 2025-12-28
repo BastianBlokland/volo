3d8a9950fd3b88b4099159ce38526fe4ff145fce
Bastian Blokland <mail@bastian.tech>
2025-12-28 13:53:40 +0200

    ide: NeoVim config (#1541)
    * ide: Add nvim config
    
    * ide: Fix inconsistent casing

---
+++ .nvim.lua
@@ -0,0 +1,24 @@
+
+-- Register script language.
+vim.filetype.add({ extension = { script = "voloscript", } })
+
+-- NOTE: For syntax highlighting install the tree-sitter grammar:
+-- https://github.com/BastianBlokland/tree-sitter-voloscript
+
+-- Register script LSP.
+local root = vim.fn.expand('%:p:h')
+local lsp_path = root .. "/bin/lsp"
+local binder_path = root .. "/assets/schemas/script_*_binder.json"
+if vim.fn.executable(lsp_path) == 1 then
+  vim.lsp.config.volo_lsp = {
+    cmd = { lsp_path, "--stdio", "--binders", binder_path },
+    filetypes = { "voloscript" },
+    root_markers = { ".git" },
+  }
+  vim.lsp.enable("volo_lsp")
+else
+  vim.notify("volo_lsp: No lsp binary found", vim.log.levels.ERROR)
+end
+
+-- Auto format on save.
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
