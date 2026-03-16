fe396cf963910d25292dae7f4b8cc4dd59bff2f4
Bastian Blokland <mail@bastian.tech>
2026-03-16 18:33:53 +0200

    gap: Add dummy wayland c file

--- libs/gap/src/pal.c
+++ libs/gap/src/pal.c
@@ -1,4 +1,6 @@
-#if defined(VOLO_LINUX)
+#if defined(VOLO_LINUX) && defined(VOLO_WAYLAND)
+#include "pal_linux_wayland.c"
+#elif defined(VOLO_LINUX)
 #include "pal_linux_xcb.c"
 #elif defined(VOLO_WIN32)
 #include "pal_win32.c"
---
+++ libs/gap/src/pal_linux_wayland.c
@@ -0,0 +1,13 @@
+#include "core/array.h"
+#include "core/diag.h"
+#include "core/dynlib.h"
+#include "core/dynstring.h"
+#include "core/math.h"
+#include "core/utf8.h"
+#include "log/logger.h"
+
+#include "pal.h"
+
+/**
+ * Wayland client implementation.
+ */
